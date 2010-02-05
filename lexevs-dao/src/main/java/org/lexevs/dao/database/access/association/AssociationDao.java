4d856eddeb66493a7adb934aed66f5cfd387595d
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-02-05 19:35:28 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@3923 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/main/java/org/lexevs/dao/database/access/association/AssociationDao.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/access/association/AssociationDao.java
@@ -1,9 +1,14 @@
 package org.lexevs.dao.database.access.association;
 
 import org.LexGrid.relations.Association;
+import org.LexGrid.relations.AssociationSource;
 
 public interface AssociationDao {
 
-	public void insert(String codingScheme, String version, Association record);
+	public String insertAssociation(String codingScheme, String version, Association association);
+	
+	public Association getAssociationBy(String codingScheme, String version, Association association);
+	
+	public String insertAssociationSource(String codingScheme, String version, AssociationSource source);
 
 }
