c8071e1296edcf84cdc5f3b0decbd8a8278c31e1
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-02-05 18:56:39 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@3920 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/main/java/org/lexevs/dao/database/jdbc/AbstractJdbcDao.java
+++
@@ -1,7 +0,0 @@
-package org.lexevs.dao.database.jdbc;
-
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
-
-public abstract class AbstractJdbcDao extends JdbcDaoSupport {
-
-}
--- lexevs-dao/src/main/java/org/lexevs/dao/database/jdbc/sqlbuilder/repository/codingscheme/CodingSchemeSqlBuilder.java
+++
@@ -1,7 +0,0 @@
-package org.lexevs.dao.database.jdbc.sqlbuilder.repository.codingscheme;
-
-import org.lexevs.dao.database.jdbc.sqlbuilder.AbstractDatabaseTypeAwareSqlBuilder;
-
-public class CodingSchemeSqlBuilder extends AbstractDatabaseTypeAwareSqlBuilder {
-
-}
