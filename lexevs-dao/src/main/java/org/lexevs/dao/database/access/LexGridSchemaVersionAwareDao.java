240ccde2600bbf91711d26dd29e953c994416445
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-03-02 17:40:56 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4146 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/main/java/org/lexevs/dao/database/access/LexGridSchemaVersionAwareDao.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/access/LexGridSchemaVersionAwareDao.java
@@ -5,4 +5,10 @@ import org.lexevs.dao.database.schemaversion.LexGridSchemaVersion;
 public interface LexGridSchemaVersionAwareDao {
 
 	public boolean supportsLgSchemaVersion(LexGridSchemaVersion version);
+	
+	public <T> T executeInTransaction(IndividualDaoCallback<T> callback);
+	
+	public interface IndividualDaoCallback<T> {
+		public T execute();
+	}
 }
