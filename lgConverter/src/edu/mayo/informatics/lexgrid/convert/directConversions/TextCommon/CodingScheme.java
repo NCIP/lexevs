50b727663d8c66ed857cde2c198ba71d5d29a7d8
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2010-03-05 21:26:40 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4217 c4b421de-a31f-834e-8902-8020e42e311c

--- lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/TextCommon/CodingScheme.java
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/TextCommon/CodingScheme.java
@@ -51,8 +51,8 @@ public class CodingScheme {
     public String copyright = "Copyright: (c) 2004-2007 Mayo Foundation for Medical Education and Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the triple-shield Mayo logo are trademarks and service marks of MFMER.Except as contained in the copyright notice above, or as used to identify MFMER as the author of this software, the trade names, trademarks, service marks, or product names of the copyright holder shall not be used in advertising, promotion or otherwise in connection with this software without prior written authorization of the copyright holder. Licensed under the Eclipse Public License, Version 1.0 (the \"License\"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.eclipse.org/legal/epl-v10.html.";
     public Concept[] concepts;
     public Association[] associations;
-
     public boolean isTypeB = false;
+    public static final String PAR = "PAR"; // constant variable for "PAR"
 
     public static org.LexGrid.codingSchemes.CodingScheme toCodingScheme(CodingScheme codingScheme) {
         org.LexGrid.codingSchemes.CodingScheme cs = new org.LexGrid.codingSchemes.CodingScheme();
@@ -119,7 +119,7 @@ public class CodingScheme {
         relations.setContainerName(codingScheme.codingSchemeName + "Relation");
         
         AssociationPredicate ap = new AssociationPredicate();
-        ap.setAssociationName("PAR");  //TODO: a constant for PAR
+        ap.setAssociationName(PAR);  
         
         for (Association a : codingScheme.associations) {
             AssociationSource as = new AssociationSource();
