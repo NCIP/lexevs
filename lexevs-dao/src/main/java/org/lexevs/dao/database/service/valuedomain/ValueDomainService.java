aa5be48fa161f63a3e2bc376321e455759273bc0
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-03-10 12:38:21 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4258 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/main/java/org/lexevs/dao/database/service/valuedomain/ValueDomainService.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/service/valuedomain/ValueDomainService.java
@@ -1,5 +1,7 @@
 package org.lexevs.dao.database.service.valuedomain;
 
+import java.util.List;
+
 import org.LexGrid.valueDomains.ValueDomainDefinition;
 import org.LexGrid.valueDomains.ValueDomains;
 import org.lexevs.dao.database.service.DatabaseService;
@@ -8,5 +10,9 @@ public interface ValueDomainService extends DatabaseService {
 
 	public void insertValueDomainDefinition(ValueDomainDefinition definition);
 	
-	public void insertValueDomains(ValueDomains definition);
+	public void insertValueDomains(ValueDomains valueDomains, String systemReleaseUri);
+	
+	public List<String> findByValueUrisByDomainName(String valueDomainName);
+	
+	public ValueDomainDefinition getValueDomainDefinitionByUri(String uri);
 }
