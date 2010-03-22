6d36d340195582b6782fcf50cba6e4c82fa8ce73
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2010-03-22 17:19:12 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4369 c4b421de-a31f-834e-8902-8020e42e311c

--- lgConverter/test/java/edu/mayo/informatics/lexgrid/convert/validator/processor/DefaultResolverProcessorTest.java
+++ lgConverter/test/java/edu/mayo/informatics/lexgrid/convert/validator/processor/DefaultResolverProcessorTest.java
@@ -1,11 +1,15 @@
 package edu.mayo.informatics.lexgrid.convert.validator.processor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.easymock.classextension.EasyMock;
 import org.junit.Assert;
 import org.junit.Test;
 
+import edu.mayo.informatics.lexgrid.convert.validator.error.LoadValidationError;
+import edu.mayo.informatics.lexgrid.convert.validator.error.ResolvedLoadValidationError;
+import edu.mayo.informatics.lexgrid.convert.validator.error.WrappingLoadValidationError;
 import edu.mayo.informatics.lexgrid.convert.validator.resolution.Resolver;
 
 public class DefaultResolverProcessorTest {
@@ -38,5 +42,29 @@ public class DefaultResolverProcessorTest {
         List<Resolver> resolverList = processorResolver.getResolversForCode("123");
         
         Assert.assertEquals(0, resolverList.size());    
+    }
+    
+    @Test
+    public void testResolve() {
+        DefaultResolverProcessor processorResolver = new DefaultResolverProcessor();
+        
+        LoadValidationError err = EasyMock.createMock(LoadValidationError.class);
+        EasyMock.expect(err.getErrorCode()).andReturn("123");
+        EasyMock.replay(err);
+        
+        List<LoadValidationError> errList = new ArrayList<LoadValidationError>();
+        errList.add(err);
+        
+        Resolver resolver = EasyMock.createMock(Resolver.class);
+        EasyMock.expect(resolver.isResolverValidForError("123")).andReturn(true);
+        EasyMock.expect(resolver.resolveError(err)).andReturn(new WrappingLoadValidationError(null, null));
+        EasyMock.replay(resolver);
+        
+        processorResolver.addResolver(resolver);
+      
+        List<ResolvedLoadValidationError> l = processorResolver.resolve(errList);
+        
+        Assert.assertEquals(1, l.size());
+        
     }
 }
