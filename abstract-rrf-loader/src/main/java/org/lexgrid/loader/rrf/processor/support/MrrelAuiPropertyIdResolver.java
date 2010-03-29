521ed02fb3e57091da5ea69ca549fb1f46d113e2
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-03-29 13:52:36 +0000

    Added PropertyLink processing.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4423 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ abstract-rrf-loader/src/main/java/org/lexgrid/loader/rrf/processor/support/MrrelAuiPropertyIdResolver.java
@@ -0,0 +1,31 @@
+package org.lexgrid.loader.rrf.processor.support;
+
+import org.lexgrid.loader.processor.support.PropertyIdResolver;
+import org.lexgrid.loader.rrf.model.Mrrel;
+import org.springframework.util.Assert;
+
+public class MrrelAuiPropertyIdResolver implements PropertyIdResolver<Mrrel>{
+
+	public enum Aui {AUI1, AUI2};
+	
+	private Aui aui;
+	
+	public String getPropertyId(Mrrel item) {
+		Assert.notNull(aui);
+		if(aui.equals(Aui.AUI1)) {
+			return item.getAui1();
+		} else if(aui.equals(Aui.AUI2)) {
+			return item.getAui2();
+		}
+		
+		else throw new RuntimeException("Cannot get Aui Property Id.");	
+	}
+
+	public Aui getAui() {
+		return aui;
+	}
+
+	public void setAui(Aui aui) {
+		this.aui = aui;
+	}
+}
