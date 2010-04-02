a645a2bea10ffa30fa062e9970d8cfa3c781b505
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-02 16:01:20 +0000

    Interface for logging and status reporting.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4501 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbInterfaces/src/org/LexGrid/LexBIG/Utility/logging/StatusReporter.java
@@ -0,0 +1,12 @@
+package org.LexGrid.LexBIG.Utility.logging;
+
+import org.LexGrid.LexBIG.DataModel.Core.LogEntry;
+import org.LexGrid.LexBIG.DataModel.Core.types.LogLevel;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.ProcessStatus;
+
+public interface StatusReporter {
+
+	public LogEntry[] getLog(LogLevel level);
+	
+	public ProcessStatus getStatus();
+}
