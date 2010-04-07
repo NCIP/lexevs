f3e084b8b3d9d675a95682977daa74d04788b249
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-07 21:41:58 +0000

    Updated to point to the 2010 streaming xml loader
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4611 c4b421de-a31f-834e-8902-8020e42e311c

--- lbImpl/src/org/LexGrid/LexBIG/Impl/loaders/LexGridLoaderImpl.java
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/loaders/LexGridLoaderImpl.java
@@ -27,20 +27,26 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.LexGrid.LexBIG.DataModel.InterfaceElements.ExtensionDescription;
 import org.LexGrid.LexBIG.DataModel.InterfaceElements.LoadStatus;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.types.ProcessState;
 import org.LexGrid.LexBIG.Exceptions.LBException;
 import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
 import org.LexGrid.LexBIG.Exceptions.LBParameterException;
 import org.LexGrid.LexBIG.Extensions.Load.LexGrid_Loader;
 import org.LexGrid.LexBIG.Extensions.Load.options.OptionHolder;
 import org.LexGrid.LexBIG.Impl.Extensions.ExtensionRegistryImpl;
+import org.LexGrid.codingSchemes.CodingScheme;
 import org.lexevs.dao.database.service.exception.CodingSchemeAlreadyLoadedException;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import edu.mayo.informatics.lexgrid.convert.directConversions.StreamingXMLToSQL;
+import edu.mayo.informatics.lexgrid.convert.directConversions.TextToSQL;
 import edu.mayo.informatics.lexgrid.convert.exceptions.ConnectionFailure;
 import edu.mayo.informatics.lexgrid.convert.formats.Option;
 import edu.mayo.informatics.lexgrid.convert.formats.inputFormats.LexGridXML;
+import edu.mayo.informatics.lexgrid.convert.options.BooleanOption;
+import edu.mayo.informatics.lexgrid.convert.options.StringOption;
 import edu.mayo.informatics.lexgrid.convert.utility.URNVersionPair;
 
 /**
@@ -50,67 +56,66 @@ import edu.mayo.informatics.lexgrid.convert.utility.URNVersionPair;
  * @author <A HREF="mailto:erdmann.jesse@mayo.edu">Jesse Erdmann</A>
  * @version subversion $Revision: $ checked in on $Date: $
  */
+/**
+ * @author  <A HREF="mailto:scott.bauer@mayo.edu">Scott Bauer </A>
+ *
+ */
 public class LexGridLoaderImpl extends BaseLoader implements LexGrid_Loader {
     private static final long serialVersionUID = 5405545553067402760L;
-    public final static String name = "LexGridLoader";
+    public final static String name = "LexGrid_Loader";
     private final static String description = "This loader loads LexGrid XML files into the LexGrid database.";
-
+    
+    public final static String FORCE_VALIDATION = "Do not Validate";
+    private boolean validate = false;
+    
     public LexGridLoaderImpl() {
        super();
     }
     
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Extensions.Load.LexGrid_Loader#validate(java.net.URI, int)
+     */
     public void validate(URI uri, int validationLevel) throws LBParameterException {
-        // Verify the file exists ...
-        try {
-            setInUse();
-            
-            // Verify content ...
-
-            if (validationLevel == 0) {
-                SAXParserFactory.newInstance().newSAXParser().parse(new File(uri), new DefaultHandler());
-            } else if (validationLevel == 1) {
-                // Load a WXS schema, represented by a Schema instance
-                SAXParserFactory spf = SAXParserFactory.newInstance();
-                spf.setNamespaceAware(true);
-                spf.setValidating(true);
-                SAXParser sp = spf.newSAXParser();
-                sp.setProperty("http://java.sun.com/xml/jaxp/properties/schemaLanguage",
-                        "http://www.w3.org/2001/XMLSchema");
-                sp.setProperty("http://java.sun.com/xml/jaxp/properties/schemaSource", getSchemaURL().toString());
-                DefaultHandler handler = new DefaultHandler() {
-                    @Override
-                    public void error(SAXParseException e) throws SAXException {
-                        throw e;
-                    }
-                };
-                sp.parse(new File(uri), handler);
-            } else {
-                throw new LBParameterException("Unsupported validation level");
-            }
-        } catch (LBParameterException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new LBParameterException(e.toString());
-        } finally {
-            inUse = false;
-        }
+        throw new UnsupportedOperationException();
     }
 
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Extensions.Load.LexGrid_Loader#load(java.net.URI, boolean, boolean)
+     */
     public void load(URI uri, boolean stopOnErrors, boolean async) throws LBParameterException, LBInvocationException {
       //
     }
     
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.loaders.BaseLoader#declareAllowedOptions(org.LexGrid.LexBIG.Extensions.Load.options.OptionHolder)
+     */
     @Override
     protected OptionHolder declareAllowedOptions(OptionHolder holder) {
+        BooleanOption forceValidation = new BooleanOption(LexGridLoaderImpl.FORCE_VALIDATION, false);
+        holder.getBooleanOptions().add(forceValidation);
         return holder;
     }
 
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.loaders.BaseLoader#doLoad()
+     */
     @Override
     protected URNVersionPair[] doLoad() throws CodingSchemeAlreadyLoadedException {
-        // TODO Auto-generated method stub (IMPLEMENT!)
-        throw new UnsupportedOperationException();
+        StreamingXMLToSQL loader = new StreamingXMLToSQL();
+        
+        CodingScheme codingScheme = loader.load(
+                this.getResourceUri(), 
+                this.getLogger(), 
+                this.getOptions().getBooleanOption(LexGridLoaderImpl.FORCE_VALIDATION).getOptionValue());
+   
+        this.getStatus().setState(ProcessState.COMPLETED);
+        this.getStatus().setErrorsLogged(false);
+        return this.constructVersionPairsFromCodingSchemes(codingScheme);
     }
 
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.Extensions.AbstractExtendable#buildExtensionDescription()
+     */
     @Override
     protected ExtensionDescription buildExtensionDescription() {
         ExtensionDescription temp = new ExtensionDescription();
@@ -122,22 +127,40 @@ public class LexGridLoaderImpl extends BaseLoader implements LexGrid_Loader {
         return temp;
     }
 
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Extensions.Load.LexGrid_Loader#getSchemaURL()
+     */
     public URI getSchemaURL() {
-        try {
-            return new URI("http://LexGrid.org/schema/" + getSchemaVersion() + "/LexGrid/service.xsd");
-        } catch (URISyntaxException e) {
-            getLogger().error("Unexpected Error", e);
-            return null;
-        }
+        throw new UnsupportedOperationException();
     }
 
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Extensions.Load.LexGrid_Loader#getSchemaVersion()
+     */
     public String getSchemaVersion() {
-        return "2005/01";
+        throw new UnsupportedOperationException();
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#finalize()
+     */
     public void finalize() throws Throwable {
         getLogger().loadLogDebug("Freeing LexGridLoaderImpl");
         super.finalize();
     }
-
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        LexGridLoaderImpl loader = new LexGridLoaderImpl();
+        loader.addBooleanOptionValue(LexGridLoaderImpl.FORCE_VALIDATION, false);
+        URI uri = null;
+        try {
+            uri = new URI(args[0]);
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        loader.load(uri);
+    }
 }
