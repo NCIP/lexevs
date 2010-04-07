9d2e287810554c91476b895006c941dbdc224a29
dwarkans <dwarkans@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-07 20:09:25 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4596 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/ibatis/picklist/parameter/InsertOrUpdatePickListEntryBean.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.lexevs.dao.database.ibatis.picklist.parameter;
+
+import org.LexGrid.valueSets.PickListEntryNode;
+import org.lexevs.dao.database.ibatis.parameter.IdableParameterBean;
+
+/**
+ * The Class to handle insert or update PickListEntry table.
+ * 
+ * @author <a href="mailto:dwarkanath.sridhar@mayo.edu">Sridhar Dwarkanath</a>
+ */
+public class InsertOrUpdatePickListEntryBean extends IdableParameterBean {
+
+	/** The pick list entry node. */
+	private PickListEntryNode pickListEntryNode;
+	
+	/** The pickListGuid. */
+	private String pickListGuid;
+	
+	private String entityCodeNamespace;
+	
+	private String entityCode;
+	
+	private long entryOrder;
+	
+	private boolean isDefault;
+	
+	private boolean matchIfNoContext;
+	
+	private String propertyId;
+	
+	private boolean include;
+	
+	/**
+	 * @return the pickListEntryNode
+	 */
+	public PickListEntryNode getPickListEntryNode() {
+		return pickListEntryNode;
+	}
+
+	/**
+	 * @param pickListEntryNode the pickListEntryNode to set
+	 */
+	public void setPickListEntryNode(PickListEntryNode pickListEntryNode) {
+		this.pickListEntryNode = pickListEntryNode;
+	}
+
+	/**
+	 * @return the pickListGuid
+	 */
+	public String getPickListGuid() {
+		return pickListGuid;
+	}
+
+	/**
+	 * @param pickListGuid the pickListGuid to set
+	 */
+	public void setPickListGuid(String pickListGuid) {
+		this.pickListGuid = pickListGuid;
+	}
+
+	/**
+	 * @return the entityCodeNamespace
+	 */
+	public String getEntityCodeNamespace() {
+		return entityCodeNamespace;
+	}
+
+	/**
+	 * @param entityCodeNamespace the entityCodeNamespace to set
+	 */
+	public void setEntityCodeNamespace(String entityCodeNamespace) {
+		this.entityCodeNamespace = entityCodeNamespace;
+	}
+
+	/**
+	 * @return the entityCode
+	 */
+	public String getEntityCode() {
+		return entityCode;
+	}
+
+	/**
+	 * @param entityCode the entityCode to set
+	 */
+	public void setEntityCode(String entityCode) {
+		this.entityCode = entityCode;
+	}
+
+	/**
+	 * @return the entryOrder
+	 */
+	public long getEntryOrder() {
+		return entryOrder;
+	}
+
+	/**
+	 * @param entryOrder the entryOrder to set
+	 */
+	public void setEntryOrder(long entryOrder) {
+		this.entryOrder = entryOrder;
+	}
+
+	/**
+	 * @return the isDefault
+	 */
+	public boolean isDefault() {
+		return isDefault;
+	}
+
+	/**
+	 * @param isDefault the isDefault to set
+	 */
+	public void setDefault(boolean isDefault) {
+		this.isDefault = isDefault;
+	}
+
+	/**
+	 * @return the matchIfNoContext
+	 */
+	public boolean isMatchIfNoContext() {
+		return matchIfNoContext;
+	}
+
+	/**
+	 * @param matchIfNoContext the matchIfNoContext to set
+	 */
+	public void setMatchIfNoContext(boolean matchIfNoContext) {
+		this.matchIfNoContext = matchIfNoContext;
+	}
+
+	/**
+	 * @return the propertyId
+	 */
+	public String getPropertyId() {
+		return propertyId;
+	}
+
+	/**
+	 * @param propertyId the propertyId to set
+	 */
+	public void setPropertyId(String propertyId) {
+		this.propertyId = propertyId;
+	}
+
+	/**
+	 * @return the include
+	 */
+	public boolean isInclude() {
+		return include;
+	}
+
+	/**
+	 * @param include the include to set
+	 */
+	public void setInclude(boolean include) {
+		this.include = include;
+	}
+
+	
+}
+
