917cae9114ba1d5bd54ed606667cdc558c8224bc
dwarkans <dwarkans@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-09 20:37:27 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4663 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lgValueSets/src/org/lexgrid/valuesets/helper/VSDConstants.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.lexgrid.valuesets.helper;
+
+/**
+ * Constants used in value domain and pick list implementation.
+ * 
+ * @author <A HREF="mailto:dwarkanath.sridhar@mayo.edu">Sridhar Dwarkanath</A>
+ */
+public class VSDConstants {
+	public static final String TBL_VALUE_DOMAIN = "valueDomain";
+	public static final String TBL_VALUE_DOMAIN_TABLE_META_DATA = "vdTableMetaData";
+	public static final String TBL_VD_ENTRY = "vdEntry";
+	public static final String TBL_PICK_LIST = "vdPickList";
+	public static final String TBL_PL_ENTRY = "vdPLEntry";
+	public static final String TBL_ENTRY_IDS = "vdEntryIds";
+	public static final String TBL_ENTRY_TYPE = "vdEntryType";
+	public static final String TBL_MAPPING = "vdMapping";
+    public static final String TBL_PROPERTY = "vdProperty";
+    public static final String TBL_ENTRY_STATE = "vdEntryState";
+    public static final String TBL_SYSTEM_RELEASE = "vdSystemRelease";
+    
+    public static final String ENTRY_STATE_TYPE_ATTRIBUTE = "attribute";
+    
+    public static final int ENTRYID_INCREMENT = 1000;
+	
+}
