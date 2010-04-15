8a1269ef0cf591e18b773351a49617c065f0a4e7
raor <raor@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-15 18:19:32 +0000

    Revision API Exception
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4743 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbInterfaces/src/org/LexGrid/LexBIG/Exceptions/LBRevisionException.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.LexBIG.Exceptions;
+
+import org.LexGrid.LexBIG.Exceptions.LBException;
+
+
+public class LBRevisionException extends LBException {
+
+/**
+	 * 
+	 */
+private static final long serialVersionUID = 6335488236350630144L;
+
+	public LBRevisionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public LBRevisionException(String message) {
+		super(message);
+	}
+	
+	public LBRevisionException(Throwable cause) {
+		super(cause.getMessage(), cause);
+	}
+
+}
