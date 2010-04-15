56748b5d180d2d9940d149dc9c9a14629464f424
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-15 20:05:12 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4755 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbInterfaces/src/org/LexGrid/LexBIG/Extensions/Load/ClaML_Loader.java
@@ -0,0 +1,10 @@
+package org.LexGrid.LexBIG.Extensions.Load;
+
+import java.net.URI;
+
+import org.LexGrid.LexBIG.Exceptions.LBException;
+
+public interface ClaML_Loader {
+	public void load(String path, boolean stopOnErrors, boolean async) throws LBException;
+	public void validate(URI source, int validationLevel) throws LBException;
+}
