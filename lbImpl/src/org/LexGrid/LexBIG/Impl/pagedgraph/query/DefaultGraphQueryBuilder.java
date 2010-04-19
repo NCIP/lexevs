e4a6765f5c724fe86557796e06d2b1063b962e21
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-19 11:16:07 +0000

    Builder-pattern style framework for holding Graph Restriction options.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4790 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/pagedgraph/query/DefaultGraphQueryBuilder.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.LexBIG.Impl.pagedgraph.query;
+
+import org.LexGrid.LexBIG.DataModel.Collections.NameAndValueList;
+import org.LexGrid.LexBIG.DataModel.Core.NameAndValue;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
+import org.lexevs.dao.database.service.codednodegraph.model.GraphQuery;
+import org.lexevs.dao.database.service.codednodegraph.model.GraphQuery.QualifierNameValuePair;
+import org.springframework.util.StringUtils;
+
+/**
+ * The Class DefaultGraphQueryBuilder.
+ * 
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
+public class DefaultGraphQueryBuilder implements GraphQueryBuilder {
+    
+    /** The graph query. */
+    private GraphQuery graphQuery = new GraphQuery();
+
+    public DefaultGraphQueryBuilder(){
+        super();
+    }
+    
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#getQuery()
+     */
+    @Override
+    public GraphQuery getQuery() {
+        return this.graphQuery;
+    }
+
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#restrictToAssociations(org.LexGrid.LexBIG.DataModel.Collections.NameAndValueList, org.LexGrid.LexBIG.DataModel.Collections.NameAndValueList)
+     */
+    @Override
+    public void restrictToAssociations(NameAndValueList association, NameAndValueList associationQualifiers)
+            throws LBInvocationException, LBParameterException {
+        for(NameAndValue nameAndValue : association.getNameAndValue()) {
+            //TODO: resolve URIs
+            if(StringUtils.hasText(nameAndValue.getContent())){
+                throw new UnsupportedOperationException();
+            }
+            graphQuery.getRestrictToAssociations().add(nameAndValue.getName());
+           
+        }
+        for(NameAndValue nameAndValue : associationQualifiers.getNameAndValue()) {
+            String qualName = nameAndValue.getName();
+            String qualValue = nameAndValue.getContent();
+            
+            if(StringUtils.hasText(qualValue) && !StringUtils.hasText(qualName)) {
+                throw new LBParameterException("When applying a Qualifier Restriction onto an Association," +
+                		" you must not specify a Qualifier VALUE without a Qualifier NAME");
+            }
+            
+            graphQuery.getRestrictToAssociationsQualifiers().add(new QualifierNameValuePair(qualName, qualValue));
+           
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#restrictToCodeSystem(java.lang.String)
+     */
+    @Override
+    public void restrictToCodeSystem(String codingScheme) throws LBInvocationException, LBParameterException {
+        // TODO Auto-generated method stub (IMPLEMENT!)
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#restrictToCodes(org.LexGrid.LexBIG.LexBIGService.CodedNodeSet)
+     */
+    @Override
+    public void restrictToCodes(CodedNodeSet codes) throws LBInvocationException, LBParameterException {
+        // TODO Auto-generated method stub (IMPLEMENT!)
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#restrictToDirectionalNames(org.LexGrid.LexBIG.DataModel.Collections.NameAndValueList, org.LexGrid.LexBIG.DataModel.Collections.NameAndValueList)
+     */
+    @Override
+    public void restrictToDirectionalNames(NameAndValueList directionalNames,
+            NameAndValueList associationQualifiers) throws LBInvocationException, LBParameterException {
+        
+    }
+
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#restrictToSourceCodeSystem(java.lang.String)
+     */
+    @Override
+    public void restrictToSourceCodeSystem(String codingScheme) throws LBInvocationException,
+            LBParameterException {
+        // TODO Auto-generated method stub (IMPLEMENT!)
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#restrictToSourceCodes(org.LexGrid.LexBIG.LexBIGService.CodedNodeSet)
+     */
+    @Override
+    public void restrictToSourceCodes(CodedNodeSet codes) throws LBInvocationException, LBParameterException {
+        // TODO Auto-generated method stub (IMPLEMENT!)
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#restrictToTargetCodeSystem(java.lang.String)
+     */
+    @Override
+    public void restrictToTargetCodeSystem(String codingScheme) throws LBInvocationException,
+            LBParameterException {
+        // TODO Auto-generated method stub (IMPLEMENT!)
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.LexGrid.LexBIG.Impl.pagedgraph.query.GraphQueryBuilder#restrictToTargetCodes(org.LexGrid.LexBIG.LexBIGService.CodedNodeSet)
+     */
+    @Override
+    public void restrictToTargetCodes(CodedNodeSet codes) throws LBInvocationException, LBParameterException {
+        // TODO Auto-generated method stub (IMPLEMENT!)
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/pagedgraph/query/GraphQueryBuilder.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.LexBIG.Impl.pagedgraph.query;
+
+import org.LexGrid.LexBIG.DataModel.Collections.NameAndValueList;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
+import org.lexevs.dao.database.service.codednodegraph.model.GraphQuery;
+
+/**
+ * The Interface GraphQueryBuilder.
+ * 
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
+public interface GraphQueryBuilder {
+    
+    /**
+     * Gets the query.
+     * 
+     * @return the query
+     */
+    public GraphQuery getQuery();
+    
+    /**
+     * Restrict the graph to the nodes that participate as a source or target of
+     * the named association and, if supplied, the named association qualifiers.
+     * 
+     * @param association List of associations used to restrict the graph.  The name and
+     * value for each item in the list will be compared against the
+     * id and URI of supported associations for participating
+     * coding schemes.
+     * @param associationQualifiers If supplied, restriction only applies to associations that are
+     * qualified by one or more of the supplied qualifiers.  The name
+     * and value for each item in the list will be compared against
+     * the id and URI of supported association qualifiers for
+     * participating coding schemes.
+     * 
+     * @return A new CodedNodeGraph representing the filtered result.
+     * 
+     * @throws LBInvocationException,LBParameterException      * @throws LBInvocationException the LB invocation exception
+     * @throws LBParameterException the LB parameter exception
+     */
+    void restrictToAssociations(
+            NameAndValueList association,
+            NameAndValueList associationQualifiers)
+            throws LBInvocationException,LBParameterException;
+
+    /**
+     * Restrict the graph to the nodes that participate as a source or target of
+     * an association whose directional name matches the one provided and, if
+     * supplied, the named association qualifiers. A directional name is
+     * considered to be either the forward or reverse label registered to an
+     * association defined by the ontology. Forward and reverse names are
+     * optionally assigned to each association. For example, an association
+     * 'lineage' may have a forward name 'ancestorOf' and reverse name
+     * 'descendantOf'.
+     * 
+     * @param directionalNames List of directionalNames used to restrict the graph. A
+     * directional name is compared against the forward and reverse
+     * names for defined associations. If a given name matches more
+     * than one forward or reverse label, all corresponding
+     * associations are included in the restriction.
+     * @param associationQualifiers If supplied, restriction only applies to associations that are
+     * qualified by one or more of the supplied qualifiers. The name
+     * and value for each item in the list will be compared against
+     * the id and URI of supported association qualifiers for
+     * participating coding schemes.
+     * 
+     * @return A new CodedNodeGraph representing the filtered result.
+     * 
+     * @throws LBInvocationException ,LBParameterException
+     * @throws LBParameterException the LB parameter exception
+     */
+    void restrictToDirectionalNames(
+            NameAndValueList directionalNames,
+            NameAndValueList associationQualifiers)
+            throws LBInvocationException,LBParameterException;  
+    
+    /**
+     * Return a graph that contains only the codes that are present in the
+     * supplied list, and all edges that still have a source and target code
+     * remaining.
+     * 
+     * @param codes Codes to filter on.
+     * 
+     * @return A new CodedNodeGraph representing the filtered result.
+     * 
+     * @throws LBInvocationException,LBParameterException      * @throws LBInvocationException the LB invocation exception
+     * @throws LBParameterException the LB parameter exception
+     */
+    void restrictToCodes(CodedNodeSet codes)
+            throws LBInvocationException,LBParameterException;
+
+    /**
+     * Restrict the graph to codes (source and target) that originate
+     * from the supplied code system. Note: edges defined by other code systems
+     * will still be resolved if associated with both source and target nodes
+     * for the restricted code system.
+     * 
+     * @param codingScheme The local name or URI of the coding scheme to filter on.
+     * 
+     * @return A new CodedNodeGraph representing the filtered result.
+     * 
+     * @throws LBInvocationException,LBParameterException      * @throws LBInvocationException the LB invocation exception
+     * @throws LBParameterException the LB parameter exception
+     */
+    void restrictToCodeSystem(String codingScheme)
+            throws LBInvocationException,LBParameterException;
+
+    /**
+     * Restrict the graph to associations that have one of the codes in the
+     * supplied list as source codes.
+     * 
+     * @param codes Codes to filter on.
+     * 
+     * @return A new CodedNodeGraph representing the filtered result.
+     * 
+     * @throws LBInvocationException,LBParameterException      * @throws LBInvocationException the LB invocation exception
+     * @throws LBParameterException the LB parameter exception
+     */
+    void restrictToSourceCodes(CodedNodeSet codes)
+            throws LBInvocationException,LBParameterException;
+
+    /**
+     * Restrict the graph to edges that have codes from the specified
+     * code system as a source.
+     * 
+     * @param codingScheme The local name or URI of the coding scheme to filter on.
+     * 
+     * @return A new CodedNodeGraph representing the filtered result.
+     * 
+     * @throws LBInvocationException,LBParameterException      * @throws LBInvocationException the LB invocation exception
+     * @throws LBParameterException the LB parameter exception
+     */
+    void restrictToSourceCodeSystem(String codingScheme)
+            throws LBInvocationException,LBParameterException;
+
+    /**
+     * Restrict the graph to associations that have one of the codes in the
+     * supplied list as target codes.
+     * 
+     * @param codes Codes to filter on.
+     * 
+     * @return A new CodedNodeGraph representing the filtered result.
+     * 
+     * @throws LBInvocationException,LBParameterException      * @throws LBInvocationException the LB invocation exception
+     * @throws LBParameterException the LB parameter exception
+     */
+    void restrictToTargetCodes(CodedNodeSet codes)
+            throws LBInvocationException,LBParameterException;
+
+    /**
+     * Restrict the graph to edges that have codes from the specified
+     * code system as a target.
+     * 
+     * @param codingScheme The local name or URI of the coding scheme to filter on.
+     * 
+     * @return A new CodedNodeGraph representing the filtered result.
+     * 
+     * @throws LBInvocationException,LBParameterException      * @throws LBInvocationException the LB invocation exception
+     * @throws LBParameterException the LB parameter exception
+     */
+    void restrictToTargetCodeSystem(String codingScheme)
+            throws LBInvocationException,LBParameterException;
+}
