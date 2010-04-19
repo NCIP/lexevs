615f8868683d783c772b15750322db10c1e8a8ee
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-19 11:02:23 +0000

    Changes for CodedNodeGraph implementation.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4788 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/main/java/org/lexevs/paging/codednodegraph/TripleUidIterator.java
+++ lexevs-dao/src/main/java/org/lexevs/paging/codednodegraph/TripleUidIterator.java
@@ -2,61 +2,71 @@ package org.lexevs.paging.codednodegraph;
 
 import java.util.List;
 
-import org.lexevs.dao.database.access.LexGridSchemaVersionAwareDao.IndividualDaoCallback;
 import org.lexevs.dao.database.access.codednodegraph.CodedNodeGraphDao;
 import org.lexevs.dao.database.access.codednodegraph.CodedNodeGraphDao.TripleNode;
+import org.lexevs.dao.database.service.codednodegraph.CodedNodeGraphService;
+import org.lexevs.dao.database.service.codednodegraph.model.GraphQuery;
+import org.lexevs.locator.LexEvsServiceLocator;
 import org.lexevs.paging.AbstractPageableIterator;
 
 public class TripleUidIterator extends AbstractPageableIterator<String>{
 
-	private CodedNodeGraphDao codedNodeGraphDao;
-	private String codingSchemeUid;
-	private String associationPredicateUid;
+	private String codingSchemeUri;
+	private String codingSchemeVersion;
+	private String relationsContainerName;
+	private String associationPredicateName;
 	private String entityCode;
 	private String entityCodeNamespace;
+	private GraphQuery graphQuery;
 	private TripleNode tripleNode;
 
 	public TripleUidIterator(
-			CodedNodeGraphDao codedNodeGraphDao, 
-			String codingSchemeUid, 
-			String associationPredicateUid, 
+			String codingSchemeUri, 
+			String codingSchemeVersion, 
+			String relationsContainerName,
+			String associationPredicateName,
 			String entityCode,
 			String entityCodeNamespace,
+			GraphQuery graphQuery,
 			TripleNode tripleNode,
 			int pageSize) {
 		super(pageSize);
-		this.codedNodeGraphDao = codedNodeGraphDao;
-		this.codingSchemeUid = codingSchemeUid;
-		this.associationPredicateUid = associationPredicateUid;
+		this.codingSchemeUri = codingSchemeUri;
+		this.codingSchemeVersion = codingSchemeVersion;
+		this.relationsContainerName = relationsContainerName;
+		this.associationPredicateName = associationPredicateName;
 		this.entityCode = entityCode;
 		this.entityCodeNamespace = entityCodeNamespace;
+		this.graphQuery = graphQuery;
 		this.tripleNode = tripleNode;
 	}
 	@Override
 	protected List<String> doPage(final int currentPosition, final int pageSize) {
+		CodedNodeGraphService codedNodeGraphService =
+			LexEvsServiceLocator.getInstance().getDatabaseServiceManager().getCodedNodeGraphService();
 
-		return codedNodeGraphDao.executeInTransaction(new IndividualDaoCallback<List<String>>(){
-
-			@Override
-			public List<String> execute() {
-				if(tripleNode.equals(TripleNode.SUBJECT)) {
-					return codedNodeGraphDao.getTripleUidsContainingSubject(
-							codingSchemeUid, 
-							associationPredicateUid, 
-							entityCode,
-							entityCodeNamespace,
-							currentPosition, 
-							pageSize);
-				} else {
-					return codedNodeGraphDao.getTripleUidsContainingObject(
-							codingSchemeUid, 
-							associationPredicateUid, 
-							entityCode,
-							entityCodeNamespace,
-							currentPosition, 
-							pageSize);
-				}
-			}
-		});
+		if(tripleNode.equals(TripleNode.SUBJECT)) {
+			return codedNodeGraphService.getTripleUidsContainingSubject(
+					codingSchemeUri, 
+					codingSchemeVersion, 
+					relationsContainerName,
+					associationPredicateName,
+					entityCode,
+					entityCodeNamespace,
+					graphQuery,
+					currentPosition, 
+					pageSize);
+		} else {
+			return codedNodeGraphService.getTripleUidsContainingObject(
+					codingSchemeUri, 
+					codingSchemeVersion, 
+					relationsContainerName,
+					associationPredicateName,
+					entityCode,
+					entityCodeNamespace,
+					graphQuery,
+					currentPosition,
+					pageSize);
+		}
 	}
 }
