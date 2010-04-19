8d9ba11ef971ed69497a1bbe89272923052e332e
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-19 22:54:00 +0000

    Helper class for avoiding n+1 selects while populating association qualifiers.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4837 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/ibatis/codednodegraph/model/QualifierAddingAssociatedConcept.java
@@ -0,0 +1,24 @@
+package org.lexevs.dao.database.ibatis.codednodegraph.model;
+
+import java.util.List;
+
+import org.LexGrid.LexBIG.DataModel.Collections.NameAndValueList;
+import org.LexGrid.LexBIG.DataModel.Core.AssociatedConcept;
+import org.LexGrid.LexBIG.DataModel.Core.NameAndValue;
+
+public class QualifierAddingAssociatedConcept extends AssociatedConcept {
+
+	private static final long serialVersionUID = 4411803013641184558L;
+	
+	List<?> associationQualification;
+	
+	public void setAssociationQualification(NameAndValue qualification) {
+		if(qualification != null) {
+			if(this.getAssociationQualifiers() == null) {
+				this.setAssociationQualifiers(new NameAndValueList());
+			}
+
+			this.getAssociationQualifiers().addNameAndValue(qualification);
+		}
+	}
+}
