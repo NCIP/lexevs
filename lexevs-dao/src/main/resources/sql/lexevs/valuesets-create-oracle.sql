6f4ab6ac4d3e4bf727d83e19cda5644b2620c1e6
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-22 19:12:22 +0000

    Oracle scripts
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4922 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/main/resources/sql/lexevs/valuesets-create-oracle.sql
+++ lexevs-dao/src/main/resources/sql/lexevs/valuesets-create-oracle.sql
@@ -1,4 +1,4 @@
-CREATE TABLE valueSetDefinition
+CREATE TABLE @PREFIX@valueSetDefinition
 (
 	valueSetDefGuid      VARCHAR2(36) NOT NULL,
 	valueSetDefURI       VARCHAR2(250) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE valueSetDefinition
 ;
 
 
-CREATE TABLE vsdEntry
+CREATE TABLE @PREFIX@vsdEntry
 (
 	vsdEntryGuid             VARCHAR2(36) NOT NULL,
 	valueSetDefGuid          VARCHAR2(36) NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE vsdEntry
 ;
 
 
-CREATE TABLE vsEntryState
+CREATE TABLE @PREFIX@vsEntryState
 (
 	entryStateGuid      VARCHAR2(36) NOT NULL,
 	entryGuid           VARCHAR2(36) NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE vsEntryState
 ;
 
 
-CREATE TABLE vsMultiAttrib
+CREATE TABLE @PREFIX@vsMultiAttrib
 (
 	vsMultiAttribGuid  VARCHAR2(36) NOT NULL,
 	referenceGuid      VARCHAR2(36) NOT NULL,
@@ -75,7 +75,7 @@ CREATE TABLE vsMultiAttrib
 ;
 
 
-CREATE TABLE vsPickList
+CREATE TABLE @PREFIX@vsPickList
 (
 	vsPickListGuid              VARCHAR2(36) NOT NULL,
 	pickListId                  VARCHAR2(50) NOT NULL,
@@ -96,7 +96,7 @@ CREATE TABLE vsPickList
 ;
 
 
-CREATE TABLE vsPLEntry
+CREATE TABLE @PREFIX@vsPLEntry
 (
 	vsPLEntryGuid        VARCHAR2(36) NOT NULL,
 	vsPickListGuid       VARCHAR2(36) NOT NULL,
@@ -119,7 +119,7 @@ CREATE TABLE vsPLEntry
 ;
 
 
-CREATE TABLE vsProperty
+CREATE TABLE @PREFIX@vsProperty
 (
 	vsPropertyGuid        VARCHAR2(36) NOT NULL,
 	referenceGuid         VARCHAR2(36) NOT NULL,
@@ -144,7 +144,7 @@ CREATE TABLE vsProperty
 ;
 
 
-CREATE TABLE vsPropertyMultiAttrib
+CREATE TABLE @PREFIX@vsPropertyMultiAttrib
 (
 	vsPropMultiAttribGuid  VARCHAR2(36) NOT NULL,
 	vsPropertyGuid         VARCHAR2(36) NOT NULL,
@@ -158,7 +158,7 @@ CREATE TABLE vsPropertyMultiAttrib
 ;
 
 
-CREATE TABLE vsSupportedAttrib
+CREATE TABLE @PREFIX@vsSupportedAttrib
 (
 	vsSuppAttribGuid        VARCHAR2(36) NOT NULL,
 	referenceGuid           VARCHAR2(36),
@@ -176,135 +176,122 @@ CREATE TABLE vsSupportedAttrib
 
 
 
-ALTER TABLE valueSetDefinition ADD CONSTRAINT PK_valueSetDef 
+ALTER TABLE @PREFIX@valueSetDefinition ADD CONSTRAINT PK_valueSetDef 
 	PRIMARY KEY (valueSetDefGuid)
 ;
 
-ALTER TABLE vsdEntry ADD CONSTRAINT PK_vsdEntry 
+ALTER TABLE @PREFIX@vsdEntry ADD CONSTRAINT PK_vsdEntry 
 	PRIMARY KEY (vsdEntryGuid)
 ;
 
-ALTER TABLE vsEntryState ADD CONSTRAINT PK_vsEntryState 
+ALTER TABLE @PREFIX@vsEntryState ADD CONSTRAINT PK_vsEntryState 
 	PRIMARY KEY (entryStateGuid)
 ;
 
-ALTER TABLE vsMultiAttrib ADD CONSTRAINT PK_vsMultiAttrib 
+ALTER TABLE @PREFIX@vsMultiAttrib ADD CONSTRAINT PK_vsMultiAttrib 
 	PRIMARY KEY (vsMultiAttribGuid)
 ;
 
-ALTER TABLE vsPickList ADD CONSTRAINT PK_vsPickList 
+ALTER TABLE @PREFIX@vsPickList ADD CONSTRAINT PK_vsPickList 
 	PRIMARY KEY (vsPickListGuid)
 ;
 
-ALTER TABLE vsPLEntry ADD CONSTRAINT PK_vsPLEntry 
+ALTER TABLE @PREFIX@vsPLEntry ADD CONSTRAINT PK_vsPLEntry 
 	PRIMARY KEY (vsPLEntryGuid)
 ;
 
-ALTER TABLE vsProperty ADD CONSTRAINT PK_vsProperty 
+ALTER TABLE @PREFIX@vsProperty ADD CONSTRAINT PK_vsProperty 
 	PRIMARY KEY (vsPropertyGuid)
 ;
 
-ALTER TABLE vsPropertyMultiAttrib ADD CONSTRAINT PK_vsPropertyMultiAttrib 
+ALTER TABLE @PREFIX@vsPropertyMultiAttrib ADD CONSTRAINT PK_vsPropertyMultiAttrib 
 	PRIMARY KEY (vsPropMultiAttribGuid)
 ;
 
-ALTER TABLE vsSupportedAttrib ADD CONSTRAINT PK_vsMapping 
+ALTER TABLE @PREFIX@vsSupportedAttrib ADD CONSTRAINT PK_vsMapping 
 	PRIMARY KEY (vsSuppAttribGuid)
 ;
 
-
-CREATE INDEX idx_vsdName ON valueSetDefinition
-(valueSetDefName ASC)
+ALTER TABLE @PREFIX@valueSetDefinition
+	ADD CONSTRAINT UQ_vsdName UNIQUE (valueSetDefName)
 ;
 
-CREATE INDEX idx_vsdURI ON valueSetDefinition
+CREATE INDEX idx_vsdURI ON @PREFIX@valueSetDefinition
 (valueSetDefURI ASC)
 ;
 
-ALTER TABLE vsdEntry
+ALTER TABLE @PREFIX@vsdEntry
 	ADD CONSTRAINT UQ_vsdEntry_valueSetDefGuid UNIQUE (valueSetDefGuid)
 ;
 
-ALTER TABLE vsdEntry
+ALTER TABLE @PREFIX@vsdEntry
 	ADD CONSTRAINT UQ_vsdEntry UNIQUE (ruleOrder)
 ;
 
-CREATE INDEX idx_valueSetDefGuid ON vsdEntry
-(valueSetDefGuid ASC)
-;
-
-CREATE INDEX idx_vsdEnt_entityCode ON vsdEntry
+CREATE INDEX idx_vsdEnt_entityCode ON @PREFIX@vsdEntry
 (entityCode ASC)
 ;
 
-CREATE INDEX idx_pickListId ON vsPickList
+CREATE INDEX idx_pickListId ON @PREFIX@vsPickList
 (pickListId ASC)
 ;
 
-CREATE INDEX idx_representsVSD ON vsPickList
-(representsValueSetDef ASC)
+ALTER TABLE @PREFIX@vsPickList
+	ADD CONSTRAINT UQ_representsVSD UNIQUE (representsValueSetDef)
 ;
 
-ALTER TABLE vsPLEntry
+ALTER TABLE @PREFIX@vsPLEntry
 	ADD CONSTRAINT UQ_vsPLEntry_vsPickListGuid UNIQUE (vsPickListGuid)
 ;
 
-ALTER TABLE vsPLEntry
+ALTER TABLE @PREFIX@vsPLEntry
 	ADD CONSTRAINT UQ_vsPLEntry UNIQUE (plEntryId)
 ;
 
-CREATE INDEX idx_vsPickListGuid ON vsPLEntry
-(vsPickListGuid ASC)
-;
 
-CREATE INDEX idx_entityCode ON vsPLEntry
+
+CREATE INDEX idx_entityCode ON @PREFIX@vsPLEntry
 (entityCode ASC)
 ;
 
-CREATE INDEX idx_vsProperty ON vsProperty
+CREATE INDEX idx_vsProperty ON @PREFIX@vsProperty
 (referenceGuid ASC, propertyId ASC, propertyName ASC)
 ;
 
-ALTER TABLE vsProperty
+ALTER TABLE @PREFIX@vsProperty
 	ADD CONSTRAINT UQ_vsProperty UNIQUE (referenceGuid, propertyId, propertyName)
 ;
 
-CREATE INDEX idx_vsPropertyGuid ON vsPropertyMultiAttrib
+CREATE INDEX idx_vsPropertyGuid ON @PREFIX@vsPropertyMultiAttrib
 (vsPropertyGuid ASC)
 ;
 
-ALTER TABLE vsSupportedAttrib
+ALTER TABLE @PREFIX@vsSupportedAttrib
 	ADD CONSTRAINT UQ_vsMapping UNIQUE (referenceGuid, supportedAttributeTag, id)
 ;
 
-CREATE INDEX idx_saReferenceGuid ON vsSupportedAttrib
+CREATE UNIQUE INDEX idx_saReferenceGuid ON @PREFIX@vsSupportedAttrib
 (referenceGuid ASC)
 ;
 
-
-ALTER TABLE valueSetDefinition ADD CONSTRAINT FK_vsd_releaseGuid 
-	FOREIGN KEY (releaseGuid) REFERENCES systemRelease (releaseGuid)
+ALTER TABLE @PREFIX@valueSetDefinition ADD CONSTRAINT FK_vsd_releaseGuid 
+	FOREIGN KEY (releaseGuid) REFERENCES @PREFIX@systemRelease (releaseGuid)
 ;
 
-ALTER TABLE vsdEntry ADD CONSTRAINT FK_vsdEnt_vsdGuid 
-	FOREIGN KEY (valueSetDefGuid) REFERENCES valueSetDefinition (valueSetDefName)
+ALTER TABLE @PREFIX@vsdEntry ADD CONSTRAINT FK_vsdEnt_vsdGuid 
+	FOREIGN KEY (valueSetDefGuid) REFERENCES @PREFIX@valueSetDefinition (valueSetDefName)
 ON DELETE CASCADE
 ;
 
-ALTER TABLE vsEntryState ADD CONSTRAINT FK_vsES_prevEntryStateGuid 
-	FOREIGN KEY (prevEntryStateGuid) REFERENCES vsEntryState (entryStateGuid)
+ALTER TABLE @PREFIX@vsEntryState ADD CONSTRAINT FK_vsES_prevEntryStateGuid 
+	FOREIGN KEY (prevEntryStateGuid) REFERENCES @PREFIX@vsEntryState (entryStateGuid)
 ;
 
-ALTER TABLE vsPickList ADD CONSTRAINT FK_vsdPL_releaseGuid 
-	FOREIGN KEY (releaseGuid) REFERENCES systemRelease (releaseGuid)
+ALTER TABLE @PREFIX@vsPickList ADD CONSTRAINT FK_vsdPL_releaseGuid 
+	FOREIGN KEY (releaseGuid) REFERENCES @PREFIX@systemRelease (releaseGuid)
 ;
 
-ALTER TABLE vsPLEntry ADD CONSTRAINT FK_vsPLEnt_vsPLGuid 
-	FOREIGN KEY (vsPickListGuid) REFERENCES vsPickList (representsValueSetDef)
-ON DELETE CASCADE
-;
-
-ALTER TABLE vsPropertyMultiAttrib ADD CONSTRAINT FK_vsProperty 
-	FOREIGN KEY (vsPropertyGuid) REFERENCES vsProperty (referenceGuid, propertyId, propertyName)
+ALTER TABLE @PREFIX@vsPLEntry ADD CONSTRAINT FK_vsPLEnt_vsPLGuid 
+	FOREIGN KEY (vsPickListGuid) REFERENCES @PREFIX@vsPickList (representsValueSetDef)
 ON DELETE CASCADE
 ;
