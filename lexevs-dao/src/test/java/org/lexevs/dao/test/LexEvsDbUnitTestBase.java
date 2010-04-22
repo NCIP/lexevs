5451d1b845cb7a8522854e15a5dedc3853bfb4d9
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-22 12:18:30 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4904 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/test/java/org/lexevs/dao/test/LexEvsDbUnitTestBase.java
+++ lexevs-dao/src/test/java/org/lexevs/dao/test/LexEvsDbUnitTestBase.java
@@ -32,7 +32,6 @@ import org.lexevs.cache.MethodCachingProxy;
 import org.lexevs.dao.database.operation.LexEvsDatabaseOperations;
 import org.lexevs.dao.database.prefix.PrefixResolver;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -47,17 +46,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class LexEvsDbUnitTestBase extends DataSourceBasedDBTestCase {
 
 	/** The Constant CREATE_COMMON_SCRIPT. */
-//	private static final String CREATE_COMMON_SCRIPT = "sql/lexevs/common-create-hsqldb.sql";
-	private static final String CREATE_COMMON_SCRIPT = "sql/lexevs/common-create-mysql.sql";
+ 	private static final String CREATE_COMMON_SCRIPT = "sql/lexevs/common-create-hsqldb.sql";
 	
 	/** The Constant CREATE_CODINGSCHEME_SCRIPT. */
-//	private static final String CREATE_CODINGSCHEME_SCRIPT = "sql/lexevs/codingscheme-create-hsqldb.sql";
-	private static final String CREATE_CODINGSCHEME_SCRIPT = "sql/lexevs/codingscheme-create-mysql.sql";
+ 	private static final String CREATE_CODINGSCHEME_SCRIPT = "sql/lexevs/codingscheme-create-hsqldb.sql";
 	
 	/** The Constant CREATE_VD_PICKLIST_SCRIPT. */
-//	private static final String CREATE_VD_PICKLIST_SCRIPT = "sql/lexevs/valuesets-create-hsqldb.sql";
+ 	private static final String CREATE_VD_PICKLIST_SCRIPT = "sql/lexevs/valuesets-create-hsqldb.sql";
 	
-//	private static final String CREATE_CODINGSCHEME_HISTORY_SCRIPT = "sql/lexevs/codingschemehistory-create-hsqldb.sql";
+ 	private static final String CREATE_CODINGSCHEME_HISTORY_SCRIPT = "sql/lexevs/codingschemehistory-create-hsqldb.sql";
 	
 	/** The data source. */
 	@Resource
@@ -78,70 +75,20 @@ public class LexEvsDbUnitTestBase extends DataSourceBasedDBTestCase {
     public static void setSystemProp() {
         System.setProperty("LG_CONFIG_FILE", "src/test/resources/lbconfig.props");
     }
-	
-	/**
-	 * Test set up.
-	 */
-	@Test
-	/*public void testSetUp() {
-		assertTrue(true);
-	}*/
-	
+
 	/* (non-Javadoc)
 	 * @see org.dbunit.DatabaseTestCase#setUp()
 	 */
 	@Before
 	public void setUp() throws Exception {
-		/*methodCachingProxy.clearAll();
-		
+		methodCachingProxy.clearAll();
 		String prefix = prefixResolver.resolveDefaultPrefix();
-		JdbcOperations jdbcOps = new SimpleJdbcTemplate(dataSource).getJdbcOperations();
 		
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "csSupportedAttrib");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "csMultiAttrib");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "propertyMultiAttrib");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "propertyLinks");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "property");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "entityType");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "entityAssnQuals");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "entityAssnsToEntity");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "entityAssnsToEntityTr");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "entityAssnsToData");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "associationPredicate");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "associationEntity");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "relation");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "entity");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "codingScheme");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "entryState");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "revision");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "systemRelease");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "registryMetaData");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "registry");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_associationEntity");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_codingScheme");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_csMultiAttrib");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_entity");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_entityAssnQuals");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_entityAssnsToData");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_entityAssnsToEntity");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_property");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_propertyLinks");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_propertyMultiAttrib");
-		jdbcOps.execute("DROP TABLE IF EXISTS " + prefix + "h_relation");		
-		jdbcOps.execute("DROP TABLE IF EXISTS vsdEntry");
-		jdbcOps.execute("DROP TABLE IF EXISTS valueSetDefinition");
-		jdbcOps.execute("DROP TABLE IF EXISTS vsMultiAttrib");		
-		jdbcOps.execute("DROP TABLE IF EXISTS vsPLEntry");
-		jdbcOps.execute("DROP TABLE IF EXISTS vsPickList");
-		jdbcOps.execute("DROP TABLE IF EXISTS vsPropertyMultiAttrib");
-		jdbcOps.execute("DROP TABLE IF EXISTS vsProperty");
-		jdbcOps.execute("DROP TABLE IF EXISTS vsEntryState");
-		jdbcOps.execute("DROP TABLE IF EXISTS vsSupportedAttrib");
-		
+		new SimpleJdbcTemplate(dataSource).getJdbcOperations().execute("DROP SCHEMA PUBLIC CASCADE");
 		lexEvsDatabaseOperations.getDatabaseUtility().executeScript(new ClassPathResource(CREATE_COMMON_SCRIPT), prefix, prefix);
 		lexEvsDatabaseOperations.getDatabaseUtility().executeScript(new ClassPathResource(CREATE_CODINGSCHEME_SCRIPT), prefix, prefix);
 		lexEvsDatabaseOperations.getDatabaseUtility().executeScript(new ClassPathResource(CREATE_VD_PICKLIST_SCRIPT), prefix, prefix);
-		lexEvsDatabaseOperations.getDatabaseUtility().executeScript(new ClassPathResource(CREATE_CODINGSCHEME_HISTORY_SCRIPT), prefix, prefix);*/
+		lexEvsDatabaseOperations.getDatabaseUtility().executeScript(new ClassPathResource(CREATE_CODINGSCHEME_HISTORY_SCRIPT), prefix, prefix);
 	}
 	
 	/* (non-Javadoc)
@@ -149,8 +96,8 @@ public class LexEvsDbUnitTestBase extends DataSourceBasedDBTestCase {
 	 */
 	@After
 	public void tearDown() throws Exception {
-		/*super.tearDown();
-		new SimpleJdbcTemplate(dataSource).getJdbcOperations().execute("SHUTDOWN");*/
+		super.tearDown();
+		new SimpleJdbcTemplate(dataSource).getJdbcOperations().execute("SHUTDOWN");
 	}
 	
 	/* (non-Javadoc)
