1d66bab5d146a81e24134de8e2461df548cd8c47
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-26 13:46:01 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4964 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/access/association/model/Node.java
@@ -0,0 +1,20 @@
+package org.lexevs.dao.database.access.association.model;
+
+public class Node {
+
+	private String entityCode;
+	private String entityCodeNamespace;
+	
+	public String getEntityCode() {
+		return entityCode;
+	}
+	public void setEntityCode(String entityCode) {
+		this.entityCode = entityCode;
+	}
+	public String getEntityCodeNamespace() {
+		return entityCodeNamespace;
+	}
+	public void setEntityCodeNamespace(String entityCodeNamespace) {
+		this.entityCodeNamespace = entityCodeNamespace;
+	}
+}
