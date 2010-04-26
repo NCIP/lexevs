d5e5f9cddb81b87aa45cfdc1688a98359024c8db
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-04-26 13:47:42 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@4969 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lexevs-dao/src/test/java/org/lexevs/dao/database/operation/transitivity/DefaultTransitivityBuilderTest.java
@@ -0,0 +1,221 @@
+package org.lexevs.dao.database.operation.transitivity;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.lexevs.dao.test.LexEvsDbUnitTestBase;
+import org.lexevs.registry.model.RegistryEntry;
+import org.lexevs.registry.service.Registry;
+import org.lexevs.registry.utility.RegistryUtility;
+import org.lexevs.system.service.LexEvsResourceManagingService;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class DefaultTransitivityBuilderTest extends LexEvsDbUnitTestBase {
+
+	@Resource
+	private DefaultTransitivityBuilder defaultTransitivityBuilder;
+	
+	@Resource
+	private Registry registry;
+	
+	@Resource
+	private LexEvsResourceManagingService lexEvsResourceManagingService;
+	
+	@Test
+	public void getRegistryEntryForCodingSchemeName() throws Exception {
+		JdbcTemplate template = new JdbcTemplate(this.getDataSource());
+		
+		registry.addNewItem(RegistryUtility.codingSchemeToRegistryEntry("csuri", "csversion"));
+		
+		template.execute("Insert into codingScheme (codingSchemeGuid, codingSchemeName, codingSchemeUri, representsVersion) " +
+				"values ('cs-guid', 'csname', 'csuri', 'csversion')");
+		
+		lexEvsResourceManagingService.refresh();
+		
+		template.execute("Insert into cssupportedattrib (csSuppAttribGuid, codingSchemeGuid, supportedAttributeTag, id, assnCodingScheme, assnNamespace, assnEntityCode) " +
+				"values ('cssa-guid', 'cs-guid', 'Association', 'test-assoc', 'csname', 'ae-code', 'ae-codens')");
+
+		template.execute("insert into " +
+				"relation (relationGuid, codingSchemeGuid, containerName) " +
+				"values ('rel-guid', 'cs-guid', 'c-name')");
+		
+		template.execute("insert into " +
+				"associationpredicate (associationPredicateGuid," +
+				"relationGuid, associationName) values " +
+				"('ap-guid', 'rel-guid', 'test-assoc')");
+		
+		template.execute("Insert into entity (entityGuid, codingSchemeGuid, entityCode, entityCodeNamespace, isTransitive) " +
+				"values ('eguid', 'cs-guid', 'ae-code', 'ae-codens', true)");
+		
+		template.execute("insert into entityassnstoentity" +
+				" values ('eae-guid1'," +
+				" 'ap-guid'," +
+				" 's-code', " +
+				" 's-ns'," +
+				" 't-code1'," +
+				" 't-ns1'," +
+				" 'ai-id', null, null, null, null, null, null, null, null)");
+		
+		RegistryEntry entry = 
+			defaultTransitivityBuilder.getRegistryEntryForCodingSchemeName("csname");
+		
+		assertNotNull(entry);
+		
+		assertEquals("csuri", entry.getResourceUri());
+		assertEquals("csversion", entry.getResourceVersion());
+	}
+	
+	@Test
+	public void getTransitiveAssociationPredicateIds() throws Exception {
+		JdbcTemplate template = new JdbcTemplate(this.getDataSource());
+		
+		registry.addNewItem(RegistryUtility.codingSchemeToRegistryEntry("csuri", "csversion"));
+		
+		template.execute("Insert into codingScheme (codingSchemeGuid, codingSchemeName, codingSchemeUri, representsVersion) " +
+				"values ('cs-guid', 'csname', 'csuri', 'csversion')");
+		
+		lexEvsResourceManagingService.refresh();
+		
+		template.execute("Insert into cssupportedattrib (csSuppAttribGuid, codingSchemeGuid, supportedAttributeTag, id, assnCodingScheme, assnEntityCode, assnNamespace) " +
+				"values ('cssa-guid', 'cs-guid', 'Association', 'test-assoc', 'csname', 'ae-code', 'ae-codens')");
+
+		template.execute("insert into " +
+				"relation (relationGuid, codingSchemeGuid, containerName) " +
+				"values ('rel-guid', 'cs-guid', 'c-name')");
+		
+		template.execute("insert into " +
+				"associationpredicate (associationPredicateGuid," +
+				"relationGuid, associationName) values " +
+				"('ap-guid', 'rel-guid', 'test-assoc')");
+		
+		template.execute("Insert into entity (entityGuid, codingSchemeGuid, entityCode, entityCodeNamespace, isTransitive) " +
+				"values ('eguid', 'cs-guid', 'ae-code', 'ae-codens', true)");
+		
+		template.execute("Insert into entitytype (entityGuid, entityType) " +
+				"values ('eguid', 'association')");
+		
+		List<String> transitiveAssocs = 
+			defaultTransitivityBuilder.getTransitiveAssociationPredicateIds("csuri", "csversion");
+		
+		assertEquals(1, transitiveAssocs.size());
+		assertEquals("ap-guid", transitiveAssocs.get(0));
+	}
+	
+	@Test
+	public void isTransitive() throws Exception {
+		JdbcTemplate template = new JdbcTemplate(this.getDataSource());
+		
+		registry.addNewItem(RegistryUtility.codingSchemeToRegistryEntry("csuri", "csversion"));
+		
+		template.execute("Insert into codingScheme (codingSchemeGuid, codingSchemeName, codingSchemeUri, representsVersion) " +
+				"values ('cs-guid', 'csname', 'csuri', 'csversion')");
+		
+		lexEvsResourceManagingService.refresh();
+		
+		template.execute("Insert into cssupportedattrib (csSuppAttribGuid, codingSchemeGuid, supportedAttributeTag, id, assnCodingScheme, assnNamespace, assnEntityCode) " +
+				"values ('cssa-guid', 'cs-guid', 'Association', 'test-assoc', 'csname', 'ae-code', 'ae-codens')");
+
+		template.execute("insert into " +
+				"relation (relationGuid, codingSchemeGuid, containerName) " +
+				"values ('rel-guid', 'cs-guid', 'c-name')");
+		
+		template.execute("insert into " +
+				"associationpredicate (associationPredicateGuid," +
+				"relationGuid, associationName) values " +
+				"('ap-guid', 'rel-guid', 'test-assoc')");
+		
+		template.execute("Insert into entity (entityGuid, codingSchemeGuid, entityCode, entityCodeNamespace, isTransitive) " +
+				"values ('eguid', 'cs-guid', 'ae-code', 'ae-codens', true)");
+		
+		template.execute("Insert into entitytype (entityGuid, entityType) " +
+				"values ('eguid', 'association')");
+		
+		boolean isTransitive =
+			defaultTransitivityBuilder.isTransitive("csuri", "csversion", "ae-code", "ae-codens");
+		
+		assertTrue(isTransitive);
+	}
+	
+	@Test
+	public void isNotTransitive() throws Exception {
+		JdbcTemplate template = new JdbcTemplate(this.getDataSource());
+		
+		registry.addNewItem(RegistryUtility.codingSchemeToRegistryEntry("csuri", "csversion"));
+		
+		template.execute("Insert into codingScheme (codingSchemeGuid, codingSchemeName, codingSchemeUri, representsVersion) " +
+				"values ('cs-guid', 'csname', 'csuri', 'csversion')");
+		
+		lexEvsResourceManagingService.refresh();
+		
+		template.execute("Insert into cssupportedattrib (csSuppAttribGuid, codingSchemeGuid, supportedAttributeTag, id, assnCodingScheme, assnNamespace, assnEntityCode) " +
+				"values ('cssa-guid', 'cs-guid', 'Association', 'test-assoc', 'csname', 'ae-code', 'ae-codens')");
+
+		template.execute("insert into " +
+				"relation (relationGuid, codingSchemeGuid, containerName) " +
+				"values ('rel-guid', 'cs-guid', 'c-name')");
+		
+		template.execute("insert into " +
+				"associationpredicate (associationPredicateGuid," +
+				"relationGuid, associationName) values " +
+				"('ap-guid', 'rel-guid', 'test-assoc')");
+		
+		template.execute("Insert into entity (entityGuid, codingSchemeGuid, entityCode, entityCodeNamespace, isTransitive) " +
+				"values ('eguid', 'cs-guid', 'ae-code', 'ae-codens', false)");
+		
+		template.execute("Insert into entitytype (entityGuid, entityType) " +
+				"values ('eguid', 'association')");
+		
+		boolean isTransitive =
+			defaultTransitivityBuilder.isTransitive("csuri", "csversion", "ae-code", "ae-codens");
+		
+		assertFalse(isTransitive);
+	}
+	
+	@Test
+	public void testCount() throws Exception {
+
+		JdbcTemplate template = new JdbcTemplate(this.getDataSource());
+		
+		registry.addNewItem(RegistryUtility.codingSchemeToRegistryEntry("csuri", "csversion"));
+
+		template.execute("Insert into codingScheme (codingSchemeGuid, codingSchemeName, codingSchemeUri, representsVersion) " +
+				"values ('cs-guid', 'csname', 'csuri', 'csversion')");
+		
+		lexEvsResourceManagingService.refresh();
+		
+		template.execute("Insert into cssupportedattrib (csSuppAttribGuid, codingSchemeGuid, supportedAttributeTag, id, assnCodingScheme, assnEntityCode, assnNamespace) " +
+				"values ('cssa-guid', 'cs-guid', 'Association', 'test-assoc', 'csname', 'ae-code', 'ae-codens')");
+
+		template.execute("insert into " +
+				"relation (relationGuid, codingSchemeGuid, containerName) " +
+				"values ('rel-guid', 'cs-guid', 'c-name')");
+		
+		template.execute("insert into " +
+				"associationpredicate (associationPredicateGuid," +
+				"relationGuid, associationName) values " +
+				"('ap-guid', 'rel-guid', 'test-assoc')");
+		
+		template.execute("Insert into entity (entityGuid, codingSchemeGuid, entityCode, entityCodeNamespace, isNavigable, isTransitive) " +
+				"values ('eguid', 'cs-guid', 'ae-code', 'ae-codens', true, true)");
+		
+		template.execute("Insert into entitytype (entityGuid, entityType) " +
+				"values ('eguid', 'association')");
+		
+		template.execute("insert into entityassnstoentity" +
+				" values ('eae-guid1'," +
+				" 'ap-guid'," +
+				" 's-code', " +
+				" 's-ns'," +
+				" 't-code1'," +
+				" 't-ns1'," +
+				" 'ai-id', null, null, null, null, null, null, null, null)");
+
+		defaultTransitivityBuilder.computeTransitivityTable("csuri", "csversion");
+		
+		int count = template.queryForInt("select count(*) from entityassnstoentitytr");
+		
+		assertEquals(1, count);
+	}
+}
