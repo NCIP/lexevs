30094364fda863cc29971d375ee7ecddbe91286f
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-05-06 15:40:08 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5160 c4b421de-a31f-834e-8902-8020e42e311c

--- loader-framework-core/src/main/java/org/lexgrid/loader/properties/ConnectionPropertiesFactory.java
+++ loader-framework-core/src/main/java/org/lexgrid/loader/properties/ConnectionPropertiesFactory.java
@@ -20,6 +20,8 @@ package org.lexgrid.loader.properties;
 
 import java.util.Properties;
 
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+
 /**
  * A factory for creating ConnectionProperties objects.
  */
@@ -39,6 +41,7 @@ public interface ConnectionPropertiesFactory {
 	 * @param version the version
 	 * 
 	 * @return the properties for existing load
+	 * @throws LBParameterException 
 	 */
-	public Properties getPropertiesForExistingLoad(String codingScheme, String version);
+	public Properties getPropertiesForExistingLoad(String codingScheme, String version) throws LBParameterException;
 }
