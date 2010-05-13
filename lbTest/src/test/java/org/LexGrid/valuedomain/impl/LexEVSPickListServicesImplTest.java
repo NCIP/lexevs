67af49304e09a688bd1722b987496ef1a302c853
dwarkans <dwarkans@c4b421de-a31f-834e-8902-8020e42e311c>
2010-05-13 18:42:33 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5345 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/valuedomain/impl/LexEVSPickListServicesImplTest.java
+++ lbTest/src/test/java/org/LexGrid/valuedomain/impl/LexEVSPickListServicesImplTest.java
@@ -284,7 +284,7 @@ public class LexEVSPickListServicesImplTest extends TestCase{
 	public void testResolvePickListForTermForCompleteDomain() throws LBException {
 		ResolvedPickListEntryList pickLists = getPickListService().resolvePickListForTerm("SRITEST:FA:MSOntologyAndHyphaInMycelium", "hypha", MatchAlgorithms.LuceneQuery.name(), null, null, false);
 		
-		assertTrue(pickLists.getResolvedPickListEntryCount() == 4);
+		assertTrue(pickLists.getResolvedPickListEntryCount() == 9);
 		
 		Iterator<ResolvedPickListEntry> plItr = pickLists.iterateResolvedPickListEntry();
 		while(plItr.hasNext())
@@ -293,6 +293,10 @@ public class LexEVSPickListServicesImplTest extends TestCase{
 			assertTrue(pickList.getEntityCode().equals("FAO:0001014")
 					|| pickList.getEntityCode().equals("FAO:0001013")
 					|| pickList.getEntityCode().equals("FAO:0001004")
+					|| pickList.getEntityCode().equals("FAO:0001012")
+					|| pickList.getEntityCode().equals("FAO:0001015")
+					|| pickList.getEntityCode().equals("FAO:0001017")
+					|| pickList.getEntityCode().equals("FAO:0001020")
 					|| pickList.getEntityCode().equals("FAO:0001003"));
 			assertTrue(pickList.getEntityCodeNamespace().equals("fungal_anatomy"));
 			assertTrue(pickList.getPickText().contains("hypha"));
@@ -402,7 +406,7 @@ public class LexEVSPickListServicesImplTest extends TestCase{
 	public void testResolvePickListForCompleteDomain() throws LBException {
 		ResolvedPickListEntryList pickLists = getPickListService().resolvePickList("SRITEST:FA:hyphaLeafOnly", true);
 		
-		assertTrue(pickLists.getResolvedPickListEntryCount() == 3);
+		assertTrue(pickLists.getResolvedPickListEntryCount() == 5);
 		
 		Iterator<ResolvedPickListEntry> plItr = pickLists.iterateResolvedPickListEntry();
 		while(plItr.hasNext())
@@ -412,6 +416,10 @@ public class LexEVSPickListServicesImplTest extends TestCase{
 						&& pickList.getPickText().equals("hypha with dolipore septa"))
 					|| (pickList.getEntityCode().equals("FAO:0001014")
 						&& pickList.getPickText().equals("aseptate hypha in mycelium"))
+					|| (pickList.getEntityCode().equals("FAO:0001017")
+						&& pickList.getPickText().equals("hyphal tip, hypha with dolipore septa in mycelium"))
+					|| (pickList.getEntityCode().equals("FAO:0001020")
+						&& pickList.getPickText().equals("hyphal tip, aseptate hypha in mycelium"))
 					|| (pickList.getEntityCode().equals("FAO:0001013")
 						&& pickList.getPickText().equals("hypha with dolipore septa, in mycelium")));
 			assertTrue(pickList.getEntityCodeNamespace().equals("fungal_anatomy"));
@@ -465,19 +473,6 @@ public class LexEVSPickListServicesImplTest extends TestCase{
 						|| plEntry.getPickListEntryId().equals("PLGMp2"));
 			}
 		}
-	}
-	
-	@Test
-	public void testGetPickListIdByEntityReference() throws LBException{
-		System.out.println("in testGetPickListIdByEntityReference");
-		
-		Map<String, String> plRefs = getPickListService().getReferencedPLDefinitions("entityCode 2", null, null, false);
-		
-		System.out.println("is plRef null? " + (plRefs == null));
-//		for (String plId : plRefs.keySet())
-//		{
-//			System.out.println("pl ids : " + plId);
-//		}
 	}
 	
 	private LexEVSPickListDefinitionServices getPickListService(){
