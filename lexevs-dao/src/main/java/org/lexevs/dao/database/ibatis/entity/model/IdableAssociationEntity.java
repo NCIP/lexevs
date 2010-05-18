25c8e703eae3691b778b5b41b7de9fa9da582631
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-05-18 19:05:18 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5467 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/main/java/org/lexevs/dao/database/ibatis/entity/model/IdableAssociationEntity.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/ibatis/entity/model/IdableAssociationEntity.java
@@ -1,8 +1,9 @@
 package org.lexevs.dao.database.ibatis.entity.model;
 
+import org.LexGrid.proxy.CastorProxy;
 import org.LexGrid.relations.AssociationEntity;
 
-public class IdableAssociationEntity extends AssociationEntity {
+public class IdableAssociationEntity extends AssociationEntity implements CastorProxy {
 
 	private static final long serialVersionUID = 6631733263624593955L;
 	private String id;
--- lexevs-dao/src/main/java/org/lexevs/dao/database/ibatis/entity/model/IdableEntity.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/ibatis/entity/model/IdableEntity.java
@@ -1,8 +1,9 @@
 package org.lexevs.dao.database.ibatis.entity.model;
 
 import org.LexGrid.concepts.Entity;
+import org.LexGrid.proxy.CastorProxy;
 
-public class IdableEntity extends Entity {
+public class IdableEntity extends Entity implements CastorProxy {
 
 	private static final long serialVersionUID = 6631733263624593955L;
 	private String id;
