975a86e0740ccb6ad5e6a29c3193233b6205e986
dwarkans <dwarkans@c4b421de-a31f-834e-8902-8020e42e311c>
2010-05-20 16:33:03 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5524 c4b421de-a31f-834e-8902-8020e42e311c

--- lgValueSets/src/org/lexgrid/conceptdomain/util/ConceptDomainConstants.java
+++ lgValueSets/src/org/lexgrid/conceptdomain/util/ConceptDomainConstants.java
@@ -1,8 +1,8 @@
 package org.lexgrid.conceptdomain.util;
 
 public class ConceptDomainConstants {
-	public static String CONCEPT_DOMAIN_CODING_SCHEME_URI = "conceptDomainCSURI";
-	public static String CONCEPT_DOMAIN_CODING_SCHEME_FORMAL_NAME = "conceptDomainCSName";
+	public static String CONCEPT_DOMAIN_CODING_SCHEME_URI = "http://lexevs.org/codingscheme/conceptdomain";
+	public static String CONCEPT_DOMAIN_CODING_SCHEME_FORMAL_NAME = "conceptDomainCodingScheme";
 	public static String CONCEPT_DOMAIN_CODING_SCHEME_LOCAL_NAME = "conceptDomainCS";
 	public static String CONCEPT_DOMAIN_CODING_SCHEME_VERSION = "1.0";
 	public static String CONCEPT_DOMAIN_ENTITY_TYPE = "conceptDomain";
