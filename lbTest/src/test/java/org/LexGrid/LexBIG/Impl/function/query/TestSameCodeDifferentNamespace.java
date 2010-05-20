01c6142d964e2ce9c8e9dadf801721f9de6930ec
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-05-20 11:10:29 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5507 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/LexBIG/Impl/function/query/TestSameCodeDifferentNamespace.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/function/query/TestSameCodeDifferentNamespace.java
@@ -18,6 +18,8 @@
  */
 package org.LexGrid.LexBIG.Impl.function.query;
 
+import lexevs.bootcamp.utility.PrintUtility;
+
 import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
 import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
 import org.LexGrid.LexBIG.Exceptions.LBParameterException;
@@ -42,7 +44,7 @@ public class TestSameCodeDifferentNamespace extends BaseCodedNodeSetTest{
      */
     public void testNoRestrictions() throws LBInvocationException, LBParameterException{
         ResolvedConceptReference[] refs = cns.resolveToList(null, null, null, -1).getResolvedConceptReference();
-        
+        PrintUtility.print(cns);
         assertTrue(contains(refs, "DifferentNamespaceConcept", AUTO_SCHEME, AUTO_SCHEME));
         assertTrue(contains(refs, "DifferentNamespaceConcept", "TestForSameCodeNamespace", AUTO_SCHEME));
     }
