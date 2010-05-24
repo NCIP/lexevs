d4bd3d1c45255b890b94b9628481d37d0de69cdb
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-05-24 19:29:21 +0000

    Factory for metadata index.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5604 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lexevs-dao/src/main/java/org/lexevs/dao/index/factory/MetadataIndexLocationFactory.java
@@ -0,0 +1,39 @@
+package org.lexevs.dao.index.factory;
+
+import java.io.File;
+
+import org.lexevs.system.constants.SystemVariables;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.Assert;
+
+public class MetadataIndexLocationFactory implements FactoryBean {
+	
+	private SystemVariables systemVariables;
+
+	@Override
+	public Object getObject() throws Exception {
+
+		return new FileSystemResource(systemVariables.getAutoLoadIndexLocation() +
+			File.separator + SystemVariables.getMetaDataIndexName());
+	}
+
+	@Override
+	public Class getObjectType() {
+		return Resource.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+	public SystemVariables getSystemVariables() {
+		return systemVariables;
+	}
+
+	public void setSystemVariables(SystemVariables systemVariables) {
+		this.systemVariables = systemVariables;
+	}
+}
