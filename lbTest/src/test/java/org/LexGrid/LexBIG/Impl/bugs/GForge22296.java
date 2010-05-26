125ca8edf32404cbe9994b09dc5575e995479f71
sharmad <sharmad@c4b421de-a31f-834e-8902-8020e42e311c>
2010-05-26 16:16:43 +0000

    GForgeBug# 22296 resolution - Checking if LexGrid Loader load a concept with parenthesis in code and description.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5668 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/bugs/GForge22296.java
@@ -0,0 +1,51 @@
+package org.LexGrid.LexBIG.Impl.bugs;
+
+import java.io.File;
+
+import org.LexGrid.LexBIG.DataModel.Collections.ResolvedConceptReferenceList;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.Impl.loaders.LexGridMultiLoaderImpl;
+import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.codingSchemes.CodingScheme;
+import org.LexGrid.relations.Relations;
+
+import junit.framework.TestCase;
+
+public class GForge22296 extends TestCase 
+{
+    public void testConceptWithParanthesis() throws LBException 
+    {
+    	LexBIGService service = ServiceHolder.instance().getLexBIGService();
+
+        CodingSchemeVersionOrTag versionOrTag = new CodingSchemeVersionOrTag();
+        versionOrTag.setVersion("1.0");
+        
+        try 
+        {
+            CodedNodeSet nodeSet = service.getCodingSchemeConcepts("Automobiles", versionOrTag);
+            
+            nodeSet.restrictToCodes(Constructors.createConceptReferenceList("C0011(5564)"));
+            
+            ResolvedConceptReferenceList resolvedCodeList = nodeSet.resolveToList(null, null, null, 1);
+            
+            ResolvedConceptReference resolvedCode = resolvedCodeList.getResolvedConceptReference()[0];
+            
+            assertNotNull(resolvedCode);
+        
+        } catch (LBInvocationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (LBParameterException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
