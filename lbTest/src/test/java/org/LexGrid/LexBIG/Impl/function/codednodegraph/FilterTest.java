2f5b95d20ff52b4c9bad45762051b24fba4fd67f
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-06-03 12:47:51 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5815 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/function/codednodegraph/FilterTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.LexBIG.Impl.function.codednodegraph;
+
+import org.LexGrid.LexBIG.DataModel.Core.AssociatedConcept;
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.ExtensionDescription;
+import org.LexGrid.LexBIG.Extensions.Query.Filter;
+import org.LexGrid.LexBIG.Impl.Extensions.AbstractExtendable;
+import org.LexGrid.LexBIG.Impl.Extensions.ExtensionRegistryImpl;
+import org.LexGrid.LexBIG.Impl.helpers.TestFilter2;
+import org.LexGrid.LexBIG.Utility.Constructors;
+
+/**
+ * The Class ResolveToListTest.
+ * 
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
+public class FilterTest extends BaseCodedNodeGraphTest {
+
+    /**
+     * Test resolve to list associated concept count.
+     * 
+     * @throws Exception the exception
+     */
+    public void testFilter() throws Exception {
+
+    	ExtensionRegistryImpl.instance().registerFilterExtension(new AllButGMFilter().getExtensionDescription());
+    	
+        ResolvedConceptReference[] rcr = 
+            cng.resolveAsList(Constructors.createConceptReference("005", AUTO_SCHEME), 
+                    true, 
+                    false, 
+                    -1, 
+                    -1, 
+                    null, 
+                    null, 
+                    null, 
+                    Constructors.createLocalNameList("allButGM"),
+                    -1).getResolvedConceptReference();
+        
+        AssociatedConcept[] acons = 
+        	rcr[0].getSourceOf().getAssociation()[0].getAssociatedConcepts().getAssociatedConcept();
+        
+        assertEquals(2,acons.length);
+        
+        assertTrue(contains(acons, "Ford"));
+        assertTrue(contains(acons, "A"));
+        assertFalse(contains(acons, "GM"));
+    }
+    
+    public void testFilterFilterOutFocus() throws Exception {
+
+    	try {
+			ExtensionRegistryImpl.instance().registerFilterExtension(new AllButGMFilter().getExtensionDescription());
+		} catch (Exception e) {
+			// may be already registered.
+		}
+    	
+        ResolvedConceptReference[] rcr = 
+            cng.resolveAsList(Constructors.createConceptReference("GM", AUTO_SCHEME), 
+                    true, 
+                    false, 
+                    -1, 
+                    -1, 
+                    null, 
+                    null, 
+                    null, 
+                    Constructors.createLocalNameList("allButGM"),
+                    -1).getResolvedConceptReference();
+        
+        assertEquals(0,rcr.length);
+    }
+    
+    private boolean contains(AssociatedConcept[] assocCons, String code) {
+    	for(AssociatedConcept con : assocCons) {
+    		if(con.getCode().equals(code)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    private static class AllButGMFilter extends AbstractExtendable implements Filter {
+
+		@Override
+		protected ExtensionDescription buildExtensionDescription() {
+			ExtensionDescription ed = new ExtensionDescription();
+			ed.setExtensionBaseClass(TestFilter2.class.getInterfaces()[0].getName());
+			ed.setExtensionClass(TestFilter2.class.getName());
+			ed.setDescription("allButGM");
+			ed.setName("allButGM");
+			
+			return ed;
+
+		}
+
+		@Override
+		public boolean match(ResolvedConceptReference ref) {
+			return ! ref.getCode().equalsIgnoreCase("GM");
+		}
+    }
+}
