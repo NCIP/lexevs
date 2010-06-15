03dd09ede9b6be0b52758f456a35b9d4b8349db1
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-06-15 15:06:43 +0000

    rudimentry authoring service shell
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5985 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/LexEVSAuthoringServiceImpl.java
@@ -0,0 +1,59 @@
+package org.LexGrid.LexBIG.Impl;
+
+import org.LexGrid.LexBIG.DataModel.Core.Association;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.LexBIGService.LexEVSAuthoringService;
+
+
+
+public class LexEVSAuthoringServiceImpl implements LexEVSAuthoringService{
+
+    @Override
+    public Association createAssociation() throws LBException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void createAssociationPredicate() throws LBException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createAssociationSource() throws LBException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createAssociationTarget() throws LBException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createCodingScheme() throws LBException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createEntity() throws LBException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createMapping() throws LBException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createRelationsContainer() throws LBException {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
