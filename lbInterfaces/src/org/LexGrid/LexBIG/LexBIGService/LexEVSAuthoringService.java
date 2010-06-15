eb814f2b3fd1edad0218b6b3e9dc9f2669e819c7
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-06-15 21:56:50 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5990 c4b421de-a31f-834e-8902-8020e42e311c

--- lbInterfaces/src/org/LexGrid/LexBIG/LexBIGService/LexEVSAuthoringService.java
+++ lbInterfaces/src/org/LexGrid/LexBIG/LexBIGService/LexEVSAuthoringService.java
@@ -1,18 +1,58 @@
 package org.LexGrid.LexBIG.LexBIGService;
 
+
+import java.util.Date;
+
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
 import org.LexGrid.LexBIG.DataModel.Core.Association;
 import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.codingSchemes.CodingScheme;
+import org.LexGrid.commonTypes.Properties;
+import org.LexGrid.concepts.Entities;
+import org.LexGrid.concepts.Entity;
+import org.LexGrid.relations.AssociationPredicate;
+import org.LexGrid.relations.AssociationQualification;
+import org.LexGrid.relations.AssociationSource;
+import org.LexGrid.relations.AssociationTarget;
+import org.LexGrid.relations.Relations;
 
 public interface LexEVSAuthoringService {
 	
-	public void createCodingScheme()throws LBException;
-	public void createEntity()throws LBException;
-	public void createRelationsContainer()throws LBException;
-	public void createAssociationPredicate()throws LBException;
-	public void createAssociationSource()throws LBException;
-	public void createAssociationTarget()throws LBException;
-	public Association createAssociation()throws LBException;
-	public void createMapping()throws LBException;
+	public CodingScheme createCodingScheme()throws LBException;
 	
-
+	public Entities createEntities(CodingScheme scheme)throws LBException;
+	
+	public Entity createEntity(Entities entities)throws LBException;
+	
+	public Relations createRelationsContainer(CodingScheme scheme,
+			String containerName,
+			Date effectiveDate,
+            AbsoluteCodingSchemeVersionReference sourceCodeSystemIdentifier,
+            AbsoluteCodingSchemeVersionReference targetCodeSystemIdentifier,
+            boolean isMapping,
+            String associationType,
+            Properties relationProperties)throws LBException;
+	
+	public AssociationPredicate createAssociationPredicate(CodingScheme scheme, String associationName)throws LBException;
+	
+	public Properties createCodingSchemeProperties(CodingScheme scheme)throws LBException;
+	
+	public AssociationSource createAssociationSource(Relations relations, AssociationPredicate predicate,
+			AbsoluteCodingSchemeVersionReference sourceCodeSystemIdentifier,
+			String sourceConceptCodeIdentifier
+			)throws LBException;
+	
+	public AssociationTarget createAssociationTarget(Association Source)throws LBException;
+	
+	public Association createAssociation()throws LBException;
+	
+	public void createAssociationMapping(Relations relation, 
+			AbsoluteCodingSchemeVersionReference sourceCodingScheme, 
+			AbsoluteCodingSchemeVersionReference targetCodingScheme,
+			AssociationSource[] associationSource,
+			String associationType,
+			String relationsContainerName,
+			Date effectiveDate,
+			AssociationQualification[] associationQualifiers
+			)throws LBException;
 }
