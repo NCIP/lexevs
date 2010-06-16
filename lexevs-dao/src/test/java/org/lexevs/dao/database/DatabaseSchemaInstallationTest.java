470711fe8bb2aa9a0a4a88eb587d43192ce288bf
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-06-16 15:54:29 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@5997 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lexevs-dao/src/test/java/org/lexevs/dao/database/DatabaseSchemaInstallationTest.java
@@ -0,0 +1,19 @@
+package org.lexevs.dao.database;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.lexevs.dao.database.operation.LexEvsDatabaseOperations;
+import org.lexevs.dao.test.LexEvsDbUnitTestBase;
+
+public class DatabaseSchemaInstallationTest extends LexEvsDbUnitTestBase{
+	
+	@Resource
+	private LexEvsDatabaseOperations lexEvsDatabaseOperations;
+	
+	@Test
+	public void testInstallCommonTables() throws Exception {
+		lexEvsDatabaseOperations.dropAllTables();
+		lexEvsDatabaseOperations.createAllTables();
+	}
+}
