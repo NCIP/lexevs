b0bac83ba69831981665f0ed06566d75451aba31
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2010-06-25 18:07:49 +0000

    add the mapping.xml for testing.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6132 c4b421de-a31f-834e-8902-8020e42e311c

--- lgConverter/src/edu/mayo/informatics/lexgrid/convert/exporters/xml/lgxml/formatters/XmlContentWriter.java
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/exporters/xml/lgxml/formatters/XmlContentWriter.java
@@ -18,108 +18,125 @@
  */
 package edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.formatters;
 
+import java.io.IOException;
 import java.io.Writer;
+import java.net.URL;
 
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeGraph;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
 import org.LexGrid.proxy.CastorProxy;
 import org.castor.xml.XMLProperties;
+import org.exolab.castor.mapping.Mapping;
+import org.exolab.castor.mapping.MappingException;
 import org.exolab.castor.xml.MarshalListener;
 import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.XMLContext;
 
 import edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.constants.LexGridConstants;
 import edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.listeners.LexGridMarshalListener;
 import edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.listeners.StreamingLexGridMarshalListener;
+import edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.listeners.TestMarshalListener;
 
 /**
  * The Class AbstractXmlMessageBodyWriter.
  * 
  * Adds common XML marshalling functionality to the AbstractMessageBodyWriter.
  */
-// public abstract class AbstractXmlContentWriter extends AbstractMessageBodyWriter {
+// public abstract class AbstractXmlContentWriter extends
+// AbstractMessageBodyWriter {
 public class XmlContentWriter {
 
-	/** The namespace cognizant marshaller. */
-	private Marshaller ns_marshaller;
-	{
-	    ns_marshaller = new Marshaller();
-	    ns_marshaller.setProperty(XMLProperties.USE_INDENTATION, "true");
-	    ns_marshaller.setMarshalAsDocument(true);
-	    ns_marshaller.setMarshalExtendedType(false);
-	    ns_marshaller.setSuppressNamespaces(false);
-	    ns_marshaller.setSchemaLocation(LexGridConstants.lgSchemaLocation);  // mct
-	    ns_marshaller.setSupressXMLDeclaration(true);
-	    ns_marshaller.setSuppressXSIType(false);
-	    ns_marshaller.setValidation(true);
-//	    ns_marshaller.setEncoding("UTF-8");
-	    ns_marshaller.setProperty(XMLProperties.PROXY_INTERFACES, CastorProxy.class.getCanonicalName());
-	    ns_marshaller.setNamespaceMapping("lgBuiltin", LexGridConstants.lgBuiltin);
-	    ns_marshaller.setNamespaceMapping("lgCommon", LexGridConstants.lgCommon);
-	    ns_marshaller.setNamespaceMapping("lgCon", LexGridConstants.lgCon);
-	    ns_marshaller.setNamespaceMapping("lgCS", LexGridConstants.lgCS);
-	    ns_marshaller.setNamespaceMapping("lgNaming", LexGridConstants.lgNaming);
-	    ns_marshaller.setNamespaceMapping("lgRel", LexGridConstants.lgRel);
-	    ns_marshaller.setNamespaceMapping("lgVD", LexGridConstants.lgVD);
-	    ns_marshaller.setNamespaceMapping("lgVer", LexGridConstants.lgVer);
-	    ns_marshaller.setNamespaceMapping("xsi", LexGridConstants.lgXSI);  // mct
-	    ns_marshaller.setInternalContext(ns_marshaller.getInternalContext());
-	}
-	
-	
-	public void marshalToXml(Object obj, CodedNodeSet cns, Writer writer, int pageSize) {
-		 Marshaller marshaller = ns_marshaller;
-		 LexGridMarshalListener listener = new LexGridMarshalListener(marshaller, cns, pageSize);
-		try {
-			marshaller.setMarshalListener(listener);
-			marshaller.setWriter(writer);
-			marshaller.marshal(obj);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} 
-	}
+    /** The namespace cognizant marshaller. */
+    private Marshaller ns_marshaller;
+    {
+        Mapping mapping = new Mapping();
+        try {
+            mapping.loadMapping("file:///c:/xml_exp/mapping.xml");            
+            
+//            XMLContext context = new XMLContext();
+//            context.addMapping(mapping);
+//            ns_marshaller = context.createMarshaller();
+            ns_marshaller = new Marshaller();
+            ns_marshaller.setMapping(mapping);
+            ns_marshaller.setProperty(XMLProperties.USE_INDENTATION, "true");
+            ns_marshaller.setMarshalAsDocument(true);
+            ns_marshaller.setMarshalExtendedType(false);
+            ns_marshaller.setSuppressNamespaces(false);
+            ns_marshaller.setSchemaLocation(LexGridConstants.lgSchemaLocation); // mct
+            ns_marshaller.setSupressXMLDeclaration(true);
+            ns_marshaller.setSuppressXSIType(false);
+            ns_marshaller.setValidation(true);
+            // ns_marshaller.setEncoding("UTF-8");
+            ns_marshaller.setProperty(XMLProperties.PROXY_INTERFACES, CastorProxy.class.getCanonicalName());
+            ns_marshaller.setNamespaceMapping("lgBuiltin", LexGridConstants.lgBuiltin);
+            ns_marshaller.setNamespaceMapping("lgCommon", LexGridConstants.lgCommon);
+            ns_marshaller.setNamespaceMapping("lgCon", LexGridConstants.lgCon);
+            ns_marshaller.setNamespaceMapping("lgCS", LexGridConstants.lgCS);
+            ns_marshaller.setNamespaceMapping("lgNaming", LexGridConstants.lgNaming);
+            ns_marshaller.setNamespaceMapping("lgRel", LexGridConstants.lgRel);
+            ns_marshaller.setNamespaceMapping("lgVD", LexGridConstants.lgVD);
+            ns_marshaller.setNamespaceMapping("lgVer", LexGridConstants.lgVer);
+            ns_marshaller.setNamespaceMapping("xsi", LexGridConstants.lgXSI); // mct
+            ns_marshaller.setInternalContext(ns_marshaller.getInternalContext());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (MappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	public void marshalToXml(Object obj, CodedNodeGraph cng, Writer writer, int pageSize) {
-		 Marshaller marshaller = ns_marshaller;
-		 LexGridMarshalListener listener = new LexGridMarshalListener(marshaller, cng, pageSize);
-		try {
-			marshaller.setMarshalListener(listener);
-			marshaller.setWriter(writer);
-			marshaller.marshal(obj);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} 
-	}
+    public void marshalToXml(Object obj, CodedNodeSet cns, Writer writer, int pageSize) {
+        Marshaller marshaller = ns_marshaller;
+        LexGridMarshalListener listener = new LexGridMarshalListener(marshaller, cns, pageSize);
+        try {
+            marshaller.setMarshalListener(listener);
+            marshaller.setWriter(writer);
+            marshaller.marshal(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/*
-	public void marshalToXml(Object obj, CodedNodeGraph cng, CodedNodeSet cns, Writer writer, int pageSize) {
-		 Marshaller marshaller = ns_marshaller;
-		 LexGridMarshalListener listener = new LexGridMarshalListener(marshaller, cng, cns, pageSize);
-		try {
-			marshaller.setMarshalListener(listener);
-			marshaller.setWriter(writer);
-			marshaller.marshal(obj);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} 
-	}
-*/	
-    public void marshalToXml(Object obj, CodedNodeGraph cng, CodedNodeSet cns, Writer writer, int pageSize, boolean useStreaming) {
+    public void marshalToXml(Object obj, CodedNodeGraph cng, Writer writer, int pageSize) {
+        Marshaller marshaller = ns_marshaller;
+        LexGridMarshalListener listener = new LexGridMarshalListener(marshaller, cng, pageSize);
+        try {
+            marshaller.setMarshalListener(listener);
+            marshaller.setWriter(writer);
+            marshaller.marshal(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+     * public void marshalToXml(Object obj, CodedNodeGraph cng, CodedNodeSet
+     * cns, Writer writer, int pageSize) { Marshaller marshaller =
+     * ns_marshaller; LexGridMarshalListener listener = new
+     * LexGridMarshalListener(marshaller, cng, cns, pageSize); try {
+     * marshaller.setMarshalListener(listener); marshaller.setWriter(writer);
+     * marshaller.marshal(obj); } catch (Exception e) { throw new
+     * RuntimeException(e); } }
+     */
+    public void marshalToXml(Object obj, CodedNodeGraph cng, CodedNodeSet cns, Writer writer, int pageSize,
+            boolean useStreaming) {
         Marshaller marshaller = ns_marshaller;
         MarshalListener listener = null;
-        if(useStreaming == true) {
+        if (useStreaming == true) {
             listener = new StreamingLexGridMarshalListener(marshaller, cng, cns, pageSize);
         } else {
-            listener = new LexGridMarshalListener(marshaller, cng, cns, pageSize); 
-        }        
-       try {
-           marshaller.setMarshalListener(listener);
-           marshaller.setWriter(writer);
-           marshaller.marshal(obj);
-           writer.close(); //close the writer after the marshaling job done
-       } catch (Exception e) {
-           throw new RuntimeException(e);
-       } 
-   }
-	
-	
+            listener = new LexGridMarshalListener(marshaller, cng, cns, pageSize);
+        }
+        try {
+            marshaller.setMarshalListener(listener);
+            marshaller.setWriter(writer);
+            marshaller.marshal(obj);
+            writer.close(); // close the writer after the marshaling job done
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
