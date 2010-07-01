e1e8b50c6922104ac6514b7fba4b53e56aa774ae
mturk <mturk@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-01 20:04:42 +0000

    class for debug.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6187 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/exporters/xml/lgxml/impl/AssociationSourceCacheNoopImpl.java
@@ -0,0 +1,39 @@
+package edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.impl;
+
+/*
+ * code based on example from: http://www.hsqldb.org/doc/guide/apb.html
+ */
+
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.relations.AssociationSource;
+
+import edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.interfaces.AssociationSourceCache;
+
+public class AssociationSourceCacheNoopImpl implements AssociationSourceCache {
+
+    public AssociationSourceCacheNoopImpl() {
+        super();
+        System.out.println("AssociationSourceCacheNoopImpl: debug version 3");
+    }
+    
+    @Override
+    public void add(AssociationSource associationSource) {
+        //
+    }
+
+    @Override
+    public void clear() {
+        //
+    }
+
+    @Override
+    public void dumpCacheContentsToStdOut() {
+        //
+    }
+
+    @Override
+    public boolean exists(ResolvedConceptReference rcr) {
+        return false;
+    }
+
+}
