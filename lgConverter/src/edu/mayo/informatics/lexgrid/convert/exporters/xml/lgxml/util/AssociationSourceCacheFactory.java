ac303c6589936808caf935f554cbc4d0f31d0f9f
mturk <mturk@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-01 20:05:03 +0000

    return noop cache.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6188 c4b421de-a31f-834e-8902-8020e42e311c

--- lgConverter/src/edu/mayo/informatics/lexgrid/convert/exporters/xml/lgxml/util/AssociationSourceCacheFactory.java
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/exporters/xml/lgxml/util/AssociationSourceCacheFactory.java
@@ -1,12 +1,14 @@
 package edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.util;
 
 import edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.impl.AssociationSourceCacheEhcacheImpl;
+import edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.impl.AssociationSourceCacheNoopImpl;
 import edu.mayo.informatics.lexgrid.convert.exporters.xml.lgxml.interfaces.AssociationSourceCache;
 
 public class AssociationSourceCacheFactory {
     
     public static AssociationSourceCache createCache() {
-        return new AssociationSourceCacheEhcacheImpl();
+        // return new AssociationSourceCacheEhcacheImpl();
+        return new AssociationSourceCacheNoopImpl();
     }
 
 }
