1564b9b738b72fd693dab20dd247df62c308e171
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-05 18:13:12 +0000

    AssociationStatus Update test
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6229 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/mapping/LexEVSAssociationStatusUpdateTest.java
@@ -0,0 +1,72 @@
+package org.LexGrid.LexBIG.mapping;
+
+import java.util.Date;
+
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
+import org.LexGrid.LexBIG.Impl.LexEVSAuthoringServiceImpl;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
+import org.LexGrid.commonTypes.EntityDescription;
+import org.LexGrid.commonTypes.Text;
+import org.LexGrid.versions.EntryState;
+import org.LexGrid.versions.Revision;
+
+import junit.framework.TestCase;
+
+public class LexEVSAssociationStatusUpdateTest extends TestCase {
+
+	LexEVSAuthoringServiceImpl authoring;
+	LexBIGService lbs;
+	LexBIGServiceManager lbsm;
+	public static String SOURCE_SCHEME = "GermanMadeParts";
+	public static String SOURCE_VERSION = "2.0";
+	public static String MAPPING_SCHEME = "http://default.mapping.container";
+	public static String MAPPING_VERSION = "1.0";
+	public static String TARGET_SCHEME = "Automobiles";
+	public static String TARGET_URN = "urn:oid:11.11.0.1";
+	public static String TARGET_VERSION = "1.0";
+
+	public void setUp() {
+
+		authoring = new LexEVSAuthoringServiceImpl();
+		lbs = LexBIGServiceImpl.defaultInstance();
+
+		try {
+			lbsm = lbs.getServiceManager(null);
+		} catch (LBException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testAssociationStatusUpdate() throws LBException {
+		Revision revision = new Revision();
+
+		revision.setChangeAgent("Mayo_Test_agent");
+		Text changeInstructions = new Text();
+		changeInstructions.setContent("Test instructions");
+		revision.setChangeInstructions(changeInstructions);
+
+		revision.setEditOrder(new Long(1));
+		EntityDescription entityDescription = new EntityDescription();
+		entityDescription.setContent("TestAssociationStatusRevision");
+		revision.setEntityDescription(entityDescription);
+
+		Date revisionDate = new Date();
+		revision.setRevisionDate(revisionDate);
+		EntryState entryState = new EntryState();
+		entryState.setContainingRevision("Mayo_Status_Revision");
+		entryState.setPrevRevision("Should_not_be_set");
+		entryState.setRelativeOrder(new Long(0));
+
+		AbsoluteCodingSchemeVersionReference scheme = new AbsoluteCodingSchemeVersionReference();
+		scheme.setCodingSchemeURN(TARGET_URN);
+		scheme.setCodingSchemeVersion(TARGET_VERSION);
+		assertTrue(authoring.setAssociationStatus(revision, entryState, scheme,
+				"relations", "hasSubtype", "005", "Automobiles", "A",
+				"Automobiles","instance001", "ACTIVE", true));
+	}
+
+	   
+}
