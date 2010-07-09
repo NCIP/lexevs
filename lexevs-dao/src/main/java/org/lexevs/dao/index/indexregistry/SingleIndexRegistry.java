7bb2853cefb579dfa062ba2152387fe41e479d21
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-09 22:29:55 +0000

    Updated Lucene registry.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6316 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/main/java/org/lexevs/dao/index/indexregistry/SingleIndexRegistry.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/index/indexregistry/SingleIndexRegistry.java
@@ -1,34 +1,151 @@
 package org.lexevs.dao.index.indexregistry;
 
-import org.lexevs.dao.index.lucenesupport.LuceneIndexTemplate;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class SingleIndexRegistry implements IndexRegistry {
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.apache.commons.lang.StringUtils;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.lexevs.dao.index.lucenesupport.BaseLuceneIndexTemplate;
+import org.lexevs.dao.index.lucenesupport.LuceneIndexTemplate;
+import org.lexevs.dao.index.lucenesupport.LuceneDirectoryFactory.NamedDirectory;
+import org.lexevs.system.constants.SystemVariables;
+import org.lexevs.system.model.LocalCodingScheme;
+import org.lexevs.system.service.SystemResourceService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.Assert;
+
+import edu.mayo.informatics.indexer.api.exceptions.InternalErrorException;
+import edu.mayo.informatics.indexer.utility.MetaData;
+
+public class SingleIndexRegistry implements IndexRegistry, InitializingBean {
 	
-	private static String DEFAULT_SINGLE_INDEX_NAME = "commonIndex";
+	public static String DEFAULT_SINGLE_INDEX_NAME = "commonIndex";
 	
-	public String indexName = DEFAULT_SINGLE_INDEX_NAME;
-	public LuceneIndexTemplate luceneIndexTemplate;
+	public String singleIndexName = DEFAULT_SINGLE_INDEX_NAME;
+	
+	private LuceneIndexTemplate luceneIndexTemplate;
+	
+	private SystemVariables systemVariables;
+	
+	private SystemResourceService systemResourceService;
+	
+	private MetaData metaData;
+	
+	private Map<String,LuceneIndexTemplate> luceneIndexNameToTemplateMap = new HashMap<String,LuceneIndexTemplate>();
+	
+	private Map<CodingSchemeUriVersionPair,String> luceneCodingSchemeToIndexNameMap = 
+		new HashMap<CodingSchemeUriVersionPair,String>();
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		luceneIndexNameToTemplateMap.put(singleIndexName, luceneIndexTemplate);
+	}
 
 	/* (non-Javadoc)
 	 * @see org.lexevs.dao.index.indexregistry.IndexRegistry#getIndexName(java.lang.String, java.lang.String)
 	 */
-	public String registerCodingSchemeIndex(String codingSchemeUri, String version) {
-		return indexName;
+	public void registerCodingSchemeIndex(String codingSchemeUri, String version, String indexName) {
+		if(!indexName.equals(singleIndexName)) {
+			LuceneIndexTemplate template = this.createLuceneIndexTemplate(indexName);
+			
+			luceneIndexNameToTemplateMap.put(indexName, template);
+		}
+		
+		luceneCodingSchemeToIndexNameMap.put(new CodingSchemeUriVersionPair(codingSchemeUri,version), indexName);
+	}
+	
+	protected Directory createIndexDirectory(String indexName) {
+		String baseIndexPath = systemVariables.getAutoLoadIndexLocation();
+		try {
+			return FSDirectory.getDirectory(baseIndexPath + File.separator + indexName);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	protected LuceneIndexTemplate createLuceneIndexTemplate(String indexName) {
+		Directory directory = this.createIndexDirectory(indexName);
+		
+		return new BaseLuceneIndexTemplate(new NamedDirectory(directory, indexName));
+	}
+	
+	@Override
+	public void unRegisterCodingSchemeIndex(String codingSchemeUri, String version) {
+		CodingSchemeUriVersionPair key = new CodingSchemeUriVersionPair(codingSchemeUri,version);
+		String indexName = 
+			this.luceneCodingSchemeToIndexNameMap.get(key);
+		
+		if(!StringUtils.isBlank(indexName)) {
+
+			if(! indexName.equals(singleIndexName)) {
+				luceneIndexNameToTemplateMap.remove(indexName);
+			}
+
+			luceneCodingSchemeToIndexNameMap.remove(key);
+
+		}
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.lexevs.dao.index.indexregistry.IndexRegistry#getLuceneIndexTemplate(java.lang.String, java.lang.String)
 	 */
 	public LuceneIndexTemplate getLuceneIndexTemplate(String codingSchemeUri, String version) {
-		return luceneIndexTemplate;
+		CodingSchemeUriVersionPair key = new CodingSchemeUriVersionPair(codingSchemeUri,version);
+		if(! this.luceneCodingSchemeToIndexNameMap.containsKey(key)) {
+			this.autoRegisterIndex(codingSchemeUri, version);
+		}
+		String indexName = luceneCodingSchemeToIndexNameMap.get(key);
+		
+		return this.luceneIndexNameToTemplateMap.get(indexName);
+	}
+	
+	protected void autoRegisterIndex(String codingSchemeUri, String version) {
+		String codingSchemeName;
+		try {
+			codingSchemeName = systemResourceService.getInternalCodingSchemeNameForUserCodingSchemeName(codingSchemeUri, version);
+		} catch (LBParameterException e) {
+			throw new RuntimeException(e);
+		}
+		
+		LocalCodingScheme lcs = LocalCodingScheme.getLocalCodingScheme(codingSchemeName, version);
+		try {
+			String indexName = metaData.getIndexMetaDataValue(lcs.getKey());
+			
+			if(StringUtils.isBlank(indexName)) {
+				throw new RuntimeException("Cannot autoregister index for CodingScheme: " + codingSchemeUri + " Version: " + version);
+			}
+			
+			Assert.state(! this.luceneIndexNameToTemplateMap.containsKey(indexName));
+			
+			this.registerCodingSchemeIndex(codingSchemeUri, version, indexName);
+			
+		} catch (InternalErrorException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	public String getIndexName() {
-		return indexName;
+	public MetaData getMetaData() {
+		return metaData;
 	}
 
-	public void setIndexName(String indexName) {
-		this.indexName = indexName;
+	public void setMetaData(MetaData metaData) {
+		this.metaData = metaData;
+	}
+
+	public String getCodingSchemeIndexName(String codingSchemeUri, String version) {
+		return this.getLuceneIndexTemplate(codingSchemeUri, version).getIndexName();
+	}
+
+	public String getSingleIndexName() {
+		return singleIndexName;
+	}
+
+	public void setSingleIndexName(String singleIndexName) {
+		this.singleIndexName = singleIndexName;
 	}
 
 	public LuceneIndexTemplate getLuceneIndexTemplate() {
@@ -37,5 +154,63 @@ public class SingleIndexRegistry implements IndexRegistry {
 
 	public void setLuceneIndexTemplate(LuceneIndexTemplate luceneIndexTemplate) {
 		this.luceneIndexTemplate = luceneIndexTemplate;
+	}
+	
+	public SystemVariables getSystemVariables() {
+		return systemVariables;
+	}
+
+	public void setSystemVariables(SystemVariables systemVariables) {
+		this.systemVariables = systemVariables;
+	}
+
+	public SystemResourceService getSystemResourceService() {
+		return systemResourceService;
+	}
+
+	public void setSystemResourceService(SystemResourceService systemResourceService) {
+		this.systemResourceService = systemResourceService;
+	}
+
+	private static class CodingSchemeUriVersionPair {
+		private String uri;
+		private String version;
+		
+		private CodingSchemeUriVersionPair(String uri, String version){
+			this.uri = uri;
+			this.version = version;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+			result = prime * result
+					+ ((version == null) ? 0 : version.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			CodingSchemeUriVersionPair other = (CodingSchemeUriVersionPair) obj;
+			if (uri == null) {
+				if (other.uri != null)
+					return false;
+			} else if (!uri.equals(other.uri))
+				return false;
+			if (version == null) {
+				if (other.version != null)
+					return false;
+			} else if (!version.equals(other.version))
+				return false;
+			return true;
+		}	
 	}
 }
