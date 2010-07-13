8ad42988d471ac7a715fc4a6147d3159e55aa152
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-13 13:34:22 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6343 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/LexBIG/mapping/LexEVSAssociationPredicateCreationTest.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/mapping/LexEVSAssociationPredicateCreationTest.java
@@ -5,12 +5,16 @@ import java.util.Date;
 import junit.framework.TestCase;
 
 import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
 import org.LexGrid.LexBIG.Exceptions.LBException;
 import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
 import org.LexGrid.LexBIG.Impl.LexEVSAuthoringServiceImpl;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.codingSchemes.CodingScheme;
 import org.LexGrid.commonTypes.EntityDescription;
 import org.LexGrid.commonTypes.Text;
+import org.LexGrid.relations.AssociationPredicate;
+import org.LexGrid.relations.Relations;
 import org.LexGrid.versions.EntryState;
 import org.LexGrid.versions.Revision;
 
@@ -25,6 +29,7 @@ public class LexEVSAssociationPredicateCreationTest extends TestCase {
 		public static String RELATIONS_CONTAINER = "relations";
 		public static String ASSOC_NAME_A = "BY";
 		public static String ASSOC_NAME_B = "NYE";
+		public static String NEW_RELATIONS_CONTAINER = "newRelation";
 		
 	   public void setUp(){
 
@@ -58,9 +63,64 @@ public class LexEVSAssociationPredicateCreationTest extends TestCase {
 		scheme.setCodingSchemeURN(TARGET_URN);
 		scheme.setCodingSchemeVersion(TARGET_VERSION);
 		authoring.createAssociationPredicate(revision, entryState, scheme, RELATIONS_CONTAINER, ASSOC_NAME_A);
+		CodingSchemeVersionOrTag csvt = new CodingSchemeVersionOrTag();
+		csvt.setVersion(TARGET_VERSION);
+		CodingScheme revisedScheme = lbs.resolveCodingScheme(TARGET_SCHEME, csvt);
+		Relations[] relations = revisedScheme.getRelations();
+		boolean predicateFound = false;
+		for(Relations rel: relations){
+			
+			AssociationPredicate[] predicates = rel.getAssociationPredicate();
+			for(AssociationPredicate predicate: predicates){
+				if(predicate.getAssociationName().equals(ASSOC_NAME_A)){
+					predicateFound = true;
+				}
+			}
+			
+		}
+		assertTrue(predicateFound);
 	}
 
-	public void testCreateNewAssociationPredicateAndRelations() {
-		
+	public void testCreateNewAssociationPredicateAndRelations() throws LBException {
+		Revision revision = new Revision();
+
+		revision.setChangeAgent("Mayo_Test_agent");
+		Text changeInstructions = new Text();
+		changeInstructions.setContent("Test instructions");
+		revision.setChangeInstructions(changeInstructions);
+
+		revision.setEditOrder(new Long(1));
+		EntityDescription entityDescription = new EntityDescription();
+		entityDescription.setContent("TestAssociationStatusRevision");
+		revision.setEntityDescription(entityDescription);
+
+		Date revisionDate = new Date();
+		revision.setRevisionDate(revisionDate);
+		EntryState entryState = new EntryState();
+		entryState.setContainingRevision("Mayo_predicate_revisionb");
+		revision.setRevisionId("Mayo_predicate_revisionb");
+		entryState.setPrevRevision("Should_not_be_set");
+		entryState.setRelativeOrder(new Long(0));
+
+		AbsoluteCodingSchemeVersionReference scheme = new AbsoluteCodingSchemeVersionReference();
+		scheme.setCodingSchemeURN(TARGET_URN);
+		scheme.setCodingSchemeVersion(TARGET_VERSION);
+		authoring.createAssociationPredicate(revision, entryState, scheme, NEW_RELATIONS_CONTAINER, ASSOC_NAME_B);
+		CodingSchemeVersionOrTag csvt = new CodingSchemeVersionOrTag();
+		csvt.setVersion(TARGET_VERSION);
+		CodingScheme revisedScheme = lbs.resolveCodingScheme(TARGET_SCHEME, csvt);
+		Relations[] relations = revisedScheme.getRelations();
+		boolean predicateFound = false;
+		for(Relations rel: relations){
+			
+			AssociationPredicate[] predicates = rel.getAssociationPredicate();
+			for(AssociationPredicate predicate: predicates){
+				if(predicate.getAssociationName().equals(ASSOC_NAME_B)){
+					predicateFound = true;
+				}
+			}
+			
+		}
+		assertTrue(predicateFound);
 	}
 }
