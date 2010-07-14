afa82e29217a277ab4c26d1a04b69c1c64a24a12
mturk <mturk@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-14 20:38:03 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6362 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/LexBIG/Impl/export/xml/lgxml/ExportAutomobiles2.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/export/xml/lgxml/ExportAutomobiles2.java
@@ -1,21 +1,9 @@
 package org.LexGrid.LexBIG.Impl.export.xml.lgxml;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
-import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeSummary;
-import org.LexGrid.LexBIG.DataModel.InterfaceElements.types.ProcessState;
 import org.LexGrid.LexBIG.Exceptions.LBException;
-import org.LexGrid.LexBIG.Extensions.Load.LexGrid_Loader;
 import org.LexGrid.LexBIG.Impl.export.xml.lgxml.util.CngFactory;
 import org.LexGrid.LexBIG.Impl.export.xml.lgxml.util.CnsFactory;
 import org.LexGrid.LexBIG.Impl.export.xml.lgxml.util.CodingSchemeChecker;
@@ -25,36 +13,18 @@ import org.LexGrid.LexBIG.Impl.export.xml.lgxml.util.ImportHelper;
 import org.LexGrid.LexBIG.Impl.export.xml.lgxml.util.Logger;
 import org.LexGrid.LexBIG.Impl.export.xml.lgxml.util.OutputDir;
 import org.LexGrid.LexBIG.Impl.export.xml.lgxml.util.TestCleaner;
-import org.LexGrid.LexBIG.Impl.exporters.LexGridExport;
-import org.LexGrid.LexBIG.Impl.loaders.LexGridMultiLoaderImpl;
-import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeGraph;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
-import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
-import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
-import org.LexGrid.LexBIG.Utility.Constructors;
-import org.LexGrid.LexBIG.Utility.ConvenienceMethods;
 
 public class ExportAutomobiles2 extends TestCase {
 
-    private final String INPUT_FILE_NAME_AUTO2 = "resources/testData/Automobiles2.xml";
+    private final String INPUT_FILE_NAME_AUTO2 = "resources/testData/lgXmlExport/Automobiles2.xml";
     
     private final String CS_AUTO2_URI = "urn:oid:11.11.0.1";
     private final String CS_AUTO2_VERSION = "1.1";
     
     private OutputDir outputDir;
     
-    private final String SEARCH_STRING_ENTITY_CODE_AUTOMOBILE = "entityCode=\"A0001\"";
-    private final String SEARCH_STRING_ENTITY_CODE_CAR = "entityCode=\"C0001\"";
-    private final String SEARCH_STRING_ENTITY_CODE_DOMESTIC_AUTO_MAKERS = "entityCode=\"005\"";
-    private final String SEARCH_STRING_ENTITY_CODE_FORD = "entityCode=\"Ford\"";
-    private final String SEARCH_STRING_ENTITY_CODE_TRUCK = "entityCode=\"T0001\"";
-    
-    private final String SEARCH_STRING_SRC_ENTITY_CODE_DOMESTIC_AUTO_MAKERS = "sourceEntityCode=\"005\"";
-    private final String SEARCH_STRING_TRG_ENTITY_CODE_FORD = "targetEntityCode=\"Ford\"";
-    private final String SEARCH_STRING_TRG_ENTITY_CODE_TIRES = "targetEntityCode=\"Tires\"";
-    
-
     private final String[] assoc1 = {"<lgRel:source",
                 "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"",
                 "xsi:schemaLocation=\"http://LexGrid.org/schema/2010/01/LexGrid/codingSchemes  http://LexGrid.org/schema/2010/01/LexGrid/codingSchemes.xsd\"",
@@ -137,75 +107,7 @@ public class ExportAutomobiles2 extends TestCase {
 		}
 		Logger.log("ExportAutomobiles2: testLexGridExportAutomibiles2: exit");
     }
-    
-    public void testVerifyEntity() {
-    	Logger.log("ExportAutomobiles2: testVerifyEntity: entry");
-    	boolean rv = false;
-		String fullyQualifiedOutputFile = ExportHelper.getExportedFileName(this.outputDir.getOutputDirAsString());
-		rv = ExportDataVerifier.verifyOutFileHasContent(fullyQualifiedOutputFile, this.entity1);
-		Assert.assertTrue("search string entity1 should exist in file", rv);
-		Logger.log("ExportAutomobiles2: testVerifyEntity: exit");
-    }
-    
-    public void testVerifyAssociation() {
-    	Logger.log("ExportAutomobiles2: testVerifyAssociation: entry");
-    	boolean rv = false;
-		String fullyQualifiedOutputFile = ExportHelper.getExportedFileName(this.outputDir.getOutputDirAsString());
-		rv = ExportDataVerifier.verifyOutFileHasContent(fullyQualifiedOutputFile, this.assoc1);
-		Assert.assertTrue("search string assoc1 should exist in file", rv);
-		Logger.log("ExportAutomobiles2: testVerifyAssociation: exit");
-    }
-    
-    public void testDoExport() {
-    	Logger.log("ExportAutomobiles2: testDoExport: entry");
-    	boolean rv = false;
-		try {
-			// create CNS and CNG objects
-			CodedNodeGraph cng = CngFactory.createCngExportAll(this.CS_AUTO2_URI, this.CS_AUTO2_VERSION);
-			CodedNodeSet cns = CnsFactory.createCnsExportAll(this.CS_AUTO2_URI, this.CS_AUTO2_VERSION);
-			
-			// export 
-			rv = ExportHelper.export(
-					this.CS_AUTO2_URI, 
-					this.CS_AUTO2_VERSION, 
-					this.outputDir.getOutputDirAsString(), 
-					true, 
-					cns, 
-					cng);
-			Assert.assertTrue("exported data to " + this.outputDir.getOutputDirAsString(), rv);
-			
-		} catch (LBException e) {
-			e.printStackTrace();
-			Assert.fail("ExportAutomobiles2: testDoExport: caught exception: " + e.getMessage());
-		}
-		Logger.log("ExportAutomobiles2: testDoExport: exit");
-    	
-    }
-    
-    public void testSetup() {
-    	Logger.log("ExportAutomobiles2: testSetup: entry");
-    	boolean rv = false;
-		this.init();
-		try {
-			
-			// check if coding scheme already exists
-			rv = CodingSchemeChecker.exists(this.CS_AUTO2_URI, this.CS_AUTO2_VERSION);
-			assertFalse("coding scheme " + this.CS_AUTO2_URI + "/" + this.CS_AUTO2_VERSION + " should not exist", rv);
-			
-			// import coding scheme
-			rv = ImportHelper.importLgXml(this.INPUT_FILE_NAME_AUTO2);
-			Assert.assertTrue("loaded LexGrid data from " + this.INPUT_FILE_NAME_AUTO2, rv);
-			
-			
-		} catch (LBException e) {
-			e.printStackTrace();
-			Assert.fail("ExportAutomobiles2: testSetup: caught exception: " + e.getMessage());
-		}
-		Logger.log("ExportAutomobiles2: testSetup: exit");
-    	
-    }
-    
-    
+        
     public void init() {
     	Logger.log("ExportAutomobiles2: init: entry");
     	
