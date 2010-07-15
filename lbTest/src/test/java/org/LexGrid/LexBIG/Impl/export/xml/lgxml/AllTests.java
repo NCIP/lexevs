b99677f156d8a912e8f31986004e588ceb223fd1
mturk <mturk@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-15 01:29:48 +0000

    wrapper for all tests
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6373 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/export/xml/lgxml/AllTests.java
@@ -0,0 +1,21 @@
+package org.LexGrid.LexBIG.Impl.export.xml.lgxml;
+
+import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		ServiceHolder.configureForSingleConfig();
+		TestSuite suite = new TestSuite("IcdCmsLoadersAllTests");
+		suite.addTestSuite(org.LexGrid.LexBIG.Impl.export.xml.lgxml.ExportAutomobiles2.class);
+		suite.addTestSuite(org.LexGrid.LexBIG.Impl.export.xml.lgxml.AssociationLoop.class);
+		suite.addTestSuite(org.LexGrid.LexBIG.Impl.export.xml.lgxml.MultipuleRelations.class);
+		suite.addTestSuite(org.LexGrid.LexBIG.Impl.export.xml.lgxml.EntitiesOnly.class);
+		suite.addTestSuite(org.LexGrid.LexBIG.Impl.export.xml.lgxml.AssociationsOnly.class);
+		suite.addTestSuite(org.LexGrid.LexBIG.Impl.export.xml.lgxml.RestrictToAssociation.class);
+		return suite;
+	}	
+}
