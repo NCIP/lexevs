49469f98e01f624247d6f1dd6bf6be4bd3192274
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-20 17:03:17 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6472 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lexevs-dao/src/test/java/org/lexevs/cache/MethodCachingInterceptorTest.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.lexevs.cache;
+
+import javax.annotation.Resource;
+
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.codingSchemes.CodingScheme;
+import org.junit.Before;
+import org.junit.Test;
+import org.lexevs.cache.annotation.Cacheable;
+import org.lexevs.dao.test.LexEvsDbUnitTestBase;
+import org.lexevs.dao.test.NonSpringManagedTestCacheBean;
+import org.lexevs.dao.test.TestCacheBean;
+import org.springframework.core.annotation.AnnotationUtils;
+
+/**
+ * The Class MethodCachingProxyTest.
+ * 
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
+public class MethodCachingInterceptorTest extends LexEvsDbUnitTestBase {
+
+	@Resource
+	private MethodCachingInterceptor testCacheProxy;
+	
+	/** The test cache bean. */
+	@Resource 
+	private TestCacheBean testCacheBean;
+	
+	@Resource
+	private CacheWrappingFactory cacheWrappingFactory;
+	
+	/**
+	 * Test cache setup.
+	 */
+	@Test
+	public void testCacheSetup(){
+		assertNotNull(this.testCacheProxy);
+		assertNotNull(this.testCacheBean);
+	}
+	
+	/**
+	 * Clear cache.
+	 */
+	@Before
+	public void clearCache(){
+		testCacheProxy.getCaches().clear();
+		testCacheBean = cacheWrappingFactory.wrapForCaching(new NonSpringManagedTestCacheBean());
+	}
+	
+	@Test
+	public void testComplexObject(){
+		AbsoluteCodingSchemeVersionReference ref = new AbsoluteCodingSchemeVersionReference();
+		ref.setCodingSchemeURN("uri");
+		ref.setCodingSchemeVersion("version");
+		
+		testCacheBean.getValueOfCompositeObject(ref);
+		
+		assertEquals(1, testCacheProxy.getCaches().get("testCache").size());
+		
+		AbsoluteCodingSchemeVersionReference ref2 = new AbsoluteCodingSchemeVersionReference();
+		ref2.setCodingSchemeURN("uri");
+		ref2.setCodingSchemeVersion("version");
+		
+		testCacheBean.getValueOfCompositeObject(ref2);
+		
+		assertEquals(1, testCacheProxy.getCaches().get("testCache").size());	
+	}
+	
+	/**
+	 * Test put in cache.
+	 */
+	@Test
+	public void testPutInCache(){
+	
+		assertEquals("onetwo", testCacheBean.getValue("one", "two"));
+		assertEquals(1, testCacheProxy.getCaches().get("testCache").size());
+		assertEquals("onetwo", testCacheProxy.getCaches().get("testCache").values().toArray()[0]);
+	}
+	
+	/**
+	 * Test put twice in cache.
+	 */
+	@Test
+	public void testPutTwiceInCache(){
+		
+		assertEquals("onetwo", testCacheBean.getValue("one", "two"));
+		assertEquals("onetwo", testCacheBean.getValue("one", "two"));
+		assertEquals(1, testCacheProxy.getCaches().get("testCache").size());
+		assertEquals("onetwo", testCacheProxy.getCaches().get("testCache").values().toArray()[0]);
+	}
+	
+	/**
+	 * Test two different in cache.
+	 */
+	@Test
+	public void testTwoDifferentInCache(){
+		
+		assertEquals("onetwo", testCacheBean.getValue("one", "two"));
+		assertEquals("threefour", testCacheBean.getValue("three", "four"));
+		assertEquals(2, testCacheProxy.getCaches().get("testCache").size());
+		assertEquals("onetwo", testCacheProxy.getCaches().get("testCache").values().toArray()[0]);
+		assertEquals("threefour", testCacheProxy.getCaches().get("testCache").values().toArray()[1]);
+	}
+	
+	/**
+	 * Test un cachable method.
+	 */
+	@Test
+	public void testUnCachableMethod(){
+		
+		assertEquals("onetwo", testCacheBean.getValueNotCachable("one", "two"));
+		
+		assertNull(testCacheProxy.getCaches().get("testCache"));
+	}
+	
+	/**
+	 * Test clear cache.
+	 */
+	@Test
+	public void testClearCache(){
+		
+		assertEquals("onetwo", testCacheBean.getValue("one", "two"));
+		assertEquals("threefour", testCacheBean.getValue("three", "four"));
+		assertEquals(2, testCacheProxy.getCaches().get("testCache").size());
+		assertEquals("onetwo", testCacheProxy.getCaches().get("testCache").values().toArray()[0]);
+		assertEquals("threefour", testCacheProxy.getCaches().get("testCache").values().toArray()[1]);
+		
+		testCacheBean.testClear();
+		assertEquals(0, testCacheProxy.getCaches().get("testCache").size());
+		
+	}
+	
+	@Test
+	public void testCloneResult(){
+		CodingScheme cs = testCacheBean.getClonedResult("uri", "version");
+		cs.setDefaultLanguage("test lang");
+		
+		CodingScheme cachedCs = testCacheBean.getClonedResult("uri", "version");
+		
+		assertNull(cachedCs.getDefaultLanguage());
+	}
+	
+	/**
+	 * The main method.
+	 * 
+	 * @param args the arguments
+	 */
+	public static void main(String[] args){
+		TestCacheBean bean = new TestCacheBean();
+		Class clazz = bean.getClass();
+		Cacheable annotation = AnnotationUtils.findAnnotation(bean.getClass(), Cacheable.class);
+		System.out.println(annotation);
+	}
+	
+
+}
