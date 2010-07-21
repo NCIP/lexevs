cabef488b1d0e7cb0a5c13cb6742d726b889892d
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-21 20:05:18 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6500 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/LexBIG/mapping/LexEVSMappingCreationHelperMethodTest.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/mapping/LexEVSMappingCreationHelperMethodTest.java
@@ -12,10 +12,13 @@ import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
 import org.LexGrid.LexBIG.Impl.LexEVSAuthoringServiceImpl;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.LexGrid.codingSchemes.CodingScheme;
+import org.LexGrid.commonTypes.Properties;
 import org.LexGrid.commonTypes.Text;
 import org.LexGrid.commonTypes.Versionable;
+import org.LexGrid.naming.Mappings;
 import org.LexGrid.relations.AssociationQualification;
 import org.LexGrid.relations.AssociationTarget;
+import org.LexGrid.relations.Relations;
 import org.LexGrid.versions.EntryState;
 
 import junit.framework.TestCase;
@@ -183,9 +186,70 @@ public class LexEVSMappingCreationHelperMethodTest extends TestCase {
 	
 	public void testCreateDefaultMappingsEntryState(){
 		
+		String revisionId = "StateRevision_7-21-2010";
+		String prevRevisionId = "Previous_state_Revision_7-20-2010";
+		EntryState entryState = authoring.createDefaultMappingsEntryState(
+				revisionId, prevRevisionId);
+		assertTrue(entryState.getContainingRevision() != null);
+		assertTrue(entryState.getContainingRevision().equals(revisionId));
+		assertTrue(entryState.getPrevRevision().equals(prevRevisionId));
+		assertTrue(entryState.getChangeType() == LexEVSAuthoringServiceImpl.CHANGE_TYPE);
+		assertTrue(entryState.getRelativeOrder() == LexEVSAuthoringServiceImpl.RELATIVE_ORDER);
+
+		EntryState entryState2 = authoring.createDefaultMappingsEntryState(
+				null, null);
+		assertNotNull(entryState2.getContainingRevision());
+		assertTrue(entryState2.getContainingRevision().equals(
+				LexEVSAuthoringServiceImpl.REVISIONID));
+	 
+	}
+	public void testCreateRelationsContainer() throws LBException{
+		EntryState entryState = authoring.createDefaultMappingsEntryState(null, null);
+		String containerName = "relations_test";
+		Date effectiveDate = new Date();
+		AbsoluteCodingSchemeVersionReference sourceCodeSystemIdentifier = new AbsoluteCodingSchemeVersionReference();
+		sourceCodeSystemIdentifier.setCodingSchemeURN(MappingTestConstants.SOURCE_URN);
+		sourceCodeSystemIdentifier.setCodingSchemeVersion(MappingTestConstants.SOURCE_VERSION);
+		AbsoluteCodingSchemeVersionReference targetCodeSystemIdentifier = new AbsoluteCodingSchemeVersionReference();
+		targetCodeSystemIdentifier.setCodingSchemeURN(MappingTestConstants.TARGET_URN);
+		targetCodeSystemIdentifier.setCodingSchemeVersion(MappingTestConstants.TARGET_VERSION);
+		boolean isMapping = true;
+		String associationType = "type_of";
+		Properties relationProperties = null;
+
+		CodingScheme codingScheme = authoring.populateCodingScheme(
+				"TestRelationsScheme", 
+				"http://test.relations.scheme", 
+				null, 
+				null, 
+				0L, 
+				"test.relations.1", 
+				null, 
+				null, 
+				null, 
+				new Mappings(), 
+				null, 
+				null, 
+				null);
+		Relations relation = authoring.createRelationsContainer(entryState, 
+				codingScheme, 
+				containerName, 
+				effectiveDate, 
+				sourceCodeSystemIdentifier, 
+				targetCodeSystemIdentifier, 
+				isMapping, 
+				associationType, 
+				relationProperties);
+		
+		assertTrue(relation.getSourceCodingScheme().equals(MappingTestConstants.SOURCE_SCHEME));
+		assertTrue(relation.getSourceCodingSchemeVersion().equals(MappingTestConstants.SOURCE_VERSION));
+		assertTrue(relation.getTargetCodingScheme().equals(MappingTestConstants.TARGET_SCHEME));
+		assertTrue(relation.getTargetCodingSchemeVersion().equals(MappingTestConstants.TARGET_VERSION));
+		assertTrue(relation.getAssociationPredicate(0).getAssociationName().equals(associationType));
 	}
 	
-	public void testCreateRelationsContainer(){
-		
-	}
+    public void testGetCodingSchemeNameForNamespace(){
+    	
+    }
+
 }
