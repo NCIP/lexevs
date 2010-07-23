42a19aa5dc3ab4338dd227e9f2ba2040e3cebc59
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-23 15:53:16 +0000

    Adjustements for the remote api -- use service-locator type lookups instead of class level dependencies to facilitate serialization.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6540 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/main/java/org/lexevs/cts2/BaseService.java
+++ cts2Impl/src/main/java/org/lexevs/cts2/BaseService.java
@@ -1,24 +1,10 @@
 package org.lexevs.cts2;
 
-import java.util.ArrayList;
-import java.util.List;
+public abstract class BaseService extends LexEvsBasedService {
 
-import org.LexGrid.LexBIG.DataModel.Collections.ExtensionDescriptionList;
-import org.LexGrid.LexBIG.DataModel.InterfaceElements.ExtensionDescription;
-import org.LexGrid.LexBIG.Exceptions.LBException;
-import org.LexGrid.LexBIG.Extensions.ExtensionRegistry;
-import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
-import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
-import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
-
-
-public abstract class BaseService {
-
-	private LexBIGServiceManager lbsm_ = null;
-    private LexBIGService lbs_ = null;
+    private transient LexEvsCTS2 lexEvsCTS2;
     private ServiceInfo serviceInfo_ = null;
-    protected static LexEvsCTS2 lexevsCTS2_ = LexEvsCTS2Impl.defaultInstance();
-	
+ 
 	public static enum SortableProperties {
         matchToQuery, code, codeSystem, entityDescription, conceptStatus, isActive
     };
@@ -33,97 +19,11 @@ public abstract class BaseService {
     	
     	return serviceInfo_;
     }
-    
-    public ExtensionDescriptionList getSupportedSearchAlgorithms() throws LBException{
-    	ExtensionRegistry extensionRegistry = getLexBIGServiceManager().getExtensionRegistry();
-    	if (extensionRegistry != null)
-    		return extensionRegistry.getGenericExtensions();
-    	
-    	return null;
-    }
-    
-    public List<String> getSupportedSearchAlgorithmNames() throws LBException{
-    	List<String> searchAlgNames = new ArrayList<String>();
-    	ExtensionRegistry extensionRegistry = getLexBIGServiceManager().getExtensionRegistry();
-    	if (extensionRegistry != null && extensionRegistry.getSearchExtensions() != null)
-    	{
-    		for (ExtensionDescription ed : extensionRegistry.getSearchExtensions().getExtensionDescription())
-    			searchAlgNames.add(ed.getName());
-    	}
-    	return searchAlgNames;
-    }
-    
-    public ExtensionDescriptionList getSupportedLoaders() throws LBException{
-    	ExtensionRegistry extensionRegistry = getLexBIGServiceManager().getExtensionRegistry();
-    	if (extensionRegistry != null)
-    		return extensionRegistry.getLoadExtensions();
-    	
-    	return null;
-    }
-    
-    public List<String> getSupportedLoaderNames() throws LBException{
-    	List<String> loaderNames = new ArrayList<String>();
-    	ExtensionRegistry extensionRegistry = getLexBIGServiceManager().getExtensionRegistry();
-    	if (extensionRegistry != null && extensionRegistry.getLoadExtensions() != null)
-    	{
-    		for (ExtensionDescription ed : extensionRegistry.getLoadExtensions().getExtensionDescription())
-    			loaderNames.add(ed.getName());
-    	}
-    	return loaderNames;
-    }
-    
-    public ExtensionDescriptionList getSupportedExporters() throws LBException{
-    	ExtensionRegistry extensionRegistry = getLexBIGServiceManager().getExtensionRegistry();
-    	if (extensionRegistry != null)
-    		return extensionRegistry.getExportExtensions();
-    	
-    	return null;
-    }
-    
-    public List<String> getSupportedExporterNames() throws LBException{
-    	List<String> exporterNames = new ArrayList<String>();
-    	ExtensionRegistry extensionRegistry = getLexBIGServiceManager().getExtensionRegistry();
-    	if (extensionRegistry != null && extensionRegistry.getExportExtensions() != null)
-    	{
-    		for (ExtensionDescription ed : extensionRegistry.getExportExtensions().getExtensionDescription())
-    			exporterNames.add(ed.getName());
-    	}
-    	return exporterNames;
-    }
-    
-    public ExtensionDescriptionList getSupportedFilters() throws LBException{
-    	ExtensionRegistry extensionRegistry = getLexBIGServiceManager().getExtensionRegistry();
-    	if (extensionRegistry != null)
-    		return extensionRegistry.getFilterExtensions();
-    	
-    	return null;
-    }
-    
-    public List<String> getSupportedFilterNames() throws LBException{
-    	List<String> filterNames = new ArrayList<String>();
-    	ExtensionRegistry extensionRegistry = getLexBIGServiceManager().getExtensionRegistry();
-    	if (extensionRegistry != null && extensionRegistry.getFilterExtensions() != null)
-    	{
-    		for (ExtensionDescription ed : extensionRegistry.getFilterExtensions().getExtensionDescription())
-    			filterNames.add(ed.getName());
-    	}
-    	return filterNames;
-    }
-    
-    public LexBIGServiceManager getLexBIGServiceManager() throws LBException{
-		if (lbsm_ == null)
-			lbsm_ = getLexBIGService().getServiceManager(null);
-		
-		return lbsm_;
+
+	protected LexEvsCTS2 getLexEvsCTS2() {
+		if(this.lexEvsCTS2 == null) {
+			this.lexEvsCTS2 = LexEvsCTS2Impl.defaultInstance();
+		}
+		return this.lexEvsCTS2;
 	}
-	
-    public LexBIGService getLexBIGService(){
-		if (lbs_ == null)
-			lbs_ = LexBIGServiceImpl.defaultInstance();
-		
-		return lbs_;
-	}   
-    
-    
-    
 }
