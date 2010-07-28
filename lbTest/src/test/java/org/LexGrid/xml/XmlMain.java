6fdc38d74bb1abb38496e2d3735eb283df9b9810
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-28 20:43:41 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6624 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/xml/XmlMain.java
+++ lbTest/src/test/java/org/LexGrid/xml/XmlMain.java
@@ -11,7 +11,7 @@ public class XmlMain {
 	 */
 	public static void main(String[] args) throws IOException {
 		SchemeXml sg = new SchemeXml();
-		sg.createXml(new File("big.xml")); // make sure this file does not exist
+		sg.createXml(new File("test_associations.xml")); // make sure this file does not exist
 		System.out.println("done");
 		
 	}
