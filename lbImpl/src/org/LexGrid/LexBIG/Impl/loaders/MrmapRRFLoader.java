da8072019aab2c2563e60d57788a44ff08748dfe
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-28 15:33:07 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6619 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/loaders/MrmapRRFLoader.java
@@ -0,0 +1,42 @@
+package org.LexGrid.LexBIG.Impl.loaders;
+
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.ExtensionDescription;
+import org.LexGrid.LexBIG.Extensions.Load.options.OptionHolder;
+
+import edu.mayo.informatics.lexgrid.convert.utility.URNVersionPair;
+
+public class MrmapRRFLoader extends BaseLoader {
+
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -689653003698478622L;
+
+    @Override
+    protected OptionHolder declareAllowedOptions(OptionHolder holder) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected URNVersionPair[] doLoad() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected ExtensionDescription buildExtensionDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
