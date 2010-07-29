7e29ff7baeb5a7b0864e68bf28922f6666c3ff6b
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-29 22:18:08 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6662 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/edu/mayo/informatics/lexgrid/convert/directConversions/mrmap/CreateMrMapSchemeTest.java
@@ -0,0 +1,9 @@
+package edu.mayo.informatics.lexgrid.convert.directConversions.mrmap;
+
+import junit.framework.TestCase;
+
+public class CreateMrMapSchemeTest extends TestCase {
+public void TestCreateMrMapScheme(){
+	
+}
+}
