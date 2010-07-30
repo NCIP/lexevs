3c2c17bf85040007b702da7dc33b0dea71072b90
mturk <mturk@c4b421de-a31f-834e-8902-8020e42e311c>
2010-07-30 22:20:33 +0000

    new part.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6691 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/export/xml/lgxml/util/XmlExporterTestOutputDir.java
@@ -0,0 +1,11 @@
+package org.LexGrid.LexBIG.Impl.export.xml.lgxml.util;
+
+import org.LexGrid.LexBIG.Impl.export.common.util.AbstractOutputDir;
+
+public class XmlExporterTestOutputDir extends AbstractOutputDir {
+
+	public XmlExporterTestOutputDir() {
+		super("lgxmlout");
+	}
+
+}
