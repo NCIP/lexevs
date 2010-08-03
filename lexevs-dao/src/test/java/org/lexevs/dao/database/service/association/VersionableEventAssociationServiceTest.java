3c49f2266f1e3569490745aa6fd7f9aa0b6166c7
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-03 22:11:35 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6730 c4b421de-a31f-834e-8902-8020e42e311c

--- lexevs-dao/src/test/java/org/lexevs/dao/database/service/association/VersionableEventAssociationServiceTest.java
+++ lexevs-dao/src/test/java/org/lexevs/dao/database/service/association/VersionableEventAssociationServiceTest.java
@@ -21,13 +21,15 @@ package org.lexevs.dao.database.service.association;
 import javax.annotation.Resource;
 
 import org.LexGrid.codingSchemes.CodingScheme;
+import org.LexGrid.commonTypes.Text;
+import org.LexGrid.relations.AssociationData;
 import org.LexGrid.relations.AssociationPredicate;
 import org.LexGrid.relations.AssociationSource;
 import org.LexGrid.relations.AssociationTarget;
 import org.LexGrid.relations.Relations;
 import org.junit.Before;
 import org.junit.Test;
-import org.lexevs.dao.database.service.codingscheme.CodingSchemeService;
+import org.lexevs.dao.database.service.association.AssociationService.AssociationTriple;
 import org.lexevs.dao.database.service.event.registry.ExtensionLoadingListenerRegistry;
 import org.lexevs.dao.database.service.relation.VersionableEventRelationService;
 import org.lexevs.dao.database.service.version.AuthoringService;
@@ -195,5 +197,103 @@ public class VersionableEventAssociationServiceTest extends LexEvsDbUnitTestBase
 	
 		JdbcTemplate template = new JdbcTemplate(dataSource);
 		assertEquals(1, template.queryForObject("Select count(*) from entityassnstoentity", Integer.class));
+	}
+	
+	@Test
+	@Transactional
+	public void testGetAssociationTripleTarget() throws Exception {
+	
+		CodingScheme scheme = new CodingScheme();
+		scheme.setApproxNumConcepts(111l);
+		scheme.setCodingSchemeName("testName");
+		scheme.setCodingSchemeURI("uri");
+		scheme.setRepresentsVersion("v1");
+
+		this.authoringService.loadRevision(scheme, null, null);
+		
+		Relations relations = new Relations();
+		relations.setContainerName("containerName");
+		
+		AssociationPredicate ap = new AssociationPredicate();
+		ap.setAssociationName("aName");
+		
+		relations.addAssociationPredicate(ap);
+		
+		versionableEventRelationService.insertRelation("uri", "v1", relations);
+
+		AssociationSource source = new AssociationSource();
+		source.setSourceEntityCode("source-code");
+		source.setSourceEntityCodeNamespace("source-ns");
+		
+		AssociationTarget target = new AssociationTarget();
+		target.setAssociationInstanceId("testAssocId");
+		target.setTargetEntityCode("target-code");
+		target.setTargetEntityCodeNamespace("target-ns");
+		
+		source.addTarget(target);
+		
+		versionableEventAssociationService.insertAssociationSource("uri", "v1", "containerName", "aName", source);
+	
+		AssociationTriple triple = 
+			versionableEventAssociationService.getAssociationTripleByAssociationInstanceId("uri", "v1", "testAssocId");
+		
+		assertEquals("containerName", triple.getRelationContainerName());
+		assertEquals("aName", triple.getAssociationPredicateName());
+		assertEquals("source-code", triple.getAssociationSource().getSourceEntityCode());
+		assertEquals("source-ns", triple.getAssociationSource().getSourceEntityCodeNamespace());
+		assertEquals(1,triple.getAssociationSource().getTargetCount());
+		assertEquals(0,triple.getAssociationSource().getTargetDataCount());
+		assertEquals("target-code", triple.getAssociationSource().getTarget(0).getTargetEntityCode());
+		assertEquals("target-ns", triple.getAssociationSource().getTarget(0).getTargetEntityCodeNamespace());
+		assertEquals("testAssocId", triple.getAssociationSource().getTarget(0).getAssociationInstanceId());
+	}
+	
+	@Test
+	@Transactional
+	public void testGetAssociationTripleData() throws Exception {
+	
+		CodingScheme scheme = new CodingScheme();
+		scheme.setApproxNumConcepts(111l);
+		scheme.setCodingSchemeName("testName");
+		scheme.setCodingSchemeURI("uri");
+		scheme.setRepresentsVersion("v1");
+
+		this.authoringService.loadRevision(scheme, null, null);
+		
+		Relations relations = new Relations();
+		relations.setContainerName("containerName");
+		
+		AssociationPredicate ap = new AssociationPredicate();
+		ap.setAssociationName("aName");
+		
+		relations.addAssociationPredicate(ap);
+		
+		versionableEventRelationService.insertRelation("uri", "v1", relations);
+
+		AssociationSource source = new AssociationSource();
+		source.setSourceEntityCode("source-code");
+		source.setSourceEntityCodeNamespace("source-ns");
+		
+		AssociationData data = new AssociationData();
+		data.setAssociationInstanceId("testAssocId");
+		Text text = new Text();
+		text.setContent("test data");
+		data.setAssociationDataText(text);
+		
+		source.addTargetData(data);
+		
+		versionableEventAssociationService.insertAssociationSource("uri", "v1", "containerName", "aName", source);
+	
+		AssociationTriple triple = 
+			versionableEventAssociationService.getAssociationTripleByAssociationInstanceId("uri", "v1", "testAssocId");
+		
+		assertEquals("containerName", triple.getRelationContainerName());
+		assertEquals("aName", triple.getAssociationPredicateName());
+		assertEquals("source-code", triple.getAssociationSource().getSourceEntityCode());
+		assertEquals("source-ns", triple.getAssociationSource().getSourceEntityCodeNamespace());
+		assertEquals(0,triple.getAssociationSource().getTargetCount());
+		assertEquals(1,triple.getAssociationSource().getTargetDataCount());
+		assertEquals("test data", triple.getAssociationSource().getTargetData(0).getAssociationDataText().getContent());
+		assertEquals("testAssocId", triple.getAssociationSource().getTargetData(0).getAssociationInstanceId());
 	}
 }
