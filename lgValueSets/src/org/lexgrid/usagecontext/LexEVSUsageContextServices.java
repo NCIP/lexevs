772387c435fc1a4df3fa5aacaf9836d2d22e07da
dwarkans <dwarkans@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-05 21:37:35 +0000

    Usage Context CTS 2 implementation.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6768 c4b421de-a31f-834e-8902-8020e42e311c

--- lgValueSets/src/org/lexgrid/usagecontext/LexEVSUsageContextServices.java
+++ lgValueSets/src/org/lexgrid/usagecontext/LexEVSUsageContextServices.java
@@ -3,12 +3,12 @@ package org.lexgrid.usagecontext;
 import java.io.Serializable;
 import java.util.List;
 
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeSummary;
 import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
 import org.LexGrid.LexBIG.Exceptions.LBException;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet.SearchDesignationOption;
 import org.LexGrid.codingSchemes.CodingScheme;
-import org.LexGrid.commonTypes.Properties;
 import org.LexGrid.concepts.Entity;
 
 /**
@@ -24,22 +24,37 @@ public interface LexEVSUsageContextServices extends Serializable {
 	 * @return the usage context coding scheme
 	 * @throws LBException
 	 */
-	public CodingScheme getUsageContextCodingScheme(CodingSchemeVersionOrTag versionOrTag) throws LBException;
+	public CodingScheme getUsageContextCodingScheme(String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException;
+	
+	/**
+	 * Gets the Usage Context coding scheme summary.
+	 * 
+	 * @param codingSchemeNameOrURI usage context coding scheme name or uri
+	 * @param versionOrTag usage context coding scheme version or tag
+	 * @return the usage context coding scheme summary
+	 * @throws LBException
+	 */
+	public CodingSchemeSummary getUsageContextCodingSchemeSummary(String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException;
+	
 	
 	/**
 	 * Returns UsageContext entity object with usageContextId.
 	 * 
 	 * @param usageContextId  id of usage context
+	 * @param namespace usage context namespace
+	 * @param codingSchemeNameOrURI usage context coding scheme name or uri
 	 * @param versionOrTag usageContext coding scheme version or tag
 	 * @return usage context entity object
 	 * @throws LBException
 	 */
-	public Entity getUsageContextEntity(String usageContextId, CodingSchemeVersionOrTag versionOrTag) throws LBException;
+	public Entity getUsageContextEntity(String usageContextId, String namespace, String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException;
 	
 	/**
 	 * Returns list of usage context entities matching the name provided.
 	 * @param usageContextName 
 	 * 			  match name of usage context
+	 * @param codingSchemeNameOrURI 
+	 * 			  usage context coding scheme name or uri
 	 * @param versionOrTag 
 	 * 			  usage context coding scheme version or tag
 	 * @param option 
@@ -54,83 +69,41 @@ public interface LexEVSUsageContextServices extends Serializable {
 	 * @return list of entities containing matching usage context name
 	 * @throws LBException
 	 */
-	public List<Entity> getUsageContextEntitisWithName(String usageContextName, CodingSchemeVersionOrTag versionOrTag, SearchDesignationOption option, String matchAlgorithm, String language) throws LBException;
+	public List<Entity> getUsageContextEntitisWithName(String usageContextName, String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag, SearchDesignationOption option, String matchAlgorithm, String language) throws LBException;
 	
 	/**
 	 * Returns coded node set for usage context entities.
 	 * 
+	 * @param codingSchemeNameOrURI 
+	 * 			  usage context coding scheme name or uri
 	 * @param versionOrTag 
 	 * 			  usage context coding scheme version or tag
 	 * @return codedNodeSet of usage context entities
 	 * @throws LBException
 	 */
-	public CodedNodeSet getUsageContextCodedNodeSet(CodingSchemeVersionOrTag versionOrTag) throws LBException;
+	public CodedNodeSet getUsageContextCodedNodeSet(String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException;
 	
 	/**
 	 * Gets all the usage context found in the system as entities.
 	 * 
+	 * @param codingSchemeNameOrURI 
+	 * 			  usage context coding scheme name or uri
 	 * @param versionOrTag 
 	 * 			  usage context coding scheme version or tag
 	 * @return List of usage context entities
 	 * @throws LBException
 	 */
-	public List<Entity> listAllUsageContextEntities(CodingSchemeVersionOrTag versionOrTag) throws LBException;
+	public List<Entity> listAllUsageContextEntities(String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException;
 	
 	/**
 	 * Returns all the usage context identifiers found in the system.
 	 * 
+	 * @param codingSchemeNameOrURI 
+	 * 			  usage context coding scheme name or uri
 	 * @param versionOrTag 
 	 * 			  usage context coding scheme version or tag
 	 * @return List of usage context identifiers
 	 * @throws LBException
 	 */
-	public List<String> listAllUsageContextIds(CodingSchemeVersionOrTag versionOrTag) throws LBException;
-	
-	/**
-	 * Inserts UsageContext into UsageContext coding scheme.
-	 * 
-	 * @param usageContextId - 
-	 * 				Id of UsageContext
-	 * @param usageContextName -
-	 * 				UsageContext name
-	 * @param revisionId - 
-	 * 				revision id of UsageContext
-	 * @param description - 
-	 * 				UsageContext description
-	 * @param status - 
-	 * 				UsageContext status
-	 * @param isActive -
-	 * 				is usage context active
-	 * @param properties -
-	 * 				UsageContext properties
-	 * @param versionOrTag -
-	 * 			  UsageContext coding scheme version or tag
-	 * 
-	 * @throws LBException
-	 */
-	public void insertUsageContext(String usageContextId, String usageContextName, 
-			String revisionId, String description, String status, boolean isActive, Properties properties, CodingSchemeVersionOrTag versionOrTag) throws LBException;
-	
-	/**
-	 * Inserts UsageContext entity into UsageContext coding scheme.
-	 * 
-	 * @param usageContextn - 
-	 * 				UsageContext entity object to be inserted
-	 * @param versionOrTag -
-	 * 			  UsageContext coding scheme version or tag
-	 * @throws LBException
-	 */
-	public void insertUsageContext(Entity usageContext, CodingSchemeVersionOrTag versionOrTag) throws LBException;
-	
-	/**
-	 * Remove UsageContext from the UsageContext coding scheme.
-	 * 
-	 * @param usageContextId - 
-	 * 				Identifier of the UsageContext that will be removed
-	 * @param versionOrTag -
-	 * 			  UsageContext coding scheme version or tag
-	 * @throws LBException
-	 */
-	public void removeUsageContext(String usageContextId, CodingSchemeVersionOrTag versionOrTag) throws LBException;
-	
+	public List<String> listAllUsageContextIds(String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException;	
 }
--- lgValueSets/src/org/lexgrid/usagecontext/impl/LexEVSUsageContextServicesImpl.java
+++ lgValueSets/src/org/lexgrid/usagecontext/impl/LexEVSUsageContextServicesImpl.java
@@ -6,7 +6,7 @@ package org.lexgrid.usagecontext.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeSummary;
 import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
 import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
 import org.LexGrid.LexBIG.Exceptions.LBException;
@@ -19,22 +19,12 @@ import org.LexGrid.LexBIG.Utility.Constructors;
 import org.LexGrid.LexBIG.Utility.Iterators.ResolvedConceptReferencesIterator;
 import org.LexGrid.LexBIG.Utility.LBConstants.MatchAlgorithms;
 import org.LexGrid.codingSchemes.CodingScheme;
-import org.LexGrid.commonTypes.EntityDescription;
-import org.LexGrid.commonTypes.Properties;
-import org.LexGrid.commonTypes.Text;
 import org.LexGrid.concepts.Entity;
-import org.LexGrid.concepts.Presentation;
-import org.LexGrid.util.sql.lgTables.SQLTableConstants;
-import org.LexGrid.versions.EntryState;
-import org.LexGrid.versions.types.ChangeType;
-import org.apache.commons.lang.StringUtils;
-import org.lexevs.dao.database.service.codingscheme.CodingSchemeService;
-import org.lexevs.dao.database.service.entity.EntityService;
-import org.lexevs.dao.index.service.entity.EntityIndexService;
+import org.lexevs.dao.database.service.DatabaseServiceManager;
 import org.lexevs.locator.LexEvsServiceLocator;
+import org.lexevs.system.service.SystemResourceService;
 import org.lexgrid.usagecontext.LexEVSUsageContextServices;
 import org.lexgrid.usagecontext.util.UsageContextConstants;
-import org.lexgrid.valuesets.helper.CodingSchemeBuilder;
 
 /**
  * Implementation of LexEVSUsageContextServices.
@@ -51,6 +41,7 @@ public class LexEVSUsageContextServicesImpl implements LexEVSUsageContextService
 	private transient LexBIGService lbsvc_;
 	
 	private static LexEVSUsageContextServices ucServ_;
+	private DatabaseServiceManager databaseServiceManager = LexEvsServiceLocator.getInstance().getDatabaseServiceManager();
 	
 	public static LexEVSUsageContextServices defaultInstance(){
 		if (ucServ_ == null)
@@ -59,48 +50,46 @@ public class LexEVSUsageContextServicesImpl implements LexEVSUsageContextService
 		return ucServ_;
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#getUsageContextCodedNodeSet(org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
+	/*
+	 * (non-Javadoc)
+	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#getUsageContextCodedNodeSet(java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
 	 */
 	@Override
 	public CodedNodeSet getUsageContextCodedNodeSet(
-			CodingSchemeVersionOrTag versionOrTag) throws LBException {
-		CodingSchemeVersionOrTag csVT = versionOrTag;
-		if (csVT == null || StringUtils.isEmpty(csVT.getVersion()))
-		{
-			csVT = Constructors.createCodingSchemeVersionOrTag(null, 
-					UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_VERSION);
-		}
-		return getLexBIGService().getNodeSet(UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI, 
-				csVT, Constructors.createLocalNameList(UsageContextConstants.USAGE_CONTEXT_ENTITY_TYPE));
+			String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException {
+		return getLexBIGService().getNodeSet(codingSchemeNameOrURI, versionOrTag, 
+				Constructors.createLocalNameList(UsageContextConstants.USAGE_CONTEXT_ENTITY_TYPE));
 	}
 
-	/* (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#getUsageContextCodingScheme(org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
+	/*
+	 * (non-Javadoc)
+	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#getUsageContextCodingScheme(java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
 	 */
 	@Override
-	public CodingScheme getUsageContextCodingScheme(
+	public CodingScheme getUsageContextCodingScheme(String codingSchemeNameOrURI, 
 			CodingSchemeVersionOrTag versionOrTag) throws LBException {
-		CodingSchemeVersionOrTag csVT = versionOrTag;
-		if (csVT == null || StringUtils.isEmpty(csVT.getVersion()))
-		{
-			csVT = Constructors.createCodingSchemeVersionOrTag(null, 
-					UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_VERSION);
-		}
-		return getLexBIGService().resolveCodingScheme(UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI, 
-				csVT);
+		return getLexBIGService().resolveCodingScheme(codingSchemeNameOrURI, versionOrTag);
+	}
+	
+	@Override
+	public CodingSchemeSummary getUsageContextCodingSchemeSummary(String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException {
+		String version = versionOrTag == null ? null : versionOrTag.getVersion();
+		
+		return databaseServiceManager.getCodingSchemeService().
+        	getCodingSchemeSummaryByUriAndVersion(getCodeSystemURI(codingSchemeNameOrURI), version);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#getUsageContextEntitisWithName(java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag, org.LexGrid.LexBIG.LexBIGService.CodedNodeSet.SearchDesignationOption, java.lang.String, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#getUsageContextEntitisWithName(java.lang.String, java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag, org.LexGrid.LexBIG.LexBIGService.CodedNodeSet.SearchDesignationOption, java.lang.String, java.lang.String)
 	 */
 	@Override
 	public List<Entity> getUsageContextEntitisWithName(String usageContextName,
-			CodingSchemeVersionOrTag versionOrTag,
+			String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag,
 			SearchDesignationOption option, String matchAlgorithm,
 			String language) throws LBException {
 		List<Entity> entityList = new ArrayList<Entity>();		
-		CodedNodeSet cns = getUsageContextCodedNodeSet(versionOrTag);
+		CodedNodeSet cns = getUsageContextCodedNodeSet(codingSchemeNameOrURI, versionOrTag);
 		
 		if (cns != null)
 		{
@@ -120,151 +109,37 @@ public class LexEVSUsageContextServicesImpl implements LexEVSUsageContextService
 		return entityList;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#getUsageContextEntity(java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
+	/*
+	 * (non-Javadoc)
+	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#getUsageContextEntity(java.lang.String, java.lang.String, java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
 	 */
 	@Override
-	public Entity getUsageContextEntity(String usageContextId,
+	public Entity getUsageContextEntity(String usageContextId, String namespace, String codingSchemeNameOrURI, 
 			CodingSchemeVersionOrTag versionOrTag) throws LBException {
-		CodingSchemeVersionOrTag csVT = versionOrTag;
-		if (csVT == null || StringUtils.isEmpty(csVT.getVersion()))
+		Entity cdEntity = null;
+		CodedNodeSet cns = this.getUsageContextCodedNodeSet(codingSchemeNameOrURI, versionOrTag);
+		if (cns != null)
 		{
-			csVT = Constructors.createCodingSchemeVersionOrTag(null, 
-					UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_VERSION);
+			cns.restrictToCodes(Constructors.createConceptReferenceList(usageContextId));
+			ResolvedConceptReferencesIterator rcrItr = cns.resolve(null, null, null, null, true);
+			if (rcrItr != null && rcrItr.hasNext())
+			{
+				ResolvedConceptReference rcr = rcrItr.next();
+				cdEntity = rcr.getEntity();
+			}
 		}
-		return getDatabaseEntityService().getEntity(UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI, 
-				csVT.getVersion(),
-				usageContextId, 
-				UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_FORMAL_NAME);
+		return cdEntity;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#insertUsageContext(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, boolean, org.LexGrid.commonTypes.Properties, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
+	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#listAllUsageContextEntities(java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
 	 */
 	@Override
-	public void insertUsageContext(String usageContextId,
-			String usageContextName, String revisionId, String description,
-			String status, boolean isActive, Properties properties,
-			CodingSchemeVersionOrTag versionOrTag) throws LBException {
-		if (StringUtils.isEmpty(usageContextName))
-			throw new LBException("Usage Context name can not be empty");
-		
-		if (StringUtils.isEmpty(usageContextId))
-			throw new LBException("Usage Context id can not be empty");
-		
-		// create an entity object for concept domain
-		Entity entity = new Entity();
-		entity.setEntityCode(usageContextId);
-		entity.setEntityCodeNamespace(UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_FORMAL_NAME);
-		EntityDescription ed = new EntityDescription();
-		ed.setContent(description);
-		entity.setEntityDescription(ed);
-		entity.setStatus(status);
-		entity.setIsActive(isActive);
-		entity.addEntityType(UsageContextConstants.USAGE_CONTEXT_ENTITY_TYPE);
-		
-		if (StringUtils.isNotEmpty(revisionId))
-		{
-			EntryState es = new EntryState();
-			es.setContainingRevision(revisionId);
-			es.setChangeType(ChangeType.NEW);
-			entity.setEntryState(es);
-		}
-		
-		if (StringUtils.isNotEmpty(usageContextName))
-		{
-			Presentation pres = new Presentation();
-			pres.setPropertyName(SQLTableConstants.TBLCOLVAL_TEXTUALPRESENTATION);
-			Text text = new Text();
-			text.setContent(usageContextName);
-			pres.setValue(text);
-			pres.setIsPreferred(true);
-			
-			entity.addPresentation(pres);
-		}
-		
-		if (properties != null)
-			entity.addAnyProperties(properties.getPropertyAsReference());
-		
-		insertUsageContext(entity, versionOrTag);
-
-	}
-
-	/* (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#insertUsageContext(org.LexGrid.concepts.Entity, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
-	 */
-	@Override
-	public void insertUsageContext(Entity usageContext,
-			CodingSchemeVersionOrTag versionOrTag) throws LBException {
-		if (usageContext == null)
-			return;
-		
-		if (!validateType(usageContext))
-		{
-			throw new LBException("Invalid entity type found in entity object. Only valid entity type for usage context entity is 'usageContext'");
-		}
-		
-		CodingSchemeVersionOrTag csVT = versionOrTag;
-		if (csVT == null || StringUtils.isEmpty(csVT.getVersion()))
-		{
-			csVT = Constructors.createCodingSchemeVersionOrTag(null, 
-					UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_VERSION);
-		}
-		
-		CodingScheme cs = null;
-		
-		try {
-				cs = getLexBIGService().resolveCodingScheme(UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI, 
-				csVT);
-		} catch (LBParameterException e){ // if usage context coding scheme does not exists in the system, create it.
-			if (e.getMessage().indexOf("No URI found") != -1)
-			{
-				CodingSchemeBuilder csBuilder = new CodingSchemeBuilder();
-				cs = csBuilder.build(UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI,
-						UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_FORMAL_NAME,
-					csVT.getVersion(),
-					UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_FORMAL_NAME,
-					null, 
-					null, 
-					null, 
-					null);
-				
-				// insert usage context coding scheme into system
-				getCodingSchemeService().insertCodingScheme(cs, null);
-				
-				AbsoluteCodingSchemeVersionReference acsvr = Constructors.createAbsoluteCodingSchemeVersionReference(
-						UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI, 
-						csVT.getVersion());
-				
-				// activate usage context coding scheme
-				getLexBIGService().getServiceManager(null).activateCodingSchemeVersion(acsvr);
-				
-				if (StringUtils.isNotEmpty(csVT.getTag()))
-					getLexBIGService().getServiceManager(null).setVersionTag(acsvr, csVT.getTag());
-				
-				// create empty Lucene entry for usage context coding scheme
-				getEntityIndexService().createIndex(acsvr);				
-			}
-			else
-			{
-				throw new LBException("Problem inserting usage context", e);
-			}
-		}
-		
-		// insert concept domain
-		getDatabaseEntityService().insertEntity(UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI, 
-				csVT.getVersion(), usageContext);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#listAllUsageContextEntities(org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
-	 */
-	@Override
-	public List<Entity> listAllUsageContextEntities(
+	public List<Entity> listAllUsageContextEntities(String codingSchemeNameOrURI, 
 			CodingSchemeVersionOrTag versionOrTag) throws LBException {
 		List<Entity> entityList = new ArrayList<Entity>();
-		CodedNodeSet cns = getUsageContextCodedNodeSet(versionOrTag);
+		CodedNodeSet cns = getUsageContextCodedNodeSet(codingSchemeNameOrURI, versionOrTag);
 		
 		if (cns != null)
 		{
@@ -278,14 +153,15 @@ public class LexEVSUsageContextServicesImpl implements LexEVSUsageContextService
 		return entityList;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#listAllUsageContextIds(org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
+	/*
+	 * (non-Javadoc)
+	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#listAllUsageContextIds(java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
 	 */
 	@Override
-	public List<String> listAllUsageContextIds(
+	public List<String> listAllUsageContextIds(String codingSchemeNameOrURI, 
 			CodingSchemeVersionOrTag versionOrTag) throws LBException {
 		List<String> idsList = new ArrayList<String>();
-		List<Entity> entityList = listAllUsageContextEntities(versionOrTag);
+		List<Entity> entityList = listAllUsageContextEntities(codingSchemeNameOrURI, versionOrTag);
 		for (Entity entity : entityList)
 		{
 			idsList.add(entity.getEntityCode());
@@ -293,52 +169,6 @@ public class LexEVSUsageContextServicesImpl implements LexEVSUsageContextService
 		return idsList;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.lexgrid.usagecontext.LexEVSUsageContextServices#removeUsageContext(java.lang.String, org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag)
-	 */
-	@Override
-	public void removeUsageContext(String usageContextId,
-			CodingSchemeVersionOrTag versionOrTag) throws LBException {
-		if (StringUtils.isEmpty(usageContextId))
-			throw new LBException("usage context id can not be empty");
-		
-		if (versionOrTag == null || StringUtils.isEmpty(versionOrTag.getVersion()))
-			throw new LBException("Version can not be empty");
-		
-		Entity entity = getUsageContextEntity(usageContextId, versionOrTag);
-		
-		if (entity != null)
-		{
-			// remove from database
-			getDatabaseEntityService().
-					removeEntity(
-							UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI,
-							versionOrTag.getVersion(),
-							entity);
-			// remove from lucene index
-			getEntityIndexService().deleteEntityFromIndex(UsageContextConstants.USAGE_CONTEXT_DEFAULT_CODING_SCHEME_URI,
-				versionOrTag.getVersion(),
-				entity);
-		}
-		else
-		{
-			throw new LBException("No usage context entity found with id : " + usageContextId);
-		}
-	}
-	
-	private boolean validateType(Entity usageContext){
-		if (usageContext.getEntityTypeCount() == 0)
-			return false;
-		
-		for (String type : usageContext.getEntityTypeAsReference())
-		{
-			if (type.equalsIgnoreCase(UsageContextConstants.USAGE_CONTEXT_ENTITY_TYPE))
-				return true;
-		}
-		
-		return false;
-	}
-	
 	private LexBIGService getLexBIGService(){
 		if (lbsvc_ == null)
 			lbsvc_ = LexBIGServiceImpl.defaultInstance();
@@ -346,15 +176,9 @@ public class LexEVSUsageContextServicesImpl implements LexEVSUsageContextService
 		return lbsvc_;
 	}
 	
-	private EntityService getDatabaseEntityService() {
-		return LexEvsServiceLocator.getInstance().getDatabaseServiceManager().getEntityService();
-	}
-	
-	private CodingSchemeService getCodingSchemeService() {
-		return LexEvsServiceLocator.getInstance().getDatabaseServiceManager().getCodingSchemeService();
-	}
-	
-	private EntityIndexService getEntityIndexService() {
-		return LexEvsServiceLocator.getInstance().getIndexServiceManager().getEntityIndexService();
+	private String getCodeSystemURI(String codeSystemNameOrUri) throws LBParameterException{
+		SystemResourceService systemResourceService = LexEvsServiceLocator.getInstance().getSystemResourceService();
+		
+		return systemResourceService.getUriForUserCodingSchemeName(codeSystemNameOrUri);
 	}
 }
