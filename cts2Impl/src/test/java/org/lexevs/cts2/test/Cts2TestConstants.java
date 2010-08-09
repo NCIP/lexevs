8d786e47d21ba231a01ef5d7c44fcb57653450a2
stanclc <stanclc@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-09 15:51:24 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6793 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/java/org/lexevs/cts2/test/Cts2TestConstants.java
+++ cts2Impl/src/test/java/org/lexevs/cts2/test/Cts2TestConstants.java
@@ -5,4 +5,7 @@ public class Cts2TestConstants {
 	public static String CTS2_AUTOMOBILES_URI = "urn:oid:11.11.0.5";
 	public static String CTS2_AUTOMOBILES_VERSION = "1.0";
 	public static String CTS2_AUTOMOBILES_NAME = "Cts2Automobiles";
+	
+    public static String CTS2_CREATE_URI = "urn:oid:11.11.0.99";
+    public static String CTS2_CREATE_VERSION = "1.0";
 }
