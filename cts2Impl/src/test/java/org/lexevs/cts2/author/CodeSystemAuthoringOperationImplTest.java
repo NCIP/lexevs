5f422aea1e9452979aea69c98bd43ff1b6ac151e
stanclc <stanclc@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-09 19:02:05 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6796 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/java/org/lexevs/cts2/author/CodeSystemAuthoringOperationImplTest.java
+++ cts2Impl/src/test/java/org/lexevs/cts2/author/CodeSystemAuthoringOperationImplTest.java
@@ -27,6 +27,7 @@ import org.LexGrid.concepts.Entity;
 import org.LexGrid.concepts.Presentation;
 import org.LexGrid.naming.Mappings;
 import org.LexGrid.naming.SupportedCodingScheme;
+import org.LexGrid.naming.SupportedLanguage;
 import org.junit.Test;
 import org.lexevs.cts2.LexEvsCTS2Impl;
 import org.lexevs.cts2.core.update.RevisionInfo;
@@ -123,37 +124,31 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 			revInfo.setEditOrder(1L);
 			revInfo.setRevisionDate(new Date());
 			revInfo.setRevisionId(randomID);
-			
-	//		String codingSchemeName = "New Coding Scheme - Updated";
-	//        String codingSchemeURI = "urn:oid:11.11.0.99";
-	//        String formalName = "CTS 2 API Created Code System";
-	//        String defaultLanguage = "";
-	//        Long approxNumConcepts = new Long(1);
-	//        String representsVersion = "1.0";
-	//        List<String> localNameList = Arrays.asList(""); 
-	//        
-	//        Source source = new Source();
-	//        source.setContent("source");
-	//        List<Source> sourceList = Arrays.asList(source);
-	//        
-	//        Text copyright = new Text();
-	//        Mappings mappings = new Mappings();
-	//        Properties properties = new Properties();
-	        
+				        
 	        
 	        String codingSchemeURI = Cts2TestConstants.CTS2_CREATE_URI;
 			String representsVersion = Cts2TestConstants.CTS2_CREATE_VERSION;
 	        
 			String codingSchemeName = "New Coding Scheme - Updated";
-	        String formalName = null;
-	        String defaultLanguage = null;
+	        String formalName = "CTS 2 API Created Code System - Updated";
+	        String defaultLanguage = "eng";
 	        Long approxNumConcepts = 0L;
-	        List<String> localNameList = null; 
-	        List<Source> sourceList = null;
-	        Text copyright = null;
-	        Mappings mappings = null;
 	        
+	        List<String> localNameList = Arrays.asList("local name"); 
 	        
+	    	Source source = new Source();
+	    	source.setContent("source");
+	    	List<Source> sourceList = Arrays.asList(source);
+	        
+	        Text copyright = new Text();
+	        copyright.setContent("CTS 2 API");
+	        
+	        Mappings mappings = new Mappings();
+	        SupportedLanguage csSupportedLanguage = new SupportedLanguage();
+	        csSupportedLanguage.setContent("eng");
+	        csSupportedLanguage.setLocalId("langLocalId");
+	        mappings.addSupportedLanguage(csSupportedLanguage);
+	        	        
 	        
 	        CodeSystemAuthoringOperation codeSystemAuthOp = LexEvsCTS2Impl.defaultInstance().getAuthoringOperation().getCodeSystemAuthoringOperation();
 	               
@@ -172,6 +167,13 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 	        
 	
 	        assertEquals("New Coding Scheme - Updated", codeScheme.getCodingSchemeName());
+	        assertEquals("CTS 2 API Created Code System - Updated", codeScheme.getFormalName());
+	        assertEquals("eng", codeScheme.getDefaultLanguage());
+	        assertEquals("CTS 2 API", codeScheme.getCopyright().getContent());
+	        assertEquals(1, codeScheme.getMappings().getSupportedLanguageCount());
+	        assertEquals(1, codeScheme.getLocalNameCount());
+	        assertEquals(1, codeScheme.getSourceCount());
+	        
 	        
 			
 		}
@@ -179,6 +181,7 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 	@Test
 		public void testUpdateCodeSystemMappings()   throws LBException, URISyntaxException{
 
+			String randomID = UUID.randomUUID().toString();
 			
 			RevisionInfo revInfo = new RevisionInfo();
 			revInfo.setChangeAgent("changeAgent");
@@ -186,17 +189,17 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 			revInfo.setDescription("new description");
 			revInfo.setEditOrder(1L);
 			revInfo.setRevisionDate(new Date());
-			revInfo.setRevisionId("R901");
+			revInfo.setRevisionId(randomID);
 			
 
 	        Mappings mappings = new Mappings();
 	        SupportedCodingScheme scScheme = new SupportedCodingScheme();
 	        scScheme.setLocalId("localId");
+	        scScheme.setContent("supported coding Scheme");
 	        mappings.addSupportedCodingScheme(scScheme);
-	        
-	        
-	        String codingSchemeURI = "urn:oid:11.11.0.99";
-	        String representsVersion = "1.0";
+	        	        
+	        String codingSchemeURI = Cts2TestConstants.CTS2_CREATE_URI;
+			String representsVersion = Cts2TestConstants.CTS2_CREATE_VERSION;
 	        
 			String codingSchemeName = null;
 	        String formalName = null;
@@ -206,7 +209,6 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 	        List<Source> sourceList = null;
 	        Text copyright = null;
 
-	        
 	        
 	        
 	        CodeSystemAuthoringOperation codeSystemAuthOp = LexEvsCTS2Impl.defaultInstance().getAuthoringOperation().getCodeSystemAuthoringOperation();
@@ -224,14 +226,15 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 	        															mappings 
 	        															);
 	        
-	
-				
-			System.out.println("Coding Scheme URI : " + codeScheme.getCodingSchemeURI());
+			
+			assertEquals(1, codeScheme.getMappings().getSupportedCodingSchemeCount());
 			
 		}
 
 	@Test
 	public void testAddCodeSystemProperty()   throws LBException, URISyntaxException{
+		
+		String randomID = UUID.randomUUID().toString();
 		
 		RevisionInfo revInfo = new RevisionInfo();
 		revInfo.setChangeAgent("changeAgent");
@@ -239,10 +242,10 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 		revInfo.setDescription("new description");
 		revInfo.setEditOrder(1L);
 		revInfo.setRevisionDate(new Date());
-		revInfo.setRevisionId("R502");
+		revInfo.setRevisionId(randomID);
 	    
-	    String codingSchemeURI = "urn:oid:11.11.0.99";
-	    String representsVersion = "1.0";
+	    String codingSchemeURI = Cts2TestConstants.CTS2_CREATE_URI;
+		String representsVersion = Cts2TestConstants.CTS2_CREATE_VERSION;
 	    
 		String codingSchemeName = null;
 	
@@ -253,11 +256,11 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 		prop.setPropertyId("propertyId1");
 		prop.setPropertyName("propertyName");
 		prop.setIsActive(false);
-		prop.setLanguage("english updated 206");
-		prop.setOwner("owner updated 206");
+		prop.setLanguage("english");
+		prop.setOwner("owner");
 		prop.setPropertyType(PropertyTypes.PROPERTY.name());
 		Text text = new Text();
-		text.setContent("content updated 206");
+		text.setContent("content");
 		text.setDataType("Text datatype");
 		prop.setValue(text);
 		prop.setDegreeOfFidelity("degreeOfFidelity");
@@ -270,12 +273,12 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 		prop2.setPropertyId("propertyId2");
 		prop2.setPropertyName("propertyName-2");
 		prop2.setIsActive(false);
-		prop2.setLanguage("english updated 206-2");
-		prop2.setOwner("owner updated 206-2");
+		prop2.setLanguage("english-2");
+		prop2.setOwner("owner-2");
 		prop2.setPropertyType(PropertyTypes.PROPERTY.name());
 		Text text2 = new Text();
-		text2.setContent("content updated 206-2");
-		text2.setDataType("Text datatype-2");
+		text2.setContent("content-2");
+		text2.setDataType("Text-2");
 		prop2.setValue(text);
 		prop2.setDegreeOfFidelity("degreeOfFidelity-2");
 		prop2.setMatchIfNoContext(true);
@@ -291,8 +294,8 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 	    															representsVersion, 
 	    															properties);
 	    
-			
-		System.out.println("Coding Scheme URI : " + codeScheme.getCodingSchemeURI());
+		
+		assertEquals(2, codeScheme.getProperties().getPropertyCount());
 		
 	}
 	
@@ -307,8 +310,9 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 		revInfo.setRevisionDate(new Date());
 		revInfo.setRevisionId("R503");
 	    
-	    String codingSchemeURI = "urn:oid:11.11.0.99";
-	    String representsVersion = "1.0";
+	    
+	    String codingSchemeURI = Cts2TestConstants.CTS2_CREATE_URI;
+		String representsVersion = Cts2TestConstants.CTS2_CREATE_VERSION;
 	    
 		String codingSchemeName = null;
 	
@@ -351,16 +355,19 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
 	@Test
 	public void testRemoveCodeSystemProperty()   throws LBException, URISyntaxException{
 		
+		String randomID = UUID.randomUUID().toString();
+		
 		RevisionInfo revInfo = new RevisionInfo();
 		revInfo.setChangeAgent("changeAgent");
 		revInfo.setChangeInstruction("changeInstruction");
 		revInfo.setDescription("new description");
 		revInfo.setEditOrder(1L);
 		revInfo.setRevisionDate(new Date());
-		revInfo.setRevisionId("R603");
+		revInfo.setRevisionId(randomID);
         
-        String codingSchemeURI = "urn:oid:11.11.0.99";
-        String representsVersion = "1.0";
+        
+        String codingSchemeURI = Cts2TestConstants.CTS2_CREATE_URI;
+		String representsVersion = Cts2TestConstants.CTS2_CREATE_VERSION;
         
         String propertyId = "propertyId1";
         
@@ -372,8 +379,8 @@ public class CodeSystemAuthoringOperationImplTest extends Cts2BaseTest {
         															representsVersion, 
         															propertyId);
         
-			
-		System.out.println("Coding Scheme URI : " + codeScheme.getCodingSchemeURI());
+		
+		assertEquals(1, codeScheme.getProperties().getPropertyCount());
 		
 	}
 
