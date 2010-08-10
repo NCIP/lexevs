03c031299e827e65179c83e7d742fd47b3c3ece6
dwarkans <dwarkans@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-10 19:14:59 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6844 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/java/org/lexevs/cts2/admin/load/ValueSetLoadOperationImplTest.java
+++ cts2Impl/src/test/java/org/lexevs/cts2/admin/load/ValueSetLoadOperationImplTest.java
@@ -3,7 +3,11 @@
  */
 package org.lexevs.cts2.admin.load;
 
+import static junit.framework.Assert.assertTrue;
+
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +20,10 @@ import org.LexGrid.valueSets.types.DefinitionOperator;
 import org.junit.Test;
 import org.lexevs.cts2.LexEvsCTS2;
 import org.lexevs.cts2.LexEvsCTS2Impl;
+import org.lexgrid.valuesets.LexEVSPickListDefinitionServices;
+import org.lexgrid.valuesets.LexEVSValueSetDefinitionServices;
+import org.lexgrid.valuesets.impl.LexEVSPickListDefinitionServicesImpl;
+import org.lexgrid.valuesets.impl.LexEVSValueSetDefinitionServicesImpl;
 
 import edu.mayo.informatics.lexgrid.convert.utility.URNVersionPair;
 
@@ -24,7 +32,9 @@ import edu.mayo.informatics.lexgrid.convert.utility.URNVersionPair;
  *
  */
 public class ValueSetLoadOperationImplTest {
-
+	private LexEVSValueSetDefinitionServices vds_;
+	private LexEVSPickListDefinitionServices pls_;
+	
 	/**
 	 * Test method for {@link org.lexevs.cts2.admin.load.ValueSetLoadOperationImpl#load(java.net.URI, java.net.URI, java.lang.String, java.lang.Boolean)}.
 	 * @throws LBException 
@@ -33,24 +43,40 @@ public class ValueSetLoadOperationImplTest {
 	public void testLoadURIURIStringBoolean() throws LBException {
 		LexEvsCTS2 cts2 = LexEvsCTS2Impl.defaultInstance();
 		ValueSetLoadOperation vsLoadOp = cts2.getAdminOperation().getValueSetLoadOperation();
+		
 		URNVersionPair[] urns = vsLoadOp.load(new File(
-				"src/test/resources/testData/VSDTestData.xml").toURI(), null, "LexGrid_Loader", true);
+						"src/test/resources/testData/valueSets/vdTestData.xml").toURI(), 
+						null, "LexGrid_Loader", true);
 		
-		System.out.println("vsd loads " + urns.length);
+		assertTrue("Number of VSD loaded : " + urns.length, urns.length == 18);	
+
+		urns = vsLoadOp.load(new File(
+						"src/test/resources/testData/valueSets/VSDOnlyTest.xml").toURI(), 
+						null, "LexGrid_Loader", true);
+
+		assertTrue("Number of VSD loaded : " + urns.length, urns.length == 1);
 		
-		for(URNVersionPair urn : urns)
-		{
-			System.out.println("vsd loaded : " + urn.getUrn() + " - " + urn.getVersion());
+		for (URNVersionPair urn : urns) {
+			assertTrue(urn.getUrn().equals("SRITEST:AUTO:PropertyRefTest1-VSDONLY"));
+			assertTrue(urn.getVersion().equals("R001"));
 		}
+
+		urns = vsLoadOp.load(new File(
+				"src/test/resources/testData/valueSets/pickListTestData.xml").toURI(), 
+				null, "LexGrid_Loader", true);
+		
+		assertTrue("Number of PLD loaded : " + urns.length, urns.length == 10);
 		
 		urns = vsLoadOp.load(new File(
-				"../lbTest/resources/testData/valueDomain/pickListTestData.xml").toURI(), null, "LexGrid_Loader", true);
-		for(URNVersionPair urn : urns)
-		{
-			System.out.println("vsd loaded : " + urn.getUrn() + " - " + urn.getVersion());
-		}
+				"src/test/resources/testData/valueSets/pickListOnlyTest.xml").toURI(), 
+				null, "LexGrid_Loader", true);
 		
-		System.out.println("pld loads " + urns.length);
+		assertTrue("Number of PLD loaded : " + urns.length, urns.length == 1);
+		
+		for (URNVersionPair urn : urns) {
+			assertTrue(urn.getUrn().equals("SRITEST:AUTO:DomesticAutoMakers-PickListOnly"));
+			assertTrue(urn.getVersion() == null);
+		}
 	}
 
 	/**
@@ -60,7 +86,7 @@ public class ValueSetLoadOperationImplTest {
 	@Test
 	public void testLoadValueSetDefinitionURIBoolean() throws LBException {
 		ValueSetDefinition vsd = new ValueSetDefinition();
-		vsd.setValueSetDefinitionURI("JUNIT:TEST:VSD1");
+		vsd.setValueSetDefinitionURI("SRITEST:JUNIT:TEST:VSD1");
 		vsd.setValueSetDefinitionName("JUnit test vsd 1");
 		vsd.setConceptDomain("cd");
 		vsd.addRepresentsRealmOrContext("context");
@@ -87,7 +113,62 @@ public class ValueSetLoadOperationImplTest {
 		LexEvsCTS2 cts2 = LexEvsCTS2Impl.defaultInstance();
 		ValueSetLoadOperation vsLoadOp = cts2.getAdminOperation().getValueSetLoadOperation();
 		String vsdURI = vsLoadOp.load(vsd, null, true);
-		System.out.println("vsdURI : " + vsdURI);
+		
+		assertTrue(vsdURI.equals("SRITEST:JUNIT:TEST:VSD1"));
 	}
-
+	
+	@Test
+	public void testRemoveAllTestValueDomains() throws LBException, URISyntaxException {
+		List<String> uris = getValueSetDefinitionService().listValueSetDefinitions(null);
+		assertTrue(uris.size() > 0);
+		
+		for (String uri : uris)
+		{
+			if (uri.startsWith("SRITEST:"))
+				getValueSetDefinitionService().removeValueSetDefinition(new URI(uri));
+		}
+		
+		// check if we missed any test valueDomains
+		uris = getValueSetDefinitionService().listValueSetDefinitions(null);
+		
+		for (String uri : uris)
+		{
+			if (uri.toString().startsWith("SRITEST:"))
+				assertTrue("Not all test value domains were deleted.",false);
+		}
+	}
+	
+	@Test
+	public void testRemoveAllTestPickLists() throws LBException {
+		List<String> pickListIds = getPickListService().listPickListIds();
+		for (String pickListId : pickListIds)
+		{
+			if (pickListId.startsWith("SRITEST"))
+				getPickListService().removePickList(pickListId);
+		}
+		
+		// check if we missed any test pick lists
+		pickListIds = getPickListService().listPickListIds();
+		for (String pickListId : pickListIds)
+		{
+			if (pickListId.startsWith("SRITEST"))
+				assertTrue("Not all test pick lists were deleted.", false);
+		}
+		
+		pickListIds.clear();
+	}
+	
+	private LexEVSValueSetDefinitionServices getValueSetDefinitionService(){
+		if (vds_ == null) {
+			vds_ = LexEVSValueSetDefinitionServicesImpl.defaultInstance();
+		}
+		return vds_;
+	}
+	
+	private LexEVSPickListDefinitionServices getPickListService(){
+		if (pls_ == null) {
+			pls_ = LexEVSPickListDefinitionServicesImpl.defaultInstance();
+		}
+		return pls_;
+	}
 }
