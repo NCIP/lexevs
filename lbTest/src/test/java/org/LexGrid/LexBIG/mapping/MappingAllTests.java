9810539c431e886534113a2bb523c547721be2dc
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-19 14:53:26 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6954 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/LexBIG/mapping/MappingAllTests.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/mapping/MappingAllTests.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
 package org.LexGrid.LexBIG.mapping;
 
 import junit.framework.Test;
@@ -9,8 +27,8 @@ public class MappingAllTests {
 		TestSuite suite = new TestSuite(
 				"Mapping Authoring Test");
 		//$JUnit-BEGIN$
-		suite.addTestSuite(LexEVSAssociationPredicateCreationTest.class);
 		suite.addTestSuite(LexEVSAssociationStatusUpdateTest.class);
+		suite.addTestSuite(LexEVSAssociationPredicateCreationTest.class);
 		suite.addTestSuite(LexEVSMappingCreationHelperMethodTest.class);
 		suite.addTestSuite(LexEVSMappingCreationTest.class);
 		suite.addTestSuite(LexEVSLoadedMappingsTest.class);
