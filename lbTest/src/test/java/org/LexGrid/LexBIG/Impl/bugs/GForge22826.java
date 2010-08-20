6b7451347fe1ecb6db4663e4eb555543602486e1
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-20 16:31:21 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6984 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/LexBIG/Impl/bugs/GForge22826.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/bugs/GForge22826.java
@@ -1,81 +1,29 @@
 package org.LexGrid.LexBIG.Impl.bugs;
 
 import java.io.File;
-import java.sql.Timestamp;
-import java.util.Date;
 
 import junit.framework.TestCase;
 
-import org.LexGrid.LexBIG.DataModel.Core.Association;
 import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
 import org.LexGrid.LexBIG.Exceptions.LBException;
-import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
-import org.LexGrid.LexBIG.Exceptions.LBParameterException;
 import org.LexGrid.LexBIG.Impl.loaders.LexGridMultiLoaderImpl;
 import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
-import org.LexGrid.codingSchemes.CodingScheme;
-import org.LexGrid.commonTypes.Source;
-import org.LexGrid.relations.Relations;
-import org.LexGrid.versions.EntryState;
-import org.LexGrid.versions.types.ChangeType;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.LexBIG.Utility.Iterators.ResolvedConceptReferencesIterator;
 
 public class GForge22826 extends TestCase {
 
     
     public void testAssociationEntryState() throws LBException {
-        CodingSchemeVersionOrTag tagOrVersion = new CodingSchemeVersionOrTag();
-        tagOrVersion.setVersion("1.0");
-        
-        try {
-            CodingScheme resolvedCodingScheme = ServiceHolder.instance().getLexBIGService().resolveCodingScheme("Automobiles", tagOrVersion);
-            Relations[] relations = resolvedCodingScheme.getRelations();
-           
-            /* TODO fix this test
-            for (int i = 0; i < relations.length; i++) {
-                Association[] associations = relations[i].getAssociation();
-                
-                for (int j = 0; j < associations.length; j++) {
-                    String associationName = associations[j].getEntityCode();
-                    if( "hasSubtype".equals(associationName) ) {
-                        Date effDate = associations[j].getEffectiveDate();
-                        Date expDate = associations[j].getExpirationDate();
-                        Source owner = associations[j].getOwner();
-                        String status = associations[j].getStatus();
-                        EntryState entryState = associations[j].getEntryState();
-                        
-                        assertNotNull(effDate);
-                        assertEquals(effDate.getTime(), Timestamp.valueOf(
-                                "2001-12-17 09:30:47").getTime());
-                        
-                        assertNotNull(expDate);
-                        assertEquals(expDate.getTime(), Timestamp.valueOf(
-                        "2001-12-19 09:30:47").getTime());
-                        
-                        assertNotNull(owner);
-                        assertEquals(owner.getContent(), "lexEVS");
-                        
-                        assertEquals(status, "Active");
-                        
-                        assertNotNull(entryState);
-                        assertEquals(entryState.getContainingRevision().trim(), "");
-                        assertTrue(entryState.getRelativeOrder() == 0L);
-                        assertTrue(entryState.getChangeType() == ChangeType.NEW);
-                        
-                        return;
-                    }
-                }
-                
-            }
-            */
-            fail();
-        } catch (LBInvocationException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (LBParameterException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+    	CodingSchemeVersionOrTag tagOrVersion = new CodingSchemeVersionOrTag();
+    	tagOrVersion.setVersion("1.0");
+
+    	ResolvedConceptReferencesIterator itr = 
+    		ServiceHolder.instance().getLexBIGService().
+    			getNodeSet("Automobiles", null, Constructors.createLocalNameList("association")).resolve(null, null, null);
+    	
+    	assert(itr.hasNext());
     }
     
     /**
