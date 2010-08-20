93741234bd59816d50ec3243175458513a8d548c
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-20 16:23:33 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@6983 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/LexGrid/LexBIG/Impl/bugs/GForge21923.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/bugs/GForge21923.java
@@ -45,14 +45,14 @@ public class GForge21923 extends LexBIGServiceTestCase {
        Method method = SQLImplementedMethods.class.getDeclaredMethod("mapToCodingSchemeName", new Class[]{SQLInterface.class, String.class, String.class});
        method.setAccessible(true);
        
-       SQLInterface sqlInterface = ResourceManager.instance().getSQLInterface(THES_SCHEME, THES_VERSION);
+       SQLInterface sqlInterface = ResourceManager.instance().getSQLInterface(AUTO_SCHEME, AUTO_VERSION);
        
        int maxConnectionsPerDb = LexEvsServiceLocator.getInstance().getSystemResourceService().getSystemVariables().getMaxConnectionsPerDB();
        
        //This used to leak one connection per call -- we'll call it 3x the defined maxConnectionsPerDb, just to make sure.
        //If this reaches the end, the JUnit will have passed. If it blocks (i.e., the pool is exhausted) this will never complete.
        for(int i=0;i< maxConnectionsPerDb * 3;i++){
-           method.invoke(new SQLImplementedMethods(), sqlInterface, THES_SCHEME, String.valueOf(i));
+           method.invoke(new SQLImplementedMethods(), sqlInterface, AUTO_SCHEME, String.valueOf(i));
        }
        
        assertTrue(true);
