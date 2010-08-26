cf372d7063e9c772375615c247255e7fd3fe59dd
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-08-26 10:11:30 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7055 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/codednodeset/LuceneOnlyToNodeListCodedNodeSet.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.LexBIG.Impl.codednodeset;
+
+import org.LexGrid.LexBIG.DataModel.Collections.ConceptReferenceList;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.Exceptions.LBResourceUnavailableException;
+import org.LexGrid.LexBIG.Utility.Constructors;
+
+public class LuceneOnlyToNodeListCodedNodeSet extends SingleLuceneIndexCodedNodeSet {
+    
+    private static final long serialVersionUID = -5959522938971242708L;
+    
+    public LuceneOnlyToNodeListCodedNodeSet() {
+        super();
+    }
+ 
+    public LuceneOnlyToNodeListCodedNodeSet(String uri, String version, ConceptReferenceList codeList) throws LBInvocationException, LBParameterException, LBResourceUnavailableException {
+        super(uri, Constructors.createCodingSchemeVersionOrTagFromVersion(version), null, null);
+        
+        this.restrictToCodes(codeList);
+    }
+}
