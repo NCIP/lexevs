32ee31c923e8b9d27564952a51a5ac1aa8890719
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-09-01 21:04:07 +0000

    Changed wording for NCI meta loader script.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7169 c4b421de-a31f-834e-8902-8020e42e311c

--- lbAdmin/scripts/LoadMetaBatch.bat
+++ lbAdmin/scripts/LoadMetaBatch.bat
@@ -1,13 +1,9 @@
 @echo off
-REM Loads NCI MetaThesaurus content, provided as a collection of RRF files in a
-REM single directory.  Files may comprise the entire UMLS distribution
-REM or pruned via the MetamorphoSys tool.  A complete list of
-REM source vocabularies is available online at
-REM http://www.nlm.nih.gov/research/umls/metaa1.html.
+REM Loads NCI Metathesaurus content, provided as a collection of RRF files in a
+REM single directory.
 REM
 REM Options:
 REM   -in,--input <uri> URI or path of the directory containing the NLM files
-REM   -s,--source vocabularies to load.
 REM
 REM Example: LoadMetaBatch -in "file:///path/to/directory/"
 REM
