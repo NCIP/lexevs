c8b0a8ef91f750f219b48d13a285b34d3e3b07f2
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-09-07 13:39:39 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7233 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/edu/mayo/informatics/lexgrid/convert/directConversions/mrmap/MrMapReaderTest.java
+++ lbTest/src/test/java/edu/mayo/informatics/lexgrid/convert/directConversions/mrmap/MrMapReaderTest.java
@@ -29,7 +29,7 @@ public class MrMapReaderTest extends TestCase {
 	public  void testReader() throws IOException{
     RRFLineReader reader = null;
         
-       reader = new RRFLineReader("C:\\Documents and Settings\\m029206\\My Documents\\mappings\\NCIt-to-ICD9mappings.MRMAP.RRF");
+       reader = new RRFLineReader("resources/testData/mrmap_mapping/NCIt-to-ICD9mappings.MRMAP.RRF");
        String[] list = reader.readRRFLine();
        assertTrue(list.length == 25);
        reader.close();
