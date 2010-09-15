c706a62abea3374dd1a88146d19899f8ebdc3e67
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-09-15 02:33:20 +0000

    Removed some unused annotations.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7331 c4b421de-a31f-834e-8902-8020e42e311c

--- lgModel/src/org/LexGrid/annotations/LgProxyField.java
+++
@@ -1,7 +0,0 @@
-package org.LexGrid.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface LgProxyField {}
