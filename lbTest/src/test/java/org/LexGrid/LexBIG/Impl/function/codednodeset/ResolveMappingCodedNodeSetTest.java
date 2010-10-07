bff089893af8c90137f2eb29f94f8cd0f699dc53
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-10-07 13:22:32 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7696 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/function/codednodeset/ResolveMappingCodedNodeSetTest.java
@@ -0,0 +1,77 @@
+package org.LexGrid.LexBIG.Impl.function.codednodeset;
+
+import org.LexGrid.LexBIG.DataModel.Collections.ResolvedConceptReferenceList;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Impl.testUtility.DataTestUtils;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet.SearchDesignationOption;
+import org.LexGrid.util.PrintUtility;
+
+public class ResolveMappingCodedNodeSetTest extends BaseCodedNodeSetTest {
+
+    @Override
+    protected String getTestID() {
+        return "ResolveMappingCodedNodeSet Tests";
+    }
+
+
+    public void testResolveMappingScheme() throws LBException{
+    	CodedNodeSet cns = 
+    		lbs.getNodeSet(MAPPING_SCHEME_URI, null, null);
+    	
+    	ResolvedConceptReferenceList list = cns.resolveToList(null, null, null, -1);
+    	
+    	assertEquals(9,list.getResolvedConceptReferenceCount());
+    	
+    	DataTestUtils.isConceptReferencePresent(list, "E0001");
+    	DataTestUtils.isConceptReferencePresent(list, "Jaguar");
+    	DataTestUtils.isConceptReferencePresent(list, "A0001");
+    	DataTestUtils.isConceptReferencePresent(list, "R0001");
+    	DataTestUtils.isConceptReferencePresent(list, "C0001");
+    	DataTestUtils.isConceptReferencePresent(list, "T0001");
+    	DataTestUtils.isConceptReferencePresent(list, "005");
+    	DataTestUtils.isConceptReferencePresent(list, "P0001");
+    	DataTestUtils.isConceptReferencePresent(list, "Ford");
+    }
+    
+    public void testSearchMapping() throws LBException{
+    	CodedNodeSet cns = 
+    		lbs.getNodeSet(MAPPING_SCHEME_URI, null, null);
+ 
+    	cns = cns.restrictToMatchingDesignations("Jaguar", SearchDesignationOption.ALL, "LuceneQuery", null);
+    	
+    	ResolvedConceptReferenceList list = cns.resolveToList(null, null, null, -1);
+    	
+    	assertEquals(1,list.getResolvedConceptReference().length);
+    	
+    	assertEquals("Jaguar",list.getResolvedConceptReference()[0].getCode());
+    }
+    
+    public void testSearchMappingBothCodingSchemes() throws LBException{
+    	CodedNodeSet cns = 
+    		lbs.getNodeSet(MAPPING_SCHEME_URI, null, null);
+ 
+    	cns = cns.restrictToMatchingDesignations("*r*", SearchDesignationOption.ALL, "LuceneQuery", null);
+    	
+    	ResolvedConceptReferenceList list = cns.resolveToList(null, null, null, -1);
+    	
+    	PrintUtility.print(list);
+    	
+    	assertEquals(6,list.getResolvedConceptReference().length);
+    	
+    	/*
+    	"C0001"
+    	"005"
+    	"Ford"
+    	"T0001"
+    	"Jaguar"
+    	"R0001"
+    	*/
+    	DataTestUtils.isConceptReferencePresent(list, "Jaguar");
+    	DataTestUtils.isConceptReferencePresent(list, "R0001");
+    	DataTestUtils.isConceptReferencePresent(list, "C0001");
+    	DataTestUtils.isConceptReferencePresent(list, "T0001");
+    	DataTestUtils.isConceptReferencePresent(list, "005");
+    	DataTestUtils.isConceptReferencePresent(list, "Ford");	
+    }
+}
