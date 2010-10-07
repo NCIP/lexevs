905aa1220ce1bb7fec8593d37c961bd49fb77146
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2010-10-07 17:02:17 +0000

    add jUnit test for gforge item 29841
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7721 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/bugs/GForge29841.java
@@ -0,0 +1,58 @@
+package org.LexGrid.LexBIG.Impl.bugs;
+
+
+import org.LexGrid.LexBIG.DataModel.Collections.AssociationList;
+import org.LexGrid.LexBIG.DataModel.Collections.ResolvedConceptReferenceList;
+import org.LexGrid.LexBIG.DataModel.Core.AssociatedConcept;
+import org.LexGrid.LexBIG.DataModel.Core.Association;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.Impl.function.LexBIGServiceTestCase;
+import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeGraph;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.commonTypes.Property;
+import org.LexGrid.concepts.Entity;
+
+public class GForge29841 extends LexBIGServiceTestCase {
+    final static String testID = "GForge29841";
+    
+    @Override
+    protected String getTestID() {
+        return testID;
+    }
+    
+    /**
+     * 
+     * 
+     * GForge #29841
+     * https://gforge.nci.nih.gov/tracker/index.php?func=detail&aid=29841&group_id=491&atid=1850
+     * 
+     * @throws Throwable
+     */
+    public void testDuplicateInfoWithAnonymousClass() throws Throwable {
+    	LexBIGService lbs = ServiceHolder.instance().getLexBIGService();
+    	CodingSchemeVersionOrTag csvt = Constructors.createCodingSchemeVersionOrTagFromVersion(PIZZA_SCHEME_VERSION);
+    	CodedNodeGraph cng = lbs.getNodeGraph(PIZZA_SCHEME_NAME, csvt, null);
+    	
+    	AssociatedConcept focus = new AssociatedConcept();
+        focus.setCode("VegetarianPizza");
+        focus.setCodeNamespace("pizza.owl");
+
+        int counter = 0; 
+        ResolvedConceptReferenceList list = cng.resolveAsList(focus, true, false, 1, -1, null, null, null, null, -1);
+        ResolvedConceptReference conRef = list.getResolvedConceptReference()[0];
+        AssociationList assnList = conRef.getSourceOf();
+        for (Association assn : assnList.getAssociation()) {
+        	for (AssociatedConcept con : assn.getAssociatedConcepts().getAssociatedConcept()) {
+        		counter++;
+        	}
+        }
+        // for VegetarianPizza, ensure it only has three associations
+        assertEquals(counter, 3);
+        
+    }
+    
+   
+}
