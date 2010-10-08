2905118de1f4007a7c30edc5632e0f50cc185dd5
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2010-10-08 06:42:28 +0000

    fixed gforge item 29842
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7741 c4b421de-a31f-834e-8902-8020e42e311c

--- lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/protegeOwl/ProtegeOwl2LG.java
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/protegeOwl/ProtegeOwl2LG.java
@@ -2622,7 +2622,7 @@ public class ProtegeOwl2LG {
     protected void resolveAssociationProperty(AssociationEntity assocEntity, RDFProperty rdfProp) {
         int i = 0;
         
-        // functional property, inverse functional property and object property is in rdf type collection 
+        // functional, inverse functional, transitive and object property is in rdf type collection 
         for (Iterator itr = rdfProp.getRDFTypes().iterator(); itr.hasNext();) {
             RDFSClass rdfsClass = (RDFSClass) itr.next();
             Property pro = CreateUtils.createComment(generatePropertyID(++i), "type", 
