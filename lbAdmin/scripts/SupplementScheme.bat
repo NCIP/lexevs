487073afba43d474b39ba9a14b7fa3775a35e9dd
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-10-14 19:04:27 +0000

    Supplement admin script.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7913 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbAdmin/scripts/SupplementScheme.bat
@@ -0,0 +1,20 @@
+@echo off
+REM Utility to register, unregister, and list Coding Scheme Supplements
+REM 
+REM
+REM  Usage: SupplementScheme  [-r] [-u] [-l]
+REM     [-parentUri parentUri]
+REM     [-parentVersion parentVersion]
+REM		[-supplementUri supplementUri] 
+REM     [-supplementVersion supplementVersion] 
+REM  -r                                    Register Coding Scheme as a Supplement
+REM  -u                                    Unregister Coding Scheme as a Supplement
+REM  -l,--list                             List Supplements
+REM  -parentUri <parentUri>                Parent URI.
+REM  -parentVersion <parentVersion>        Parent Version.
+REM  -supplementUri <supplementUri>        Supplement URI.
+REM  -supplementVersion <supplementVersion>Supplement Version.
+REM 
+REM  Example: SupplementScheme -u -parentUri "urn:oid:2.16.840.1.113883.3.26.1.1" -parentVersion "05.09e" -supplementUri "http://supplement.ontology.org" -supplementVersion "1.0.1" 
+REM 
+java -Xmx500m -cp "..\runtime\lbPatch.jar;..\runtime\lbRuntime.jar" org.LexGrid.LexBIG.admin.SupplementScheme %*
---
+++ lbAdmin/scripts/SupplementScheme.sh
@@ -0,0 +1,18 @@
+#  Utility to register, unregister, and list Coding Scheme Supplements
+#
+#  Usage: SupplementScheme  [-r] [-u] [-l]
+#     [-parentUri parentUri]
+#     [-parentVersion parentVersion]
+#		[-supplementUri supplementUri] 
+#     [-supplementVersion supplementVersion] 
+#  -r                                    Register Coding Scheme as a Supplement
+#  -u                                    Unregister Coding Scheme as a Supplement
+#  -l,--list                             List Supplements
+#  -parentUri <parentUri>                Parent URI.
+#  -parentVersion <parentVersion>        Parent Version.
+#  -supplementUri <supplementUri>        Supplement URI.
+#  -supplementVersion <supplementVersion>Supplement Version.
+# 
+#  Example: SupplementScheme -u -parentUri "urn:oid:2.16.840.1.113883.3.26.1.1" -parentVersion "05.09e" -supplementUri "http://supplement.ontology.org" -supplementVersion "1.0.1" 
+# 
+java -Xmx500m -cp "..\runtime\lbPatch.jar:..\runtime\lbRuntime.jar" org.LexGrid.LexBIG.admin.SupplementScheme $@
