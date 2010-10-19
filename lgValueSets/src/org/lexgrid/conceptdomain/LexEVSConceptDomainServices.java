c84b2eb0e78ae0f77b83e4467c81e281cbd90afd
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2010-10-19 12:41:21 +0000

    committing copyright changes
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@7989 c4b421de-a31f-834e-8902-8020e42e311c

--- lgValueSets/src/org/lexgrid/conceptdomain/LexEVSConceptDomainServices.java
+++ lgValueSets/src/org/lexgrid/conceptdomain/LexEVSConceptDomainServices.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
 package org.lexgrid.conceptdomain;
 
 import java.io.Serializable;
@@ -13,8 +31,8 @@ import org.LexGrid.codingSchemes.CodingScheme;
 import org.LexGrid.concepts.Entity;
 
 public interface LexEVSConceptDomainServices extends Serializable {
-	
-	/**
+
+/**
 	 * Gets the concept domain coding scheme.
 	 * 
 	 * @param codingSchemeNameOrURI concept domain coding scheme name or uri
@@ -22,7 +40,7 @@ public interface LexEVSConceptDomainServices extends Serializable {
 	 * @return the concept domain coding scheme
 	 * @throws LBException
 	 */
-	public CodingScheme getConceptDomainCodingScheme(String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException;
+public CodingScheme getConceptDomainCodingScheme(String codingSchemeNameOrURI, CodingSchemeVersionOrTag versionOrTag) throws LBException;
 	
 	/**
 	 * Gets the concept domain coding scheme summary.
--- lgValueSets/src/org/lexgrid/valuesets/helper/PLEntryNodeSortUtil.java
+++ lgValueSets/src/org/lexgrid/valuesets/helper/PLEntryNodeSortUtil.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
 package org.lexgrid.valuesets.helper;
 
 import java.util.Arrays;
@@ -11,11 +29,11 @@ public class PLEntryNodeSortUtil {
 	public static final int ASCENDING = 1;
 	public static final int DESCENDING = 2;
 	public static final int CUSTOM = 3;
-	
-	/**
+
+/**
 	 * comparator for sorting in descending order of the pick text.
 	 */
-	@SuppressWarnings("unchecked")
+@SuppressWarnings("unchecked")
 	static Comparator<ResolvedPickListEntry> SortAscending = new Comparator() {
 
 		public int compare(Object o1, Object o2) {
