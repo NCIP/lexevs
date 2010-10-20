25ae4ef663f14fe0657ece27c5dbfa8e14af5b85
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2010-10-20 06:44:00 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8021 c4b421de-a31f-834e-8902-8020e42e311c

--- lexrdf/src/org/lexgrid/exporter/owlrdf/LexRdf.java
+++ lexrdf/src/org/lexgrid/exporter/owlrdf/LexRdf.java
@@ -21,9 +21,6 @@ package org.lexgrid.exporter.owlrdf;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.vocabulary.OWL;
-import com.hp.hpl.jena.vocabulary.RDFS;
 
 public class LexRdf {
 
@@ -36,4 +33,5 @@ private static Model model = ModelFactory.createDefaultModel();
     public static Property matchIfNoContext = model.createProperty(LexRdfConstants.LEXRDF_MATCH_IF_NO_CONTEXT);
     public static Property representationalForm = model.createProperty(LexRdfConstants.LEXRDF_REPRESENTATIONAL_FORM);
     public static Property propertyLink	= model.createProperty(LexRdfConstants.LEXRDF_PROPERTY_LINK);
+    public static Property associationQualification	= model.createProperty(LexRdfConstants.LEXRDF_ASSOCIATION_QUALIFICATION);
 }
