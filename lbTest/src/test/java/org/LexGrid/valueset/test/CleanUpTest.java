39faf798706a8826b61d0243ed2a497e0237e8f4
dwarkans <dwarkans@c4b421de-a31f-834e-8902-8020e42e311c>
2010-11-02 20:00:20 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8164 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/valueset/test/CleanUpTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.valueset.test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
+import org.LexGrid.LexBIG.Utility.ConvenienceMethods;
+import org.LexGrid.valueSets.PickListDefinition;
+import org.junit.Test;
+import org.lexgrid.valuesets.LexEVSPickListDefinitionServices;
+import org.lexgrid.valuesets.LexEVSValueSetDefinitionServices;
+import org.lexgrid.valuesets.impl.LexEVSPickListDefinitionServicesImpl;
+import org.lexgrid.valuesets.impl.LexEVSValueSetDefinitionServicesImpl;
+
+/**
+ * This test removes the terminologies loaded by the JUnit tests.
+ * 
+ * @author <A HREF="mailto:dwarkanath.sridhar@mayo.edu">Sridhar Dwarkanath</A>
+ * @version subversion $Revision: $ checked in on $Date: $
+ */
+public class CleanUpTest extends TestCase {
+    
+	private LexEVSValueSetDefinitionServices vds_;
+	private LexEVSPickListDefinitionServices pls_;
+	
+	public void testRemoveAutombiles() throws LBException {
+        LexBIGServiceManager lbsm = LexBIGServiceImpl.defaultInstance().getServiceManager(null);
+
+        AbsoluteCodingSchemeVersionReference a = ConvenienceMethods.createAbsoluteCodingSchemeVersionReference(
+                "urn:oid:11.11.0.1", "1.0");
+
+        lbsm.deactivateCodingSchemeVersion(a, null);
+
+        lbsm.removeCodingSchemeVersion(a);
+    }
+	
+	public void testRemoveAutombilesV2() throws LBException {
+        LexBIGServiceManager lbsm = LexBIGServiceImpl.defaultInstance().getServiceManager(null);
+        
+        AbsoluteCodingSchemeVersionReference a = ConvenienceMethods.createAbsoluteCodingSchemeVersionReference(
+                "urn:oid:11.11.0.1", "1.1");
+
+        lbsm.deactivateCodingSchemeVersion(a, null);
+
+        lbsm.removeCodingSchemeVersion(a);
+    }
+	
+	public void testRemoveGermanMadeParts() throws LBException {
+        LexBIGServiceManager lbsm = LexBIGServiceImpl.defaultInstance().getServiceManager(null);
+
+        AbsoluteCodingSchemeVersionReference a = ConvenienceMethods.createAbsoluteCodingSchemeVersionReference(
+                "urn:oid:11.11.0.2", "2.0");
+
+        lbsm.deactivateCodingSchemeVersion(a, null);
+
+        lbsm.removeCodingSchemeVersion(a);
+    }
+
+    public void testRemoveObo() throws LBException {
+        LexBIGServiceManager lbsm = LexBIGServiceImpl.defaultInstance().getServiceManager(null);
+
+        AbsoluteCodingSchemeVersionReference a = ConvenienceMethods.createAbsoluteCodingSchemeVersionReference(
+                "urn:lsid:bioontology.org:fungal_anatomy", "UNASSIGNED");
+
+        lbsm.deactivateCodingSchemeVersion(a, null);
+        lbsm.removeCodingSchemeVersion(a);
+
+    }
+    
+    @Test
+	public void testRemoveAllTestValueSetDefs() throws LBException, URISyntaxException {
+		List<String> uris = getValueSetDefService().listValueSetDefinitions(null);
+		assertTrue(uris.size() > 0);
+		
+		for (String uri : uris)
+		{
+			if (uri.startsWith("SRITEST:"))
+				getValueSetDefService().removeValueSetDefinition(new URI(uri));
+		}
+		
+		// check if we missed any test valueDomains
+		uris = getValueSetDefService().listValueSetDefinitions(null);
+		
+		for (String uri : uris)
+		{
+			if (uri.toString().startsWith("SRITEST:"))
+				assertFalse("Not all test value domains were deleted.",true);
+		}
+	}
+	
+	/**
+	 * Test method for {@link org.lexgrid.valuedomain.impl.LexEVSPickListServicesImpl#removePickList(java.lang.String)}.
+	 * @throws LBException 
+	 * @throws RemoveException 
+	 */
+	@Test
+	public void testRemovePickList() {
+		try {
+			getPickListService().removePickList("SRITEST:AUTO:DomesticAutoMakers");
+		} catch (LBException e) {
+			assertFalse("problem removing pickList.", true);
+		}
+		
+		PickListDefinition pickList = null;
+		try {
+			pickList = getPickListService().getPickListDefinitionById("SRITEST:AUTO:DomesticAutoMakers");
+			assertTrue(pickList == null);
+		} catch (LBException e) {
+			if (e.getMessage().indexOf("Problem gettingPickLists") != -1)
+				assertTrue(pickList == null);
+		}
+		
+		
+	}
+	
+	/**
+	 * Test method for {@link org.lexgrid.valuedomain.impl.LexEVSPickListServicesImpl#removeAllPickLists()}.
+	 * @throws LBException 
+	 * @throws RemoveException 
+	 */
+	@Test
+	public void testRemoveAllTestPickLists() throws LBException {
+		List<String> pickListIds = getPickListService().listPickListIds();
+		for (String pickListId : pickListIds)
+		{
+			if (pickListId.startsWith("SRITEST"))
+				getPickListService().removePickList(pickListId);
+		}
+		
+		// check if we missed any test pick lists
+		pickListIds = getPickListService().listPickListIds();
+		for (String pickListId : pickListIds)
+		{
+			if (pickListId.startsWith("SRITEST"))
+				assertFalse("Not all test pick lists were deleted.", true);
+		}
+		
+		pickListIds.clear();
+	}
+	private LexEVSValueSetDefinitionServices getValueSetDefService(){
+		if (vds_ == null) {
+			vds_ = LexEVSValueSetDefinitionServicesImpl.defaultInstance();
+		}
+		return vds_;
+	}
+	
+	private LexEVSPickListDefinitionServices getPickListService(){
+		if (pls_ == null) {
+			pls_ = LexEVSPickListDefinitionServicesImpl.defaultInstance();
+		}
+		return pls_;
+	}
+}
---
+++ lbTest/src/test/java/org/LexGrid/valueset/test/LoadTestDataTest.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.valueset.test;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.types.ProcessState;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.Extensions.Load.OBO_Loader;
+import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
+import org.LexGrid.LexBIG.Impl.loaders.LexGridMultiLoaderImpl;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
+import org.LexGrid.LexBIG.Utility.LBConstants;
+import org.junit.Test;
+import org.lexgrid.valuesets.LexEVSPickListDefinitionServices;
+import org.lexgrid.valuesets.LexEVSValueSetDefinitionServices;
+import org.lexgrid.valuesets.impl.LexEVSPickListDefinitionServicesImpl;
+import org.lexgrid.valuesets.impl.LexEVSValueSetDefinitionServicesImpl;
+
+/**
+ * This set of tests loads the necessary data for the value set and pick list definition test.
+ * 
+ * @author <A HREF="mailto:dwarkanath.sridhar@mayo.edu">Sridhar Dwarkanath</A>
+ * @version subversion $Revision: $ checked in on $Date: $
+ */
+public class LoadTestDataTest extends TestCase {
+	private LexEVSValueSetDefinitionServices vds_;
+	private LexEVSPickListDefinitionServices pls_;
+	
+	public LoadTestDataTest(String serverName) {
+		super(serverName);
+	}
+
+	public void testLoadAutombilesV1() throws LBParameterException,
+			LBInvocationException, InterruptedException, LBException {
+	    loadXML("resources/testData/valueDomain/Automobiles.xml", "devel");
+	}
+	
+	public void testLoadAutombilesV2() throws LBParameterException,
+            LBInvocationException, InterruptedException, LBException {
+	     loadXML("resources/testData/valueDomain/AutomobilesV2.xml", LBConstants.KnownTags.PRODUCTION.toString());
+	}
+
+	public void testLoadGermanMadeParts() throws LBParameterException,
+            LBInvocationException, InterruptedException, LBException {
+        loadXML("resources/testData/German_Made_Parts.xml", LBConstants.KnownTags.PRODUCTION.toString());
+    }
+	
+	private void loadXML(String fileName, String tag)throws LBException, InterruptedException {
+        LexBIGServiceManager lbsm = LexBIGServiceImpl.defaultInstance().getServiceManager(null);
+
+        LexGridMultiLoaderImpl loader = (LexGridMultiLoaderImpl) lbsm
+                .getLoader("LexGrid_Loader");
+        
+        // load non-async - this should block
+        loader.load(new File(fileName).toURI(), true, false);
+        
+        while (loader.getStatus().getEndTime() == null) {
+            Thread.sleep(2000);
+        }
+        assertTrue(loader.getStatus().getEndTime() != null);
+        assertTrue(loader.getStatus().getState().equals(ProcessState.COMPLETED));
+        assertFalse(loader.getStatus().getErrorsLogged().booleanValue());
+        
+        lbsm.activateCodingSchemeVersion(loader.getCodingSchemeReferences()[0]);
+        
+        lbsm.setVersionTag(loader.getCodingSchemeReferences()[0], tag);
+	}
+
+	/**
+	 * @throws InterruptedException
+	 * @throws LBException
+	 */
+	public void testLoadObo() throws InterruptedException, LBException {
+		LexBIGServiceManager lbsm = LexBIGServiceImpl.defaultInstance().getServiceManager(null);
+
+		OBO_Loader loader = (OBO_Loader) lbsm.getLoader("OBOLoader");
+
+		loader.load(new File(
+				"resources/testData/fungal_anatomy.obo").toURI(),
+				null, true, true);
+
+		while (loader.getStatus().getEndTime() == null) {
+			Thread.sleep(2000);
+		}
+		assertTrue(loader.getStatus().getState().equals(ProcessState.COMPLETED));
+		assertFalse(loader.getStatus().getErrorsLogged().booleanValue());
+
+		lbsm.activateCodingSchemeVersion(loader.getCodingSchemeReferences()[0]);
+
+		lbsm.setVersionTag(loader.getCodingSchemeReferences()[0],
+				LBConstants.KnownTags.PRODUCTION.toString());
+	}
+	
+	/**
+	 * gForge #24967
+	 * Test to determine that ValueSet Services will not throw null pointer exception when there are
+	 * no Value Set Definition in the system. Also to remove if any test value set definitions are
+	 * present.
+	 *  
+	 * @throws LBException
+	 * @throws URISyntaxException
+	 */
+	@Test
+	public void testCheckValueSetDef() throws LBException, URISyntaxException{
+		List<String> uris = getValueSetDefService().listValueSetDefinitions(null);
+		
+		// check if we missed any test valueSefDefs
+		uris = getValueSetDefService().listValueSetDefinitions(null);
+		
+		for (String uri : uris)
+		{
+			if (uri.toString().startsWith("SRITEST:"))
+				assertFalse("Not all test value domains were deleted.",true);
+		}
+	}
+	
+	@Test
+	public void testLoadPickList() throws LBException {
+		getPickListService().loadPickList("resources/testData/valueDomain/pickListTestData.xml", true);
+	}
+	
+	@Test
+	public void testLoadValueSetDef() throws Exception {
+		getValueSetDefService().loadValueSetDefinition("resources/testData/valueDomain/vdTestData.xml", true);
+	}
+
+	private LexEVSValueSetDefinitionServices getValueSetDefService(){
+		if (vds_ == null) {
+			vds_ = LexEVSValueSetDefinitionServicesImpl.defaultInstance();
+		}
+		return vds_;
+	}
+	
+	private LexEVSPickListDefinitionServices getPickListService(){
+		if (pls_ == null) {
+			pls_ = LexEVSPickListDefinitionServicesImpl.defaultInstance();
+		}
+		return pls_;
+	}
+}
