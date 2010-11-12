987e0a31c641649b6b002cd81f99c8047aa4e5dd
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-11-12 11:34:55 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8231 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/function/codednodegraph/RestrictToAnonymousTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.LexBIG.Impl.function.codednodegraph;
+
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeGraph;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.util.PrintUtility;
+
+/**
+ * The Class RestrictToAnonymousTest.
+ * 
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
+public class RestrictToAnonymousTest extends BaseCodedNodeGraphTest {
+
+    public void testRestrictToNonAnonymousForMappingScheme() throws Exception {
+    	CodedNodeGraph cng = lbs.getNodeGraph(MAPPING_SCHEME_URI, null, null);
+    	
+    	cng = cng.restrictToAnonymous(false);
+
+        ResolvedConceptReference[] rcr = 
+            cng.resolveAsList(Constructors.createConceptReference("C0001", AUTO_SCHEME, AUTO_SCHEME), 
+                    true, 
+                    false, 
+                    0, 
+                    1, 
+                    null, 
+                    null, 
+                    null, 
+                    -1).getResolvedConceptReference();
+        
+        assertEquals(1, 
+                rcr.length);
+        
+        assertTrue(rcr[0].getSourceOf() != null);
+    }
+    
+    public void testRestrictToAnonymous() throws Exception {
+    	CodedNodeGraph cng = lbs.getNodeGraph(MAPPING_SCHEME_URI, null, null);
+    	
+    	cng = cng.restrictToAnonymous(true);
+
+        ResolvedConceptReference[] rcr = 
+            cng.resolveAsList(Constructors.createConceptReference("005", AUTO_SCHEME, AUTO_SCHEME), 
+                    false, 
+                    true, 
+                    0, 
+                    1, 
+                    null, 
+                    null, 
+                    null, 
+                    -1).getResolvedConceptReference();
+        
+        assertEquals(1, 
+                rcr.length);
+        
+        PrintUtility.print(rcr[0]);
+        
+        assertTrue(rcr[0].getTargetOf().getAssociation()[0].getAssociatedConcepts().getAssociatedConcept()[0].getCode().equals("@"));
+    }
+    
+    public void testRestrictToNonAnonymous() throws Exception {
+    	CodedNodeGraph cng = lbs.getNodeGraph(MAPPING_SCHEME_URI, null, null);
+    	
+    	cng = cng.restrictToAnonymous(false);
+
+        ResolvedConceptReference[] rcr = 
+            cng.resolveAsList(Constructors.createConceptReference("005", AUTO_SCHEME, AUTO_SCHEME), 
+                    false, 
+                    true, 
+                    0, 
+                    1, 
+                    null, 
+                    null, 
+                    null, 
+                    -1).getResolvedConceptReference();
+        
+        assertEquals(1, 
+                rcr.length);
+        
+        PrintUtility.print(rcr[0]);
+        
+        assertNull(rcr[0].getTargetOf());
+    }
+   
+}
