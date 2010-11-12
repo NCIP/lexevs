9911be617660ab80161f753aef9c2850411ce185
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-11-12 11:55:09 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8233 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/function/codednodegraph/RestrictToEntityTypesTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.LexBIG.Impl.function.codednodegraph;
+
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeGraph;
+import org.LexGrid.LexBIG.Utility.Constructors;
+
+/**
+ * The Class RestrictToAnonymousTest.
+ * 
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
+public class RestrictToEntityTypesTest extends BaseCodedNodeGraphTest {
+
+    public void testRestrictToAssociation() throws Exception {
+    	CodedNodeGraph cng = lbs.getNodeGraph(CAMERA_SCHEME_MANIFEST_URN, null, null);
+    	
+    	cng = cng.restrictToEntityTypes(Constructors.createLocalNameList("association"));
+
+        ResolvedConceptReference[] rcr = 
+            cng.resolveAsList(Constructors.createConceptReference("focal-length", null, null), 
+                    true, 
+                    false, 
+                    0, 
+                    1, 
+                    null, 
+                    null, 
+                    null, 
+                    -1).getResolvedConceptReference();
+        
+        assertEquals(1, 
+                rcr.length);
+        
+        assertEquals(2,rcr[0].getSourceOf().getAssociation().length);
+    }
+    
+    public void testRestrictToAssociationWithWrong() throws Exception {
+    	CodedNodeGraph cng = lbs.getNodeGraph(CAMERA_SCHEME_MANIFEST_URN, null, null);
+    	
+    	cng = cng.restrictToEntityTypes(Constructors.createLocalNameList("invalid"));
+
+        ResolvedConceptReference[] rcr = 
+            cng.resolveAsList(Constructors.createConceptReference("focal-length", null, null), 
+                    true, 
+                    false, 
+                    0, 
+                    1, 
+                    null, 
+                    null, 
+                    null, 
+                    -1).getResolvedConceptReference();
+        
+        assertEquals(1, 
+                rcr.length);
+        
+        assertNull(rcr[0].getSourceOf());
+    }
+}
