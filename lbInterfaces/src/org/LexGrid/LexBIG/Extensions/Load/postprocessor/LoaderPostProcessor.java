307def6fdfbd17a605e303713f879a83b47d1126
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-11-19 18:50:45 +0000

    Added Javadocs.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8300 c4b421de-a31f-834e-8902-8020e42e311c

--- lbInterfaces/src/org/LexGrid/LexBIG/Extensions/Load/postprocessor/LoaderPostProcessor.java
+++ lbInterfaces/src/org/LexGrid/LexBIG/Extensions/Load/postprocessor/LoaderPostProcessor.java
@@ -22,7 +22,26 @@ import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
 import org.LexGrid.LexBIG.Extensions.Generic.GenericExtension;
 import org.LexGrid.LexBIG.Extensions.Load.OntologyFormat;
 
+/**
+ * The Interface LoaderPostProcessor allows user defined process logic 
+ * to be injected into a Loader load process in the form of an Extension.
+ * 
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
 public interface LoaderPostProcessor extends GenericExtension {
 
+	/**
+	 * Executes a Loader post process. Loader post processes can be used to modify
+	 * database content, do extra cleanup, or send notifications, for example.
+	 * 
+	 * NOTE: Post Process error/exception conditions will not effect Loader status.
+	 * 
+	 * Implementors can assume that database content has been loaded at the point of
+	 * this call, but the load is not yet in a completed state and Lucene indexing
+	 * has not been done.
+	 * 
+	 * @param reference the uri/version of the requesting Coding Scheme
+	 * @param ontFormat the OntologyFormat of the requesting Loader
+	 */
 	public void runPostProcess(AbsoluteCodingSchemeVersionReference reference, OntologyFormat ontFormat);
 }
