8ec0bb3fe45a14fd20d9bbdeb6d8031132fa8cf1
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2010-12-16 15:15:38 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8397 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbAdmin/scripts/RebuildTransitivityTable.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Rebuilds transitivity table associated with the specified coding scheme
+REM
+REM Options:
+REM   -u,--urn <urn> URN uniquely identifying the code system.
+REM   -v,--version <id> Version identifier.
+REM   -f,--force Force clear (no confirmation).
+REM 
+REM Note: If the URN and version values are unspecified, a
+REM list of available coding schemes will be presented for
+REM user selection.
+REM
+REM Example: RebuildTransitivityTable -u "urn:oid:2.16.840.1.113883.3.26.1.1" -v "05.09e"
+REM
+java -Xmx1500m -XX:PermSize=256m -cp "..\runtime\lbPatch.jar;..\runtime\lbRuntime.jar" org.LexGrid.LexBIG.admin.RebuildTransitivityTable %*
