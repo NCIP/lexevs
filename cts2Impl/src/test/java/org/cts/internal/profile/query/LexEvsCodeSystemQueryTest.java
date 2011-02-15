8a88e56cb74392118dd0a627d75622b18eb891dc
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-02-15 15:47:16 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8604 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/java/org/cts/internal/profile/query/LexEvsCodeSystemQueryTest.java
+++ cts2Impl/src/test/java/org/cts/internal/profile/query/LexEvsCodeSystemQueryTest.java
@@ -5,11 +5,18 @@ import static org.junit.Assert.assertTrue;
 
 import javax.annotation.Resource;
 
+import org.LexGrid.LexBIG.DataModel.Collections.CodingSchemeRenderingList;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeSummary;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.CodingSchemeRendering;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.cts.test.BaseCts2Test;
 import org.cts2.codesystem.CodeSystemDirectory;
 import org.cts2.internal.profile.query.LexEvsCodeSystemQuery;
+import org.cts2.internal.uri.factory.CodeSystemDirectoryURIFactory;
 import org.cts2.profile.BaseService;
 import org.cts2.uri.DirectoryURI;
+import org.easymock.classextension.EasyMock;
 import org.junit.Test;
 
 public class LexEvsCodeSystemQueryTest extends BaseCts2Test {
@@ -19,6 +26,9 @@ public class LexEvsCodeSystemQueryTest extends BaseCts2Test {
 	
 	@Resource
 	private BaseService baseService;
+	
+	@Resource
+	private CodeSystemDirectoryURIFactory codeSystemDirectoryURIFactory;
 	
 	@Test
 	public void testInit(){
@@ -36,7 +46,21 @@ public class LexEvsCodeSystemQueryTest extends BaseCts2Test {
 	}
 	
 	@Test
-	public void testResolveDirectoryURINotNull(){
+	public void testResolveDirectoryURINotNull() throws LBInvocationException{
+		LexBIGService lbs = EasyMock.createMock(LexBIGService.class);
+		
+		CodingSchemeRenderingList csrl = new CodingSchemeRenderingList();
+		CodingSchemeRendering csr = new CodingSchemeRendering();
+		csr.setCodingSchemeSummary(new CodingSchemeSummary());
+		csr.getCodingSchemeSummary().setCodingSchemeURI("testURI");
+		csrl.addCodingSchemeRendering(csr);
+		
+		EasyMock.expect(lbs.getSupportedCodingSchemes()).andReturn(csrl).anyTimes();
+		EasyMock.replay(lbs);
+		
+		lexEvsCodeSystemQuery.setLexBigService(lbs);
+		codeSystemDirectoryURIFactory.setLexBigService(lbs);
+		
 		DirectoryURI<CodeSystemDirectory> directoryUri = lexEvsCodeSystemQuery.getAllCodeSystems();
 		CodeSystemDirectory csd = directoryUri.resolve(null, null);
 		assertNotNull(csd);
--- cts2Impl/src/test/java/org/cts/test/BaseCts2Test.java
+++ cts2Impl/src/test/java/org/cts/test/BaseCts2Test.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)  
 @ContextConfiguration( 
-		value = "/cts2-application-config.xml")
+		value = "/cts2-application-config-test.xml")
 public class BaseCts2Test {
 
 }
