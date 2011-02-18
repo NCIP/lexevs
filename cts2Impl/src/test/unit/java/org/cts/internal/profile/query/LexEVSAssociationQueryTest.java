6b6f57b3d046a6cc7485c4a26df98d6be76e2234
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2011-02-18 22:50:02 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8671 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ cts2Impl/src/test/unit/java/org/cts/internal/profile/query/LexEVSAssociationQueryTest.java
@@ -0,0 +1,67 @@
+package org.cts.internal.profile.query;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.annotation.Resource;
+
+import org.LexGrid.LexBIG.DataModel.Collections.CodingSchemeRenderingList;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeSummary;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.CodingSchemeRendering;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.cts.test.BaseCts2UnitTest;
+import org.cts2.association.AssociationDirectory;
+import org.cts2.codesystem.CodeSystemDirectory;
+import org.cts2.internal.profile.query.LexEvsAssociationQuery;
+import org.cts2.internal.uri.factory.CodeSystemDirectoryURIFactory;
+import org.cts2.profile.BaseService;
+import org.cts2.profile.query.AssociationQuery;
+import org.cts2.uri.DirectoryURI;
+import org.easymock.classextension.EasyMock;
+import org.junit.Test;
+
+public class LexEVSAssociationQueryTest extends BaseCts2UnitTest {
+	
+	@Resource
+	private LexEvsAssociationQuery associationQuery;
+	
+	@Resource
+	private BaseService baseService;
+	
+	@Resource
+	private CodeSystemDirectoryURIFactory codeSystemDirectoryURIFactory;
+	
+	@Test
+	public void testInit(){
+		assertNotNull(associationQuery);
+	}
+	
+	@Test
+	public void testIsAvailableThroughBaseService(){
+		assertNotNull(baseService.getQueryService().getCodeSystemQuery());
+	}
+	
+	@Test
+	public void testResolveDirectoryURINotNull() throws LBInvocationException{
+		LexBIGService lbs = EasyMock.createMock(LexBIGService.class);
+		
+		CodingSchemeRenderingList csrl = new CodingSchemeRenderingList();
+		CodingSchemeRendering csr = new CodingSchemeRendering();
+		csr.setCodingSchemeSummary(new CodingSchemeSummary());
+		csr.getCodingSchemeSummary().setCodingSchemeURI("testURI");
+		csrl.addCodingSchemeRendering(csr);
+		
+		EasyMock.expect(lbs.getSupportedCodingSchemes()).andReturn(csrl).anyTimes();
+		EasyMock.replay(lbs);
+		
+		associationQuery.setLexBigService(lbs);
+		codeSystemDirectoryURIFactory.setLexBigService(lbs);
+		
+		DirectoryURI<AssociationDirectory> directoryUri = associationQuery.getAllAssociations();
+		AssociationDirectory ad = directoryUri.resolve(null, null);
+		assertNotNull(ad);
+		assertTrue(ad.getEntryCount() > 0);
+	}
+
+}
