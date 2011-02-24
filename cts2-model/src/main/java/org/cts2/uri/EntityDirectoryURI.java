f967cd059fb71f74f6065d2108848c9995eea6e9
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-02-24 17:48:45 +0000

    Added more directory uris.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8697 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ cts2-model/src/main/java/org/cts2/uri/EntityDirectoryURI.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright: (c) 2004-2011 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.cts2.uri;
+
+import org.cts2.directory.Resolvable;
+import org.cts2.directory.Restrictable;
+import org.cts2.entity.EntityDirectory;
+import org.cts2.entity.EntityList;
+
+/**
+ * The Interface CodeSystemVersionDirectoryURI.
+ *
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
+public interface EntityDirectoryURI 
+	extends DirectoryURI, Restrictable<EntityDirectoryURI>, 
+		Resolvable<EntityDirectory,EntityList>{
+	
+}
