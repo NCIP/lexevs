4d609792f1c8196e6332dc9a24d354a706c005df
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-01 12:43:25 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8732 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/integration/java/org/cts2/internal/profile/query/LexEvsEntityDescriptionQueryServiceTestIT.java
+++ cts2Impl/src/test/integration/java/org/cts2/internal/profile/query/LexEvsEntityDescriptionQueryServiceTestIT.java
@@ -1,11 +1,15 @@
 package org.cts2.internal.profile.query;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import javax.annotation.Resource;
 
 import org.LexGrid.LexBIG.test.LexEvsTestRunner.LoadContent;
+import org.cts2.entity.EntityDirectory;
 import org.cts2.test.BaseCts2IntegrationTest;
+import org.cts2.uri.DirectoryURI;
+import org.cts2.uri.EntityDirectoryURI;
 import org.junit.Test;
 
 public class LexEvsEntityDescriptionQueryServiceTestIT extends BaseCts2IntegrationTest {
@@ -22,6 +26,39 @@ public class LexEvsEntityDescriptionQueryServiceTestIT extends BaseCts2Integrati
 	@LoadContent(contentPath="classpath:content/Automobiles.xml")
 	public void testGetEntitiesNotNull(){
 		assertNotNull(lexEvsEntityDescriptionQueryService.getEntities());
+	}
+	
+	@Test
+	@LoadContent(contentPath="classpath:content/Automobiles.xml")
+	public void testGetCount(){
+		DirectoryURI uri = 
+			lexEvsEntityDescriptionQueryService.getEntities();
+		
+		assertEquals(19,uri.count(null));
+	}
+	
+	@Test
+	@LoadContent(contentPath="classpath:content/Automobiles.xml")
+	public void testResolveEntityNotNull(){
+		EntityDirectoryURI uri = 
+			lexEvsEntityDescriptionQueryService.getEntities();
+		
+		EntityDirectory ed = 
+			lexEvsEntityDescriptionQueryService.resolve(uri, null, null);
+		
+		assertNotNull(ed);
+	}
+	
+	@Test
+	@LoadContent(contentPath="classpath:content/Automobiles.xml")
+	public void testResolveEntityCount(){
+		EntityDirectoryURI uri = 
+			lexEvsEntityDescriptionQueryService.getEntities();
+		
+		EntityDirectory ed = 
+			lexEvsEntityDescriptionQueryService.resolve(uri, null, null);
+		
+		assertEquals(19,ed.getEntryCount());
 	}
 	
 	
