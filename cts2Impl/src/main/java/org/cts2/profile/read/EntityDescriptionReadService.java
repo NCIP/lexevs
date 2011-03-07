6036233f74fe731fe53aebb6b959db6a57eeb70e
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-07 22:10:12 +0000

    New EntityDescription Read Service.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8754 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ cts2Impl/src/main/java/org/cts2/profile/read/EntityDescriptionReadService.java
@@ -0,0 +1,8 @@
+package org.cts2.profile.read;
+
+import org.cts2.codesystemversion.CodeSystemVersion;
+
+public interface EntityDescriptionReadService extends BaseReadService<CodeSystemVersion> {
+
+	
+}
