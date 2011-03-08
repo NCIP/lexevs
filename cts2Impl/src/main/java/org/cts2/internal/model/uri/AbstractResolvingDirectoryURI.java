3ef459c70adf43b8ca868821e7dc9af4e26d7b95
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-08 20:53:43 +0000

    Added some concurrency utils.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8766 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/main/java/org/cts2/internal/model/uri/AbstractResolvingDirectoryURI.java
+++ cts2Impl/src/main/java/org/cts2/internal/model/uri/AbstractResolvingDirectoryURI.java
@@ -1,32 +1,26 @@
 package org.cts2.internal.model.uri;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import org.cts2.core.Directory;
 import org.cts2.service.core.NameOrURI;
 import org.cts2.service.core.QueryControl;
 import org.cts2.service.core.ReadContext;
 import org.cts2.uri.DirectoryURI;
+import org.cts2.utility.ExecutionUtils;
 
 public abstract class AbstractResolvingDirectoryURI<T extends DirectoryURI> extends AbstractDirectoryURI<T> {
 	
-	private ExecutorService executorService = Executors.newCachedThreadPool();
-
 	public <D extends Directory<?>> D get(final QueryControl queryControl, final ReadContext readContext, final Class<D> content){
 		final QueryControl validatedQueryControl = validateQueryControl(queryControl);
-		return new AbstractTimedMethodCallback<D>(validatedQueryControl.getTimeLimit()){
+		return ExecutionUtils.callWithTimeout(new Callable<D>(){
 
 			@Override
-			protected D doExecuteMethod() {
+			public D call() {
 				return doGet(validatedQueryControl.getFormat(), validatedQueryControl.getMaxToReturn(), readContext, content);
 			}
 			
-		}.executeMethod();
+		}, queryControl);
 	}
 
 	/**
@@ -43,79 +37,4 @@ public abstract class AbstractResolvingDirectoryURI<T extends DirectoryURI> exte
 			Long maxToReturn,
 			ReadContext readContext,
 			Class<D> resolveClass);
-	
-	/**
-	 * The Class AbstractTimedMethodCallback.
-	 *
-	 * @param <E> the
-	 * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
-	 */
-	private abstract class AbstractTimedMethodCallback<E> implements TimedMethodCallback<E>{
-
-		/** The time. */
-		private Long time;
-		
-		/**
-		 * Instantiates a new abstract timed method callback.
-		 *
-		 * @param time the time
-		 */
-		private AbstractTimedMethodCallback(Long time){
-			this.time = time;
-		}
-
-		/* (non-Javadoc)
-		 * @see org.cts2.internal.uri.AbstractResolvingDirectoryURI.TimedMethodCallback#executeMethod()
-		 */
-		@Override
-		public E executeMethod() {
-
-			if(this.time == null){
-				return doExecuteMethod();
-			} else {
-
-				Future<E> result = AbstractResolvingDirectoryURI.this.executorService.submit(new Callable<E>(){
-
-					@Override
-					public E call() throws Exception {
-						return doExecuteMethod();
-					}
-
-				});
-
-				try {
-					return result.get(this.time, TimeUnit.MILLISECONDS);
-				} catch (TimeoutException e) {
-					throw new RuntimeException("Operation has timed out.");
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				} 
-			}
-		}
-
-		/**
-		 * Do execute method.
-		 *
-		 * @return the e
-		 */
-		protected abstract E doExecuteMethod();
-	}
-
-	/**
-	 * The Interface TimedMethodCallback.
-	 *
-	 * @param <T> the
-	 * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
-	 */
-	private interface TimedMethodCallback<T> {
-		
-		/**
-		 * Execute method.
-		 *
-		 * @return the t
-		 */
-		public T executeMethod();
-
-	}
-	
 }
