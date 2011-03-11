c5c38a63c9c0c88ea99285bcea34a11f29f00030
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-11 22:04:23 +0000

    Use the ServiceHolder for getting the LexBIGService.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8826 c4b421de-a31f-834e-8902-8020e42e311c

--- lbTest/src/test/java/org/lexevs/cts2/test/Cts2BaseTest.java
+++ lbTest/src/test/java/org/lexevs/cts2/test/Cts2BaseTest.java
@@ -18,12 +18,12 @@
  */
 package org.lexevs.cts2.test;
 
-import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
+import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 
 public class Cts2BaseTest {
 
 	protected LexBIGService getLexBIGService() {
-		return LexBIGServiceImpl.defaultInstance();
+		return ServiceHolder.instance().getLexBIGService();
 	}
 }
