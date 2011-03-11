68dcc8e6d9215309a1479c039428824f88ba968a
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-11 21:21:33 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8821 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/unit/java/org/cts/internal/mapper/ResolvedConceptReferenceToNamedEntityDescription.java
+++ cts2Impl/src/test/unit/java/org/cts/internal/mapper/ResolvedConceptReferenceToNamedEntityDescription.java
@@ -1,26 +1,47 @@
 package org.cts.internal.mapper;
 
+import static org.junit.Assert.assertEquals;
+
 import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
 import org.cts2.entity.NamedEntityDescription;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ResolvedConceptReferenceToNamedEntityDescription extends BaseDozerBeanMapperTest{
+public class ResolvedConceptReferenceToNamedEntityDescription extends
+		BaseDozerBeanMapperTest {
 	private ResolvedConceptReference ref;
 	private NamedEntityDescription mapped;
-	
+
 	@Before
 	public void initialize() {
 		ref = new ResolvedConceptReference();
+		ref.setCode("testCode");
+		ref.setCodeNamespace("testNamespace");
 		ref.setCodingSchemeName("codingSchemeName");
 		ref.setCodingSchemeURI("codingSchemeURI");
 		mapped = baseDozerBeanMapper.map(ref, NamedEntityDescription.class);
-		
-	}
-	
-	@Test
-	public void testGetCode(){
-		
+
 	}
 
+	@Test
+	public void testGetCode() {
+		assertEquals("testCode", mapped.getEntityId().getName());
+	}
+
+	@Test
+	public void testGetNamespace() {
+		assertEquals("testNamespace", mapped.getEntityId().getNamespace());
+	}
+
+	@Test
+	public void testGetCodingSchemeURI() {
+		assertEquals("codingSchemeURI", mapped.getDescribingCodeSystemVersion()
+				.getMeaning());
+	}
+
+	@Test
+	public void testGetCodingSchemeName() {
+		assertEquals("codingSchemeName", mapped
+				.getDescribingCodeSystemVersion().getContent());
+	}
 }
