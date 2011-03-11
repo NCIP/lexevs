17418805cb76d930db9b32247c887e1e47e8c4cb
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-11 18:12:25 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8804 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/main/java/org/cts2/internal/mapper/converter/EntityDirectoryEntryAboutConverter.java
+++ cts2Impl/src/main/java/org/cts2/internal/mapper/converter/EntityDirectoryEntryAboutConverter.java
@@ -1,16 +1,22 @@
 package org.cts2.internal.mapper.converter;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
 import org.LexGrid.codingSchemes.CodingScheme;
+import org.LexGrid.naming.SupportedLanguage;
+import org.LexGrid.naming.SupportedNamespace;
+import org.apache.commons.lang.StringUtils;
 import org.cts2.entity.EntityDirectoryEntry;
+import org.cts2.internal.lexevs.identity.DefaultLexEvsIdentityConverter;
 import org.cts2.internal.lexevs.identity.LexEvsIdentityConverter;
 import org.dozer.DozerConverter;
 import org.lexevs.dao.database.service.codingscheme.CodingSchemeService;
+import org.lexevs.dao.database.utility.DaoUtility;
 import org.lexevs.locator.LexEvsServiceLocator;
 
 public class EntityDirectoryEntryAboutConverter extends
 		DozerConverter<ResolvedConceptReference, EntityDirectoryEntry> {
-	private LexEvsIdentityConverter lexEvsIdentityConverter;
 
 	public EntityDirectoryEntryAboutConverter() {
 		super(ResolvedConceptReference.class, EntityDirectoryEntry.class);
@@ -26,27 +32,31 @@ public class EntityDirectoryEntryAboutConverter extends
 	public EntityDirectoryEntry convertTo(ResolvedConceptReference conRef,
 			EntityDirectoryEntry dirEntry) {
 
+		if (StringUtils.isBlank(conRef.getCode())
+				|| StringUtils.isBlank(conRef.getCodeNamespace())) {
+			return dirEntry;
+		}
 		CodingSchemeService service = LexEvsServiceLocator.getInstance()
 				.getDatabaseServiceManager().getCodingSchemeService();
 
 		CodingScheme cs = service.getCodingSchemeByUriAndVersion(
 				conRef.getCodingSchemeURI(), conRef.getCodingSchemeVersion());
 
-		String uri = this.lexEvsIdentityConverter.namespaceAndCodeToUri(cs,
-				conRef.getCodeNamespace(), conRef.getCode());
-		
-		dirEntry.setAbout(uri);
+		if (cs != null) {
+			try {
+				SupportedNamespace sns = DaoUtility.getURIMap(cs,
+						SupportedNamespace.class, conRef.getCodeNamespace());
+				if (sns != null && !StringUtils.isBlank(sns.getUri())) {
+					dirEntry.setAbout(sns.getUri()
+							+ DefaultLexEvsIdentityConverter.DEFAULT_URI_CONCAT_STRING
+							+ conRef.getCode());
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
 
 		return dirEntry;
-	}
-
-	public LexEvsIdentityConverter getLexEvsIdentityConverter() {
-		return this.lexEvsIdentityConverter;
-	}
-
-	public void setLexEvsIdentityConverter(
-			LexEvsIdentityConverter lexEvsIdentityConverter) {
-		this.lexEvsIdentityConverter = lexEvsIdentityConverter;
 	}
 
 }
