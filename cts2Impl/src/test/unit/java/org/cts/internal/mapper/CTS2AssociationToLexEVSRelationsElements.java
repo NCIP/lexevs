b59cc993f3051c96a3a196e4692967f0b48ef982
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-14 16:47:46 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8833 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/unit/java/org/cts/internal/mapper/CTS2AssociationToLexEVSRelationsElements.java
+++ cts2Impl/src/test/unit/java/org/cts/internal/mapper/CTS2AssociationToLexEVSRelationsElements.java
@@ -12,60 +12,79 @@ import org.junit.Test;
 
 public class CTS2AssociationToLexEVSRelationsElements extends
 		BaseDozerBeanMapperTest {
+	String codingSchemeName = "NCI Thesaurus";
+	String codingSchemeURI = "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#";
+	String association = "is_a";
+	String conceptCode = "C1234";
+	String conceptCode1 = "C4321";
+	String nameSpace = "NCIt";
+	
 
 	@Test
-	public void AssociationToLexBIGAssociationgTest() {
+	public void AssociationDirectoryEntryToLexBIGAssociationgTest() {
 
 		org.LexGrid.LexBIG.DataModel.Core.Association lbAssoc = new org.LexGrid.LexBIG.DataModel.Core.Association();
-		lbAssoc.setAssociationName("is_a");
-		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
-				lbAssoc, org.cts2.association.Association.class);
-		assertEquals("is_a", cts2Assoc.getPredicate().getContent());
+		lbAssoc.setAssociationName(association);
+		org.cts2.association.AssociationDirectoryEntry cts2Assoc = baseDozerBeanMapper.map(
+				lbAssoc, org.cts2.association.AssociationDirectoryEntry.class);
+		assertEquals(association, cts2Assoc.getPredicate().getContent());
 	}
 
 	@Test
-	public void AssociationToResolvedCodedNodeReferenceTest() {
+	public void AssociationDirectoryEntryToResolvedCodedNodeReferenceTest() {
 		ResolvedCodedNodeReference rcnr = new ResolvedCodedNodeReference();
 		Entity entity = new Entity();
-		entity.setEntityCode("C1234");
-		entity.setEntityCodeNamespace("NCIt");
+		entity.setEntityCode(conceptCode);
+		entity.setEntityCodeNamespace(nameSpace);
 		rcnr.setEntity(entity);
-		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
-				rcnr, org.cts2.association.Association.class);
-		assertEquals("C1234", cts2Assoc.getSubject().getLocalEntityName()
+		org.cts2.association.AssociationDirectoryEntry cts2Assoc = baseDozerBeanMapper.map(
+				rcnr, org.cts2.association.AssociationDirectoryEntry.class);
+		assertEquals(conceptCode, cts2Assoc.getSubject().getLocalEntityName()
 				.getName());
-		assertEquals("NCIt", cts2Assoc.getSubject().getLocalEntityName()
+		assertEquals(nameSpace, cts2Assoc.getSubject().getLocalEntityName()
 				.getNamespace());
-	}
-	public void AssociationToCodingSchemeTest(){
-		CodingScheme cs = new CodingScheme();
-		cs.setCodingSchemeName("NCI Thesaurus");
-		cs.setCodingSchemeURI("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#");
-		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
-				cs, org.cts2.association.Association.class);
-		assertEquals("NCI Thesaurus", cts2Assoc.getAssertedBy().getContent());
-		assertEquals("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#", cts2Assoc.getAssertedBy().getMeaning());
-	}
-	public void AssociationToSupportedCodingScheme(){
-		SupportedCodingScheme supCodingScheme = new SupportedCodingScheme();
-		supCodingScheme.setUri("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#");
-		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
-				supCodingScheme, org.cts2.association.Association.class);
-		assertEquals("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#",cts2Assoc.getPredicate().getMeaning());
 	}
 	@Test
-	public void AssociationToAssociatedConceptTest() {
+	public void AssociationDirectoryEntryToCodingSchemeTest(){
+		CodingScheme cs = new CodingScheme();
+		cs.setCodingSchemeName(codingSchemeName);
+		cs.setCodingSchemeURI(codingSchemeURI);
+		org.cts2.association.AssociationDirectoryEntry cts2Assoc = baseDozerBeanMapper.map(
+				cs, org.cts2.association.AssociationDirectoryEntry.class);
+		assertEquals(codingSchemeName, cts2Assoc.getAssertedBy().getContent());
+		assertEquals(codingSchemeURI, cts2Assoc.getAssertedBy().getMeaning());
+	}
+	@Test
+	public void AssociationDirectoryEntryToSupportedCodingScheme(){
+		SupportedCodingScheme supCodingScheme = new SupportedCodingScheme();
+		supCodingScheme.setUri(codingSchemeURI);
+		org.cts2.association.AssociationDirectoryEntry cts2Assoc = baseDozerBeanMapper.map(
+				supCodingScheme, org.cts2.association.AssociationDirectoryEntry.class);
+		assertEquals(codingSchemeURI,cts2Assoc.getPredicate().getMeaning());
+	}
+	@Test
+	public void AssociationDirectoryEntryToAssociatedConceptTest() {
 		AssociatedConcept ac = new AssociatedConcept();
 		Entity entity = new Entity();
-		entity.setEntityCode("C4321");
-		entity.setEntityCodeNamespace("NCIt");
+		entity.setEntityCode(conceptCode1);
+		entity.setEntityCodeNamespace(nameSpace);
 		ac.setReferencedEntry(entity);
-		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
-				ac, org.cts2.association.Association.class);
-		assertEquals("C4321", cts2Assoc.getTarget().getEntity()
+		org.cts2.association.AssociationDirectoryEntry cts2Assoc = baseDozerBeanMapper.map(
+				ac, org.cts2.association.AssociationDirectoryEntry.class);
+		assertEquals(conceptCode1, cts2Assoc.getTarget().getEntity()
 				.getLocalEntityName().getName());
-		assertEquals("NCIt", cts2Assoc.getSubject().getLocalEntityName()
+		assertEquals(nameSpace, cts2Assoc.getSubject().getLocalEntityName()
 				.getNamespace());
+	}
+	@Test
+	public void AssociationDirectoryEntryToCodingSchemeDefinition(){
+		CodingScheme cs = new CodingScheme();
+		cs.setCodingSchemeName(codingSchemeName);
+		cs.setCodingSchemeURI(codingSchemeURI);
+		org.cts2.association.AssociationDirectoryEntry cts2Assoc = baseDozerBeanMapper.map(
+				cs, org.cts2.association.AssociationDirectoryEntry.class);
+		assertEquals(codingSchemeName, cts2Assoc.getAssertedBy().getContent());
+		assertEquals(codingSchemeURI, cts2Assoc.getAssertedBy().getMeaning());
 	}
 
 	@Test
@@ -76,4 +95,72 @@ public class CTS2AssociationToLexEVSRelationsElements extends
 				ad, org.cts2.association.Association.class);
 		assertEquals("id123", cts2Assoc.getExternalStatementId());
 	}
+	@Test
+	public void AssociationToLexBIGAssociationgTest() {
+
+		org.LexGrid.LexBIG.DataModel.Core.Association lbAssoc = new org.LexGrid.LexBIG.DataModel.Core.Association();
+		lbAssoc.setAssociationName(association);
+		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
+				lbAssoc, org.cts2.association.Association.class);
+		assertEquals(association, cts2Assoc.getPredicate().getContent());
+	}
+
+	@Test
+	public void AssociationToResolvedCodedNodeReferenceTest() {
+		ResolvedCodedNodeReference rcnr = new ResolvedCodedNodeReference();
+		Entity entity = new Entity();
+		entity.setEntityCode(conceptCode);
+		entity.setEntityCodeNamespace(nameSpace);
+		rcnr.setEntity(entity);
+		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
+				rcnr, org.cts2.association.Association.class);
+		assertEquals(conceptCode, cts2Assoc.getSubject().getLocalEntityName()
+				.getName());
+		assertEquals(nameSpace, cts2Assoc.getSubject().getLocalEntityName()
+				.getNamespace());
+	}
+	@Test
+	public void AssociationToCodingSchemeTest(){
+		CodingScheme cs = new CodingScheme();
+		cs.setCodingSchemeName(codingSchemeName);
+		cs.setCodingSchemeURI(codingSchemeURI);
+		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
+				cs, org.cts2.association.Association.class);
+		assertEquals(codingSchemeName, cts2Assoc.getAssertedBy().getContent());
+		assertEquals(codingSchemeURI, cts2Assoc.getAssertedBy().getMeaning());
+	}
+	@Test
+	public void AssociationToSupportedCodingScheme(){
+		SupportedCodingScheme supCodingScheme = new SupportedCodingScheme();
+		supCodingScheme.setUri(codingSchemeURI);
+		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
+				supCodingScheme, org.cts2.association.Association.class);
+		assertEquals(codingSchemeURI,cts2Assoc.getPredicate().getMeaning());
+	}
+	@Test
+	public void AssociationToAssociatedConceptTest() {
+		AssociatedConcept ac = new AssociatedConcept();
+		Entity entity = new Entity();
+		entity.setEntityCode(conceptCode1);
+		entity.setEntityCodeNamespace(nameSpace);
+		ac.setReferencedEntry(entity);
+		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
+				ac, org.cts2.association.Association.class);
+		assertEquals(conceptCode1, cts2Assoc.getTarget().getEntity()
+				.getLocalEntityName().getName());
+		assertEquals(nameSpace, cts2Assoc.getSubject().getLocalEntityName()
+				.getNamespace());
+	}
+	@Test
+	public void AssociationToCodingSchemeDefinition(){
+		CodingScheme cs = new CodingScheme();
+		cs.setCodingSchemeName(codingSchemeName);
+		cs.setCodingSchemeURI(codingSchemeURI);
+		org.cts2.association.Association cts2Assoc = baseDozerBeanMapper.map(
+				cs, org.cts2.association.Association.class);
+		assertEquals(codingSchemeName, cts2Assoc.getAssertedBy().getContent());
+		assertEquals(codingSchemeURI, cts2Assoc.getAssertedBy().getMeaning());
+	}
+
+	
 }
