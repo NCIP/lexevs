0335e00c119cf7b4d5d42eba0b26e20ecb49e45e
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-15 11:20:06 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8840 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/unit/java/org/cts2/internal/model/uri/DefaultCodeSystemVersionDirectoryURITest.java
+++ cts2Impl/src/test/unit/java/org/cts2/internal/model/uri/DefaultCodeSystemVersionDirectoryURITest.java
@@ -30,4 +30,69 @@ public class DefaultCodeSystemVersionDirectoryURITest {
 		
 		assertEquals(1,returnList.getCodingSchemeRenderingCount());
 	}
+	
+	@Test
+	public void Test_Active_Or_All_With_Active_Only_And_Inactive_CSV(){
+		
+		
+		CodingSchemeRenderingList csrl = new CodingSchemeRenderingList();
+		CodingSchemeRendering csr = new CodingSchemeRendering();
+		csr.setRenderingDetail(new RenderingDetail());
+		csr.getRenderingDetail().setVersionStatus(CodingSchemeVersionStatus.INACTIVE);
+		
+		csrl.addCodingSchemeRendering(csr);
+		
+		DefaultCodeSystemVersionDirectoryURI uri = new DefaultCodeSystemVersionDirectoryURI(csrl, null);
+		
+		CodingSchemeRenderingList returnList = 
+			uri.restrictToActiveOrAll(ActiveOrAll.ACTIVE_ONLY);
+		
+		assertEquals(0,returnList.getCodingSchemeRenderingCount());
+	}
+	
+	@Test
+	public void Test_Active_Or_All_With_Active_Only_With_Two(){
+
+		CodingSchemeRenderingList csrl = new CodingSchemeRenderingList();
+		CodingSchemeRendering csr = new CodingSchemeRendering();
+		csr.setRenderingDetail(new RenderingDetail());
+		csr.getRenderingDetail().setVersionStatus(CodingSchemeVersionStatus.INACTIVE);
+		
+		CodingSchemeRendering csr2 = new CodingSchemeRendering();
+		csr2.setRenderingDetail(new RenderingDetail());
+		csr2.getRenderingDetail().setVersionStatus(CodingSchemeVersionStatus.ACTIVE);
+		
+		csrl.addCodingSchemeRendering(csr);
+		csrl.addCodingSchemeRendering(csr2);
+		
+		DefaultCodeSystemVersionDirectoryURI uri = new DefaultCodeSystemVersionDirectoryURI(csrl, null);
+		
+		CodingSchemeRenderingList returnList = 
+			uri.restrictToActiveOrAll(ActiveOrAll.ACTIVE_ONLY);
+		
+		assertEquals(1,returnList.getCodingSchemeRenderingCount());
+	}
+	
+	@Test
+	public void Test_Active_Or_All_With_Either_With_Two(){
+
+		CodingSchemeRenderingList csrl = new CodingSchemeRenderingList();
+		CodingSchemeRendering csr = new CodingSchemeRendering();
+		csr.setRenderingDetail(new RenderingDetail());
+		csr.getRenderingDetail().setVersionStatus(CodingSchemeVersionStatus.INACTIVE);
+		
+		CodingSchemeRendering csr2 = new CodingSchemeRendering();
+		csr2.setRenderingDetail(new RenderingDetail());
+		csr2.getRenderingDetail().setVersionStatus(CodingSchemeVersionStatus.ACTIVE);
+		
+		csrl.addCodingSchemeRendering(csr);
+		csrl.addCodingSchemeRendering(csr2);
+		
+		DefaultCodeSystemVersionDirectoryURI uri = new DefaultCodeSystemVersionDirectoryURI(csrl, null);
+		
+		CodingSchemeRenderingList returnList = 
+			uri.restrictToActiveOrAll(ActiveOrAll.ACTIVE_AND_INACTIVE);
+		
+		assertEquals(2,returnList.getCodingSchemeRenderingCount());
+	}
 }
