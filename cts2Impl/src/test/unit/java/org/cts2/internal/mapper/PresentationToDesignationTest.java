60597980420457b0cd7f609a39f1e49f8f304b9d
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-17 22:09:22 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8890 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/unit/java/org/cts2/internal/mapper/PresentationToDesignationTest.java
+++ cts2Impl/src/test/unit/java/org/cts2/internal/mapper/PresentationToDesignationTest.java
@@ -2,16 +2,22 @@ package org.cts2.internal.mapper;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.annotation.Resource;
+
 import org.LexGrid.commonTypes.Text;
 import org.LexGrid.concepts.Presentation;
-import org.cts2.core.DesignationFidelityReference;
-import org.cts2.core.LanguageReference;
 import org.cts2.entity.Designation;
+import org.cts2.entity.types.DesignationRole;
+import org.cts2.internal.lexevs.identity.DefaultLexEvsIdentityConverter;
+import org.cts2.internal.lexevs.identity.LexEvsIdentityConverter;
+import org.cts2.internal.mapper.converter.PreferredToDesignationRoleConverter;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PresentationToDesignationTest extends BaseDozerBeanMapperTest{
 
+	@Resource 
+	private PreferredToDesignationRoleConverter converter;
 	private Presentation presentation;
 	private Designation designation;
 	
@@ -24,22 +30,28 @@ public class PresentationToDesignationTest extends BaseDozerBeanMapperTest{
 		presentation.setLanguage("en");
 		Text value = new Text();
 		value.setContent("testValue");
+		value.setDataType("string");
 		presentation.setValue(value);
+		presentation.setPropertyName("property name");
 		
 		presentation.addUsageContext("test usage context 1");
 		presentation.addUsageContext("test usage context 2");
 		presentation.addUsageContext("test usage context 3");
-	
+		
+		LexEvsIdentityConverter lexEvsIdentityConverter = new DefaultLexEvsIdentityConverter();
+		this.converter.setLexEvsIdentityConverter(lexEvsIdentityConverter);
+		
 		designation = baseDozerBeanMapper.map(presentation, Designation.class);
 		
-//		// renderURI
-//		designation.setCorrespondingStatement(correspondingStatement);
+		//		// renderURI
+//		designation.setCorrespondingStatement(correspondingStatement); n/a
 //		// enum: PREFERRED, ALTERNATIVE, HIDDEN
 //		designation.setDesignationRole(designationRole); 
-//		designation.setDesignationType(designationType);
-//		designation.setFormat(format);
-//		designation.setSchema(schema);
-//		designation.setUsageContext(vUsageContextArray);
+//		designation.setDesignationType(designationType); - property name
+//		designation.setFormat(format); value type
+//		designation.setSchema(schema); n/a
+		// qualifier, source post to wiki
+		
 	}
 	
 	@Test
@@ -67,5 +79,21 @@ public class PresentationToDesignationTest extends BaseDozerBeanMapperTest{
 		assertEquals("test usage context 1", designation.getUsageContext(0).getContent());
 		assertEquals("test usage context 2", designation.getUsageContext(1).getContent());
 		assertEquals("test usage context 3", designation.getUsageContext(2).getContent());
+	}
+	
+	@Test
+	public void testGetValueDataType() {
+		assertEquals("string", designation.getFormat().getContent());
+	}
+	
+	@Test
+	public void testGetPropertyName(){
+		assertEquals("property name", designation.getDesignationType().getContent());
+	}
+	
+	@Test
+	public void testGetIsPreferred() {
+		assertEquals(DesignationRole.ALTERNATIVE, designation.getDesignationRole());
+		
 	}
 }
