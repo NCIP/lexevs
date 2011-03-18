dc5b846cfb99ae87d540af05ae564405b728127e
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-18 19:10:16 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8902 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/unit/java/org/cts2/internal/mapper/DefinitionToDefinitionTest.java
+++ cts2Impl/src/test/unit/java/org/cts2/internal/mapper/DefinitionToDefinitionTest.java
@@ -2,12 +2,21 @@ package org.cts2.internal.mapper;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.annotation.Resource;
+
 import org.LexGrid.commonTypes.Text;
-import org.cts2.entity.EntityDirectoryEntry;
+import org.cts2.core.types.DefinitionRole;
+import org.cts2.internal.lexevs.identity.DefaultLexEvsIdentityConverter;
+import org.cts2.internal.lexevs.identity.LexEvsIdentityConverter;
+import org.cts2.internal.mapper.converter.DefinitionPreferredToDefinitionRoleConverter;
+import org.cts2.internal.mapper.converter.PresentationPreferredToDesignationRoleConverter;
 import org.junit.Before;
 import org.junit.Test;
 
 public class DefinitionToDefinitionTest extends BaseDozerBeanMapperTest{
+	@Resource 
+	private DefinitionPreferredToDefinitionRoleConverter converter;
+	
 	private org.LexGrid.concepts.Definition lgDef;
 	private org.cts2.core.Definition ctsDef;
 	
@@ -20,6 +29,9 @@ public class DefinitionToDefinitionTest extends BaseDozerBeanMapperTest{
 		t.setDataType("string");
 		t.setContent("content");
 		lgDef.setValue(t);
+		
+		LexEvsIdentityConverter lexEvsIdentityConverter = new DefaultLexEvsIdentityConverter();
+		this.converter.setLexEvsIdentityConverter(lexEvsIdentityConverter);
 		
 		ctsDef = baseDozerBeanMapper.map(lgDef, org.cts2.core.Definition.class);
 //		ctsDef.setAssertedByCodeSystemVersion(assertedByCodeSystemVersion)
@@ -52,6 +64,11 @@ public class DefinitionToDefinitionTest extends BaseDozerBeanMapperTest{
 	@Test
 	public void testGetDataType() {
 		assertEquals("string", ctsDef.getFormat().getContent());
+	}
+	
+	@Test
+	public void testGetIsPrefferred(){
+		assertEquals(DefinitionRole.INFORMATIVE, ctsDef.getDefinitionRole());
 	}
 
 }
