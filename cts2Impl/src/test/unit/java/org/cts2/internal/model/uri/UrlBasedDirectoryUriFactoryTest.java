e703f4d999bf2d7b133f13a3584c07fce2cbb1f9
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-21 21:34:57 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8939 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/unit/java/org/cts2/internal/model/uri/UrlBasedDirectoryUriFactoryTest.java
+++ cts2Impl/src/test/unit/java/org/cts2/internal/model/uri/UrlBasedDirectoryUriFactoryTest.java
@@ -19,18 +19,39 @@
 package org.cts2.internal.model.uri;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import org.cts2.internal.model.uri.factory.DirectoryUriUtils;
+import java.io.StringWriter;
+
+import org.cts2.codesystem.CodeSystemDirectory;
+import org.cts2.uri.CodeSystemDirectoryURI;
 import org.cts2.uri.CodeSystemVersionDirectoryURI;
+import org.cts2.uri.UrlBasedDirectoryUriFactory;
+import org.exolab.castor.xml.Marshaller;
 import org.junit.Test;
 
-public class DirectoryUriUtilsTest {
+public class UrlBasedDirectoryUriFactoryTest {
 
 	@Test
 	public void testCreateDirectoryUriUrlProxy(){
 		CodeSystemVersionDirectoryURI uri = 
-			DirectoryUriUtils.createUrlBasedDirectoryUri("http://test.org", CodeSystemVersionDirectoryURI.class);
+			UrlBasedDirectoryUriFactory.createUrlBasedDirectoryUri("http://test.org", CodeSystemVersionDirectoryURI.class);
 		
 		assertNotNull(uri);
+	}
+	
+	@Test
+	public void testMarshall() throws Exception{
+		
+		CodeSystemDirectory dir = new CodeSystemDirectory();
+		
+		CodeSystemDirectoryURI uri = UrlBasedDirectoryUriFactory.createUrlBasedDirectoryUri("http://test.org", CodeSystemDirectoryURI.class);
+		
+		dir.setNext(uri);
+		
+		StringWriter sw = new StringWriter();
+		Marshaller.marshal(dir, sw);
+		
+		assertTrue(sw.toString().contains("next=\"http://test.org\""));
 	}
 }
