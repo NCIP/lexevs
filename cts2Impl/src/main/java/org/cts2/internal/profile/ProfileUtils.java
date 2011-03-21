a27139a271211429dc1a5b09ac75c1aaef3446a0
petersonkj <petersonkj@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-21 12:22:32 +0000

    Updated ProfileUtils.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8908 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/main/java/org/cts2/internal/profile/ProfileUtils.java
+++ cts2Impl/src/main/java/org/cts2/internal/profile/ProfileUtils.java
@@ -1,13 +1,48 @@
+/*
+ * Copyright: (c) 2004-2011 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
 package org.cts2.internal.profile;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.DataModel.Core.types.CodingSchemeVersionStatus;
 import org.LexGrid.LexBIG.DataModel.InterfaceElements.CodingSchemeRendering;
 import org.LexGrid.LexBIG.Exceptions.LBException;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.LexGrid.LexBIG.Utility.Constructors;
 
+/**
+ * The Class ProfileUtils.
+ *
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
 public class ProfileUtils {
 
+	/**
+	 * Union all.
+	 *
+	 * @param lexBigService the lex big service
+	 * @return the coded node set
+	 * @throws LBException the LB exception
+	 */
 	public static CodedNodeSet unionAll(LexBIGService lexBigService) throws LBException {
 		CodedNodeSet cns = null;
 		for(CodingSchemeRendering csr : lexBigService.getSupportedCodingSchemes().getCodingSchemeRendering()){
@@ -24,5 +59,24 @@ public class ProfileUtils {
 		}
 		
 		return cns;
+	}
+	
+	/**
+	 * Gets the all active coding schemes.
+	 *
+	 * @param lexBigService the lex big service
+	 * @return the all active coding schemes
+	 * @throws LBException the LB exception
+	 */
+	public static List<AbsoluteCodingSchemeVersionReference> getAllActiveCodingSchemes(LexBIGService lexBigService) throws LBException {
+		List<AbsoluteCodingSchemeVersionReference> returnList = new ArrayList<AbsoluteCodingSchemeVersionReference>();
+		
+		for(CodingSchemeRendering csr : lexBigService.getSupportedCodingSchemes().getCodingSchemeRendering()){
+			if(csr.getRenderingDetail().getVersionStatus().equals(CodingSchemeVersionStatus.ACTIVE)){
+				returnList.add(Constructors.createAbsoluteCodingSchemeVersionReference(csr.getCodingSchemeSummary()));
+			}
+		}
+		
+		return returnList;
 	}
 }
