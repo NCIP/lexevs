af3fdd3864cadb47d3498fbd6ee2c7ee0b01d93c
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-21 14:00:37 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8914 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/main/java/org/cts2/internal/model/uri/DefaultAssociationDirectoryURI.java
+++ cts2Impl/src/main/java/org/cts2/internal/model/uri/DefaultAssociationDirectoryURI.java
@@ -1,79 +1,235 @@
-/*
- * Copyright: (c) 2004-2011 Mayo Foundation for Medical Education and 
- * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
- * triple-shield Mayo logo are trademarks and service marks of MFMER.
- *
- * Except as contained in the copyright notice above, or as used to identify 
- * MFMER as the author of this software, the trade names, trademarks, service
- * marks, or product names of the copyright holder shall not be used in
- * advertising, promotion or otherwise in connection with this software without
- * prior written authorization of the copyright holder.
- * 
- * Licensed under the Eclipse Public License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
- * 
- * 		http://www.eclipse.org/legal/epl-v10.html
- * 
- */
 package org.cts2.internal.model.uri;
 
+import org.LexGrid.LexBIG.DataModel.Collections.ConceptReferenceList;
+import org.LexGrid.LexBIG.DataModel.Collections.NameAndValueList;
+import org.LexGrid.LexBIG.DataModel.Collections.ResolvedConceptReferenceList;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
+import org.LexGrid.LexBIG.DataModel.Core.ConceptReference;
+import org.LexGrid.LexBIG.DataModel.Core.NameAndValue;
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.Impl.CodedNodeSetImpl;
+import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeGraph;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.cts2.core.Directory;
+import org.cts2.core.Filter;
+import org.cts2.core.FilterComponent;
+import org.cts2.core.TargetExpression;
 import org.cts2.internal.mapper.BeanMapper;
-import org.cts2.internal.model.uri.restrict.AssociationRestrictionHandler;
+import org.cts2.service.core.EntityNameOrURI;
+import org.cts2.service.core.NameOrURI;
+import org.cts2.service.core.QueryControl;
 import org.cts2.service.core.ReadContext;
 import org.cts2.uri.AssociationDirectoryURI;
+import org.cts2.uri.EntityDirectoryURI;
 
 /**
- * The Class DefaultEntityDirectoryURI.
+ * @author <a href="mailto:scott.bauer@mayo.edu">Scott Bauer</a>
  *
- * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
  */
-public class DefaultAssociationDirectoryURI extends AbstractNonIterableLexEvsBackedResolvingDirectoryURI<CodedNodeGraph,AssociationDirectoryURI> implements AssociationDirectoryURI {
-	
-	/** The coded node set. */
+public class DefaultAssociationDirectoryURI extends
+		AbstractResolvingDirectoryURI<AssociationDirectoryURI> implements AssociationDirectoryURI {
+
 	private CodedNodeGraph codedNodeGraph;
-	
-	/** The bean mapper. */
 	private BeanMapper beanMapper;
+	private LexBIGService lbs;
 	
-	/**
-	 * Instantiates a new default code system version directory uri.
-	 *
-	 * @param codedNodeSet the coded node set
-	 * @param restrictionHandler the restriction handler
-	 * @param beanMapper the bean mapper
-	 */
-	public DefaultAssociationDirectoryURI(
-			CodedNodeGraph codedNodeGraph,
-			AssociationRestrictionHandler restrictionHandler,
-			BeanMapper beanMapper) {
-		super(restrictionHandler);
+	public DefaultAssociationDirectoryURI(CodedNodeGraph codedNodeGraph, BeanMapper beanMapper){
+		super();
 		this.codedNodeGraph = codedNodeGraph;
 		this.beanMapper = beanMapper;
+		lbs = LexBIGServiceImpl.defaultInstance();
+	}
+ 
+	@Override
+	protected <D extends Directory<?>> D doGet(
+			QueryControl queryControl, ReadContext readContext,
+			Class<D> resolveClass) {
+		ResolvedConceptReferenceList rcrList = null;
+		try {
+			ConceptReference reference = Constructors.createConceptReference(queryControl.getFormat().getName(), queryControl.getFormat().getUri());
+			 rcrList = codedNodeGraph.resolveAsList(reference, true, false, 1, 1, null, null, null, null, queryControl.getMaxToReturn().intValue());
+		} catch (LBInvocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LBParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return beanMapper.map(
+	    		rcrList, 
+				resolveClass);
 	}
 
-	@Override
-	protected CodedNodeGraph getOriginalState() {
-		return this.codedNodeGraph;
-	}
+
 
 	@Override
-	protected <O> O transform(CodedNodeGraph lexevsObject, Class<O> clazz) {
+	protected int doCount(ReadContext readContext) {
+		return 0;
+	}
+	
+	public EntityDirectoryURI doGetAllSourceAndTargetEntities(
+			EntityDirectoryURI directory, QueryControl queryControl,
+			ReadContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public EntityDirectoryURI doGetPredicates(AssociationDirectoryURI directory,
+			QueryControl queryControl, ReadContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public EntityDirectoryURI doGetSourceEntities(
+			AssociationDirectoryURI directory, QueryControl queryControl,
+			ReadContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public EntityDirectoryURI doGetTargetEntities(
+			AssociationDirectoryURI directory, QueryControl queryControl,
+			ReadContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected String resolveForCodeSystemVersion() throws Exception{
+		ResolvedConceptReferenceList rcrl = codedNodeGraph.resolveAsList(null, true, false, 1, 1, null, null, null, null, 1);
+		if(rcrl.getResolvedConceptReference().length < 1){
+			throw new Exception("Exception not created for resolving AssociationDirectoryURI node graph");
+		}
+		ResolvedConceptReference ref = rcrl.getResolvedConceptReference(0);
+		return ref.getCodingSchemeVersion();		
+	}
+	
+	protected String resolveForCodeSystemName() throws Exception{
+		ResolvedConceptReferenceList rcrl = codedNodeGraph.resolveAsList(null, true, false, 1, 1, null, null, null, null, 1);
+		if(rcrl.getResolvedConceptReference().length < 1){
+			throw new Exception("Exception not created for resolving AssociationDirectoryURI node graph");
+		}
+		ResolvedConceptReference ref = rcrl.getResolvedConceptReference(0);
+		return ref.getCodingSchemeName();		
+	}
+	
+	/**
+	 * This method resets the class coded node graph data member.  All current restrictions are lost as a result.
+	 * @param directory
+	 * @param codeSystemVersion
+	 * @return
+	 * @throws Exception
+	 */
+	public AssociationDirectoryURI doRestrictToCodeSystemVersion(
+			AssociationDirectoryURI directory, NameOrURI codeSystemVersion) throws Exception {
+		CodingSchemeVersionOrTag versionOrTag = new CodingSchemeVersionOrTag();
+		versionOrTag.setVersion(resolveForCodeSystemVersion());
+	    codedNodeGraph = lbs.getNodeGraph(resolveForCodeSystemName(), versionOrTag, null);
+		
+		return this;
+	}
+
+	
+	public AssociationDirectoryURI doRestrictToPredicate(
+			AssociationDirectoryURI directory, EntityNameOrURI predicate) {
+		NameAndValueList association = Constructors.createNameAndValueList(predicate.getEntityName().getName(), null);
+		try {
+			codedNodeGraph.restrictToAssociations(association, null);
+		} catch (LBInvocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LBParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return this;
+	}
+
+	
+	public AssociationDirectoryURI doRestrictToSourceEntity(
+			AssociationDirectoryURI directory, EntityNameOrURI sourceEntity) {
+		CodedNodeSet set = new CodedNodeSetImpl();
+		ConceptReferenceList codeList = Constructors.createConceptReferenceList(sourceEntity.getEntityName().getName());
+	
+		try {
+			set.restrictToCodes(codeList);
+			codedNodeGraph.restrictToSourceCodes(set);
+		} catch (LBInvocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LBParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return this;
+	}
+
+	
+	public AssociationDirectoryURI doRestrictToSourceOrTargetEntity(
+			AssociationDirectoryURI directory, EntityNameOrURI entity) {
+		CodedNodeSet set = new CodedNodeSetImpl();
+		ConceptReferenceList codeList = Constructors.createConceptReferenceList(entity.getEntityName().getName());
+		try {
+			set.restrictToCodes(codeList);
+			codedNodeGraph.restrictToCodes(set);
+		} catch (LBInvocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LBParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return this;
+	}
+
+	
+	public AssociationDirectoryURI doRestrictToTargetEntity(
+			AssociationDirectoryURI directory, EntityNameOrURI target) {
+		CodedNodeSet set = new CodedNodeSetImpl();
+		ConceptReferenceList codeList = Constructors.createConceptReferenceList(target.getEntityName().getName());
+	
+		try {
+			set.restrictToCodes(codeList);
+			codedNodeGraph.restrictToTargetCodes(set);
+		} catch (LBInvocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LBParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return this;
+	}
+
+	
+	public AssociationDirectoryURI doRestrictToTargetExpression(
+			AssociationDirectoryURI directory, TargetExpression target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public AssociationDirectoryURI doRestrictToTargetLiteral(
+			AssociationDirectoryURI directory, String target) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected AssociationDirectoryURI clone() {
+	public AssociationDirectoryURI restrict(Filter filter) {
 		// TODO Auto-generated method stub
-		return this;
-	}
-
-	@Override
-	protected int doCount(ReadContext readContext) {
-		// TODO Auto-generated method stub
-		return 0;
+		return null;
 	}
 
 }
--- cts2Impl/src/main/java/org/cts2/internal/profile/query/LexEvsAssociationQueryService.java
+++ cts2Impl/src/main/java/org/cts2/internal/profile/query/LexEvsAssociationQueryService.java
@@ -2,10 +2,15 @@ package org.cts2.internal.profile.query;
 
 import org.cts2.association.AssociationDirectory;
 import org.cts2.association.AssociationList;
+import org.cts2.core.TargetExpression;
+import org.cts2.internal.model.uri.DefaultAssociationDirectoryURI;
 import org.cts2.profile.query.AssociationQueryService;
+import org.cts2.service.core.EntityNameOrURI;
+import org.cts2.service.core.NameOrURI;
 import org.cts2.service.core.QueryControl;
 import org.cts2.service.core.ReadContext;
 import org.cts2.uri.AssociationDirectoryURI;
+import org.cts2.uri.EntityDirectoryURI;
 
 /**
  * The class LexEVSAssociationQuery
@@ -17,24 +22,103 @@ public class LexEvsAssociationQueryService extends
 	AbstractBaseQueryService<AssociationDirectoryURI> 
 	implements AssociationQueryService {
 
+
 	@Override
-	public AssociationDirectoryURI getAllAssociations() {
+	public AssociationDirectory resolve(
+			AssociationDirectoryURI associationQueryURI,
+			QueryControl queryControl, ReadContext readContext) {
+		return associationQueryURI.get(queryControl, readContext, AssociationDirectory.class);
+	}
+
+	@Override
+	public AssociationList resolveAsList(
+			AssociationDirectoryURI associationQueryURI,
+			QueryControl queryControl, ReadContext readContext) {
+		return associationQueryURI.get(queryControl, readContext, AssociationList.class);
+	}
+
+	@Override
+	public EntityDirectoryURI getAllSourceAndTargetEntities(
+			EntityDirectoryURI directory, QueryControl queryControl,
+			ReadContext context) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public AssociationDirectory resolve(
-			AssociationDirectoryURI codeSystemQueryURI,
-			QueryControl queryControl, ReadContext readContext) {
-		return codeSystemQueryURI.get(queryControl, readContext, AssociationDirectory.class);
+	public EntityDirectoryURI getPredicates(AssociationDirectoryURI directory,
+			QueryControl queryControl, ReadContext context) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	public AssociationList resolveAsList(
-			AssociationDirectoryURI codeSystemQueryURI,
-			QueryControl queryControl, ReadContext readContext) {
-		return codeSystemQueryURI.get(queryControl, readContext, AssociationList.class);
+	public EntityDirectoryURI getSourceEntities(
+			AssociationDirectoryURI directory, QueryControl queryControl,
+			ReadContext context) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+	@Override
+	public EntityDirectoryURI getTargetEntities(
+			AssociationDirectoryURI directory, QueryControl queryControl,
+			ReadContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AssociationDirectoryURI restrictToCodeSystemVersion(
+			AssociationDirectoryURI directory, NameOrURI codeSystemVersion) {
+		return null;
+	}
+
+	@Override
+	public AssociationDirectoryURI restrictToPredicate(
+			AssociationDirectoryURI directory, EntityNameOrURI predicate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AssociationDirectoryURI restrictToSourceEntity(
+			AssociationDirectoryURI directory, EntityNameOrURI sourceEntity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AssociationDirectoryURI restrictToSourceOrTargetEntity(
+			AssociationDirectoryURI directory, EntityNameOrURI entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AssociationDirectoryURI restrictToTargetEntity(
+			AssociationDirectoryURI directory, EntityNameOrURI target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AssociationDirectoryURI restrictToTargetExpression(
+			AssociationDirectoryURI directory, TargetExpression target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AssociationDirectoryURI restrictToTargetLiteral(
+			AssociationDirectoryURI directory, String target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
 
 }
