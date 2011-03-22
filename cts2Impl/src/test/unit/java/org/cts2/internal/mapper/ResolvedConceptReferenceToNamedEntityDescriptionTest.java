2fc0859a0047810e5443ba26c0f23fcd9c6fc569
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-22 19:16:19 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@8951 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/test/unit/java/org/cts2/internal/mapper/ResolvedConceptReferenceToNamedEntityDescriptionTest.java
+++ cts2Impl/src/test/unit/java/org/cts2/internal/mapper/ResolvedConceptReferenceToNamedEntityDescriptionTest.java
@@ -23,6 +23,7 @@ import org.cts2.internal.mapper.converter.DefinitionPreferredToDefinitionRoleCon
 import org.cts2.internal.mapper.converter.NamedEntityDescriptionAboutConverter;
 import org.cts2.internal.mapper.converter.NamedEntityDescriptionDefinitionListConverter;
 import org.cts2.internal.mapper.converter.NamedEntityDescriptionDesignationListConverter;
+import org.cts2.internal.mapper.converter.NamedEntityDescriptionNoteListConverter;
 import org.cts2.internal.mapper.converter.NamedEntityDescriptionPropertyListConverter;
 import org.cts2.internal.mapper.converter.PresentationPreferredToDesignationRoleConverter;
 import org.easymock.classextension.EasyMock;
@@ -36,7 +37,7 @@ public class ResolvedConceptReferenceToNamedEntityDescriptionTest extends
 	private NamedEntityDescriptionAboutConverter namedEntityDescriptionAboutConverter;
 	@Resource
 	private PresentationPreferredToDesignationRoleConverter presentationConverter;
-	@Resource 
+	@Resource
 	private DefinitionPreferredToDefinitionRoleConverter definitionConverter;
 	@Resource
 	private NamedEntityDescriptionPropertyListConverter namedEntityDescriptionPropertyListConverter;
@@ -44,11 +45,12 @@ public class ResolvedConceptReferenceToNamedEntityDescriptionTest extends
 	private NamedEntityDescriptionDesignationListConverter namedEntityDescriptionDesignationListConverter;
 	@Resource
 	private NamedEntityDescriptionDefinitionListConverter namedEntityDescriptionDefinitionListConverter;
-	
+	@Resource
+	private NamedEntityDescriptionNoteListConverter namedEntityDescriptionNoteListConverter;
+
 	private ResolvedConceptReference ref;
 	private NamedEntityDescription mapped;
 
-	
 	@Before
 	public void initialize() {
 		ref = new ResolvedConceptReference();
@@ -57,27 +59,28 @@ public class ResolvedConceptReferenceToNamedEntityDescriptionTest extends
 		ref.setCodingSchemeName("codingSchemeName");
 		ref.setCodingSchemeURI("testUri");
 		ref.setCodingSchemeVersion("testVersion");
-		
+
 		Entity entity = new Entity();
 		entity.setEntityCode("testCode");
 		entity.setEntityCodeNamespace("testNamespace");
-		
+
 		ref.setEntity(entity);
-		
-		//presentation
+
+		// presentation
 		this.initPresentations();
 
-		//property
+		// property
 		this.initProperties();
-		
-		//comment
+
+		// comment
 		this.initComments();
-		
-		//definition
+
+		// definition
 		this.initDefinitions();
-		
-		CodingSchemeService css = EasyMock.createMock(CodingSchemeService.class);
-		
+
+		CodingSchemeService css = EasyMock
+				.createMock(CodingSchemeService.class);
+
 		CodingScheme cs = new CodingScheme();
 		cs.setMappings(new Mappings());
 		cs.getMappings().addSupportedNamespace(new SupportedNamespace());
@@ -92,51 +95,69 @@ public class ResolvedConceptReferenceToNamedEntityDescriptionTest extends
 		cs.getMappings().addSupportedDataType(new SupportedDataType());
 		cs.getMappings().getSupportedDataType(0).setLocalId("string");
 		cs.getMappings().getSupportedDataType(0).setUri("string uri");
-		cs.getMappings().addSupportedDegreeOfFidelity(new SupportedDegreeOfFidelity());
+		cs.getMappings().addSupportedDegreeOfFidelity(
+				new SupportedDegreeOfFidelity());
 		cs.getMappings().getSupportedDegreeOfFidelity(0).setLocalId("testFed");
 		cs.getMappings().getSupportedDegreeOfFidelity(0).setUri("testFed uri");
 		cs.getMappings().addSupportedContext(new SupportedContext());
-		cs.getMappings().getSupportedContext(0).setLocalId("test usage context 1");
-		cs.getMappings().getSupportedContext(0).setUri("test usage context 1 uri");
+		cs.getMappings().getSupportedContext(0)
+				.setLocalId("test usage context 1");
+		cs.getMappings().getSupportedContext(0)
+				.setUri("test usage context 1 uri");
 		cs.getMappings().addSupportedContext(new SupportedContext());
-		cs.getMappings().getSupportedContext(1).setLocalId("test usage context 2");
-		cs.getMappings().getSupportedContext(1).setUri("test usage context 2 uri");
+		cs.getMappings().getSupportedContext(1)
+				.setLocalId("test usage context 2");
+		cs.getMappings().getSupportedContext(1)
+				.setUri("test usage context 2 uri");
 		cs.getMappings().addSupportedContext(new SupportedContext());
-		cs.getMappings().getSupportedContext(2).setLocalId("test usage context 3");
-		cs.getMappings().getSupportedContext(2).setUri("test usage context 3 uri");
-		
+		cs.getMappings().getSupportedContext(2)
+				.setLocalId("test usage context 3");
+		cs.getMappings().getSupportedContext(2)
+				.setUri("test usage context 3 uri");
+
 		cs.setRepresentsVersion("testVersion");
 		cs.setCodingSchemeURI("testUri");
-		
-		
-		EasyMock.expect(css.getCodingSchemeByUriAndVersion("testUri", "testVersion")).andReturn(cs).times(4);
-		
+
+		EasyMock.expect(
+				css.getCodingSchemeByUriAndVersion("testUri", "testVersion"))
+				.andReturn(cs).times(5);
+
 		EasyMock.replay(css);
-		
+
 		this.namedEntityDescriptionAboutConverter.setCodingSchemeService(css);
 		LexEvsIdentityConverter lexEvsIdentityConverter = new DefaultLexEvsIdentityConverter();
-		this.namedEntityDescriptionAboutConverter.setLexEvsIdentityConverter(lexEvsIdentityConverter);
-		this.presentationConverter.setLexEvsIdentityConverter(lexEvsIdentityConverter);
+		this.namedEntityDescriptionAboutConverter
+				.setLexEvsIdentityConverter(lexEvsIdentityConverter);
+		this.presentationConverter
+				.setLexEvsIdentityConverter(lexEvsIdentityConverter);
 		definitionConverter.setLexEvsIdentityConverter(lexEvsIdentityConverter);
-		namedEntityDescriptionPropertyListConverter.setBaseDozerBeanMapper(baseDozerBeanMapper);
+		namedEntityDescriptionPropertyListConverter
+				.setBaseDozerBeanMapper(baseDozerBeanMapper);
 		namedEntityDescriptionPropertyListConverter.setCodingSchemeService(css);
-		namedEntityDescriptionDesignationListConverter.setBaseDozerBeanMapper(baseDozerBeanMapper);
-		namedEntityDescriptionDesignationListConverter.setCodingSchemeService(css);
-		namedEntityDescriptionDefinitionListConverter.setBaseDozerBeanMapper(baseDozerBeanMapper);
-		namedEntityDescriptionDefinitionListConverter.setCodingSchemeService(css);
-		
+		namedEntityDescriptionDesignationListConverter
+				.setBaseDozerBeanMapper(baseDozerBeanMapper);
+		namedEntityDescriptionDesignationListConverter
+				.setCodingSchemeService(css);
+		namedEntityDescriptionDefinitionListConverter
+				.setBaseDozerBeanMapper(baseDozerBeanMapper);
+		namedEntityDescriptionDefinitionListConverter
+				.setCodingSchemeService(css);
+		namedEntityDescriptionNoteListConverter
+				.setBaseDozerBeanMapper(baseDozerBeanMapper);
+		namedEntityDescriptionNoteListConverter.setCodingSchemeService(css);
+
 		mapped = baseDozerBeanMapper.map(ref, NamedEntityDescription.class);
-//		mapped.setAbout(about)
-//		mapped.setDefinition(vDefinitionArray)
-//		mapped.setDesignation(vDesignationArray)
-//		mapped.setEntityType(vEntityTypeArray)
-//		mapped.setNote(vNoteArray)
-//		mapped.setEntryState(entryState)			?
-//		mapped.setProperty(vPropertyArray)
-//		mapped.setEquivalentEntities(vEquivalentEntitiesArray)
-//		mapped.setDescribingCodeSystemVersion(describingCodeSystemVersion)
-//		mapped.setInstances(instances)
-		
+		// mapped.setAbout(about)
+		// mapped.setDefinition(vDefinitionArray)
+		// mapped.setDesignation(vDesignationArray)
+		// mapped.setEntityType(vEntityTypeArray)
+		// mapped.setNote(vNoteArray)
+		// mapped.setEntryState(entryState) ?
+		// mapped.setProperty(vPropertyArray)
+		// mapped.setEquivalentEntities(vEquivalentEntitiesArray)
+		// mapped.setDescribingCodeSystemVersion(describingCodeSystemVersion)
+		// mapped.setInstances(instances)
+
 	}
 
 	@Test
@@ -160,82 +181,109 @@ public class ResolvedConceptReferenceToNamedEntityDescriptionTest extends
 		assertEquals("codingSchemeName", mapped
 				.getDescribingCodeSystemVersion().getContent());
 	}
-	
+
 	@Test
 	public void testGetAbout() {
 		assertEquals("testUri:testCode", mapped.getAbout());
 	}
-	
+
 	@Test
 	public void testGetPresentationCount() {
 		assertEquals(2, mapped.getDesignationCount());
 	}
-	
+
 	@Test
 	public void testGetPresentation() {
-		assertEquals("testFed", mapped.getDesignation(0).getDegreeOfFidelity().getContent());
+		assertEquals("testFed", mapped.getDesignation(0).getDegreeOfFidelity()
+				.getContent());
 		assertEquals("en", mapped.getDesignation(0).getLanguage().getContent());
-		assertEquals("testPropertyID", mapped.getDesignation(0).getExternalIdentifier());
+		assertEquals("testPropertyID", mapped.getDesignation(0)
+				.getExternalIdentifier());
 		assertEquals("testValue", mapped.getDesignation(0).getValue());
-		assertEquals("test usage context 1", mapped.getDesignation(0).getUsageContext(0).getContent());
-		assertEquals("test usage context 2", mapped.getDesignation(0).getUsageContext(1).getContent());
-		assertEquals("test usage context 3", mapped.getDesignation(0).getUsageContext(2).getContent());
-		assertEquals(DesignationRole.ALTERNATIVE, mapped.getDesignation(0).getDesignationRole());
-		assertEquals("en uri", mapped.getDesignation(0).getLanguage().getMeaning());
-		assertEquals("testFed uri", mapped.getDesignation(0).getDegreeOfFidelity().getMeaning());
-		assertEquals("string uri", mapped.getDesignation(0).getFormat().getMeaning());
-		assertEquals("test usage context 1 uri", mapped.getDesignation(0).getUsageContext(0).getMeaning());
-		assertEquals("test usage context 2 uri", mapped.getDesignation(0).getUsageContext(1).getMeaning());
-		assertEquals("test usage context 3 uri", mapped.getDesignation(0).getUsageContext(2).getMeaning());
+		assertEquals("test usage context 1", mapped.getDesignation(0)
+				.getUsageContext(0).getContent());
+		assertEquals("test usage context 2", mapped.getDesignation(0)
+				.getUsageContext(1).getContent());
+		assertEquals("test usage context 3", mapped.getDesignation(0)
+				.getUsageContext(2).getContent());
+		assertEquals(DesignationRole.ALTERNATIVE, mapped.getDesignation(0)
+				.getDesignationRole());
+		assertEquals("en uri", mapped.getDesignation(0).getLanguage()
+				.getMeaning());
+		assertEquals("testFed uri", mapped.getDesignation(0)
+				.getDegreeOfFidelity().getMeaning());
+		assertEquals("string uri", mapped.getDesignation(0).getFormat()
+				.getMeaning());
+		assertEquals("test usage context 1 uri", mapped.getDesignation(0)
+				.getUsageContext(0).getMeaning());
+		assertEquals("test usage context 2 uri", mapped.getDesignation(0)
+				.getUsageContext(1).getMeaning());
+		assertEquals("test usage context 3 uri", mapped.getDesignation(0)
+				.getUsageContext(2).getMeaning());
 	}
-	
+
 	@Test
 	public void testGetPropertyCount() {
 		assertEquals(2, mapped.getPropertyCount());
 	}
-	
+
 	@Test
 	public void testGetProperty() {
-		assertEquals("propertyId", mapped.getProperty(0).getExternalIdentifier());
+		assertEquals("propertyId", mapped.getProperty(0)
+				.getExternalIdentifier());
 		assertEquals("content", mapped.getProperty(0).getValue().getValue());
-		assertEquals("string", mapped.getProperty(0).getValue().getFormat().getContent());
-		assertEquals("string uri", mapped.getProperty(0).getValue().getFormat().getMeaning());
-		assertEquals("test lang", mapped.getProperty(0).getValue().getLanguage().getContent());
-		assertEquals("test lang uri", mapped.getProperty(0).getValue().getLanguage().getMeaning());
+		assertEquals("string", mapped.getProperty(0).getValue().getFormat()
+				.getContent());
+		assertEquals("string uri", mapped.getProperty(0).getValue().getFormat()
+				.getMeaning());
+		assertEquals("test lang", mapped.getProperty(0).getValue()
+				.getLanguage().getContent());
+		assertEquals("test lang uri", mapped.getProperty(0).getValue()
+				.getLanguage().getMeaning());
 	}
 
 	@Test
 	public void testGetCommentCount() {
 		assertEquals(2, mapped.getNoteCount());
 	}
-	
+
 	@Test
-	public void testGetComment(){
+	public void testGetComment() {
 		assertEquals("content", mapped.getNote(0).getValue());
-		assertEquals("testLanguage", mapped.getNote(0).getLanguage().getContent());
+		assertEquals("test lang", mapped.getNote(0).getLanguage()
+				.getContent());
 		assertEquals("string", mapped.getNote(0).getFormat().getContent());
 		assertEquals("propertyId", mapped.getNote(0).getExternalIdentifier());
+		assertEquals("string uri", mapped.getNote(0).getFormat().getMeaning());
+		assertEquals("test lang uri", mapped.getNote(0).getLanguage().getMeaning());
 	}
-	
+
 	@Test
-	public void testGetDefinitionCount(){
+	public void testGetDefinitionCount() {
 		assertEquals(2, mapped.getDefinitionCount());
 	}
-	
+
 	@Test
-	public void testGetDefinition(){
-		assertEquals("test propertyid", mapped.getDefinition(0).getExternalIdentifier());
-		assertEquals("test lang", mapped.getDefinition(0).getLanguage().getContent());
+	public void testGetDefinition() {
+		assertEquals("test propertyid", mapped.getDefinition(0)
+				.getExternalIdentifier());
+		assertEquals("test lang", mapped.getDefinition(0).getLanguage()
+				.getContent());
 		assertEquals("content", mapped.getDefinition(0).getValue());
 		assertEquals("string", mapped.getDefinition(0).getFormat().getContent());
-		assertEquals(DefinitionRole.INFORMATIVE, mapped.getDefinition(0).getDefinitionRole());
-		assertEquals("test usage context 1", mapped.getDefinition(0).getUsageContext().getContent());
-		assertEquals("test lang uri", mapped.getDefinition(0).getLanguage().getMeaning());
-		assertEquals("string uri", mapped.getDefinition(0).getFormat().getMeaning());
-		assertEquals("test usage context 1 uri", mapped.getDefinition(0).getUsageContext().getMeaning());
-		
+		assertEquals(DefinitionRole.INFORMATIVE, mapped.getDefinition(0)
+				.getDefinitionRole());
+		assertEquals("test usage context 1", mapped.getDefinition(0)
+				.getUsageContext().getContent());
+		assertEquals("test lang uri", mapped.getDefinition(0).getLanguage()
+				.getMeaning());
+		assertEquals("string uri", mapped.getDefinition(0).getFormat()
+				.getMeaning());
+		assertEquals("test usage context 1 uri", mapped.getDefinition(0)
+				.getUsageContext().getMeaning());
+
 	}
-	
+
 	private void initProperties() {
 		org.LexGrid.commonTypes.Property prop1 = new org.LexGrid.commonTypes.Property();
 		prop1.setPropertyId("propertyId");
@@ -246,27 +294,27 @@ public class ResolvedConceptReferenceToNamedEntityDescriptionTest extends
 		prop1.setLanguage("test lang");
 		org.LexGrid.commonTypes.Property prop2 = new org.LexGrid.commonTypes.Property();
 		prop2.setPropertyId("prop2");
-		
+
 		ref.getEntity().addProperty(prop1);
 		ref.getEntity().addProperty(prop2);
 	}
-	
+
 	private void initComments() {
 		org.LexGrid.concepts.Comment com1 = new org.LexGrid.concepts.Comment();
-		com1.setLanguage("testLanguage");
+		com1.setLanguage("test lang");
 		Text t = new Text();
 		t.setContent("content");
 		t.setDataType("string");
 		com1.setValue(t);
 		com1.setPropertyId("propertyId");
 		org.LexGrid.concepts.Comment com2 = new org.LexGrid.concepts.Comment();
-		com2.setLanguage("testLanguage2");
+		com2.setLanguage("test lang2");
 		com2.setPropertyId("propertyId2");
-		
+
 		ref.getEntity().addComment(com1);
 		ref.getEntity().addComment(com2);
 	}
-	
+
 	private void initDefinitions() {
 		org.LexGrid.concepts.Definition def1 = new org.LexGrid.concepts.Definition();
 		def1.setPropertyId("test propertyid");
@@ -279,11 +327,11 @@ public class ResolvedConceptReferenceToNamedEntityDescriptionTest extends
 		def1.addUsageContext("test usage context 2");
 		org.LexGrid.concepts.Definition def2 = new org.LexGrid.concepts.Definition();
 		def2.setPropertyId("test propertyid 2");
-		
+
 		ref.getEntity().addDefinition(def1);
 		ref.getEntity().addDefinition(def2);
 	}
-	
+
 	private void initPresentations() {
 		org.LexGrid.concepts.Presentation presentation1 = new org.LexGrid.concepts.Presentation();
 		presentation1.setDegreeOfFidelity("testFed");
@@ -303,7 +351,7 @@ public class ResolvedConceptReferenceToNamedEntityDescriptionTest extends
 		Text t2 = new Text();
 		t2.setContent("2 content");
 		t2.setDataType("string");
-		
+
 		ref.getEntity().addPresentation(presentation1);
 		ref.getEntity().addPresentation(presentation2);
 	}
