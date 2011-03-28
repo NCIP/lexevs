6b976a2b238326d120952aad7b6f005b99b3292f
bauerhs <bauerhs@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-28 18:15:50 +0000

    Cleanup and copyright added
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9016 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/main/java/org/cts2/internal/model/directory/ResolvedConceptReferencesIteratorBackedAssociationList.java
+++ cts2Impl/src/main/java/org/cts2/internal/model/directory/ResolvedConceptReferencesIteratorBackedAssociationList.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright: (c) 2004-2011 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
 package org.cts2.internal.model.directory;
 
 import java.util.Iterator;
@@ -41,23 +59,23 @@ public class ResolvedConceptReferencesIteratorBackedAssociationList extends
 	public ResolvedConceptReferencesIteratorBackedAssociationList(
 			CodedNodeGraph codedNodeGraph, BeanMapper beanMapper)
 			throws LBException {
-		this(codedNodeGraph.resolveAsList(null, true, false, -1, -1, null,
+		this(codedNodeGraph.resolveAsList(null, true, false, 1000, -1, null,
 				null, null, null, -1), beanMapper);
 	}
 
 	/**
-	 * Instantiates a new resolved concept references iterator backed
+	 * Instantiates a new resolved concept references list backed
 	 * Association directory.
 	 * 
-	 * @param iterator
+	 * @param list
 	 *            the iterator
 	 * @param beanMapper
 	 *            the bean mapper
 	 */
 	public ResolvedConceptReferencesIteratorBackedAssociationList(
-			ResolvedConceptReferenceList iterator, BeanMapper beanMapper) {
+			ResolvedConceptReferenceList list, BeanMapper beanMapper) {
 		try {
-			this.cache = this.buildCacheList(iterator, beanMapper);
+			this.cache = this.buildCacheList(list, beanMapper);
 		} catch (LBException e) {
 			throw new RuntimeException(e);
 		}
@@ -72,10 +90,11 @@ public class ResolvedConceptReferencesIteratorBackedAssociationList extends
 		return new PagingList<AssociationListEntry>(
 				this.buildDirectoryEntryIterator(iterator, beanMapper),
 				list.getResolvedConceptReferenceCount());
+		
 	}
 
 	/**
-	 * Builds the directory entry iterator.
+	 * Builds the List entry iterator.
 	 * 
 	 * @param iterator
 	 *            the iterator
