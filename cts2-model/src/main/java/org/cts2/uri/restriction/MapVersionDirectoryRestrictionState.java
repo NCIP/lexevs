99a772a889ea2523c9a80debdb42a48390101cac
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-28 18:28:21 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9017 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ cts2-model/src/main/java/org/cts2/uri/restriction/MapVersionDirectoryRestrictionState.java
@@ -0,0 +1,15 @@
+package org.cts2.uri.restriction;
+
+import org.cts2.uri.MapVersionDirectoryURI;
+
+/**
+ * The Class MapVersionDirectoryRestrictionState
+ * 
+ * @author <a href="mailto:lian.zonghui@mayo.edu">Zonghui Lian</a>
+ * 
+ */
+public class MapVersionDirectoryRestrictionState extends
+		RestrictionState<MapVersionDirectoryURI> {
+
+	//TODO
+}
