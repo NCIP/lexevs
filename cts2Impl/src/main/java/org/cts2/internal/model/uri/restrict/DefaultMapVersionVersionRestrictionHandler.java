395de3e09ed7091ecda21b4531e2be4e18733e14
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-31 19:22:43 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9073 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/main/java/org/cts2/internal/model/uri/restrict/DefaultMapVersionVersionRestrictionHandler.java
+++ cts2Impl/src/main/java/org/cts2/internal/model/uri/restrict/DefaultMapVersionVersionRestrictionHandler.java
@@ -3,22 +3,15 @@ package org.cts2.internal.model.uri.restrict;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.LexGrid.LexBIG.DataModel.Collections.CodingSchemeRenderingList;
 import org.LexGrid.LexBIG.DataModel.InterfaceElements.CodingSchemeRendering;
-import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
-import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.cts2.core.MatchAlgorithmReference;
-import org.cts2.internal.match.OperationExecutingModelAttributeReference;
 import org.cts2.internal.match.ResolvableModelAttributeReference;
-import org.cts2.uri.CodeSystemVersionDirectoryURI;
 import org.cts2.uri.MapVersionDirectoryURI;
-import org.cts2.uri.restriction.CodeSystemVersionRestrictionState.RestrictToEntitiesRestriction;
 
 public class DefaultMapVersionVersionRestrictionHandler
 		extends
 		AbstractIterableLexEvsBackedRestrictionHandler<CodingSchemeRendering, MapVersionDirectoryURI> {
 
-	private LexBIGService lexBigService;
 
 	@Override
 	public List<ResolvableModelAttributeReference<CodingSchemeRendering>> registerSupportedModelAttributeReferences() {
