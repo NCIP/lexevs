8b458e20d2601209eeec66ab6a32f0b91f4904e2
LianZ <LianZ@c4b421de-a31f-834e-8902-8020e42e311c>
2011-03-31 19:02:40 +0000

    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9069 c4b421de-a31f-834e-8902-8020e42e311c

--- cts2Impl/src/main/java/org/cts2/internal/model/uri/DefaultMapVersionDirectoryURI.java
+++ cts2Impl/src/main/java/org/cts2/internal/model/uri/DefaultMapVersionDirectoryURI.java
@@ -1,152 +1,105 @@
 package org.cts2.internal.model.uri;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.LexGrid.LexBIG.DataModel.Collections.CodingSchemeRenderingList;
 import org.LexGrid.LexBIG.DataModel.InterfaceElements.CodingSchemeRendering;
-import org.LexGrid.LexBIG.Exceptions.LBException;
-import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
-import org.LexGrid.LexBIG.Exceptions.LBParameterException;
-import org.LexGrid.LexBIG.Extensions.Generic.MappingExtension;
-import org.LexGrid.LexBIG.Extensions.Generic.MappingExtension.Mapping;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
-import org.LexGrid.LexBIG.Utility.Constructors;
 import org.cts2.core.types.SetOperator;
 import org.cts2.internal.mapper.BeanMapper;
-import org.cts2.internal.model.directory.ResolvedConceptReferencesIteratorBackedMapVersionDirectory;
-import org.cts2.internal.model.directory.ResolvedConceptReferencesIteratorBackedMapVersionListDirectory;
-import org.cts2.internal.model.uri.restrict.NonIterableBasedResolvingRestrictionHandler;
-import org.cts2.internal.profile.ProfileUtils;
-import org.cts2.map.MapVersion;
-import org.cts2.map.MapVersionDirectory;
-import org.cts2.map.MapVersionList;
-import org.cts2.service.core.ReadContext;
-import org.cts2.uri.DirectoryURI;
-import org.cts2.uri.EntityDirectoryURI;
+import org.cts2.internal.model.uri.restrict.IterableBasedResolvingRestrictionHandler;
+import org.cts2.service.core.NameOrURIList;
 import org.cts2.uri.MapVersionDirectoryURI;
 import org.cts2.uri.restriction.MapVersionDirectoryRestrictionState;
-import org.cts2.uri.restriction.RestrictionState;
 import org.cts2.uri.restriction.SetComposite;
 
+import com.google.common.collect.Iterables;
+
+import scala.actors.threadpool.Arrays;
+
+/**
+ * 
+ * @author <a href="mailto:lian.zonghui@mayo.edu">Zonghui Lian</a>
+ *
+ */
 public class DefaultMapVersionDirectoryURI
 		extends
-		AbstractNonIterableLexEvsBackedResolvingDirectoryURI<CodingSchemeRenderingList, MapVersionDirectoryURI>
+		AbstractIterableLexEvsBackedResolvingDirectoryURI<CodingSchemeRendering, MapVersionDirectoryURI>
 		implements MapVersionDirectoryURI {
 
-	/** The coded node set. */
-	// private CodedNodeSet codedNodeSet;
-
-	/** The bean mapper. */
 	private BeanMapper beanMapper;
-
 	private LexBIGService lexBigService;
+	private CodingSchemeRenderingList codingSchemeRenderingList;
+	private MapVersionDirectoryRestrictionState restrictionState = new MapVersionDirectoryRestrictionState(); //TODO double check MapVersionDirectoryRestrictionState
 
-	private MapVersionDirectoryRestrictionState mapVersionDirectoryRestrictionState = new MapVersionDirectoryRestrictionState();
-
+	/**
+	 * Instantiates a new default map version directory uri.
+	 * @param lexBigService
+	 * @param codingSchemeRenderingList
+	 * @param restrictionHandler
+	 * @param beanMapper
+	 */
 	public DefaultMapVersionDirectoryURI(
 			LexBIGService lexBigService,
-			NonIterableBasedResolvingRestrictionHandler<CodingSchemeRenderingList, MapVersionDirectoryURI> restrictionHandler,
-			BeanMapper beanMapper) {
+			CodingSchemeRenderingList codingSchemeRenderingList,
+			IterableBasedResolvingRestrictionHandler<CodingSchemeRendering, MapVersionDirectoryURI> restrictionHandler,
+			BeanMapper beanMapper
+			) {
 		super(restrictionHandler);
 		this.lexBigService = lexBigService;
 		this.beanMapper = beanMapper;
+		this.codingSchemeRenderingList = codingSchemeRenderingList;
 	}
 
 	@Override
 	public MapVersionDirectoryRestrictionState getRestrictionState() {
-		return this.mapVersionDirectoryRestrictionState;
+		return this.restrictionState;
 	}
 
 	@Override
-	protected CodingSchemeRenderingList getOriginalState() {
-		CodingSchemeRenderingList codingSchemeRenderingList = new CodingSchemeRenderingList();
-		try {
-			MappingExtension mappingExtension = (MappingExtension) this.lexBigService
-					.getGenericExtension("MappingExtension");
-			for (CodingSchemeRendering csr : this.lexBigService
-					.getSupportedCodingSchemes().getCodingSchemeRendering()) {
-				if (mappingExtension.isMappingCodingScheme(csr
-						.getCodingSchemeSummary().getCodingSchemeURI(),
-						Constructors
-								.createCodingSchemeVersionOrTagFromVersion(csr
-										.getCodingSchemeSummary()
-										.getRepresentsVersion()))) {
-					codingSchemeRenderingList.addCodingSchemeRendering(csr);
-				}
-			}
-		} catch (LBInvocationException e) {
-			throw new RuntimeException(e);
-		} catch (LBParameterException e) {
-			throw new RuntimeException(e);
-		} catch (IndexOutOfBoundsException e) {
-			throw new RuntimeException(e);
-		} catch (LBException e) {
-			throw new RuntimeException(e);
-		}
-
-		return codingSchemeRenderingList;
-	}
-
-	private List<Mapping> getMappingList(
-			CodingSchemeRenderingList codingSchemeRenderingList)
-			throws LBException {
-		List<Mapping> list = new ArrayList<Mapping>();
-		MappingExtension mappingExtension = (MappingExtension) this.lexBigService
-				.getGenericExtension("MappingExtension");
-		for (CodingSchemeRendering csr : codingSchemeRenderingList
-				.getCodingSchemeRendering()) {
-			list.add(mappingExtension.getMapping(csr.getCodingSchemeSummary()
-					.getCodingSchemeURI(), Constructors
-					.createCodingSchemeVersionOrTagFromVersion(csr
-							.getCodingSchemeSummary().getRepresentsVersion()),
-					null));
-		}
-		return list;
+	public MapVersionDirectoryURI restrictToCodeSystems(
+			MapVersionDirectoryURI directory, NameOrURIList codeSystems) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected <O> O transform(CodingSchemeRenderingList lexevsObject,
+	protected Iterable<CodingSchemeRendering> getOriginalState() {
+		return Arrays.asList(this.codingSchemeRenderingList.getCodingSchemeRendering());
+	}
+
+	@Override
+	protected <O> O transform(Iterable<CodingSchemeRendering> lexevsObject,
 			Class<O> clazz) {
-		try {
-			List<Mapping> list = this.getMappingList(lexevsObject);
-			if (clazz.equals(MapVersionDirectory.class)) {
-				return (O) new ResolvedConceptReferencesIteratorBackedMapVersionDirectory(
-						list, this.beanMapper);
-			}
-			if (clazz.equals(MapVersionList.class)) {
-				return (O) new ResolvedConceptReferencesIteratorBackedMapVersionListDirectory(
-						list, this.beanMapper);
-			}
-		} catch (LBException e) {
-			// TODO: real cts2 exception here
-			throw new IllegalStateException();
-		}
-
-		// TODO: real cts2 exception here
-		throw new IllegalStateException();
-	}
-
-	@Override
-	protected MapVersionDirectoryURI clone() {
-		return this;
-	}
-
-	@Override
-	protected int doCount(ReadContext readContext) {
-		// TODO Auto-generated method stub
-		return 0;
+		CodingSchemeRenderingList csrl = new CodingSchemeRenderingList();
+		csrl.setCodingSchemeRendering(Iterables.toArray(lexevsObject,CodingSchemeRendering.class));
+		
+		return this.beanMapper.map(csrl, clazz);
 	}
 
 	@Override
 	protected MapVersionDirectoryURI createSetOperatedDirectoryURI(
 			SetOperator setOperator, MapVersionDirectoryURI directoryUri1,
 			MapVersionDirectoryURI directoryUri2) {
-		// TODO Auto-generated method stub
-		return null;
+		DefaultMapVersionDirectoryURI newUri = 
+			new DefaultMapVersionDirectoryURI(
+					this.lexBigService, 
+					this.codingSchemeRenderingList, 
+					this.getRestrictionHandler(), 
+					this.beanMapper);
+		
+		newUri.getRestrictionState().setSetComposite(new SetComposite<MapVersionDirectoryURI>());
+		newUri.getRestrictionState().getSetComposite().setDirectoryUri1(directoryUri1);
+		newUri.getRestrictionState().getSetComposite().setDirectoryUri2(directoryUri2);
+		
+		newUri.getRestrictionState().getSetComposite().setSetOperator(setOperator);
+		
+		return newUri;
+	}
 
-
+	@Override
+	protected MapVersionDirectoryURI clone() {
+		// TODO 
+		return this;
 	}
 
 }
