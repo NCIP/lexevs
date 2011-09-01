4a3deda906a1f6ed18470d40237397f864809bb9
dsuesse <dsuesse@c4b421de-a31f-834e-8902-8020e42e311c>
2011-09-01 20:02:26 +0000

    Adds unit test for Jira issue 456: UMLS Loader - Property readers are not configured to skip entires with undesired SAB's
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9234 c4b421de-a31f-834e-8902-8020e42e311c

--- umls-loader/src/test/java/org/lexgrid/loader/umls/UmlsBatchLoaderTest.java
+++ umls-loader/src/test/java/org/lexgrid/loader/umls/UmlsBatchLoaderTest.java
@@ -25,6 +25,7 @@ import java.io.File;
 import org.LexGrid.LexBIG.Extensions.Load.UmlsBatchLoader;
 import org.junit.Before;
 import org.junit.Test;
+import org.lexevs.locator.LexEvsServiceLocator;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobExecution;
 
@@ -57,9 +58,19 @@ public class UmlsBatchLoaderTest extends BaseTestRrf {
 		JobExecution job = loader.getJobExecution();
 		assertTrue(job.getExitStatus().equals(ExitStatus.COMPLETED));	
 		removeAIRScheme();
-	}	
-
-
+	}
+	
+	@Test
+	public void testUmlsMultipleSABsLoad() throws Exception {
+		UmlsBatchLoader loader = new UmlsBatchLoaderImpl();
+		loader.getOptions().getStringOption(UmlsBatchLoaderImpl.SAB_OPTION).setOptionValue("RCD");
+		loader.getOptions().getBooleanOption(UmlsBatchLoaderImpl.ASYNC_OPTION).setOptionValue(false);
+		loader.load(new File(RRF_DIRECTORY).toURI());
+		JobExecution job = loader.getJobExecution();
+		assertTrue(job.getExitStatus().equals(ExitStatus.COMPLETED));
+		removeRCDScheme();
+	}
+	
 	/**
 	 * Test single db failed umls load.
 	 * 
@@ -102,5 +113,10 @@ public class UmlsBatchLoaderTest extends BaseTestRrf {
 		assertTrue(restartJob.getExitStatus().equals(ExitStatus.COMPLETED));
 
 		removeAIRScheme();
+	}
+	
+	public void removeRCDScheme() throws Exception {	
+		LexEvsServiceLocator.getInstance().getSystemResourceService().
+			removeCodingSchemeResourceFromSystem("urn:oid:2.16.840.1.113883.6.6", "1999");
 	}	
 }
