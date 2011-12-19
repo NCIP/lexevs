4ffc40dd16fb6b14d9d83b43c8a939522297669f
hsbauer <hsbauer@c4b421de-a31f-834e-8902-8020e42e311c>
2011-12-19 17:48:20 +0000

    more scripting updates
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9252 c4b421de-a31f-834e-8902-8020e42e311c

--- lbAdmin/scripts/ExportLgXML.bat
+++ lbAdmin/scripts/ExportLgXML.bat
@@ -1,22 +1,21 @@
 @echo off
-REM Exports content from the repository to a file in the LexGrid canonical
-REM XML format.
+REM ExportLgXML
+REM	Exports content from the repository to a file in the LexGrid canonical XML format.
 REM
-REM Options:
-REM   -out,--output <uri> URI or path of the directory to contain the
-REM        resulting XML file.  The file name will be automatically derived
-REM        from the coding scheme name.   
-REM   -u,--urn <name> URN or local name of the coding scheme to export.
-REM   -v,--version <id> The assigned tag/label or absolute version
-REM        identifier of the coding scheme.
-REM   -f,--force If specified, allows the destination file to be
-REM        overwritten if present.
-REM 
+REM    usage: ExportLgXML [-xc] [-an null] [-xall] -v null -u name -out uri [-xa] [-f] 
+REM    -an,--associationsName Export associations with this name. Only valid with export type 'xa' 
+REM    -f,--force If specified, allows the destination file to be overwritten if present. 
+REM   -out,--output <uri> URI or path of the directory to contain the resulting XML file. The file name will be automatically derived from the coding scheme name. 
+REM    -u,--urn <name> URN or local name of the coding scheme to export. 
+REM    -v,--version The assigned tag/label or absolute version identifier of the coding scheme. 
+REM    -xa,--exportAssociationsType of export: export only associations. 
+REM    -xall,--exportAll Type of export: export all content. Default behavior. 
+REM   -xc,--exportConcepts Type of export: export only concepts. 
+REM
+REM Example: ExportLgXML -out "file:///path/to/dir" -u "NCI Thesaurus" -v "05.06e" -f 
+REM
 REM Note: If the coding scheme and version values are unspecified,
 REM a list of available coding schemes will be presented for
 REM user selection.
-REM
-REM Example: ExportLgXML -out "file:///path/to/dir" -f
-REM Example: ExportLgXML -out "file:///path/to/dir" -u "NCI_Thesaurus" -v "PRODUCTION" -f
 REM
 java -Xmx1500m -cp "..\runtime\lbPatch.jar;..\runtime\lbRuntime.jar" org.LexGrid.LexBIG.admin.ExportLgXML %*
--- lbAdmin/scripts/ExportLgXML.sh
+++ lbAdmin/scripts/ExportLgXML.sh
@@ -1,16 +1,17 @@
-# Exports content from the repository to a file in the LexGrid canonical
-# XML format.
+# ExportLgXML
+#	Exports content from the repository to a file in the LexGrid canonical XML format.
 #
-# Options:
-#   -out,--output <uri> URI or path of the directory to contain the
-#        resulting XML file.  The file name will be automatically derived
-#        from the coding scheme name.   
-#   -u,--urn <name> URN or local name of the coding scheme to export.
-#   -v,--version <id> The assigned tag/label or absolute version
-#        identifier of the coding scheme.
-#   -f,--force If specified, allows the destination file to be
-#        overwritten if present.
-# 
+#    usage: ExportLgXML [-xc] [-an null] [-xall] -v null -u name -out uri [-xa] [-f] 
+#    -an,--associationsName Export associations with this name. Only valid with export type 'xa' 
+#    -f,--force If specified, allows the destination file to be overwritten if present. 
+#    -out,--output <uri> URI or path of the directory to contain the resulting XML file. The file name will be automatically derived from the coding scheme name. 
+#    -u,--urn <name> URN or local name of the coding scheme to export. 
+#    -v,--version The assigned tag/label or absolute version identifier of the coding scheme. 
+#    -xa,--exportAssociationsType of export: export only associations. 
+#    -xall,--exportAll Type of export: export all content. Default behavior. 
+#    -xc,--exportConcepts Type of export: export only concepts. 
+#
+# Example: ExportLgXML -out "file:///path/to/dir" -u "NCI Thesaurus" -v "05.06e" -f 
 # Note: If the coding scheme and version values are unspecified,
 # a list of available coding schemes will be presented for
 # user selection.
