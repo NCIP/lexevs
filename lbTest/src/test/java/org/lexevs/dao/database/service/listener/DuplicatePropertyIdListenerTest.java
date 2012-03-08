b0074853eda3fbcf417e0123f76658fd6d03e92a
sahana29 <sahana29@c4b421de-a31f-834e-8902-8020e42e311c>
2012-03-08 16:46:21 +0000

    LEXEVS-468 - PropertyId on Property is Required. Added a new Junit test case to validate the changes
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9277 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbTest/src/test/java/org/lexevs/dao/database/service/listener/DuplicatePropertyIdListenerTest.java
@@ -0,0 +1,28 @@
+package org.lexevs.dao.database.service.listener;
+
+import org.LexGrid.LexBIG.Impl.function.LexBIGServiceTestCase;
+import org.LexGrid.commonTypes.Property;
+import org.LexGrid.concepts.Entity;
+import org.junit.Test;
+
+public class DuplicatePropertyIdListenerTest extends LexBIGServiceTestCase {
+
+	@Test
+	public void testForNullPropertyId() {
+		DuplicatePropertyIdListener listener = new DuplicatePropertyIdListener();
+		
+		Entity entity = new Entity();
+		
+		Property prop = new Property();
+		
+		entity.addProperty(prop);
+
+		listener.doValidate("", "", entity);
+	}
+
+	@Override
+	protected String getTestID() {
+		return DuplicatePropertyIdListenerTest.class.getName();
+	}
+
+}
