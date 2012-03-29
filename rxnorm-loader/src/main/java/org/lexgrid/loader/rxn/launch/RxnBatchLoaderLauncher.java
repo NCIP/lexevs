ce941483544d4046ae0c25d3defc2378f58a9085
hsbauer <hsbauer@c4b421de-a31f-834e-8902-8020e42e311c>
2012-03-29 14:33:08 +0000

    Updates to integrate to LexEVS and to correct association not being processed into supported attributes.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9296 c4b421de-a31f-834e-8902-8020e42e311c

--- rxnorm-loader/src/main/java/org/lexgrid/loader/rxn/launch/RxnBatchLoaderLauncher.java
+++ rxnorm-loader/src/main/java/org/lexgrid/loader/rxn/launch/RxnBatchLoaderLauncher.java
@@ -21,17 +21,29 @@ package org.lexgrid.loader.rxn.launch;
 import java.io.File;
 import java.net.URI;
 
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.DataModel.Core.LogEntry;
+import org.LexGrid.LexBIG.DataModel.Core.types.LogLevel;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.LoadStatus;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.Extensions.Load.OntologyFormat;
+import org.LexGrid.LexBIG.Extensions.Load.RxNormBatchLoader;
+import org.LexGrid.LexBIG.Extensions.Load.options.OptionHolder;
 import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
+import org.LexGrid.LexBIG.Preferences.loader.LoadPreferences.LoaderPreferences;
+import org.LexGrid.LexOnt.CodingSchemeManifest;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 import org.lexgrid.loader.rxn.RxnBatchLoaderImpl;
+import org.springframework.batch.core.JobExecution;
 
 /**
  * The Class RxnBatchLoaderLauncher.
  * 
  * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
  */
-public class RxnBatchLoaderLauncher {
+public class RxnBatchLoaderLauncher{
 	
 	 /** The rrf dir. */
  	@Option(name="-in")   
@@ -70,8 +82,8 @@ public class RxnBatchLoaderLauncher {
 	 * @throws Exception the exception
 	 */
 	private void load() throws Exception {
-//		RxnBatchLoader loader = (RxnBatchLoader)LexBIGServiceImpl.defaultInstance().getServiceManager(null).getLoader("RxnBatchLoader");
-		RxnBatchLoaderImpl loader = new RxnBatchLoaderImpl();
+		RxnBatchLoaderImpl loader = (RxnBatchLoaderImpl)LexBIGServiceImpl.defaultInstance().getServiceManager(null).getLoader("RxNormBatchLoader");
+//		RxnBatchLoaderImpl loader = new RxnBatchLoaderImpl();
 				
 		if(uri == null && version == null){
 			loader.loadRxn(new File(rrfDir).toURI(), sab);
@@ -119,4 +131,110 @@ public class RxnBatchLoaderLauncher {
 	public void setSab(String sab) {
 		this.sab = sab;
 	}
+
+//	public JobExecution getJobExecution() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public void load(URI resource) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public OptionHolder getOptions() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public void clearLog() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public AbsoluteCodingSchemeVersionReference[] getCodingSchemeReferences() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public LogEntry[] getLog(LogLevel level) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public LoadStatus getStatus() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public void setCodingSchemeManifest(
+//			CodingSchemeManifest codingSchemeManifest) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public CodingSchemeManifest getCodingSchemeManifest() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public void setCodingSchemeManifestURI(URI codingSchemeManifestUri)
+//			throws LBException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public URI getCodingSchemeManifestURI() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public LoaderPreferences getLoaderPreferences() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public void setLoaderPreferences(LoaderPreferences loaderPreferences)
+//			throws LBParameterException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public void setLoaderPreferences(URI loaderPreferencesURI)
+//			throws LBParameterException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public OntologyFormat getOntologyFormat() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public String getName() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public String getDescription() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public String getProvider() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public String getVersion() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//
+//	public void removeLoad(String uri, String version)
+//			throws LBParameterException {
+//		// TODO Auto-generated method stub
+//		
+//	}
 }
--- rxnorm-loader/src/main/java/org/lexgrid/loader/rxn/processor/RxndocAssociationProcessor.java
+++ rxnorm-loader/src/main/java/org/lexgrid/loader/rxn/processor/RxndocAssociationProcessor.java
@@ -20,10 +20,19 @@ package org.lexgrid.loader.rxn.processor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 //import org.LexGrid.commonTypes.Association;
 //import org.LexGrid.persistence.model.AssociationId;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.codingSchemes.CodingScheme;
+import org.LexGrid.custom.concepts.EntityFactory;
+import org.LexGrid.relations.AssociationEntity;
+import org.LexGrid.relations.AssociationPredicate;
+import org.LexGrid.relations.Relations;
+import org.lexevs.locator.LexEvsServiceLocator;
 import org.lexgrid.loader.dao.template.SupportedAttributeTemplate;
 //import org.lexgrid.loader.processor.CodingSchemeNameAwareProcessor;
 import org.lexgrid.loader.processor.CodingSchemeIdAwareProcessor;
@@ -35,6 +44,7 @@ import org.lexgrid.loader.rrf.model.Mrhier;
 import org.lexgrid.loader.rrf.processor.AbstractMrhierProcessor;
 import org.lexgrid.loader.rrf.processor.support.HcdQualifierResolver;
 import org.lexgrid.loader.rrf.staging.MrconsoStagingDao;
+import org.lexgrid.loader.wrappers.CodingSchemeIdHolder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
@@ -44,16 +54,144 @@ import org.springframework.util.Assert;
  * 
  * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
  */
-public class RxndocAssociationProcessor extends CodingSchemeIdAwareProcessor implements InitializingBean, ItemProcessor<ArrayList,List<Object>> {
+public class RxndocAssociationProcessor extends CodingSchemeIdAwareProcessor implements ItemProcessor<List<Mrdoc>,List<CodingSchemeIdHolder<AssociationEntity>>> {
 
-	/** The mrconso staging dao. */
-	private MrconsoStagingDao mrconsoStagingDao;
-	
-	/** The qualifier resolver. */
-	private QualifierResolver<Mrhier> qualifierResolver = new HcdQualifierResolver();
 	
 	private SupportedAttributeTemplate supportedAttributeTemplate;
 	
+	
+	public List<CodingSchemeIdHolder<AssociationEntity>> process(List<Mrdoc> items) throws Exception {
+		List<CodingSchemeIdHolder<AssociationEntity>> returnList = new ArrayList<CodingSchemeIdHolder<AssociationEntity>>();
+	
+		String relationName = getRelationName(items);
+		String reverseName = getReverseName(items);
+		String expandedName = getExpandedName(items);
+		
+		CodingScheme cs = LexEvsServiceLocator.getInstance().
+			getDatabaseServiceManager().
+				getCodingSchemeService().
+					getCodingSchemeByUriAndVersion(
+							this.getCodingSchemeIdSetter().getCodingSchemeUri(),
+							this.getCodingSchemeIdSetter().getCodingSchemeVersion());
+		
+		Set<String> loadedAssociations = new HashSet<String>();
+		
+		for(Relations relations : cs.getRelations()) {
+			for(AssociationPredicate predicate : relations.getAssociationPredicate()) {
+				loadedAssociations.add(predicate.getAssociationName());
+			}
+		}
+		
+		if(loadedAssociations.contains(relationName)) {
+			supportedAttributeTemplate.addSupportedAssociation(
+					getCodingSchemeIdSetter().getCodingSchemeUri(), 
+					getCodingSchemeIdSetter().getCodingSchemeVersion(), 
+					relationName, 
+					relationName, 
+					expandedName);
+			returnList.add(
+					new CodingSchemeIdHolder<AssociationEntity>(
+							this.getCodingSchemeIdSetter(), 
+							buildAssociationEntity(relationName, reverseName, expandedName)));
+		}
+		
+		return returnList;
+	}
+	
+	/**
+	 * Builds the association.
+	 * 
+	 * @param containerName the container name
+	 * @param relationName the relation name
+	 * @param reverseName the reverse name
+	 * @param expandedName the expanded name
+	 * 
+	 * @return the association
+	 */
+	protected AssociationEntity buildAssociationEntity(String relationName, String reverseName, String expandedName){
+		AssociationEntity assoc = EntityFactory.createAssociation();
+		assoc.setForwardName(relationName);
+		assoc.setReverseName(reverseName);
+		assoc.setIsTransitive(isTransitive(relationName));
+		assoc.setIsNavigable(true);
+		assoc.setEntityDescription(Constructors.createEntityDescription(expandedName));
+		assoc.setEntityCode(relationName);
+		return assoc;
+	}
+	
+	/**
+	 * Checks if is transitive.
+	 * 
+	 * @param relationName the relation name
+	 * 
+	 * @return true, if is transitive
+	 */
+	protected boolean isTransitive(String relationName){
+		return RrfLoaderConstants.TRANSITIVE_ASSOCIATIONS.contains(relationName);
+	}
+	
+	/**
+	 * Gets the expanded name.
+	 * 
+	 * @param items the items
+	 * 
+	 * @return the expanded name
+	 */
+	private String getExpandedName(List<Mrdoc> items){
+		for(Mrdoc mrdoc : items){
+			if(mrdoc.getType().equals(RrfLoaderConstants.RELATION_EXPANDED_FORM)){
+				return mrdoc.getExpl();
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Gets the reverse name.
+	 * 
+	 * @param items the items
+	 * 
+	 * @return the reverse name
+	 */
+	private String getReverseName(List<Mrdoc> items){
+		for(Mrdoc mrdoc : items){
+			if(mrdoc.getType().endsWith(RrfLoaderConstants.RELATION_INVERSE)){
+				return mrdoc.getExpl();
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Gets the relation name.
+	 * 
+	 * @param items the items
+	 * 
+	 * @return the relation name
+	 */
+	private String getRelationName(List<Mrdoc> items){
+		Assert.notEmpty(items);
+		return items.get(0).getValue();
+	}
+
+	/**
+	 * Gets the supported attribute template.
+	 * 
+	 * @return the supported attribute template
+	 */
+	public SupportedAttributeTemplate getSupportedAttributeTemplate() {
+		return supportedAttributeTemplate;
+	}
+
+	/**
+	 * Sets the supported attribute template.
+	 * 
+	 * @param supportedAttributeTemplate the new supported attribute template
+	 */
+	public void setSupportedAttributeTemplate(
+			SupportedAttributeTemplate supportedAttributeTemplate) {
+		this.supportedAttributeTemplate = supportedAttributeTemplate;
+	}
 //	/** The rrf post processing dao. */
 //	private RrfPostProcessingDao rrfPostProcessingDao;
 //	
@@ -201,23 +339,16 @@ public class RxndocAssociationProcessor extends CodingSchemeIdAwareProcessor imp
 //			SupportedAttributeTemplate supportedAttributeTemplate) {
 //		this.supportedAttributeTemplate = supportedAttributeTemplate;
 //	}
-	protected List<String> getPathToRootAuis(String pathToRoot){	
-		String[] auis = pathToRoot.split("\\.");
-		return Arrays.asList(auis);
-	}
-
-	public List<Object> process(ArrayList list) throws Exception {return null;}
-	
-	public MrconsoStagingDao getMrconsoStagingDao() {
-		return mrconsoStagingDao;
-	}
-
-	public void setMrconsoStagingDao(MrconsoStagingDao mrconsoStagingDao) {
-		this.mrconsoStagingDao = mrconsoStagingDao;
-	}
-
-	public void afterPropertiesSet() throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+//	protected List<String> getPathToRootAuis(String pathToRoot){	
+//		String[] auis = pathToRoot.split("\\.");
+//		return Arrays.asList(auis);
+//	}
+//
+//	public List<Object> process(ArrayList list) throws Exception {return null;}
+//	
+//
+//	public void afterPropertiesSet() throws Exception {
+//		// TODO Auto-generated method stub
+//		
+//	}
 }
