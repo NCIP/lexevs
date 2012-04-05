3b1e5a63259c388e2376fb606a610de3f435cdd0
sdyadav <sdyadav@c4b421de-a31f-834e-8902-8020e42e311c>
2012-04-05 16:05:39 +0000

    NCI Tracker # 445 : Changed description to 'This loader loads exclusively formatted RxNorm RRF contents into the LexGrid database.'. To make it clear about the loader to the user.
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9311 c4b421de-a31f-834e-8902-8020e42e311c

--- lbInterfaces/src/org/LexGrid/LexBIG/Extensions/Load/RxNormBatchLoader.java
+++ lbInterfaces/src/org/LexGrid/LexBIG/Extensions/Load/RxNormBatchLoader.java
@@ -7,7 +7,7 @@ import org.LexGrid.LexBIG.Exceptions.LBParameterException;
 public interface RxNormBatchLoader extends SpringBatchLoader {
 	public String NAME = "RxNormBatchLoader";
 	public String VERSION = "1.1";
-	public String DESCRIPTION = "This loader loads proprietary RxNorm RRF contents into the LexGrid database";
+	public String DESCRIPTION = "This loader loads exclusively formatted RxNorm RRF contents into the LexGrid database.";
 	
 	/**
 	 * Load meta.
