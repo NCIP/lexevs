408aa350e140670b78e46ea90ceabfbb6fed614b
sahana29 <sahana29@c4b421de-a31f-834e-8902-8020e42e311c>
2013-01-16 13:43:25 +0000

    Added script to load Metathesaurus history in 6.0
    git-svn-id: https://lexevs.svn.sourceforge.net/svnroot/lexevs/base/v6/trunk/lexevs_api@9377 c4b421de-a31f-834e-8902-8020e42e311c

---
+++ lbAdmin/scripts/LoadUMLSHistory.bat
@@ -0,0 +1,17 @@
+# Loads UMLS History
+#
+# Options:
+#   -in,--input <uri> URI or path specifying location of the source file
+#   -v, --validate <int> Perform validation of the candidate
+#         resource without loading data.  If specified, the '-a' and '-t'
+#         options are ignored.  Supported levels of validation include:
+#         0 = Verify document is valid
+#   -a, --activate ActivateScheme on successful load; if unspecified the
+#         vocabulary is loaded but not activated.
+#   -r, --replace Replace exisiting file. 
+#   -t, --tag <id> An optional tag ID (e.g. 'PRODUCTION' or 'TEST') to assign.
+#
+# Example: LoadUMLSHistory -in "file:///path/to/META.folder" -r
+#          LoadUMLSHistory -in "file:///path/to/META.folder" -v 0
+#
+java -Xmx1000m -cp "../runtime/lbPatch.jar:../runtime/lbRuntime.jar" org.LexGrid.LexBIG.admin.LoadUMLSHistory $@
