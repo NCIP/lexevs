42ca33c8a891836e1c23b9cf971b9521926392ee
Linda Hardie <hardie.linda@mayo.edu>
2013-05-09 14:58:00 -0500

    refactoring medDRA loader code

--- lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/medDRA/MedDRA_ImportFiles.java
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/medDRA/MedDRA_ImportFiles.java
@@ -42,49 +42,17 @@ import au.com.bytecode.opencsv.bean.CsvToBean;
 public class MedDRA_ImportFiles {
 	
 	private static String medDRADataDirectory = "C:/Users/m113216/Downloads/MedDRA_16_0_English/MedAscii/";
-	private static String [] medDRADataFiles = {
-		"meddra_history_english.asc", 
-		"meddra_release.asc", 
-		"llt.asc", 
-		"pt.asc", 
-		"hlt.asc", 
-		"hlt_pt.asc", 
-		"hlgt.asc", 
-		"hlgt_hlt.asc",
-		"soc.asc", 
-		"soc_hlgt.asc", 
-		"mdhier.asc", 
-		"intl_ord.asc", 
-		"smq_list.asc", 
-		"smq_content.asc"};
-	
-	private static Class [] medDRAClasses = {
-		MedDRA_record_meddra_history.class, 
-		MedDRA_record_meddra_release.class, 
-		MedDRA_record_llt.class, 
-		MedDRA_record_pt.class,
-		MedDRA_record_hlt.class, 
-		MedDRA_record_hlt_pt.class, 
-		MedDRA_record_hlgt.class, 
-		MedDRA_record_hlgt_hlt.class,
-		MedDRA_record_soc.class,
-		MedDRA_record_soc_hlgt.class, 
-		MedDRA_record_mdhier.class,
-		MedDRA_record_intl_ord.class,
-		MedDRA_record_smq_list.class, 
-		MedDRA_record_smq_content.class};
-	
 
-	
 	public static void main(String [] args){
 	//	testAndPrintCSVFiles();
 		Database meddraDatabase = createDatabase();
 		print(meddraDatabase);
 	}
 
-	private static void print(Database database) {
-		for(int i=0; i < medDRAClasses.length; i++){
-			database.print(medDRAClasses[i]);
+	@SuppressWarnings("unchecked")
+    private static void print(Database database) {
+		for(int i=0; i < MedDRA2LGConstants.medDRAClasses.length; i++){
+			database.print(MedDRA2LGConstants.medDRAClasses[i]);
 		}
 		
 	}
@@ -95,8 +63,8 @@ public class MedDRA_ImportFiles {
 		String input;
 		
 		try {
-			for(int i=0; i < medDRADataFiles.length; i++){
-				input = medDRADataDirectory + medDRADataFiles[i];
+			for(int i=0; i < MedDRA2LGConstants.medDRADataFiles.length; i++){
+				input = medDRADataDirectory + MedDRA2LGConstants.medDRADataFiles[i];
 				reader = new CSVReader(new FileReader(input), '$');
 				
 				while ((nextLine = reader.readNext()) != null) {
@@ -112,23 +80,24 @@ public class MedDRA_ImportFiles {
 
 	}
 	
-	public static Database createDatabase(){
+	@SuppressWarnings("unchecked")
+    public static Database createDatabase(){
 		Database meddraDatabase = new Database();
 		String input;
 		
-		for(int i=0; i < medDRADataFiles.length; i++){
-			input = medDRADataDirectory + medDRADataFiles[i];
+		for(int i=0; i < MedDRA2LGConstants.medDRADataFiles.length; i++){
+			input = medDRADataDirectory + MedDRA2LGConstants.medDRADataFiles[i];
 			try {
 				CSVReader reader = new CSVReader(new FileReader(input), '$');
 				ColumnPositionMappingStrategy<DatabaseRecord> strat = new ColumnPositionMappingStrategy<DatabaseRecord>();
-				strat.setType(medDRAClasses[i]);
-				String[] columns = getFields(medDRAClasses[i]); 
+				strat.setType(MedDRA2LGConstants.medDRAClasses[i]);
+				String[] columns = getFields(MedDRA2LGConstants.medDRAClasses[i]); 
 			
 				strat.setColumnMapping(columns);
 	
 				CsvToBean<DatabaseRecord> csv = new CsvToBean<DatabaseRecord>();
 				List<DatabaseRecord> list = csv.parse(strat, reader);
-				meddraDatabase.add(medDRAClasses[i].getName(), list);
+				meddraDatabase.add(MedDRA2LGConstants.medDRAClasses[i].getName(), list);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
@@ -137,7 +106,8 @@ public class MedDRA_ImportFiles {
 		return meddraDatabase;
 	}
 
-	private static String[] getFields(Class class1) {
+	@SuppressWarnings("rawtypes")
+    private static String[] getFields(Class class1) {
 		Field [] fields = class1.getDeclaredFields();	
 		String [] fieldnames = new String[fields.length - 1];
 		
