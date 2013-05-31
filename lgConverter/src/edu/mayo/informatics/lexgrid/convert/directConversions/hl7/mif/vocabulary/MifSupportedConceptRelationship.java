2ff54c22612e2b3e02c827c72aa2fa74b6fa541b
Kim R Frutiger <frutiger.kim@mayo.edu>
2013-05-31 15:52:48 -0500

    MIF loader updates - work in progress

---
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/hl7/mif/vocabulary/MifSupportedConceptRelationship.java
@@ -0,0 +1,68 @@
+package edu.mayo.informatics.lexgrid.convert.directConversions.hl7.mif.vocabulary;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class MifSupportedConceptRelationship implements Serializable {
+
+    private String relationshipKind;
+    private String name;
+    private String inverseName;
+    private String reflexivity;
+    private String symmetry;
+    private String transitivity;
+    
+    public MifSupportedConceptRelationship() {
+        super();
+    }
+    
+    public String getRelationshipKind() {
+        return relationshipKind;
+    }
+    
+    public void setRelationshipKind(String relationshipKind) {
+        this.relationshipKind = relationshipKind;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInverseName() {
+        return inverseName;
+    }
+
+    public void setInverseName(String inverseName) {
+        this.inverseName = inverseName;
+    }
+
+    public String getReflexivity() {
+        return reflexivity;
+    }
+
+    public void setReflexivity(String reflexivity) {
+        this.reflexivity = reflexivity;
+    }
+
+    public String getSymmetry() {
+        return symmetry;
+    }
+
+    public void setSymmetry(String symmetry) {
+        this.symmetry = symmetry;
+    }
+
+    public String getTransitivity() {
+        return transitivity;
+    }
+
+    public void setTransitivity(String transitivity) {
+        this.transitivity = transitivity;
+    }
+    
+    
+}
--- lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/hl7/mif/vocabulary/MifVocabularyModel.java
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/hl7/mif/vocabulary/MifVocabularyModel.java
@@ -2,6 +2,7 @@ package edu.mayo.informatics.lexgrid.convert.directConversions.hl7.mif.vocabular
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,17 +18,32 @@ import java.util.List;
 public class MifVocabularyModel implements Serializable {
     
     // VocabularyModel object attributes:
+    private String xmlns; // xmlns attribute of the <vocabularyModel> element
     private String name;  // name attribute of the <vocabularyModel> element
     private String title; // title attribute of the <vocabularyModel> element
     private String schemaVersion;  // schemaVersion attribute of the <vocabularyModel> element
     private String combinedId;  // combinedId attribute of the <vocabularyModel>/<packageLocation> element
-    
+    private String defaultLanguage; // There is no corresponding element in XML file - parser defaults to "en" value
+
+    // Collection below is the complete list of SupportedConceptRelationships found in the HL7 MIF Vocab XML file
+    private HashMap<String, MifSupportedConceptRelationship> supportedConceptRelationshipsMap;
+    // Collection below is the complete list of unique SupportedConceptProperty items found in the HL7 MIF Vocab XML file
+    private HashMap<String, MifSupportedConceptProperty> supportedConceptPropertiesMap;
+
     // Collection of CodeSytem objects
     private List<MifCodeSystem> codeSystems;
     
     public MifVocabularyModel() {
         super();
         codeSystems = new ArrayList<MifCodeSystem>();
+    }
+
+    public String getXmlns() {
+        return xmlns;
+    }
+
+    public void setXmlns(String xmlns) {
+        this.xmlns = xmlns;
     }
 
     public String getName() {
@@ -62,6 +78,14 @@ public class MifVocabularyModel implements Serializable {
         this.combinedId = combinedId;
     }
 
+    public String getDefaultLanguage() {
+        return defaultLanguage;
+    }
+
+    public void setDefaultLanguage(String defaultLanguage) {
+        this.defaultLanguage = defaultLanguage;
+    }
+
     public List<MifCodeSystem> getCodeSystems() {
         return codeSystems;
     }
@@ -69,6 +93,23 @@ public class MifVocabularyModel implements Serializable {
     public void setCodeSystems(List<MifCodeSystem> codeSystems) {
         this.codeSystems = codeSystems;
     }
+
+    public HashMap<String, MifSupportedConceptRelationship> getSupportedConceptRelationshipsMap() {
+        return supportedConceptRelationshipsMap;
+    }
+
+    public void setSupportedConceptRelationshipsMap(
+            HashMap<String, MifSupportedConceptRelationship> supportedConceptRelationshipsMap) {
+        this.supportedConceptRelationshipsMap = supportedConceptRelationshipsMap;
+    }
+    public HashMap<String, MifSupportedConceptProperty> getSupportedConceptPropertiesMap() {
+        return supportedConceptPropertiesMap;
+    }
+
+    public void setSupportedConceptPropertiesMap(HashMap<String, MifSupportedConceptProperty> supportedConceptPropertiesMap) {
+        this.supportedConceptPropertiesMap = supportedConceptPropertiesMap;
+    }
+
    
     
 }
