ef12dfea3c53cb7009d81dd10f8bcfe9cf9b948f
Kevin Peterson <kevin.peterson@mayo.edu>
2013-06-12 15:11:45 -0500

    Lucene adjustments for SearchExtension (AND instead of OR, and default LexEVS Analyzer).

--- lbTest/src/test/java/org/LexGrid/LexBIG/Impl/Extensions/GenericExtensions/SearchExtensionImplTest.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/Extensions/GenericExtensions/SearchExtensionImplTest.java
@@ -261,14 +261,28 @@ public class SearchExtensionImplTest extends LexBIGServiceTestCase {
 		assertFalse(itr.hasNext());
 	}
 	
-	public void testSimpleSearchFuzzyAndNegation() throws LBException {
+	public void testSimpleSearchFuzzyAndNegationWithGrouping() throws LBException {
 		LexBIGService lbs = ServiceHolder.instance().getLexBIGService();
 		SearchExtension searchExtension = (SearchExtension) lbs.getGenericExtension("SearchExtension");
 	
-		ResolvedConceptReferencesIterator itr = searchExtension.search("cor~ -Trailer");
+		ResolvedConceptReferencesIterator itr = searchExtension.search("(kar~ -Trailer) AND Car");
 		assertTrue(itr.hasNext());
 		assertEquals("C0001", itr.next().getCode());
 		assertFalse(itr.hasNext());
+	}
+	
+	public void testGrouping() throws LBException {
+		LexBIGService lbs = ServiceHolder.instance().getLexBIGService();
+		SearchExtension searchExtension = (SearchExtension) lbs.getGenericExtension("SearchExtension");
+	
+		ResolvedConceptReferencesIterator itr = searchExtension.search("((car AND -Trailer) OR (General AND Motors))");
+		Set<String> codes = new HashSet<String>();
+		while(itr.hasNext()){
+			codes.add(itr.next().getCode());
+		}
+
+		assertTrue(codes.contains("C0001"));
+		assertTrue(codes.contains("GM"));
 	}
 
 }
