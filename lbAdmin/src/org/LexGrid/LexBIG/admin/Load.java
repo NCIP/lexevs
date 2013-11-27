fa711276ef3d1f17b14afa303b7bd58182ca20e0
Kevin Peterson <kevin.peterson@mayo.edu>
2013-11-27 11:03:48 -0600

    Added generic Load scripts.

---
+++ lbAdmin/src/org/LexGrid/LexBIG/admin/Load.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * 		http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
+package org.LexGrid.LexBIG.admin;
+
+import java.net.URI;
+
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.Exceptions.LBResourceUnavailableException;
+import org.LexGrid.LexBIG.Extensions.Load.Loader;
+import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
+import org.LexGrid.annotations.LgAdminFunction;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.lexevs.system.ResourceManager;
+
+import edu.mayo.informatics.resourcereader.core.StringUtils;
+
+@LgAdminFunction
+public class Load {
+
+    public static void main(String[] args) {
+        try {
+            new Load().run(args);
+        } catch (LBResourceUnavailableException e) {
+            Util.displayTaggedMessage(e.getMessage());
+        } catch (Exception e) {
+            Util.displayAndLogError("REQUEST FAILED !!!", e);
+        }
+    }
+
+    public Load() {
+        super();
+    }
+
+    /**
+     * Primary entry point for the program.
+     * 
+     * @throws Exception
+     */
+    public void run(String[] args) throws Exception {
+        synchronized (ResourceManager.instance()) {
+
+            // Parse the command line ...
+            CommandLine cl = null;
+            Options options = getCommandOptions();
+            try {
+                cl = new BasicParser().parse(options, args);
+            } catch (ParseException e) {
+                Util
+                        .displayCommandOptions(
+                                "Load",
+                                options,
+                                "\n Load -in \"file:///path/to/file.txt\" -a"
+                                        + "\n Load -in \"file:///path/to/file.txt\"  -mf \"file:///path/to/myCodingScheme-manifest.xml\" -a"
+                                        + Util.getURIHelp(), e);
+                return;
+            }
+
+            // Interpret provided values ...
+            boolean activate = cl.hasOption("a");
+            
+            String loaderExtension = cl.getOptionValue("l");
+            
+            String manUriStr = cl.getOptionValue("mf");
+            URI manifest = null;
+
+            if (!StringUtils.isNull(manUriStr))
+                manifest = Util.string2FileURI(manUriStr);
+
+            URI source = Util.string2FileURI(cl.getOptionValue("in"));
+ 
+            Util.displayTaggedMessage("LOADING FROM URI: " + source.toString());
+
+            // Find the registered extension handling this type of load ...
+            LexBIGService lbs = LexBIGServiceImpl.defaultInstance();
+            LexBIGServiceManager lbsm = lbs.getServiceManager(null);
+            Loader loader = lbsm.getLoader(loaderExtension);
+
+            loader.setCodingSchemeManifestURI(manifest);
+            loader.load(source);
+            Util.displayLoaderStatus(loader);
+
+            // If specified, set the associated tag on the newly loaded
+            // scheme(s) ...
+            if (cl.hasOption("t")) {
+                String tag = cl.getOptionValue("t");
+                AbsoluteCodingSchemeVersionReference[] refs = loader.getCodingSchemeReferences();
+                for (int i = 0; i < refs.length; i++) {
+                    AbsoluteCodingSchemeVersionReference ref = refs[i];
+                    lbsm.setVersionTag(ref, tag);
+                    Util.displayTaggedMessage("Tag assigned>> " + ref.getCodingSchemeURN() + " Version>> "
+                            + ref.getCodingSchemeVersion());
+                }
+            }
+
+            // If requested, activate the newly loaded scheme(s) ...
+            if (activate) {
+                AbsoluteCodingSchemeVersionReference[] refs = loader.getCodingSchemeReferences();
+                for (int i = 0; i < refs.length; i++) {
+                    AbsoluteCodingSchemeVersionReference ref = refs[i];
+                    lbsm.activateCodingSchemeVersion(ref);
+                    Util.displayTaggedMessage("Scheme activated>> " + ref.getCodingSchemeURN() + " Version>> "
+                            + ref.getCodingSchemeVersion());
+                }
+            }
+        }
+    }
+
+    /**
+     * Return supported command options.
+     * 
+     * @return org.apache.commons.cli.Options
+     */
+    private Options getCommandOptions() {
+        Options options = new Options();
+        Option o;
+        
+        o = new Option("l", "loader", true, "LexEVS Loader Extension to use.");
+        o.setArgName("name");
+        o.setRequired(true);
+        options.addOption(o);
+
+        o = new Option("in", "input", true, "URI or path specifying location of the source file.");
+        o.setArgName("uri");
+        o.setRequired(true);
+        options.addOption(o);
+
+        o = new Option("mf", "manifest", true, "URI or path specifying location of the manifest file.");
+        o.setArgName("uri");
+        o.setRequired(false);
+        options.addOption(o);
+
+        o = new Option("a", "activate", false, "ActivateScheme on successful load; if unspecified the "
+                + "vocabulary is loaded but not activated.");
+        o.setRequired(false);
+        options.addOption(o);
+
+        o = new Option("t", "tag", true, "An optional tag ID (e.g. 'PRODUCTION' or 'TEST') to assign.");
+        o.setArgName("id");
+        o.setRequired(false);
+        options.addOption(o);
+
+        return options;
+    }
+
+}
