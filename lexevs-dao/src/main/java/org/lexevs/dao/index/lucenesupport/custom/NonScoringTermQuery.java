f6e310f259cda2bdb85dcf81518f9387478da77a
Scott Bauer <bauer.scott@mayo.edu>
2015-07-16 15:06:12 -0500

    Updated and commented out sections of a term query override that probably won't be used in the new implemenation

--- lexevs-dao/src/main/java/org/lexevs/dao/index/lucenesupport/custom/NonScoringTermQuery.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/index/lucenesupport/custom/NonScoringTermQuery.java
@@ -18,18 +18,22 @@
  */
 package org.lexevs.dao.index.lucenesupport.custom;
 
+import java.io.IOException;
 import java.util.Set;
 
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.ComplexExplanation;
+//import org.apache.lucene.search.ComplexExplanation;
 import org.apache.lucene.search.Explanation;
+import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.Scorer;
-import org.apache.lucene.search.Searcher;
+//import org.apache.lucene.search.Searcher;
 import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.Weight;
+import org.apache.lucene.util.Bits;
 import org.apache.lucene.util.ToStringUtils;
 
 public class NonScoringTermQuery extends TermQuery {
@@ -48,12 +52,12 @@ public class NonScoringTermQuery extends TermQuery {
 		final int maxId;
 		final float score;
 
-		MatchAllScorer(IndexReader reader, Similarity similarity, Weight w) {
+		MatchAllScorer(IndexReader reader, Weight similarity, Weight w) throws IOException {
 			super(similarity);
 			this.reader = reader;
 			id = -1;
 			maxId = reader.maxDoc() - 1;
-			score = w.getValue();
+ 			score = w.getValueForNormalization();
 		}
 
 		public Explanation explain(int doc) {
@@ -64,27 +68,57 @@ public class NonScoringTermQuery extends TermQuery {
 			return id;
 		}
 
-		public boolean next() {
-			while (id < maxId) {
-				id++;
-				if (!reader.isDeleted(id)) {
-					return true;
-				}
-			}
-			return false;
-		}
+//		public boolean next() {
+//			while (id < maxId) {
+//				id++;
+//				if (!reader.isDeleted(id)) {
+//					return true;
+//				}
+//			}
+//			return false;
+//		}
 
 		public float score() {
 			return score;
 		}
 
-		public boolean skipTo(int target) {
+		public int skipTo(int target) throws IOException {
 			id = target - 1;
-			return next();
+			return nextDoc();
+		}
+
+		@Override
+		public int freq() throws IOException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int advance(int arg0) throws IOException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public long cost() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int docID() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int nextDoc() throws IOException {
+			// TODO Auto-generated method stub
+			return 0;
 		}
 	}
 
-	private class MatchAllDocsWeight implements Weight {
+	private class MatchAllDocsWeight extends Weight {
 		
 		private static final long serialVersionUID = 8245890839430555571L;
 		
@@ -92,17 +126,18 @@ public class NonScoringTermQuery extends TermQuery {
 		private float queryWeight;
 		private float queryNorm;
 
-		public MatchAllDocsWeight(Searcher searcher) {
-			this.similarity = searcher.getSimilarity();
+		public MatchAllDocsWeight(Query searcher) {
+//			this.similarity = searcher.getSimilarity();
+			super(searcher);
 		}
 
 		public String toString() {
 			return "MatchAllDocsWeight";
 		}
 
-		public Query getQuery() {
-			return NonScoringTermQuery.this;
-		}
+//		public Query getQuery() {
+//			return NonScoringTermQuery.this;
+//		}
 
 		public float getValue() {
 			return queryWeight;
@@ -119,23 +154,58 @@ public class NonScoringTermQuery extends TermQuery {
 		}
 
 		public Scorer scorer(IndexReader reader) {
-			return new MatchAllScorer(reader, similarity, this);
+//			return new MatchAllScorer(reader, similarity, this);
+			return null;
 		}
 
 		public Explanation explain(IndexReader reader, int doc) {
 			// explain query weight
-			Explanation queryExpl = new ComplexExplanation
-			(true, getValue(), "MatchAllDocsQuery, product of:");
-			if (getBoost() != 1.0f) {
-				queryExpl.addDetail(new Explanation(getBoost(),"boost"));
-			}
-			queryExpl.addDetail(new Explanation(queryNorm,"queryNorm"));
+//			Explanation queryExpl = new ComplexExplanation
+//			(true, getValue(), "MatchAllDocsQuery, product of:");
+//			if (getBoost() != 1.0f) {
+//				queryExpl.addDetail(new Explanation(getBoost(),"boost"));
+//			}
+//			queryExpl.addDetail(new Explanation(queryNorm,"queryNorm"));
+//
+//			return queryExpl;
+			
+			return null;
+		}
 
-			return queryExpl;
+		@Override
+		public Explanation explain(LeafReaderContext arg0, int arg1)
+				throws IOException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void extractTerms(Set<Term> arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public float getValueForNormalization() throws IOException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public void normalize(float arg0, float arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Scorer scorer(LeafReaderContext arg0, Bits arg1)
+				throws IOException {
+			// TODO Auto-generated method stub
+			return null;
 		}
 	}
 
-	protected Weight createWeight(Searcher searcher) {
+	protected Weight createWeight(Query searcher) {
 		return new MatchAllDocsWeight(searcher);
 	}
 
