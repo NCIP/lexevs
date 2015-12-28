e8c38f5fc6ae25776d8478a1b2fc3dd58c56a3b8
Scott Bauer <bauer.scott@mayo.edu>
2015-12-28 17:07:22 -0600

    LEXEVS-1234. LEXEVS-1434.  First pass at a Unit test for this.

---
+++ lbTest/src/test/java/org/lexevs/dao/index/operation/DefaultLexEVSIndexOperationsWriteOverIndexTest.java
@@ -0,0 +1,91 @@
+package org.lexevs.dao.index.operation;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.types.ProcessState;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.Impl.dataAccess.CleanUpUtility;
+import org.LexGrid.LexBIG.Impl.loaders.LexGridMultiLoaderImpl;
+import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.lexevs.locator.LexEvsServiceLocator;
+import org.lexevs.registry.model.RegistryEntry;
+import org.lexevs.registry.service.Registry;
+
+public class DefaultLexEVSIndexOperationsWriteOverIndexTest {
+	static AbsoluteCodingSchemeVersionReference reference = new AbsoluteCodingSchemeVersionReference();
+	static String prefix;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		LexBIGServiceManager lbsm = ServiceHolder.instance().getLexBIGService()
+				.getServiceManager(null);
+
+		LexGridMultiLoaderImpl loader = (LexGridMultiLoaderImpl) lbsm
+				.getLoader("LexGrid_Loader");
+
+		loader.load(new File("resources/testData/Automobiles.xml").toURI(),
+				true, true);
+
+		while (loader.getStatus().getEndTime() == null) {
+			Thread.sleep(1000);
+		}
+		assertTrue(loader.getStatus().getState().equals(ProcessState.COMPLETED));
+		assertFalse(loader.getStatus().getErrorsLogged().booleanValue());
+		Registry registry = LexEvsServiceLocator.getInstance().getRegistry();
+		prefix = registry
+				.getEntriesForUri("urn:oid:11.11.0.1").get(0).getPrefix();
+		CleanUpUtility.removeUnusedDatabase(prefix);
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		LexBIGServiceManager lbsm = ServiceHolder.instance().getLexBIGService()
+				.getServiceManager(null);
+		lbsm.removeCodingSchemeVersion(reference);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testIndexDeletionAndOverwrite() throws LBParameterException, LBInvocationException, InterruptedException {
+		
+		LexBIGServiceManager lbsm;
+		try {
+			lbsm = ServiceHolder.instance().getLexBIGService()
+					.getServiceManager(null);
+			LexGridMultiLoaderImpl loader = (LexGridMultiLoaderImpl) lbsm
+					.getLoader("LexGrid_Loader");
+
+			loader.load(new File("resources/testData/Automobiles.xml").toURI(),
+					true, true);
+
+			while (loader.getStatus().getEndTime() == null) {
+				Thread.sleep(1000);
+			}
+			fail("Should fail on index creation");
+		} catch (LBException e) {
+			//success.  make it fail first then ask user for input at index time
+			System.out.println("success");
+		} 
+		finally{
+			CleanUpUtility.removeUnusedIndex("Automobiles-1_0");
+		}
+
+	}
+
+}
