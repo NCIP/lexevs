2993159f2f332fb7bd561b03b5bcc6c2bb64ef01
m091864 <endle.cory@mayo.edu>
2016-01-26 11:58:34 -0600

    LEXEVS-1520 Updated GetAllConcepts() constructor to persist the coding scheme as the URI.  Fixed associated junit SameSessionLoadandQueryTest.

--- lbTest/src/test/java/org/lexevs/dao/index/operation/SameSessionLoadandQueryTest.java
+++ lbTest/src/test/java/org/lexevs/dao/index/operation/SameSessionLoadandQueryTest.java
@@ -1,6 +1,8 @@
 package org.lexevs.dao.index.operation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -10,14 +12,14 @@ import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
 import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
 import org.LexGrid.LexBIG.DataModel.InterfaceElements.types.ProcessState;
 import org.LexGrid.LexBIG.Exceptions.LBException;
-import org.LexGrid.LexBIG.Exceptions.LBParameterException;
 import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
-import org.LexGrid.LexBIG.Impl.dataAccess.CleanUpUtility;
 import org.LexGrid.LexBIG.Impl.loaders.LexGridMultiLoaderImpl;
 import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceManager;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.LexBIG.Utility.ConvenienceMethods;
 import org.LexGrid.LexBIG.Utility.Iterators.ResolvedConceptReferencesIterator;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -45,6 +47,9 @@ public class SameSessionLoadandQueryTest {
 			Thread.sleep(1000);
 		}
 		
+		// Activate the coding scheme
+		lbsm.activateCodingSchemeVersion(ConvenienceMethods.createAbsoluteCodingSchemeVersionReference("urn:oid:11.11.0.1", "1.0"));
+		
 		assertTrue(loader.getStatus().getState().equals(ProcessState.COMPLETED));
 		assertFalse(loader.getStatus().getErrorsLogged().booleanValue());
 		Registry registry = LexEvsServiceLocator.getInstance().getRegistry();
@@ -61,10 +66,9 @@ public class SameSessionLoadandQueryTest {
 	public static void tearDownAfterClass() throws Exception {
 		LexBIGServiceManager lbsm = ServiceHolder.instance().getLexBIGService()
 				.getServiceManager(null);
-			for(AbsoluteCodingSchemeVersionReference acsv: references){
-				lbsm.removeCodingSchemeVersion(acsv);
-			}
-
+		for(AbsoluteCodingSchemeVersionReference acsv: references){
+			lbsm.removeCodingSchemeVersion(acsv);
+		}
 	}
 
 	@Before
@@ -74,14 +78,20 @@ public class SameSessionLoadandQueryTest {
 
 	@Test
 	public void testQueryAfterCreation() throws LBException {
-
+		LexBIGService lbsi = ServiceHolder.instance().getLexBIGService();
+		LexBIGServiceManager lbsm = lbsi.getServiceManager(null);
+		
 		LexBIGService lbs = LexBIGServiceImpl.defaultInstance();
 		CodingSchemeVersionOrTag csvt = new CodingSchemeVersionOrTag();
 		csvt.setVersion("1.0");
 		CodedNodeSet set = lbs.getCodingSchemeConcepts("Automobiles", csvt);
+	
+		// deactivate the coding scheme
+		AbsoluteCodingSchemeVersionReference acsvr = Constructors.createAbsoluteCodingSchemeVersionReference("urn:oid:11.11.0.1", "1.0");
+		lbsm.deactivateCodingSchemeVersion(acsvr, null);
+		
 		ResolvedConceptReferencesIterator itr = set.resolve(null, null, null);
 		assertNotNull(itr.next());
-		
 	}
 
 }
