c2132a97bd83bc5205860f161ed90f96ad4d71ce
Scott Bauer <bauer.scott@mayo.edu>
2016-02-15 15:50:20 -0600

    LEXEVS-1628.  Adding the same custom collector to another code path.  Regression tests are all passing on this as well.  Performance is much improved for search extension, but full testing remains to be done.

--- lexevs-dao/src/main/java/org/lexevs/dao/index/lucene/v2010/entity/SingleTemplateDisposableLuceneCommonEntityDao.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/index/lucene/v2010/entity/SingleTemplateDisposableLuceneCommonEntityDao.java
@@ -18,14 +18,22 @@
  */
 package org.lexevs.dao.index.lucene.v2010.entity;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
 import org.LexGrid.LexBIG.Utility.logging.LgLoggerIF;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.Collector;
+import org.apache.lucene.search.LeafCollector;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.Scorer;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.lexevs.dao.index.access.entity.CommonEntityDao;
 import org.lexevs.dao.index.indexregistry.IndexRegistry;
@@ -84,11 +92,55 @@ public class SingleTemplateDisposableLuceneCommonEntityDao extends AbstractBaseL
 		    throw new RuntimeException(errorMsg.toString());
 		}
 
-		TopScoreDocCollector hitCollector = 
-				TopScoreDocCollector.create(maxDoc);
+//		TopScoreDocCollector hitCollector = 
+//				TopScoreDocCollector.create(maxDoc);
+//
+//			template.search(query, null, hitCollector);
+//			List<ScoreDoc> scoreDocs = Arrays.asList(hitCollector.topDocs().scoreDocs);
+		final List<ScoreDoc> scoreDocs = new ArrayList<ScoreDoc>();
+		
+		//TopScoreDocCollector collector = TopScoreDocCollector.create(maxDoc);
+					
+		template.search(query, null, new Collector() {
 
-			template.search(query, null, hitCollector);
-			List<ScoreDoc> scoreDocs = Arrays.asList(hitCollector.topDocs().scoreDocs);
+			@Override
+			public LeafCollector getLeafCollector(final LeafReaderContext context)
+					throws IOException {
+				return new LeafCollector() {
+					
+					private Scorer scorer;
+
+					@Override
+					public void collect(int docId) throws IOException {
+						scoreDocs.add(new ScoreDoc(context.docBase + docId, this.scorer.score()));
+					}
+
+					@Override
+					public void setScorer(Scorer scorer) throws IOException {
+						this.scorer = scorer;
+					}
+					
+				};
+			}
+
+			@Override
+			public boolean needsScores() {
+				return true;
+			}
+		
+		});
+		//final List<ScoreDoc> docs  = Arrays.asList(collector.topDocs().scoreDocs);
+		
+		Collections.sort(scoreDocs, new Comparator<ScoreDoc>() {
+
+			@Override
+			public int compare(ScoreDoc o1, ScoreDoc o2) {
+				if(o1.score > o2.score)return -1;
+				if(o1.score < o2.score)return 1;
+				return 0;
+			}
+			
+		});
 		return scoreDocs;
 	}
 
