2fc2cdb2638af67c63f07f6b94dd0eb698932325
Scott Bauer <bauer.scott@mayo.edu>
2016-02-15 14:02:47 -0600

    This custom collector passes all the Unit tests, but has not been fully tested against the larger terminology set

--- lexevs-dao/src/main/java/org/lexevs/dao/index/lucene/v2013/search/LuceneSearchDao.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/index/lucene/v2013/search/LuceneSearchDao.java
@@ -18,7 +18,11 @@
  */
 package org.lexevs.dao.index.lucene.v2013.search;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
@@ -26,9 +30,13 @@ import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
 import org.LexGrid.LexBIG.Utility.logging.LgLoggerIF;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.Collector;
 import org.apache.lucene.search.Filter;
+import org.apache.lucene.search.LeafCollector;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.Scorer;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.lexevs.dao.database.utility.DaoUtility;
 import org.lexevs.dao.index.access.search.SearchDao;
@@ -95,12 +103,51 @@ public class LuceneSearchDao extends AbstractFilteringLuceneIndexTemplateDao imp
 			    throw new RuntimeException(errorMsg);
 			}
 			
-			TopScoreDocCollector collector = TopScoreDocCollector.create(maxDoc);
-						
-			template.search(query, null, collector);
-			final List<ScoreDoc> docs  = Arrays.asList(collector.topDocs().scoreDocs);
+			final List<ScoreDoc> scoreDocs = new ArrayList<ScoreDoc>();
 			
-			return docs;
+			//TopScoreDocCollector collector = TopScoreDocCollector.create(maxDoc);
+						
+			template.search(query, null, new Collector() {
+
+				@Override
+				public LeafCollector getLeafCollector(final LeafReaderContext context)
+						throws IOException {
+					return new LeafCollector() {
+						
+						private Scorer scorer;
+
+						@Override
+						public void collect(int docId) throws IOException {
+							scoreDocs.add(new ScoreDoc(context.docBase + docId, this.scorer.score()));
+						}
+
+						@Override
+						public void setScorer(Scorer scorer) throws IOException {
+							this.scorer = scorer;
+						}
+						
+					};
+				}
+
+				@Override
+				public boolean needsScores() {
+					return true;
+				}
+			
+			});
+			//final List<ScoreDoc> docs  = Arrays.asList(collector.topDocs().scoreDocs);
+			
+			Collections.sort(scoreDocs, new Comparator<ScoreDoc>() {
+
+				@Override
+				public int compare(ScoreDoc o1, ScoreDoc o2) {
+					if(o1.score > o2.score)return -1;
+					if(o1.score < o2.score)return 1;
+					return 0;
+				}
+				
+			});
+			return scoreDocs;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
