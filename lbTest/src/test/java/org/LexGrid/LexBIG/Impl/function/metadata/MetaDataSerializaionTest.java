603356d5ac1c88646c22bde2d835f7f5042eb02a
Scott Bauer <bauer.scott@mayo.edu>
2016-04-08 15:58:21 -0500

    Initial commit including a test case and some refactoring of deprecated lucene methods to the latest API

---
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/function/metadata/MetaDataSerializaionTest.java
@@ -0,0 +1,49 @@
+package org.LexGrid.LexBIG.Impl.function.metadata;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import org.LexGrid.LexBIG.Impl.LexBIGServiceMetadataImpl;
+import org.LexGrid.LexBIG.Impl.function.LexBIGServiceTestCase;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGServiceMetadata;
+import org.junit.Test;
+
+public class MetaDataSerializaionTest extends LexBIGServiceTestCase {
+
+	@Test
+	public void test() throws IOException, ClassNotFoundException {
+		LexBIGServiceMetadataImpl lbMetadata = new LexBIGServiceMetadataImpl();
+	    byte[] b = serialize(lbMetadata);
+	    deSerialize(b, LexBIGServiceMetadataImpl.class);
+	}
+
+	private static <T extends Serializable> byte[] serialize(T obj) 
+		       throws IOException 
+		{
+		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		    ObjectOutputStream oos = new ObjectOutputStream(baos);
+		    oos.writeObject(obj);
+		    oos.close();
+		    return baos.toByteArray();
+		}
+
+	private static <T extends Serializable> T deSerialize(byte[] b, Class<T> cl)
+		       throws IOException, ClassNotFoundException 
+		{
+		    ByteArrayInputStream bais = new ByteArrayInputStream(b);
+		    ObjectInputStream ois = new ObjectInputStream(bais);
+		    Object o = ois.readObject();
+		    return cl.cast(o);
+		}
+
+	@Override
+	protected String getTestID() {
+		return "Meta Data Serialization Test";
+	}
+}
