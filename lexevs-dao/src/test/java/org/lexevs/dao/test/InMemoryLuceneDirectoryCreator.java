166b3a13458b4e414863f53431e78f0216bff7f8
Deepak Sharma <sharma.deepak2@mayo.edu>
2016-04-20 16:21:17 -0500

    bugfix/LEXEVS-1769 - Implemented checks in RAMDirectory class for in-memory directory management for Lucene docs.

--- lexevs-dao/src/test/java/org/lexevs/dao/test/InMemoryLuceneDirectoryCreator.java
+++ lexevs-dao/src/test/java/org/lexevs/dao/test/InMemoryLuceneDirectoryCreator.java
@@ -18,12 +18,14 @@
  */
 package org.lexevs.dao.test;
 
-import java.io.File;
-
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.lexevs.dao.index.lucenesupport.LuceneDirectoryCreator;
 import org.lexevs.dao.index.lucenesupport.LuceneDirectoryFactory.NamedDirectory;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The Class InMemoryLuceneDirectoryCreator.
@@ -31,16 +33,35 @@ import org.lexevs.dao.index.lucenesupport.LuceneDirectoryFactory.NamedDirectory;
  * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
  */
 public class InMemoryLuceneDirectoryCreator implements LuceneDirectoryCreator {
+
+	private static final Object MUTEX = new Object();
+
+	private Map<String,Directory> map = new HashMap<String,Directory>();
 	
 	/* (non-Javadoc)
 	 * @see org.lexevs.dao.index.lucenesupport.LuceneDirectoryCreator#getDirectory(java.lang.String, java.io.File)
 	 */
 	@Override
-	public NamedDirectory getDirectory(String indexName, File baseDirectory) {
-		
-		Directory directory = new RAMDirectory();
-		
-		return new NamedDirectory(
-				directory, indexName);
+	public NamedDirectory getDirectory(final String indexName, File baseDirectory) {
+
+		final Directory indexDirectory;
+		synchronized (MUTEX) {
+			if (!this.map.containsKey(indexName)) {
+				Directory directory = new RAMDirectory();
+
+				this.map.put(indexName, directory);
+			}
+
+			indexDirectory = this.map.get(indexName);
+		}
+
+		return new NamedDirectory(indexDirectory, indexName) {
+			public void remove() {
+				super.remove();
+				synchronized (MUTEX) {
+					map.remove(indexName);
+				}
+			}
+		};
 	}
 }
