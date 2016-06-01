b6be6c05b4da40aab81466ffa1f467c07a25cf5e
Scott Bauer <bauer.scott@mayo.edu>
2016-06-01 15:34:01 -0500

    LEXEVS-1675.  Adding some comments, Copyright statements.

--- lbImpl/src/org/LexGrid/LexBIG/Impl/Extensions/GenericExtensions/search/ScoreDocTransformer.java
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/Extensions/GenericExtensions/search/ScoreDocTransformer.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright: (c) 2004-2010 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.eclipse.org/legal/epl-v10.html
+ * 
+ */
 package org.LexGrid.LexBIG.Impl.Extensions.GenericExtensions.search;
 
 import java.io.Serializable;
@@ -11,6 +29,10 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.lucene.document.Document;
 import org.lexevs.locator.LexEvsServiceLocator;
 
+/**
+ * @author <a href="mailto:scott.bauer@mayo.edu">Scott Bauer</a>
+ *
+ */
 @LgProxyClass
 public class ScoreDocTransformer implements Serializable {
 
