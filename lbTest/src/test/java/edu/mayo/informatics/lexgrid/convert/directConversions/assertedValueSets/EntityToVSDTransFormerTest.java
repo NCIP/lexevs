581b0b9d1a622cdc0276f065f45955e9a46b9f58
Scott Bauer <bauer.scott@mayo.edu>
2017-05-22 17:13:22 -0500

    LEXEVS-2961.  Nearly complete mapping, a number of new tests committed.  Still needs refactoring to move database calls to an upper level class

--- lbTest/src/test/java/edu/mayo/informatics/lexgrid/convert/directConversions/assertedValueSets/EntityToVSDTransFormerTest.java
+++ lbTest/src/test/java/edu/mayo/informatics/lexgrid/convert/directConversions/assertedValueSets/EntityToVSDTransFormerTest.java
@@ -8,41 +8,53 @@ import org.LexGrid.LexBIG.Exceptions.LBParameterException;
 import org.LexGrid.commonTypes.Property;
 import org.LexGrid.commonTypes.Text;
 import org.LexGrid.concepts.Entity;
+import org.LexGrid.naming.SupportedCodingScheme;
+import org.LexGrid.naming.SupportedConceptDomain;
+import org.LexGrid.naming.SupportedNamespace;
+import org.LexGrid.naming.SupportedSource;
+import org.LexGrid.valueSets.DefinitionEntry;
+import org.LexGrid.valueSets.EntityReference;
 import org.LexGrid.valueSets.ValueSetDefinition;
+import org.LexGrid.valueSets.types.DefinitionOperator;
 import org.junit.Before;
 import org.junit.Test;
 import org.lexevs.dao.database.service.codednodegraph.CodedNodeGraphService;
 import org.lexevs.locator.LexEvsServiceLocator;
+import org.lexevs.registry.model.RegistryEntry;
 
 public class EntityToVSDTransFormerTest {
 	CodedNodeGraphService service;
 	EntityToVSDTransformer transformer;
+	public static final String CODING_SCHEME_URI = "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#";
+	public static final String CODING_SCHEME = "NCI_Thesaurus";
+	public static final String VERSION = "17.02d";
+	public static final String ASSOCIATION_NAME = "Concept_In_Subset";
 	
 	@Before
 	public void setUp(){
 		service = LexEvsServiceLocator.getInstance().getDatabaseServiceManager().
 				getCodedNodeGraphService();
-		transformer = new EntityToVSDTransformer(null);
+		transformer = new EntityToVSDTransformer(null, null, null, null);
 	}
 
 	@Test
 	public void getPredicateGuidForValueSetRelationTest() {
-		transformer = new EntityToVSDTransformer(null);
-		String guid = transformer.getPredicateGuidForValueSetRelation("Concept_In_Subset", "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#", "17.02d");
+		transformer = new EntityToVSDTransformer(null, null, null, null);
+		String guid = transformer.getPredicateGuidForValueSetRelation(ASSOCIATION_NAME, CODING_SCHEME_URI, VERSION);
 		assertEquals(guid, "37456035");
 	}
 	
 	@Test
 	public void getgetEntityByCodeAndNamespaceTest(){
-		transformer = new EntityToVSDTransformer(null);
+		transformer = new EntityToVSDTransformer(null, null, null, null);
 		Entity entity = transformer.getEntityByCodeAndNamespace(
-				"http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#", "17.02d", "C12434", "Thesaurus");
+				CODING_SCHEME_URI, VERSION, "C12434", "Thesaurus");
 		assertNotNull(entity);
 	}
 	
 	@Test
 	public void entityToValueSetTransformTest() throws LBParameterException{
-		transformer = new EntityToVSDTransformer(null);
+		transformer = new EntityToVSDTransformer(null, null, null, null);
 		Entity entity = new Entity();
 		Property prop = new Property();
 		prop.setPropertyName("Contributing_Source");
@@ -56,10 +68,89 @@ public class EntityToVSDTransFormerTest {
 	
 	@Test
 	public void getProductionVersionForCodingSchemeURI() throws LBParameterException{
-		transformer = new EntityToVSDTransformer(null);
-		String version = transformer.getProductionVersionForCodingSchemeURI("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#");
+		transformer = new EntityToVSDTransformer(null, null, null, null);
+		String version = transformer.getProductionVersionForCodingSchemeURI(CODING_SCHEME_URI);
 		assertNotNull(version);
 	}
+	
+	@Test
+    public void createSupportedCodingSchemeTest(){
+		SupportedCodingScheme scheme = transformer.createSupportedCodingScheme(CODING_SCHEME, CODING_SCHEME_URI);
+		assertTrue(scheme.getLocalId().equals(CODING_SCHEME));
+		assertTrue(scheme.getContent().equals(CODING_SCHEME));
+		assertTrue(scheme.getUri().equals(CODING_SCHEME_URI));
+    }
+
+	@Test
+    public void createSupportedNamespaceTest(){
+		SupportedNamespace namespace = transformer.createSupportedNamespace(CODING_SCHEME, CODING_SCHEME_URI);
+		assertTrue(namespace.getLocalId().equals(CODING_SCHEME));
+		assertTrue(namespace.getContent().equals(CODING_SCHEME));
+		assertTrue(namespace.getUri().equals(CODING_SCHEME_URI));
+    }
+
+	@Test
+    public void createSupportedConceptDomainTest(){
+		SupportedConceptDomain cd = transformer.createSupportedConceptDomain("Intellectual Product", CODING_SCHEME_URI);
+		assertTrue(cd.getLocalId().equals("Intellectual Product"));
+		assertTrue(cd.getContent().equals("Intellectual Product"));
+		assertTrue(cd.getUri().equals(CODING_SCHEME_URI));
+    }
+	
+	@Test
+    public void createSupportedSourceTest(){
+		SupportedConceptDomain cd = transformer.createSupportedConceptDomain("CDISC", CODING_SCHEME_URI);
+		assertTrue(cd.getLocalId().equals("CDISC"));
+		assertTrue(cd.getContent().equals("CDISC"));
+		assertTrue(cd.getUri().equals(CODING_SCHEME_URI));
+    }
+ 
+	@Test
+    public void getDefaultSourceIfNullTest(){
+		String source = transformer.getDefaultSourceIfNull(null);
+		assertTrue(source.equals("Contributing_Source"));
+		String test = transformer.getDefaultSourceIfNull("testSource");
+		assertTrue(test.equals("testSource"));
+    }
+    
+	@Test
+    public void createUriTest(){
+		String uri = transformer.createUri("http://evs.nci.nih.gov/valueset/", "CDISC", "C12345");
+		assertTrue(uri.equals("http://evs.nci.nih.gov/valueset/CDISC/C12345"));
+		String test = transformer.createUri("http://evs.nci.nih.gov/valueset/", null, "C12345");
+		assertTrue(test.equals("http://evs.nci.nih.gov/valueset/C12345"));
+    }
+    
+	@Test
+    public void initValueSetDefintionTest(){
+		ValueSetDefinition def = transformer.initValueSetDefintion(CODING_SCHEME, true, "1", "NCI");
+		assertTrue(def.getDefaultCodingScheme().equals(CODING_SCHEME));
+		assertTrue(def.getIsActive());
+		assertTrue(def.getStatus().equals("1"));
+		assertTrue(def.getOwner().equals("NCI"));
+    }
+    
+	@Test
+    public void initDefinitionEntryTest(){
+		DefinitionEntry entry = transformer.initDefinitionEntry(0, DefinitionOperator.OR);
+		assertTrue(entry.getRuleOrder() == 0);
+		assertTrue(entry.getOperator().equals(DefinitionOperator.OR));
+    }
+    
+	@Test
+    public void initEntityReferenceTest(){
+		Entity entity = new Entity();
+		entity.setEntityCode("C123");
+		entity.setEntityCodeNamespace(CODING_SCHEME);
+		EntityReference ref = transformer.initEntityReference(entity);
+		assertTrue(ref.getEntityCode().equals("C123"));
+		assertTrue(ref.getEntityCodeNamespace().equals(CODING_SCHEME));
+		assertTrue(ref.getLeafOnly());
+		assertTrue(ref.getReferenceAssociation().equals(ASSOCIATION_NAME));
+		assertTrue(ref.getTargetToSource());
+		assertTrue(ref.getTransitiveClosure());
+    }
+
 	
 	
 
--- lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/assertedValueSets/EntityToVSDTransformer.java
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/assertedValueSets/EntityToVSDTransformer.java
@@ -2,8 +2,10 @@ package edu.mayo.informatics.lexgrid.convert.directConversions.assertedValueSets
 
 import org.LexGrid.LexBIG.Exceptions.LBParameterException;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.Utility.Constructors;
 import org.LexGrid.LexBIG.Utility.logging.LgMessageDirectorIF;
 import org.LexGrid.commonTypes.Property;
+import org.LexGrid.commonTypes.PropertyQualifier;
 import org.LexGrid.concepts.Entity;
 import org.LexGrid.naming.Mappings;
 import org.LexGrid.naming.SupportedCodingScheme;
@@ -13,6 +15,7 @@ import org.LexGrid.naming.SupportedSource;
 import org.LexGrid.valueSets.DefinitionEntry;
 import org.LexGrid.valueSets.EntityReference;
 import org.LexGrid.valueSets.ValueSetDefinition;
+import org.LexGrid.valueSets.types.DefinitionOperator;
 import org.lexevs.dao.database.service.codednodegraph.CodedNodeGraphService;
 import org.lexevs.dao.database.service.codingscheme.CodingSchemeService;
 import org.lexevs.dao.database.service.entity.EntityService;
@@ -22,24 +25,28 @@ import org.lexevs.registry.service.Registry;
 import org.lexevs.system.service.SystemResourceService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class EntityToVSDTransformer {
     private static final Object PRODUCTION = "PRODUCTION";
-    CodedNodeGraphService codedNodeGraphDao;
-    EntityService entityService;
-    CodingSchemeService csService;
-    SystemResourceService resourceService;
-    Registry registry;
+    private CodedNodeGraphService codedNodeGraphDao;
+    private EntityService entityService;
+    private CodingSchemeService csService;
+    private SystemResourceService resourceService;
+    private Registry registry;
     private String SOURCE_NAME = "Contributing_Source";
-    String baseURI = null;
-    String codingSchemeURI;
-       
+    private String baseURI = null;
+    private String codingSchemeURI;
+     
     private LgMessageDirectorIF messages_;
+    private String codingSchemeVersion;
+    private String owner;
     
-   public EntityToVSDTransformer(String baseURI){
+   public EntityToVSDTransformer(String baseURI, String codingSchemeUri, String codingSchemeVersion, String owner){
        codedNodeGraphDao = LexEvsServiceLocator.getInstance().
                getDatabaseServiceManager().getCodedNodeGraphService();
        entityService = LexEvsServiceLocator.getInstance().
@@ -49,6 +56,9 @@ public class EntityToVSDTransformer {
        resourceService = LexEvsServiceLocator.getInstance().getSystemResourceService();
        registry = LexEvsServiceLocator.getInstance().getRegistry();
        this.baseURI = baseURI;
+       this.codingSchemeURI = codingSchemeUri;
+       this.codingSchemeVersion = codingSchemeVersion;
+       this.owner = owner;
        
    }
     
@@ -73,38 +83,58 @@ public class EntityToVSDTransformer {
     }
     
     //Entity with more than one source will be processed into more than one definition
+    //Assumption is that this source representation is always a flat list of values
     public List<ValueSetDefinition> transformEntityToValueSetDefinitions(Entity entity, String sourceName) throws LBParameterException{
-       String uri = resourceService.getUriForUserCodingSchemeName(entity.getEntityCodeNamespace(), null);
+       //String uri = resourceService.getUriForUserCodingSchemeName(entity.getEntityCodeNamespace(), null);
        final String source = getDefaultSourceIfNull(sourceName);
        List<Property> props = entity.getPropertyAsReference();
        List<Property> list = props.stream().filter(x -> x.getPropertyName().equals(source)).collect(Collectors.toList());
        List<ValueSetDefinition> defs = new ArrayList<ValueSetDefinition>();
-       List<String> definedSources = new ArrayList<String>();
-       ValueSetDefinition def = new ValueSetDefinition();
-       DefinitionEntry entry = new DefinitionEntry();
+       HashMap<String, String> definedSources = new HashMap<String, String>();
+       ValueSetDefinition def = initValueSetDefintion(entity.getEntityCodeNamespace(), true, 
+               "1", owner);
+       DefinitionEntry entry = initDefinitionEntry(0, DefinitionOperator.OR);
        Mappings mappings = new Mappings();
-       EntityReference ref = new EntityReference();
-       ref.setEntityCode(entity.getEntityCode());
-       ref.setEntityCodeNamespace(entity.getEntityCodeNamespace());
-       mappings.setSupportedNamespace(0, createSupportedNamespace(entity.getEntityCodeNamespace(),uri));
-       mappings.setSupportedCodingScheme(0, createSupportedCodingScheme(entity.getEntityCodeNamespace(), uri));
+       EntityReference ref = initEntityReference(entity);
+       entry.setEntityReference(ref);
+       def.addDefinitionEntry(entry);
+       mappings.addSupportedNamespace(createSupportedNamespace(entity.getEntityCodeNamespace(), 
+               codingSchemeURI));
+       mappings.addSupportedCodingScheme(createSupportedCodingScheme(entity.getEntityCodeNamespace(), 
+               codingSchemeURI));
       for(Property p: list){
           if(p.getPropertyName().equals(sourceName)){
-              definedSources.add(p.getValue().getContent());
-              mappings.addSupportedSource(createSupportedSource(entity.getEntityCodeNamespace(), uri));
+              definedSources.put(p.getValue().getContent(), "PlaceHolder");
+              mappings.addSupportedSource(createSupportedSource(entity.getEntityCodeNamespace(), 
+                      codingSchemeURI));
           }     
           if(p.getPropertyName().equals("Semantic_Type")){
             def.setConceptDomain(p.getValue().getContent());
-            mappings.addSupportedConceptDomain(createSupportedConceptDomain(p.getValue().getContent(), uri));
+            mappings.addSupportedConceptDomain(createSupportedConceptDomain(p.getValue().getContent(), 
+                    codingSchemeURI));
           }
-          
-
-          
+          if(p.getPropertyName().equals("ALT_DEFINITION")){
+              for(PropertyQualifier pq: p.getPropertyQualifier()){
+                  if(pq.getPropertyQualifierName().equals("source"))
+                      if(definedSources.containsKey(pq.getValue().getContent())){
+                         definedSources.replace(pq.getValue().getContent(), "PlaceHolder", p.getValue().getContent());
+                      }
+              }
+          }
       }
+      definedSources.forEach((x,y) -> defs.add(createVSDefinitionBySource(def,x,y)));
+
        return defs;
     }
 
-    private SupportedCodingScheme createSupportedCodingScheme(String codingScheme, String uri) {
+    private ValueSetDefinition createVSDefinitionBySource(ValueSetDefinition def, String source, String definition) {
+       ValueSetDefinition newDef = def;
+       newDef.setValueSetDefinitionURI(createUri(baseURI, source, def.getDefinitionEntry(0).getEntityReference().getEntityCode()));
+       newDef.setEntityDescription(Constructors.createEntityDescription(definition));
+        return newDef;
+    }
+
+    protected SupportedCodingScheme createSupportedCodingScheme(String codingScheme, String uri) {
         SupportedCodingScheme scheme = new SupportedCodingScheme();
         scheme.setContent(codingScheme);
         scheme.setLocalId(codingScheme);
@@ -113,7 +143,7 @@ public class EntityToVSDTransformer {
         return scheme;
     }
 
-    private SupportedNamespace createSupportedNamespace(String entityCodeNamespace, String uri) {
+    protected SupportedNamespace createSupportedNamespace(String entityCodeNamespace, String uri) {
         SupportedNamespace nmsp = new SupportedNamespace();
         nmsp.setContent(entityCodeNamespace);
         nmsp.setLocalId(entityCodeNamespace);
@@ -122,7 +152,7 @@ public class EntityToVSDTransformer {
         return nmsp;
     }
 
-    private SupportedConceptDomain createSupportedConceptDomain(String content, String uri) {
+    protected SupportedConceptDomain createSupportedConceptDomain(String content, String uri) {
        SupportedConceptDomain domain = new SupportedConceptDomain();
        domain.setContent(content);
        domain.setLocalId(content);
@@ -130,11 +160,11 @@ public class EntityToVSDTransformer {
         return domain;
     }
  
-    private String getDefaultSourceIfNull(String sourceName) {
+    protected String getDefaultSourceIfNull(String sourceName) {
         return sourceName == null?SOURCE_NAME: sourceName;
     }
     
-    private SupportedSource createSupportedSource(String source, String uri){
+    protected SupportedSource createSupportedSource(String source, String uri){
         SupportedSource newSource = new SupportedSource();
         newSource.setLocalId(source);
         newSource.setContent(source);
@@ -144,7 +174,39 @@ public class EntityToVSDTransformer {
     
     protected String getProductionVersionForCodingSchemeURI(String uri) throws LBParameterException{
         List<RegistryEntry> entries =  registry.getEntriesForUri(uri);
-        return entries.stream().filter(x -> x.getTag().equals(PRODUCTION)).findFirst().get().getResourceVersion();
+        return entries.stream().filter(x -> x.getTag().equals(PRODUCTION)).
+                findFirst().get().getResourceVersion();
+    }
+    
+    protected String createUri(String base, String source, String code){
+       return base + "/" + source != null?source + "/":"" + code;
+    }
+    
+    protected ValueSetDefinition initValueSetDefintion(String namespace, boolean isActive, 
+            String status, String owner){
+        ValueSetDefinition def = new ValueSetDefinition();
+        def.setDefaultCodingScheme(namespace);
+        def.setIsActive(isActive);
+        def.setStatus(status);
+        def.setOwner(owner);
+        return def;
+    }
+    
+    protected DefinitionEntry initDefinitionEntry( long ruleOrder, DefinitionOperator oper){
+        DefinitionEntry entry = new DefinitionEntry();
+        entry.setRuleOrder(0L);
+        entry.setOperator(DefinitionOperator.OR);
+        return entry;
+    }
+    
+    protected EntityReference initEntityReference(Entity entity){
+        EntityReference ref = new EntityReference();
+        ref.setLeafOnly(true);
+        ref.setTargetToSource(true);
+        ref.setTransitiveClosure(true);
+        ref.setEntityCode(entity.getEntityCode());
+        ref.setEntityCodeNamespace(entity.getEntityCodeNamespace());
+        return ref;
     }
 
 }
