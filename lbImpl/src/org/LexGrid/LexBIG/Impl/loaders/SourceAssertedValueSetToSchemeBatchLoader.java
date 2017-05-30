8219f16c2d4713573bbd9e4edae670b6d539eba9
Scott Bauer <bauer.scott@mayo.edu>
2017-05-30 16:42:50 -0500

    LEXEVS-2962.  Basic functionality. No testing, no performance enhancements

---
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/loaders/SourceAssertedValueSetToSchemeBatchLoader.java
@@ -0,0 +1,136 @@
+package org.LexGrid.LexBIG.Impl.loaders;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.LexBIG.Utility.logging.LgMessageDirectorIF;
+import org.LexGrid.codingSchemes.CodingScheme;
+import org.LexGrid.concepts.Entity;
+import org.LexGrid.valueSets.ValueSetDefinition;
+import org.lexevs.dao.database.access.association.model.Node;
+import org.lexevs.dao.database.service.codednodegraph.CodedNodeGraphService;
+import org.lexevs.dao.database.service.codingscheme.CodingSchemeService;
+import org.lexevs.dao.database.service.entity.EntityService;
+import org.lexevs.dao.database.service.valuesets.ValueSetDefinitionService;
+import org.lexevs.locator.LexEvsServiceLocator;
+import org.lexevs.system.service.SystemResourceService;
+
+import edu.mayo.informatics.lexgrid.convert.directConversions.assertedValueSets.EntityToRVSTransformer;
+import edu.mayo.informatics.lexgrid.convert.directConversions.assertedValueSets.EntityToVSDTransformer;
+
+public class SourceAssertedValueSetToSchemeBatchLoader {
+    
+    private CodedNodeGraphService codedNodeGraphDao;
+    private EntityService entityService;
+    private CodingSchemeService csService;
+    private SystemResourceService resourceService;
+    private LexBIGService lbsvc;
+    private String codingSchemeUri;
+    private String codingSchemeVersion;
+    private String associationName;
+    private boolean targetToSource;
+    private EntityToRVSTransformer transformer;
+    private ValueSetDefinitionService valueSetDefinitionService;
+    private LgMessageDirectorIF messages;
+    private HashMap<String, String> uriToCodingSchemeNameMap;
+    
+
+    
+    public SourceAssertedValueSetToSchemeBatchLoader(String codingScheme, String version, 
+            String associationName, boolean targetToSource, String baseUri, String owner) throws LBParameterException{
+        codedNodeGraphDao = LexEvsServiceLocator.getInstance().
+                getDatabaseServiceManager().getCodedNodeGraphService();
+        entityService = LexEvsServiceLocator.getInstance().
+                getDatabaseServiceManager().getEntityService();
+        resourceService = LexEvsServiceLocator.getInstance().getSystemResourceService();
+        this.codingSchemeUri = resourceService.getUriForUserCodingSchemeName(codingScheme, version);
+        valueSetDefinitionService = LexEvsServiceLocator.getInstance().getDatabaseServiceManager().getValueSetDefinitionService();
+        csService = LexEvsServiceLocator.getInstance().getDatabaseServiceManager().getCodingSchemeService();
+        lbsvc = LexBIGServiceImpl.defaultInstance();
+        this.codingSchemeVersion = version;
+        this.associationName = associationName;
+        this.targetToSource = targetToSource;
+        AbsoluteCodingSchemeVersionReference ref = Constructors.
+                createAbsoluteCodingSchemeVersionReference(codingSchemeUri, version);
+        this.transformer = new EntityToRVSTransformer(associationName, codingSchemeUri, version, ref , lbsvc, baseUri, owner);
+        uriToCodingSchemeNameMap = new HashMap<String, String>();
+    }
+    
+    public void run(String sourceName) throws LBException{
+        processEntitiesToCodingScheme(getEntitiesForAssociation(associationName, codingSchemeUri, codingSchemeVersion), sourceName);
+    }
+    
+    
+    
+    protected List<Node> getEntitiesForAssociation(String association, String codingSchemeUri,String version){
+       String associationPredicateUid = this.getPredicateGuidForValueSetRelation(association, codingSchemeUri, version);
+       return codedNodeGraphDao.getDistinctTargetTriples(codingSchemeUri, version, associationPredicateUid);
+    }
+    
+    protected String getPredicateGuidForValueSetRelation(String associationName, String codingSchemeUri,
+            String codingSchemeVersion){
+        List<String> list = new ArrayList<String>();
+        list.add(associationName);
+        List<String> uids = codedNodeGraphDao.getAssociationPredicateUidsForNames(
+                codingSchemeUri, codingSchemeVersion, null, list);
+        if( uids == null || uids.size() < 1){
+            throw new RuntimeException("No association is asserted for :" 
+        + associationName + " in " + codingSchemeUri + ":" + codingSchemeVersion);
+        }
+        return uids.get(0);
+    }
+    
+    
+    protected Entity getEntityByCodeAndNamespace(String codingSchemeUri, String version, String code, String namespace){
+        Entity entity = entityService.getEntity(codingSchemeUri, version, code, namespace);
+        return entity;
+    }
+    
+    public void processEntitiesToCodingScheme(List<Node> nodes, String sourceName) throws LBException{
+       for(Node n: nodes){
+           Entity e = getEntityByCodeAndNamespace(codingSchemeUri, 
+                   codingSchemeVersion, n.getEntityCode(), n.getEntityCodeNamespace());
+           List<CodingScheme> schemes = transformer.transformEntityToCodingSchemes(e, sourceName);
+           for(CodingScheme s : schemes){
+               csService.insertCodingScheme(s, null);
+               uriToCodingSchemeNameMap.put(s.getCodingSchemeURI(), s.getCodingSchemeName());
+           }
+       }
+       indexResolvedValueSetCodingSchemes();
+    }
+    
+    private void indexResolvedValueSetCodingSchemes(){
+        uriToCodingSchemeNameMap.forEach((x,y) -> indexUsingMap(x,y));
+    }
+
+    private void indexUsingMap(String x, String y) {
+        ProcessRunner loader = new IndexLoaderImpl();
+        AbsoluteCodingSchemeVersionReference ref = Constructors.createAbsoluteCodingSchemeVersionReference(x, y);
+        try {
+            loader.runProcess(ref, null);
+        } catch (LBParameterException e) {
+               System.out.println("Indexing failed for value set coding scheme " + y + ":" + x);
+        }
+    }
+
+    public static void main(String[] args) {
+      try {
+        new SourceAssertedValueSetToSchemeBatchLoader("NCI_Thesaurus", "17.02d", "Concept_In_Subset", true, "http://evs.nci.nih.gov/valueset/", "NCI").run("Contributing_Source");
+    } catch (LBParameterException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } catch (LBException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+
+    }
+
+}
---
+++ lgConverter/src/edu/mayo/informatics/lexgrid/convert/directConversions/assertedValueSets/EntityToRVSTransformer.java
@@ -0,0 +1,256 @@
+package edu.mayo.informatics.lexgrid.convert.directConversions.assertedValueSets;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.LexGrid.LexBIG.DataModel.Collections.AssociatedConceptList;
+import org.LexGrid.LexBIG.DataModel.Collections.ResolvedConceptReferenceList;
+import org.LexGrid.LexBIG.DataModel.Core.AbsoluteCodingSchemeVersionReference;
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeGraph;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.LexBIG.Utility.logging.LgMessageDirectorIF;
+import org.LexGrid.codingSchemes.CodingScheme;
+import org.LexGrid.commonTypes.Property;
+import org.LexGrid.commonTypes.PropertyQualifier;
+import org.LexGrid.commonTypes.Source;
+import org.LexGrid.commonTypes.Text;
+import org.LexGrid.concepts.Entities;
+import org.LexGrid.concepts.Entity;
+import org.LexGrid.naming.Mappings;
+import org.LexGrid.naming.SupportedCodingScheme;
+import org.apache.commons.lang.StringUtils;
+import org.lexevs.dao.database.service.valuesets.ValueSetDefinitionService;
+import org.lexevs.locator.LexEvsServiceLocator;
+
+
+public class EntityToRVSTransformer {
+    private static final String SOURCE_NAME = "Contributing_Source";
+    private URI valueSetDefinitionURI;
+    private String valueSetDefinitionRevisionId;
+    private String vsVersion;
+    
+    final private String codingSchemeUri;
+    final private String csVersion;
+    final private String association;
+    final private AbsoluteCodingSchemeVersionReference ref;
+    
+    private LexBIGService svc;
+    private String baseUri;
+
+    private LexEvsServiceLocator locator = LexEvsServiceLocator.getInstance();
+
+    private ValueSetDefinitionService vsdService;
+    private String owner;
+
+    public EntityToRVSTransformer( 
+            String association,
+            String codingScheme,
+            String csVersion, 
+            AbsoluteCodingSchemeVersionReference ref, 
+            LexBIGService svc,
+            String baseURI, 
+            String owner) {
+        this.association = association;
+        this.codingSchemeUri = codingScheme;
+        this.csVersion = csVersion;
+        this.ref = ref;
+        vsdService = locator.getDatabaseServiceManager().getValueSetDefinitionService();
+        this.svc = svc;
+        this.baseUri = baseURI;
+        this.owner = owner;
+    }
+
+    public EntityToRVSTransformer(
+            String association,
+            String codingScheme,
+            String csVersion, 
+            String vsVersion, 
+            AbsoluteCodingSchemeVersionReference ref, 
+            LexBIGService svc,
+            String baseURI,
+            String owner,
+            URI valueSetDefinitionURI,
+            String valueSetDefinitionRevisionId) {
+        this.association = association;
+        this.valueSetDefinitionURI = valueSetDefinitionURI;
+        this.valueSetDefinitionRevisionId = valueSetDefinitionRevisionId;
+        this.codingSchemeUri = codingScheme;
+        this.csVersion = csVersion;
+        this.vsVersion = vsVersion;
+        this.ref = ref;
+        vsdService = locator.getDatabaseServiceManager().getValueSetDefinitionService();
+        this.svc = svc;
+        this.baseUri = baseURI;
+        this.owner = owner;
+    }
+
+    //Entity with more than one source will be processed into more than one definition
+    //Assumption is that this source representation is always a flat list of values
+    public List<CodingScheme> transformEntityToCodingSchemes(Entity entity, String sourceName) throws LBException{
+      
+        final String source = getDefaultSourceIfNull(sourceName);
+        List<Property> props = entity.getPropertyAsReference();
+
+       List<CodingScheme> schemes = new ArrayList<CodingScheme>();
+       HashMap<String, String> definedSources = new HashMap<String, String>();
+       List<Property> sourcelist = getPropertiesForPropertyName(props, source);
+       
+       sourcelist.stream().forEach(s -> definedSources.put(s.getValue().getContent(), 
+               getPropertyQualifierValueForSource(s.getPropertyQualifierAsReference()) != null?
+                       getPropertyQualifierValueForSource(s.getPropertyQualifierAsReference()): 
+                           entity.getEntityDescription().getContent()));
+       Entities vsEntities = getEntities(entity.getEntityCode());
+      definedSources.forEach((x,y) -> {
+        try {
+            schemes.add(transform(entity,x,y,vsEntities));
+        } catch (LBException e) {
+            throw new RuntimeException("", e);
+        }
+    }); 
+      if(definedSources.size() > 1 || definedSources.size() == 0 || !definedSources.containsValue(owner)){
+      schemes.add(transform(entity, owner, entity.getEntityDescription().getContent(), vsEntities));
+      }
+       return schemes;
+    }
+
+    public CodingScheme transform(Entity entity, String source, String description,  Entities entities)
+            throws LBException {
+        String codingSchemeUri = createUri(baseUri, source, entity.getEntityCode());
+        String codingSchemeVersion = csVersion == null ? "UNASSIGNED":
+                csVersion;
+
+        String codingSchemeName = description == null? entity.getEntityDescription().getContent(): description;
+
+        CodingScheme cs = null;
+
+        cs = new CodingScheme();
+
+        cs.setCodingSchemeURI(codingSchemeUri);
+        cs.setRepresentsVersion(codingSchemeVersion);
+        if (entity.getEffectiveDate() != null)
+            cs.setEffectiveDate(entity.getEffectiveDate());
+        if (entity.getExpirationDate() != null)
+            cs.setExpirationDate(entity.getExpirationDate());
+        cs.setEntryState(entity.getEntryState());
+        cs.setFormalName(codingSchemeName);
+        cs.setCodingSchemeName(truncateDefNameforCodingSchemeName(codingSchemeName));
+        cs.setIsActive(entity.getIsActive());
+        cs.setMappings(createMappings(entity));
+        cs.setOwner(entity.getOwner());
+        //init properties
+        cs.setProperties(new org.LexGrid.commonTypes.Properties());
+        Source lexSource = new Source();
+        lexSource.setContent(source);
+        cs.setSource(new Source[]{lexSource});
+        cs.setStatus(entity.getStatus());
+
+        Property prop = new Property();
+        prop.setPropertyType(AssertedValueSetDefinitionServices.GENERIC);
+        prop.setPropertyName(AssertedValueSetDefinitionServices.RESOLVED_AGAINST_CODING_SCHEME_VERSION);
+        Text txt = new Text();
+        txt.setContent(ref.getCodingSchemeURN());
+        prop.setValue(txt);
+        PropertyQualifier pq = createPropertyQualifier(AssertedValueSetDefinitionServices.VERSION,
+                ref.getCodingSchemeVersion());
+        prop.getPropertyQualifierAsReference().add(pq);
+        String csSourceName = getCodingSchemeName(ref.getCodingSchemeURN(), ref.getCodingSchemeVersion());
+        if (csSourceName != null) {
+            PropertyQualifier pQual = createPropertyQualifier(AssertedValueSetDefinitionServices.CS_NAME, csSourceName);
+            prop.getPropertyQualifierAsReference().add(pQual);
+        }
+        cs.getProperties().addProperty(prop);
+
+        cs.setEntities(entities);
+
+        return cs;
+    }
+
+    private Mappings createMappings(Entity entity) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private String getCodingSchemeName(String codingSchemeURN, String codingSchemeVersion) throws LBParameterException {
+        return locator.getSystemResourceService().getInternalCodingSchemeNameForUserCodingSchemeName(codingSchemeURN,
+                codingSchemeVersion);
+    }
+
+    private String truncateDefNameforCodingSchemeName(String name){
+        if (StringUtils.isNotEmpty(name) && name.length() > 50) {
+            name = name.substring(0, 49);
+        }
+        return name;
+    }
+    
+    public Entities getEntities(String topNodeCode) throws LBException {
+        long start = System.currentTimeMillis();
+        Entities newEntities = new Entities();
+        CodedNodeGraph graph = svc.getNodeGraph(codingSchemeUri,
+                Constructors.createCodingSchemeVersionOrTagFromVersion(csVersion), null);
+        graph.restrictToAssociations(Constructors.createNameAndValueList(this.association), null);
+        ResolvedConceptReferenceList refs = graph.resolveAsList(
+                Constructors.createConceptReference(topNodeCode, codingSchemeUri), false, true, 1, 1, null, null, null,
+                -1);
+        AssociatedConceptList concepts = null;
+        if( refs.getResolvedConceptReference(0) != null){
+            concepts = refs.getResolvedConceptReference(0).getTargetOf().getAssociation(0).getAssociatedConcepts();
+        }
+        else{
+            throw new RuntimeException("No values for value set with focus code of " + topNodeCode);
+        }
+                
+        for (ResolvedConceptReference refer : concepts.getAssociatedConcept()) {
+            newEntities.addEntity(refer.getEntity());
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Resolution Time: " + (end - start));
+        return newEntities;
+    }
+    
+    // TODO Utility Class methods to break out later
+    private PropertyQualifier createPropertyQualifier(String name, String value) {
+        PropertyQualifier pq = new PropertyQualifier();
+        pq.setPropertyQualifierName(name);
+        Text pqtxt = new Text();
+        pqtxt.setContent(value);
+        pq.setValue(pqtxt);
+        return pq;
+    }
+
+    private String getSupportedCodingSchemeNameForURI(CodingScheme cs, String URI) {
+        for (SupportedCodingScheme scs : cs.getMappings().getSupportedCodingScheme()) {
+            if (scs.getUri().equals(URI)) {
+                return scs.getLocalId();
+            }
+        }
+        return null;
+    }
+
+    protected String getDefaultSourceIfNull(String sourceName) {
+        return sourceName == null?SOURCE_NAME: sourceName;
+    }
+    
+    protected String createUri(String base, String source, String code){
+        return base + (source != null?source + "/":"") + code;
+     }
+    
+    protected List<Property> getPropertiesForPropertyName(List<Property> props, String  name){
+        return props.stream().filter(x -> x.getPropertyName().equals(name)).collect(Collectors.toList());
+    }
+    
+    protected String getPropertyQualifierValueForSource(List<PropertyQualifier> quals){
+        if(quals.stream().filter(pq -> pq.getPropertyQualifierName().equals("source")).findFirst().isPresent()){
+            return quals.stream().filter(pq -> pq.getPropertyQualifierName().equals("source")).findFirst().get().getValue().getContent();
+        }
+        return null;
+    }
+    
+
+}
