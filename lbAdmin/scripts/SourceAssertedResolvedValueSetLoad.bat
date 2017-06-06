d9d1e4e121d71f65dafb943e55b95715feba77c6
Scott Bauer <bauer.scott@mayo.edu>
2017-06-06 17:01:55 -0500

    Adding script

---
+++ lbAdmin/scripts/SourceAssertedResolvedValueSetLoad.bat
@@ -0,0 +1,22 @@
+@echo off
+REM Loads All LexGrid XML formatted Value Set Definitions in a given directory
+REM
+REM Options:
+REM
+REM    -cs, --codingScheme, usage="Name of Coding Scheme that asserts values sets" 
+REM    
+REM    -v, --version, usage="Version of the coding scheme."
+REM    
+REM    -a, --association, usage="Relationship name asserted by the codingScheme") 
+REM    
+REM    -t", --target", usage="Target to Source resolution.") 
+REM    
+REM    -uri", --uri", usage="Base uri to build the conding scheme uri upon") 
+REM    
+REM    -o", --owner", usage="Owener of the value set assertioin") 
+REM    
+REM    -s", --sourceName", usage="Gives the name of the property to resolve the source value against") 
+REM        
+REM Example: SourceAssertedResolvedValueSetLoad.sh -v "17.04d"
+REM
+java -Xmx3000m -Djava.awt.headless=true -cp "../runtime/lbPatch.jar:../runtime-components/extLib/*" org.LexGrid.LexBIG.admin.SourceAssertedResolvedValueSetBatchLauncher %*
