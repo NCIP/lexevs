0ba06613e00f38e71a7d7d9136f53533b1c8fa55
Scott Bauer <bauer.scott@mayo.edu>
2017-09-07 10:51:52 -0500

    LEXEVS-3277.  LEXEVSREM-60. Foundation for distributed bug

---
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/Extensions/tree/dao/ProxyWrappedJdbcLexEvsTreeDao.java
@@ -0,0 +1,15 @@
+package org.LexGrid.LexBIG.Impl.Extensions.tree.dao;
+
+import org.LexGrid.annotations.LgProxyClass;
+
+@LgProxyClass
+public class ProxyWrappedJdbcLexEvsTreeDao extends JdbcLexEvsTreeDao {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -58673477538771006L;
+    
+  
+
+}
