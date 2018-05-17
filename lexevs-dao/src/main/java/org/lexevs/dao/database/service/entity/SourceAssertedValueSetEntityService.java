19c9f690fe4566d138c157b307064ee1a81e71c2
Scott Bauer <bauer.scott@mayo.edu>
2018-05-17 14:50:14 -0500

    LEXEVS-3760. Javadoc updates

--- lexevs-dao/src/main/java/org/lexevs/dao/database/service/entity/SourceAssertedValueSetEntityService.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/service/entity/SourceAssertedValueSetEntityService.java
@@ -1,23 +1,30 @@
 package org.lexevs.dao.database.service.entity;
 
-import java.sql.Date;
 import java.util.List;
 
-import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
-import org.LexGrid.LexBIG.Exceptions.LBException;
-import org.LexGrid.LexBIG.Exceptions.LBRevisionException;
-import org.LexGrid.commonTypes.EntityDescription;
 import org.LexGrid.concepts.Entity;
-import org.LexGrid.relations.AssociationEntity;
 import org.LexGrid.util.assertedvaluesets.AssertedValueSetParameters;
-import org.lexevs.dao.database.service.RevisableAbstractDatabaseService;
-import org.lexevs.dao.database.service.RevisableAbstractDatabaseService.CodingSchemeUriVersionBasedEntryId;
 
 public interface SourceAssertedValueSetEntityService extends EntityService {
 
 
+	/**
+	 * @param codingSchemeUri
+	 * @param version
+	 * @param start
+	 * @param pageSize
+	 * @param params
+	 * @return
+	 */
 	public List<? extends Entity> getEntities(String codingSchemeUri, String version, int start, int pageSize, AssertedValueSetParameters params);
 
+	/**
+	 * @param codingSchemeUri
+	 * @param version
+	 * @param start
+	 * @param pageSize
+	 * @return
+	 */
 	List<String> getEntityUids(String codingSchemeUri, String version, int start, int pageSize);
 
 }
