494a23370778b6ff0961942a11ecc704e42b4ddc
Cory Endle <cory.endle@nih.gov>
2018-08-15 08:07:06 -0500

    LEXEVS-2219 - Added option to pass in metadata URI as part of the load.

--- lbAdmin/scripts/LoadOWL2.sh
+++ lbAdmin/scripts/LoadOWL2.sh
@@ -13,8 +13,15 @@
 #   -a, --activate ActivateScheme on successful load; if unspecified the
 #         vocabulary is loaded but not activated.
 #   -t, --tag <id> An optional tag ID (e.g. 'PRODUCTION' or 'TEST') to assign.
+#   -meta --meatadata input &lt;uri&gt; URI or path specifying location of the metadata source file.  
+#        metadata is applied to the code system and code system version being loaded.
+#   -mv  --validate metadata &lt;int&gt; Perform validation of the metadata source file
+#         without loading data.  Supported levels of validation include:
+#         0 = Verify document is valid
+#         metadata is validated against the code system and code system version being loaded.
 #
 # Example: LoadOWL2 -in "file:///path/to/somefile.owl" -a
 #          LoadOWL2 -in "file:///path/to/somefile.owl" -v 0
+#          LoadOWL2 -in "file:///path/to/somefile.owl" -a -meta "file:///path/to/metadata.xml" -mv 0
 #
 java -Xmx3000m -XX:MaxPermSize=256M -cp "../runtime/lbPatch.jar:../runtime-components/extLib/*" org.LexGrid.LexBIG.admin.LoadOWL2 $@
