0587ceef91a5ead9920bed28ab8cfdaa4e57e959
Cory Endle <cory.endle@nih.gov>
2018-09-11 15:10:05 -0500

    LEXEVS-4086 added option (-f) to force the value set definition removal.

--- lbAdmin/scripts/RemoveAllValueSetDefinitions.bat
+++ lbAdmin/scripts/RemoveAllValueSetDefinitions.bat
@@ -2,7 +2,10 @@
 REM
 REM WARNING: Removes all resolved value set coding schemes without discrimination
 REM
-REM
+REM Options
+REM   -f,--force Force removing all value set definitions (no confirmation).
+REM 
 REM Example: RemoveAllResolvedValueSets.bat
+REM Example: RemoveAllResolvedValueSets.bat -f
 REM
 java -Xmx3000m -cp "../runtime/lbPatch.jar;../runtime-components/extLib/*" org.lexgrid.valuesets.admin.RemoveAllValueSetDefinitions %*
