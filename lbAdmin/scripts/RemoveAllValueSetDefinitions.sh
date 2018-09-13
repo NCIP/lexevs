8751e368895f668565a6920e25e919c1fb5d40dd
Cory Endle <cory.endle@nih.gov>
2018-09-13 13:38:39 -0500

    Merging 6.5.1 loader and build changes.

--- lbAdmin/scripts/RemoveAllValueSetDefinitions.sh
+++ lbAdmin/scripts/RemoveAllValueSetDefinitions.sh
@@ -1,6 +1,9 @@
 # WARNING: Removes all value set definitions without discrimination
 #
+# Options
+#   -f,--force Force removing all value set definitions (no confirmation).
 #
 # Example: ./RemoveAllValueSetDefinitions.sh
+# Example: ./RemoveAllValueSetDefinitions.sh -f
 #
 java -Xmx3000m -cp "../runtime/lbPatch.jar:../runtime-components/extLib/*" org.lexgrid.valuesets.admin.RemoveAllValueSetDefinitions $@
