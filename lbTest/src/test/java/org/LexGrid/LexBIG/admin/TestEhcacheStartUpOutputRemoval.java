1a58553fe44081335dd867f7ea531be2bcc71219
hsbauer <bauer.scott@mayo.edu>
2019-03-21 13:58:47 -0500

    LEXEVS-4393.  This will be a stand alone test since it relies on system startup. We'll document appropriately in the JIRA Item

--- lbTest/src/test/java/org/LexGrid/LexBIG/admin/TestEhcacheStartUpOutputRemoval.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/admin/TestEhcacheStartUpOutputRemoval.java
@@ -5,44 +5,33 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 
-import static org.mockito.Matchers.startsWith;
-import static org.mockito.Matchers.contains;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.LexGrid.LexBIG.DataModel.Collections.CodingSchemeRenderingList;
 import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
-import org.LexGrid.LexBIG.Exceptions.LBParameterException;
-import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
-import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestEhcacheStartUpOutputRemoval {
+    private static PrintStream sysOut;
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-    private PrintStream sysOut;
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    	@Before
-        public void setUpStreams() {
-            sysOut = System.out;
-            System.setOut(new PrintStream(outContent));
-        }
-    	
-    	   @After
-    	    public void revertStreams() {
-    	        System.setOut(sysOut);
-    	    }
-	
-		@Test
-		public void out() throws LBInvocationException {
-	        ListSchemes.main(new String[]{});
-	        assertTrue(outContent.toString().contains("Local Name") ||
-	        		outContent.toString().contains("No coding schemes found"));
-	        assertFalse(outContent.toString().contains("ehcache"));
-		}
+	@Before
+	public void before() throws InterruptedException {
+		sysOut = System.out;
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@AfterClass
+	public static void cleanUp() {
+		System.setOut(sysOut);
+	}
+
+	@Test
+	public void outTest() throws LBInvocationException {
+		ListSchemes.main(new String[] {});
+		assertTrue(outContent.toString().contains("Local Name")
+				|| outContent.toString().contains("No coding schemes found"));
+		assertFalse(outContent.toString().contains("ehcache"));
+	}
 
 }
