105372de75e7da028086fb48986f08c593a2516d
hsbauer <bauer.scott@mayo.edu>
2019-05-21 16:38:18 -0500

    LEXEVS-3834.  This issue was fixed in LEXEVS-4535. Adding a test and source examples to verity

--- lbTest/src/test/java/org/LexGrid/LexBIG/Impl/Extensions/tree/test/TestGetTree.java
+++ lbTest/src/test/java/org/LexGrid/LexBIG/Impl/Extensions/tree/test/TestGetTree.java
@@ -3,6 +3,9 @@ package org.LexGrid.LexBIG.Impl.Extensions.tree.test;
 import static org.junit.Assert.*;
 
 import org.LexGrid.LexBIG.Impl.Extensions.tree.dao.iterator.ChildTreeNodeIterator;
+import org.LexGrid.LexBIG.Impl.Extensions.tree.model.LexEvsTree;
+import org.LexGrid.LexBIG.Impl.Extensions.tree.model.LexEvsTreeNode;
+import org.LexGrid.LexBIG.Impl.Extensions.tree.model.LexEvsTreeNode.ExpandableStatus;
 import org.LexGrid.LexBIG.Impl.Extensions.tree.service.PathToRootTreeServiceImpl;
 import org.LexGrid.LexBIG.Impl.Extensions.tree.service.TreeServiceFactory;
 import org.LexGrid.LexBIG.Impl.testUtility.ServiceHolder;
@@ -36,5 +39,16 @@ public class TestGetTree extends TestCase{
 		  iterator = pathToRootTreeServiceImpl.getTree("http://ncicb.nci.nih.gov/xml/owl/EVS/owl2lexevs.owl", 
 				  Constructors.createCodingSchemeVersionOrTagFromVersion("0.1.5"), "Patient").getCurrentFocus().getChildIterator();
 		  assertNotNull(iterator.next());
+	}
+	
+	@Test
+	public void testGetTreeWithMultipleHierarchyAsscNamesPatient() {
+		 lbs = ServiceHolder.instance().getLexBIGService();
+		  pathToRootTreeServiceImpl = (PathToRootTreeServiceImpl) TreeServiceFactory.getInstance().getTreeService(lbs);
+	        LexEvsTree tree = pathToRootTreeServiceImpl.getTree(
+	        		"http://ncicb.nci.nih.gov/xml/owl/EVS/owl2lexevs.owl", 
+	                Constructors.createCodingSchemeVersionOrTagFromVersion("0.2.0"), "C12434");
+	        LexEvsTreeNode node = tree.getCodeMap().get("C12434");
+	       assertEquals(ExpandableStatus.IS_NOT_EXPANDABLE,  node.getExpandableStatus());
 	}
 }
