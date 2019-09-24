c753a71e673724c8fd942522dc9882a23f40e141
hsbauer <bauer.scott@mayo.edu>
2019-09-24 08:20:54 -0500

    LEXEVS-4696. Code cleanup.

--- lbTest/src/test/java/edu/mayo/informatics/lexgrid/convert/directConversions/graphdb/TestGraphingDatabaseUtil.java
+++ lbTest/src/test/java/edu/mayo/informatics/lexgrid/convert/directConversions/graphdb/TestGraphingDatabaseUtil.java
@@ -10,7 +10,12 @@ import org.lexevs.dao.database.utility.GraphingDatabaseUtil;
 
 
 public class TestGraphingDatabaseUtil {
-	String[] testSet = new String[]{"rdfs:seeAlso", "http://purl.org/dc/terms/source", " Captain Kirk of the Enterprise", "_underscoredName", "{do not replace with _"};
+	String[] testSet = new String[]{
+			"rdfs:seeAlso",
+			"http://purl.org/dc/terms/source", 
+			" Captain Kirk of the Enterprise", 
+			"_underscoredName", 
+			"{do not replace with _"};
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,8 +23,9 @@ public class TestGraphingDatabaseUtil {
 
 	@Test
 	public void test() {
-
+		//Test for exceptions on any
 		Arrays.stream(testSet).forEach(x -> System.out.println(GraphingDatabaseUtil.normalizeGraphandGraphDatabaseName(x)));
+		//known use cases
 		assertTrue(Arrays.stream(testSet).anyMatch(x -> GraphingDatabaseUtil.normalizeGraphandGraphDatabaseName(x).equals("rdfs_seeAlso")));
 		assertTrue(Arrays.stream(testSet).anyMatch(x -> GraphingDatabaseUtil.normalizeGraphandGraphDatabaseName(x).equals("http___purl_org_dc_terms_source")));
 		assertTrue(Arrays.stream(testSet).anyMatch(x -> GraphingDatabaseUtil.normalizeGraphandGraphDatabaseName(x).equals("Captain_Kirk_of_the_Enterprise")));
--- lexevs-dao/src/main/java/org/lexevs/dao/database/utility/GraphingDatabaseUtil.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/utility/GraphingDatabaseUtil.java
@@ -1,7 +1,5 @@
 package org.lexevs.dao.database.utility;
 
-import org.apache.commons.lang.StringUtils;
-
 public class GraphingDatabaseUtil {
 	public static final String badChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~";
 
