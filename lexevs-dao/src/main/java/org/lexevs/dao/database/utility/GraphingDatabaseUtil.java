97f9cfbf90da4c1e056cc787079fee9d52e0d4d4
hsbauer <bauer.scott@mayo.edu>
2019-09-24 08:18:00 -0500

    LEXEVS-4696. Creating a more robust file name normalizing function.

--- lexevs-dao/src/main/java/org/lexevs/dao/database/utility/GraphingDatabaseUtil.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/utility/GraphingDatabaseUtil.java
@@ -3,15 +3,16 @@ package org.lexevs.dao.database.utility;
 import org.apache.commons.lang.StringUtils;
 
 public class GraphingDatabaseUtil {
-	
+	public static final String badChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~";
 
 	public static String normalizeGraphandGraphDatabaseName(String graphName) {
 		String result = graphName.trim();
+		result = result.replaceAll("[^a-zA-Z0-9]", "_");
 		if(result.startsWith("_")){
 			result = result.substring(1);
 		}
-		return StringUtils.replace(result, " ", "_");
-	}
+		return result;
+		}
 
 
 }
