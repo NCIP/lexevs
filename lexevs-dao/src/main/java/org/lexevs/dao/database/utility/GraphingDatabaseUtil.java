6e146d07c28913cb9de00c44f36dabbb4560c7cb
hsbauer <bauer.scott@mayo.edu>
2019-10-09 15:52:41 -0500

    LEXEVS-4696.  Improvements to Utility class and updates

--- lexevs-dao/src/main/java/org/lexevs/dao/database/utility/GraphingDatabaseUtil.java
+++ lexevs-dao/src/main/java/org/lexevs/dao/database/utility/GraphingDatabaseUtil.java
@@ -1,12 +1,11 @@
 package org.lexevs.dao.database.utility;
 
 public class GraphingDatabaseUtil {
-	public static final String badChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~";
 
 	public static String normalizeGraphandGraphDatabaseName(String graphName) {
 		String result = graphName.trim();
 		result = result.replaceAll("[^a-zA-Z0-9]", "_");
-		if(result.startsWith("_")){
+		while(result.startsWith("_")){
 			result = result.substring(1);
 		}
 		return result;
